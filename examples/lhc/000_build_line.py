8376c034e4addb91e89d12ea804c1477d552c7c2
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-04-30 22:40:02 +0200

    Line class

--- examples/lhc/000_build_line.py
+++ examples/lhc/000_build_line.py
@@ -4,50 +4,52 @@ import sixtracktools
 import pysixtrack
 
 six = sixtracktools.SixInput(".")
-line = pysixtrack.Line.from_sixinput(six)
+pyst_line = pysixtrack.Line.from_sixinput(six)
 
-elem_types = set([ee.__class__ for ee in line.elements])
 
 import xtrack as xt
 import xobjects as xo
 xtelems = []
 
 buff = xo.ContextCpu().new_buffer()
-buff = xo.ContextCupy().new_buffer()
+#buff = xo.ContextCupy().new_buffer()
 
-xtelem_types = [getattr(xt, cc.__name__) for cc in elem_types]
 
 class Line():
-    def __init__(self, element_types=(), num_elements=0,
-            _buffer=None, _context=None, _offset=None):
+    def __init__(self, sequence,
+           _context=None, _buffer=None,  _offset=None):
 
+        num_elements = len(sequence.elements)
+        elem_type_names = set([ee.__class__.__name__
+                                for ee in sequence.elements])
+        element_types = [getattr(xt, nn) for nn in elem_type_names]
         element_data_types = [cc.XoStruct for cc in element_types]
+
         ElementRefClass = xo.Ref(*element_data_types)
-        self._elements_data = ElementRefClass[num_elements](
+        LineDataClass = ElementRefClass[num_elements]
+        line_data = LineDataClass(_context=_context,
                 _buffer=_buffer, _offset=_offset)
+        elements = []
+        for ii, ee in enumerate(sequence.elements):
+            XtClass = getattr(xt, ee.__class__.__name__)
+            xt_ee = XtClass(_buffer=line_data._buffer, **ee.to_dict())
+            elements.append(xt_ee)
+            line_data[ii] = xt_ee._xobject
 
-    @property
-    def _buffer(self):
-        return self._elements_data._buffer
-
-xtline = Line(element_types=xtelem_types,
-        num_elements=len(line.elements), _buffer=buff)
+        self.elements = tuple(elements)
+        self._line_data = line_data
+        self._LineDataClass = LineDataClass
+        self._ElementRefClass = ElementRefClass
 
 print('Creating line...')
-for ii, ee in enumerate(line.elements):
-    xtclass = getattr(xt, ee.__class__.__name__)
-    xtee = xtclass(_buffer=xtline._buffer, **ee.to_dict())
-    xtelems.append(xtee)
-    xtline._elements_data[ii] = xtee._xobject
+xtline = Line(_buffer=buff, sequence=pyst_line)
 
-@property
-def _buffer(self):
-    return self._elements_data._buffer
-# Chenk the test
+# Check the test
 # ixtelems[2].knl[0]*=1.00000001
+
 print('Performing check...')
-for ie, (ee, xtee) in enumerate(zip(line.elements, xtelems)):
-    print(f'{ie}/{len(line.elements)}   ', end='\r',flush=True)
+for ie, (ee, xtee) in enumerate(zip(pyst_line.elements, xtline.elements)):
+    print(f'{ie}/{len(pyst_line.elements)}   ', end='\r',flush=True)
     dd = ee.to_dict()
     for kk in dd.keys():
         if kk == '__class__':
