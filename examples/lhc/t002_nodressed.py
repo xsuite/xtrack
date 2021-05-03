c85e0e62cb0444ebfc4067159672b6c730e53ffe
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-05-03 19:41:48 +0200

    Stilll broken

--- examples/lhc/t002_nodressed.py
+++ examples/lhc/t002_nodressed.py
@@ -9,12 +9,20 @@ context = xo.ContextCpu()
 
 #particles = xt.Particles(_context=context,
 #        pysixtrack_particles=[part0_pyst, part1_pyst])
-particles = xt.particles.ParticlesData(
+
+ParticlesData = xt.particles.ParticlesData
+
+class ParticlesData(xo.Struct):
+    num_particles = xo.Int64
+    s = xo.Float64[:]
+    x = xo.Float64[:]
+
+particles = ParticlesData(
                 num_particles=2,
                 s=np.array([1,2,]),
                 x=np.array([7,8,]))
 
-source, kernels, cdefs = xt.Particles.XoStruct._gen_c_api()
+source, kernels, cdefs = ParticlesData._gen_c_api()
 context.add_kernels([source], kernels, extra_cdef=cdefs)
 print(
         context.kernels.ParticlesData_get_x(obj=particles, i0=0))
