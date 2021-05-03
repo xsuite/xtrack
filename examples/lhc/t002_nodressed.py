3e2bc524c668a97af6c6533a2087be880f3af8ec
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-05-03 19:44:34 +0200

    Finally basic example showing the problem

--- examples/lhc/t002_nodressed.py
+++ examples/lhc/t002_nodressed.py
@@ -1,16 +1,8 @@
 import numpy as np
 
-import xtrack as xt
 import xobjects as xo
-import sixtracktools
-import pysixtrack
 
 context = xo.ContextCpu()
-
-#particles = xt.Particles(_context=context,
-#        pysixtrack_particles=[part0_pyst, part1_pyst])
-
-ParticlesData = xt.particles.ParticlesData
 
 class ParticlesData(xo.Struct):
     num_particles = xo.Int64
@@ -24,5 +16,5 @@ particles = ParticlesData(
 
 source, kernels, cdefs = ParticlesData._gen_c_api()
 context.add_kernels([source], kernels, extra_cdef=cdefs)
-print(
-        context.kernels.ParticlesData_get_x(obj=particles, i0=0))
+
+assert context.kernels.ParticlesData_get_x(obj=particles, i0=0) == particles.x[0]
