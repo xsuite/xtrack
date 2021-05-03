3c45e184a377472ba7702f3334f8fff379a4c279
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-05-03 19:26:20 +0200

    This seems to show the problem

--- examples/lhc/t002_nodressed.py
+++ examples/lhc/t002_nodressed.py
@@ -7,14 +7,19 @@ import pysixtrack
 
 context = xo.ContextCpu()
 
-ParticlesData = xt.particles.ParticlesData
-Particles = xt.Particles
-particles = Particles(
-                num_particles=3,
-                s=[1,2,3],
-                x=[7,8,9])
 
-#source, kernels, cdefs = ParticlesData._gen_c_api()
+sixdump = sixtracktools.SixDump101("res/dump3.dat")
+# TODO: The two particles look identical, to be checked
+part0_pyst = pysixtrack.Particles(**sixdump[0::2][0].get_minimal_beam())
+part1_pyst = pysixtrack.Particles(**sixdump[1::2][0].get_minimal_beam())
+
+#particles = xt.Particles(_context=context,
+#        pysixtrack_particles=[part0_pyst, part1_pyst])
+particles = xt.particles.ParticlesData(
+                num_particles=2,
+                s=np.array([1,2,]),
+                x=np.array([7,8,]))
+
 source, kernels, cdefs = xt.Particles.XoStruct._gen_c_api()
 context.add_kernels([source], kernels, extra_cdef=cdefs)
 print(
