befc45e5ad5381f23005789ca90a77dcfb7ca23f
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-05-04 15:18:59 +0200

    Drift track compiles

--- examples/lhc/003_track_something.py
+++ examples/lhc/003_track_something.py
@@ -34,7 +34,22 @@ source_drift, _, cdefs_drift = xt.Drift.XoStruct._gen_c_api()
 
 source_custom = r'''
 
-//void Drift_track_local_particle(
+void Drift_track_local_particle(DriftData el, LocalParticle* part){
+
+    double const length = DriftData_get_length(el);
+
+    double const rpp    = LocalParticle_get_rpp(part);
+    double const xp     = LocalParticle_get_px(part) * rpp;
+    double const yp     = LocalParticle_get_py(part) * rpp;
+    double const dzeta  = LocalParticle_get_rvv(part) -
+                           ( 1. + ( xp*xp + yp*yp ) / 2. );
+
+    LocalParticle_add_to_x(part, xp * length );
+    LocalParticle_add_to_y(part, yp * length );
+    LocalParticle_add_to_s(part, length);
+    LocalParticle_add_to_zeta(part, length * dzeta );
+
+}
 
 void Drift_track_particles(DriftData el, ParticlesData particles){
     int64_t npart = ParticlesData_get_num_particles(particles);
