2cb044af4698a209a20bc7844252ad79b0492833
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-05-06 08:51:30 +0200

    Forgotten file

---
+++ examples/lhc/make_short_line.py
@@ -0,0 +1,14 @@
+
+def make_short_line(line):
+    new_elements = []
+    new_names = []
+    found_types = []
+    for ee, nn in zip(line.elements, line.element_names):
+        if ee.__class__ not in found_types:
+            new_elements.append(ee)
+            new_names.append(nn)
+            found_types.append(ee.__class__)
+    line.elements = new_elements
+    line.element_names = new_names
+
+    return line
