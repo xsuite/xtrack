8a9b4cad5c7bb5a31dbcf6a66c86657f3a5be01b
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-05-07 09:00:46 +0200

    Introduced check against pysixtrack in timing example

--- examples/lhc/006_timing.py
+++ examples/lhc/006_timing.py
@@ -9,7 +9,12 @@ import pysixtrack
 from make_short_line import make_short_line
 import time
 
-short_test = False# Short line (5 elements)
+# # Quick test (for debugging)
+# short_test = True# Short line (5 elements)
+# n_part = 20
+# num_turns = int(100)
+
+short_test = False
 n_part = 20000
 num_turns = int(100)
 
@@ -54,16 +59,54 @@ part0_pyst = pysixtrack.Particles(**sixdump[0::2][0].get_minimal_beam())
 part1_pyst = pysixtrack.Particles(**sixdump[1::2][0].get_minimal_beam())
 pysixtrack_particles = [part0_pyst, part1_pyst]
 
-particles = xt.Particles(pysixtrack_particles=n_part*[part1_pyst],
+dx_array = np.linspace(-1e-4, 1e-4, n_part)
+dy_array = np.linspace(-2e-4, 2e-4, n_part)
+pysixtrack_particles = []
+for ii in range(n_part):
+    pp = part1_pyst.copy()
+    pp.x += dx_array[ii]
+    pp.y += dy_array[ii]
+    pysixtrack_particles.append(pp)
+
+particles = xt.Particles(pysixtrack_particles=pysixtrack_particles,
                          _context=context)
 #########
 # Track #
 #########
 
 print('Track!')
+print(f'context: {tracker.context}')
 t1 = time.time()
 tracker.track(particles, num_turns=num_turns)
 context.synchronize()
 t2 = time.time()
 print(f'Time {(t2-t1)*1000:.2f} ms')
 print(f'Time {(t2-t1)*1e6/num_turns/n_part:.2f} us/part/turn')
+
+
+############################
+# Check against pysixtrack #
+############################
+
+ip_check = n_part//3*2
+
+print(f'\nTest against pysixtrack over {num_turns} turns on particle {ip_check}:')
+vars_to_check = ['x', 'px', 'y', 'py', 'zeta', 'delta', 's']
+pyst_part = pysixtrack_particles[ip_check].copy()
+for iturn in range(num_turns):
+    print(f'turn {iturn}/{num_turns}', end='\r', flush=True)
+    sequence.track(pyst_part)
+
+for vv in vars_to_check:
+    pyst_value = getattr(pyst_part, vv)
+    xt_value = getattr(particles, vv)[ip_check]
+    passed = np.isclose(xt_value, pyst_value, rtol=1e-9, atol=1e-11)
+    if not passed:
+        print(f'Not passed on var {vv}!\n'
+              f'    pyst:   {pyst_value: .7e}\n'
+              f'    xtrack: {xt_value: .7e}\n')
+        raise ValueError
+    else:
+        print(f'Passed on var {vv}!\n'
+              f'    pyst:   {pyst_value: .7e}\n'
+              f'    xtrack: {xt_value: .7e}\n')
