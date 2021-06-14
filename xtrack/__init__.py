352242a7ef87178098cfbf492ed13f409639a5eb
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-06-14 15:31:38 +0200

    The monitor is allocated

--- xtrack/__init__.py
+++ xtrack/__init__.py
@@ -6,3 +6,4 @@ from .beam_elements import *
 from .line import Line
 from .particles import Particles
 from .tracker import Tracker
+from .monitors import generate_monitor_class
--- xtrack/monitors.py
+++ xtrack/monitors.py
@@ -1,21 +1,23 @@
 import xobjects as xo
 
-from ..dress_element import dress_element
-from ..general import _pkg_root
+from .dress_element import dress_element
+from .general import _pkg_root
 
 def _monitor_init(self, _context=None, _buffer=None, _offset=None,
                   start_at_turn=None, stop_at_turn=None,
                   num_particles=None):
 
-    n_turns = stop_at_turn - stop_at_turn
+    n_turns = stop_at_turn - start_at_turn
     n_records = n_turns * num_particles
 
     data_init = {nn: n_records for tt, nn in
-                     self._data_structure['per_particle_vars']}
+            self._ParticlesClass._structure['per_particle_vars']}
 
     self.xoinitialize(_context=_context, _buffer=_buffer, _offset=_offset,
-            start_at_turn=start_at_turn, stop_at_turn=stop_at_turn
+            start_at_turn=start_at_turn, stop_at_turn=stop_at_turn,
             data=data_init)
+
+    self._dressed_data = self._ParticlesClass(_xobject=self._xobject.data)
 
 
 
@@ -23,7 +25,7 @@ def generate_monitor_class(ParticlesClass):
 
     ParticlesMonitorDataClass = type(
             'ParticlesMonitorData',
-            (xo.Struct),
+            (xo.Struct,),
             {'start_at_turn': xo.Int64,
              'stop_at_turn': xo.Int64,
              'data': ParticlesClass.XoStruct})
@@ -33,7 +35,9 @@ def generate_monitor_class(ParticlesClass):
 
     ParticlesMonitorClass = type(
             'ParticlesMonitor',
-            (dress_element(ParticlesMonitorDataClass),)
-            {'_data_structure': ParticlesClass._structure})
+            (dress_element(ParticlesMonitorDataClass),),
+            {'_ParticlesClass': ParticlesClass})
 
-    return ParticlesMonitor
+    ParticlesMonitorClass.__init__ = _monitor_init
+
+    return ParticlesMonitorClass
