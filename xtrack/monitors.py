b3c2fbf54e2737fc024826d6326d2911a7cdddcd
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-06-15 10:17:48 +0200

    reshaped monitor data

--- xtrack/monitors.py
+++ xtrack/monitors.py
@@ -20,6 +20,15 @@ def _monitor_init(self, _context=None, _buffer=None, _offset=None,
     self._dressed_data = self._ParticlesClass(_xobject=self._xobject.data)
 
 
+class _FieldOfMonitor:
+    def __init__(self, name):
+        self.name = name
+
+    def __get__(self, container, ContainerType=None):
+        vv = getattr(container.data, self.name)
+        n_cols = (container.stop_at_turn - container.start_at_turn)
+        n_rows = container.n_records//n_cols
+        return vv.reshape(n_rows, n_cols)
 
 def generate_monitor_class(ParticlesClass):
 
@@ -40,5 +49,9 @@ def generate_monitor_class(ParticlesClass):
             {'_ParticlesClass': ParticlesClass})
 
     ParticlesMonitorClass.__init__ = _monitor_init
+
+    per_particle_vars = ParticlesClass._structure['per_particle_vars']
+    for tt, nn in per_particle_vars:
+        setattr(ParticlesMonitorClass, nn, _FieldOfMonitor(name=nn))
 
     return ParticlesMonitorClass
