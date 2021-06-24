e302b67a0198afa1c0fb11a541efca2fb1619a30
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-06-24 15:43:49 +0200

    Added LimitEllipse, sps example working (tested on CPU)

--- xtrack/beam_elements/apertures.py
+++ xtrack/beam_elements/apertures.py
@@ -1,15 +1,60 @@
 import xobjects as xo
 
-from ..dress_element import dress_element
+from ..dress_element import BeamElement
 from ..general import _pkg_root
 
-class LimitRectData(xo.Struct):
-    min_x = xo.Float64
-    max_x = xo.Float64
-    min_y = xo.Float64
-    max_y = xo.Float64
-LimitRectData.extra_sources = [
+
+class LimitRect(BeamElement):
+    _xofields = {
+        'min_x': xo.Float64,
+        'max_x': xo.Float64,
+        'min_y': xo.Float64,
+        'max_y': xo.Float64,
+        }
+
+LimitRect.XoStruct.extra_sources = [
         _pkg_root.joinpath('beam_elements/apertures_src/limitrect.h')]
 
-class LimitRect(dress_element(LimitRectData)):
-    pass
+
+class LimitEllipse(BeamElement):
+    _xofields = {
+            'a_squ': xo.Float64,
+            'b_squ': xo.Float64,
+            'a_b_squ': xo.Float64,
+            }
+
+    def __init__(self, a_squ=None, b_squ=None, **kwargs):
+        if a_squ is None and "a" in kwargs:
+            a = kwargs.get("a")
+            if a is not None and a > 0.0:
+                a_squ = a * a
+        if a_squ is None:
+            a_squ = 1.0
+
+        if b_squ is None and "b" in kwargs:
+            b = kwargs.get("b")
+            if b is not None and b > 0.0:
+                b_squ = b * b
+        if b_squ is None:
+            b_squ = 1.0
+
+        if a_squ > 0.0 and b_squ > 0.0:
+            a_b_squ = a_squ * b_squ
+            kwargs['a_squ'] = a_squ
+            kwargs['b_squ'] = b_squ
+            kwargs['a_b_squ'] = a_squ * b_squ
+            super().__init__(**kwargs)
+        else:
+            raise ValueError("a_squ and b_squ have to be positive definite")
+
+    def set_half_axes(self, a, b):
+        return self.set_half_axes_squ(a * a, b * b)
+
+    def set_half_axes_squ(self, a_squ, b_squ):
+        self.a_squ = a_squ
+        self.b_squ = b_squ
+        self.a_b_squ = a_squ * b_squ
+        return self
+
+LimitEllipse.XoStruct.extra_sources = [
+        _pkg_root.joinpath('beam_elements/apertures_src/limitellipse.h')]
--- xtrack/beam_elements/apertures_src/limitellipse.h
+++ xtrack/beam_elements/apertures_src/limitellipse.h
@@ -1,27 +1,24 @@
-#ifndef XTRACK_LIMITRECT_H
-#define XTRACK_LIMITRECT_H
+#ifndef XTRACK_LIMITELLIPSE_H
+#define XTRACK_LIMITELLIPSE_H
 
 /*gpufun*/
-void LimitRect_track_local_particle(LimitRectData el, LocalParticle* part){
+void LimitEllipse_track_local_particle(LimitEllipseData el, LocalParticle* part){
 
-    double const min_x = LimitRectData_get_min_x(el);
-    double const max_x = LimitRectData_get_max_x(el);
-    double const min_y = LimitRectData_get_min_y(el);
-    double const max_y = LimitRectData_get_max_y(el);
+
+    double const a_squ = LimitEllipseData_get_a_squ(el);
+    double const b_squ = LimitEllipseData_get_b_squ(el);
+    double const a_b_squ = LimitEllipseData_get_a_b_squ(el);
 
     double const n_part = LocalParticle_get_num_particles(part); //only_for_context cpu_serial cpu_openmp
     for (int ii=0; ii<n_part; ii++){ //only_for_context cpu_serial cpu_openmp
 	part->ipart = ii;            //only_for_context cpu_serial cpu_openmp
 
-
         double const x = LocalParticle_get_x(part);
         double const y = LocalParticle_get_y(part);
 
-	int64_t const is_alive = (int64_t)(
-                      (x >= min_x) &&
-		      (x <= max_x) &&
-		      (y >= min_y) &&
-		      (y <= max_y) );
+	double const temp = x*x*b_squ + y*y*a_squ; 
+
+	int64_t const is_alive = (int64_t)( temp <= a_b_squ );
 
 	// I assume that if I am in the function is because
     	if (!is_alive){
