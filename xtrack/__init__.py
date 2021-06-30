fc2f32484adce18573611374132d4791ee3fa576
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-06-30 14:54:54 +0200

    Start working on pyheadtail interface

--- xtrack/__init__.py
+++ xtrack/__init__.py
@@ -5,6 +5,7 @@ from .dress_element import dress_element, BeamElement
 from .beam_elements import *
 from .line import Line
 from .particles import Particles
+from .particles import PyHtXtParticles
 from .particles import  pyparticles_to_xtrack_dict
 from .tracker import Tracker
 
--- xtrack/particles/__init__.py
+++ xtrack/particles/__init__.py
@@ -1,1 +1,2 @@
 from .particles import *
+from .pyhtxtparticles import PyHtXtParticles
