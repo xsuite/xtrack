ea91f106581f8998dd2ee05d28087ddb8fef9039
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-07-01 09:22:23 +0200

    Working on pyheadtail interface example

---
+++ examples/pyheadtail_interface/000_tracking_example_with_pyht.py
@@ -0,0 +1,51 @@
+import pathlib
+import json
+import numpy as np
+
+import xobjects as xo
+import xline as xl
+import xtrack as xt
+
+
+fname_sequence = '../../test_data/lhc_no_bb/line_and_particle.json'
+
+num_turns = int(100)
+n_part = 200
+
+####################
+# Choose a context #
+####################
+
+context = xo.ContextCpu()
+#context = xo.ContextCupy()
+#context = xo.ContextPyopencl('0.0')
+
+##################
+# Get a sequence #
+##################
+
+with open(fname_sequence, 'r') as fid:
+     input_data = json.load(fid)
+sequence = xl.Line.from_dict(input_data['line'])
+
+##################
+# Build TrackJob #
+##################
+tracker = xt.Tracker(_context=context, sequence=sequence)
+
+######################
+# Get some particles #
+######################
+particles = xt.Particles(_context=context,
+                         p0c=6500e9,
+                         x=np.random.uniform(-1e-3, 1e-3, n_part),
+                         px=np.random.uniform(-1e-5, 1e-5, n_part),
+                         y=np.random.uniform(-2e-3, 2e-3, n_part),
+                         py=np.random.uniform(-3e-5, 3e-5, n_part),
+                         zeta=np.random.uniform(-1e-2, 1e-2, n_part),
+                         delta=np.random.uniform(-1e-4, 1e-4, n_part),
+                         )
+#########
+# Track #
+#########
+tracker.track(particles, num_turns=num_turns)
