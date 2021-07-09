bebba694398f4528cc59f9fc120c635d84ae95e4
Riccardo De Maria <riccardodemaria@gmail.com>
2021-07-09 16:22:22 +0200

    setup

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ extensions = []
 
 setup(
     name='xtrack',
-    version='0.0.0',
+    version='0.0.1',
     description='Tracking library for particle accelerators',
     url='https://github.com/xsuite/xtrack',
     author='Riccard De Maria',
@@ -21,5 +21,7 @@ setup(
     ext_modules = extensions,
     install_requires=[
         'numpy>=1.0',
+        'xobjects>=0.0.4',
+        'xline'
         ]
     )
