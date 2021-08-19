ff169a1c5bcc99863319e1628dc856abcdc901bc
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-08-19 11:18:40 +0200

    rng initialized with Particles method

--- examples/radiation/000_test_rng.py
+++ examples/radiation/000_test_rng.py
@@ -5,35 +5,27 @@ import xtrack as xt
 from pathlib import Path
 
 ctx = xo.ContextCpu()
-ctx.add_kernels(
-        sources=[
-            xt._pkg_root.joinpath(
-                'random_number_generator/rng_src/base_rng.h'),
-            xt._pkg_root.joinpath(
-                'random_number_generator/rng_src/particles_rng.h')],
-        kernels={'Particles_initialize_rand_gen': xo.Kernel(
-                     args=[
-                         xo.Arg(xt.Particles.XoStruct, name='particles'),
-                         xo.Arg(xo.UInt32, pointer=True, name='seeds'),
-                         xo.Arg(xo.Int32, name='n_init')],
-                     n_threads='n_init')})
 
 part = xt.Particles(context=ctx, p0c=6.5e12, x=[1,2,3])
-seeds = np.random.randint(low=1, high=4e9, size=part._capacity, dtype=np.uint32)
-
-seeds_dev = ctx.nparray_to_context_array(seeds)
-ctx.kernels.Particles_initialize_rand_gen(particles=part,
-        seeds=seeds, n_init=part._capacity)
+part._init_random_number_generator()
 
 class TestElement(xt.BeamElement):
      _xofields={
         'dummy': xo.Float64,
         }
-
 TestElement.XoStruct.extra_sources = [
     xt._pkg_root.joinpath('random_number_generator/rng_src/base_rng.h'),
     xt._pkg_root.joinpath('random_number_generator/rng_src/local_particle_rng.h'),
-    Path('./test_elem.h')]
+    ]
+TestElement.XoStruct.extra_sources.append('''
+/*gpufun*/
+void TestElement_track_local_particle(TestElementData el, LocalParticle* part0){
+    //start_per_particle_block (part0->part)
+        double rr = LocalParticle_generate_random_double(part);
+        LocalParticle_set_x(part, rr);
+    //end_per_particle_block
+}
+''')
 
 telem = TestElement()
 
