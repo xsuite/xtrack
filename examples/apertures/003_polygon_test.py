d4eb5bc424cc800b25817dc3701427954bc9e2c9
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-09-17 21:39:17 +0200

    Added a couple of extra checks

--- examples/apertures/003_polygon_test.py
+++ examples/apertures/003_polygon_test.py
@@ -4,11 +4,35 @@ import xobjects as xo
 import xtrack as xt
 
 context = xo.ContextPyopencl()
+np2ctx = context.nparray_to_context_array
+ctx2np = context.nparray_from_context_array
+
+
+x_vertices=np.array([1.5, 0.2, -1, -1,  1])*1e-2
+y_vertices=np.array([1.3, 0.5,  1, -1, -1])*1e-2
 
 aper = xt.LimitPolygon(
                 _context=context,
-                x_vertices=list(np.array([1.5, 0.2, -1, -1,  1])*1e-2),
-                y_vertices=list(np.array([1.3, 0.5,  1, -1, -1])*1e-2))
+                x_vertices=np2ctx(x_vertices),
+                y_vertices=np2ctx(y_vertices))
+
+# Try some particles inside
+parttest = xt.Particles(
+                _context=context,
+                p0c=6500e9,
+                x=x_vertices*0.99,
+                y=y_vertices*0.99)
+aper.track(parttest)
+assert np.allclose(ctx2np(parttest.state), 1)
+
+# Try some particles outside
+parttest = xt.Particles(
+                _context=context,
+                p0c=6500e9,
+                x=x_vertices*1.01,
+                y=y_vertices*1.01)
+aper.track(parttest)
+assert np.allclose(ctx2np(parttest.state), 0)
 
 part_gen_range = 0.02
 n_part=10000
