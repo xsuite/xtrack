cebab8f4618177eb24c5d91f2e5ab7d73cf58db5
Martin Schwinzerl <martin.schwinzerl@cern.ch>
2021-09-29 10:01:08 +0200

    cosmetic changes and linting in apertures
    - apply autopep8
    - refactors the handling of constructor arguments for
      LimitEllipse into a static method -> this can be re-used
      for the LimitRectEllipse element

--- xtrack/beam_elements/apertures.py
+++ xtrack/beam_elements/apertures.py
@@ -6,24 +6,27 @@ from ..general import _pkg_root
 
 class LimitRect(BeamElement):
     _xofields = {
-        'min_x': xo.Float64,
-        'max_x': xo.Float64,
-        'min_y': xo.Float64,
-        'max_y': xo.Float64,
-        }
+        "min_x": xo.Float64,
+        "max_x": xo.Float64,
+        "min_y": xo.Float64,
+        "max_y": xo.Float64,
+    }
+
 
 LimitRect.XoStruct.extra_sources = [
-        _pkg_root.joinpath('beam_elements/apertures_src/limitrect.h')]
+    _pkg_root.joinpath("beam_elements/apertures_src/limitrect.h")
+]
 
 
 class LimitEllipse(BeamElement):
     _xofields = {
-            'a_squ': xo.Float64,
-            'b_squ': xo.Float64,
-            'a_b_squ': xo.Float64,
-            }
+        "a_squ": xo.Float64,
+        "b_squ": xo.Float64,
+        "a_b_squ": xo.Float64,
+    }
 
-    def __init__(self, a_squ=None, b_squ=None, **kwargs):
+    @staticmethod
+    def _prepare_init_args(a_squ=None, b_squ=None, **kwargs):
         if a_squ is None and "a" in kwargs:
             a = kwargs.get("a")
             if a is not None and a > 0.0:
@@ -40,12 +43,16 @@ class LimitEllipse(BeamElement):
 
         if a_squ > 0.0 and b_squ > 0.0:
             a_b_squ = a_squ * b_squ
-            kwargs['a_squ'] = a_squ
-            kwargs['b_squ'] = b_squ
-            kwargs['a_b_squ'] = a_squ * b_squ
-            super().__init__(**kwargs)
+            kwargs["a_squ"] = a_squ
+            kwargs["b_squ"] = b_squ
+            kwargs["a_b_squ"] = a_squ * b_squ
         else:
             raise ValueError("a_squ and b_squ have to be positive definite")
+
+        return dict(**kwargs)
+
+    def __init__(self, **kwargs):
+        super().__init__(**LimitEllipse._prepare_init_args(**kwargs))
 
     def set_half_axes(self, a, b):
         return self.set_half_axes_squ(a * a, b * b)
@@ -56,5 +63,7 @@ class LimitEllipse(BeamElement):
         self.a_b_squ = a_squ * b_squ
         return self
 
+
 LimitEllipse.XoStruct.extra_sources = [
-        _pkg_root.joinpath('beam_elements/apertures_src/limitellipse.h')]
+    _pkg_root.joinpath("beam_elements/apertures_src/limitellipse.h")
+]
