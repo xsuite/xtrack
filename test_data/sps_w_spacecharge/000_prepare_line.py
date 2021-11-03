48cb105e69823d7692bb08c343c46cfc65dd233a
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-11-03 17:27:39 +0100

    Modified another test_data case

--- test_data/sps_w_spacecharge/000_prepare_line.py
+++ test_data/sps_w_spacecharge/000_prepare_line.py
@@ -6,7 +6,7 @@ from cpymad.madx import Madx
 
 import xline as xl
 import xpart as xp
-import pymask as pm
+import xobjects as xo
 
 seq_name = 'sps'
 bunch_intensity = 1e11/3
@@ -33,15 +33,50 @@ i_cav_madx = mad.sequence[seq_name].element_names().index('acta.31637')
 mad.sequence[seq_name].elements[i_cav_madx].volt = V_RF/1e6
 mad.sequence[seq_name].elements[i_cav_madx].lag = 0.5
 
-# Optics and orbit at start ring
-optics_and_orbit_at_start_ring = pm.get_optics_and_orbit_at_start_ring(
-                                                  mad, seq_name=seq_name)
+mad.use(sequence=seq_name)
+twiss_table = mad.twiss(rmatrix=True)
+
+mad_beam =  mad.sequence[seq_name].beam
+assert mad_beam.deltap == 0, "Not implemented."
+
+particle_on_madx_co = xp.Particles(
+    p0c = mad_beam.pc*1e9,
+    q0 = mad_beam.charge,
+    mass0 = mad_beam.mass*1e9,
+    s = 0,
+    x = twiss_table.x[0],
+    px = twiss_table.px[0],
+    y = twiss_table.y[0],
+    py = twiss_table.py[0],
+    tau = twiss_table.t[0],
+    ptau = twiss_table.pt[0],
+)
+
+RR_madx = np.zeros([6,6])
+
+for ii in range(6):
+    for jj in range(6):
+        RR_madx[ii, jj] = getattr(twiss_table, f're{ii+1}{jj+1}')[0]
+
+optics_and_co_at_start_ring_from_madx = {
+        'betx': twiss_table.betx[0],
+        'bety': twiss_table.bety[0],
+        'alfx': twiss_table.alfx[0],
+        'alfy': twiss_table.alfy[0],
+        'dx': twiss_table.dx[0],
+        'dy': twiss_table.dy[0],
+        'dpx': twiss_table.dpx[0],
+        'dpy': twiss_table.dpy[0],
+        'RR_madx': RR_madx,
+        'particle_on_madx_co': particle_on_madx_co.to_dict()
+        }
+
 with open('optics_and_co_at_start_ring.json', 'w') as fid:
-    json.dump(optics_and_orbit_at_start_ring, fid, cls=pm.JEncoder)
+    json.dump(optics_and_co_at_start_ring_from_madx, fid, cls=xo.JEncoder)
 
 part_on_co = xp.Particles.from_dict(
-        optics_and_orbit_at_start_ring['particle_on_madx_co'])
-RR = np.array(optics_and_orbit_at_start_ring['RR_madx'])
+        optics_and_co_at_start_ring_from_madx['particle_on_madx_co'])
+RR = np.array(optics_and_co_at_start_ring_from_madx['RR_madx'])
 
 # A test particle
 part = part_on_co.copy()
@@ -52,7 +87,7 @@ with open('line_no_spacecharge_and_particle.json', 'w') as fid:
     json.dump({
         'line': line_without_spacecharge.to_dict(keepextra=True),
         'particle': part.to_dict()},
-        fid, cls=pm.JEncoder)
+        fid, cls=xo.JEncoder)
 
 # Start space-charge configuration
 
@@ -112,5 +147,5 @@ with open('line_with_spacecharge_and_particle.json', 'w') as fid:
     json.dump({
         'line': line_with_spacecharge.to_dict(keepextra=True),
         'particle': part.to_dict()},
-        fid, cls=pm.JEncoder)
+        fid, cls=xo.JEncoder)
 
