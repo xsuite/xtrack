758d8fa61c4784c1205bc6b393fe0b5c51aa70b3
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-11-03 16:08:05 +0100

    Another test_data case

--- test_data/lhc_with_bb/000_test_against_sixtrack_and_save_line.py
+++ test_data/lhc_with_bb/000_test_against_sixtrack_and_save_line.py
@@ -1,8 +1,14 @@
-import sixtracktools
-import xline
-
+import json
 
 import numpy as np
+
+import sixtracktools
+
+import xobjects as xo
+import xline as xl
+import xtrack as xt
+import xpart as xp
+import xfields as xf
 
 
 ##############
@@ -14,10 +20,13 @@ sixinput = sixtracktools.SixInput(".")
 p0c_eV = sixinput.initialconditions[-3] * 1e6
 
 # Build xline line from sixtrack input
-line = xline.Line.from_sixinput(sixinput)
+line = xl.Line.from_sixinput(sixinput)
 
 # Info on sixtrack->pyblep conversion
 iconv = line.other_info["iconv"]
+
+# Build tracker
+tracker = xt.Tracker(sequence=line)
 
 ########################################################
 #                  Search closed orbit                 #
@@ -30,165 +39,40 @@ sixdump_all = sixtracktools.SixDump101("res/dump3.dat")
 Nele_st = len(iconv)
 sixdump_CO = sixdump_all[::2][:Nele_st]
 
-# Disable BB elements
-line.disable_beambeam()
-
-# Find closed orbit
-guess_from_sixtrack = [
-    getattr(sixdump_CO, att)[0] for att in "x px y py sigma delta".split()
-]
-part_on_CO = line.find_closed_orbit(
-    guess=guess_from_sixtrack, method="get_guess", p0c=p0c_eV
-)
+# Get closed-orbit from sixtrack 
+part_on_CO = xp.Particles(
+        p0c=p0c_eV,
+        x=sixdump_CO.x[0],
+        px=sixdump_CO.px[0],
+        y=sixdump_CO.y[0],
+        py=sixdump_CO.py[0],
+        zeta=sixdump_CO.zeta[0],
+        delta=sixdump_CO.delta[0])
 
 print("Closed orbit at start machine:")
-print("x px y py sigma delta:")
-print(part_on_CO)
+for nn in "x px y py zeta delta".split():
+    print(f"    {nn}={getattr(part_on_CO, nn)[0]:.5e}:")
 
 
 #######################################################
 #  Store closed orbit and dipole kicks at BB elements #
 #######################################################
 
-line.beambeam_store_closed_orbit_and_dipolar_kicks(
-    part_on_CO,
-    separation_given_wrt_closed_orbit_4D=True,
-    separation_given_wrt_closed_orbit_6D=True,
-)
+xf.configure_orbit_dependent_parameters_for_bb(tracker,
+                       particle_on_co=part_on_CO, xline=line)
 
 
-###########################
-# Checks against sixtrack #
-###########################
-
-# Compute closed orbit using tracking
-closed_orbit = line.track_elem_by_elem(part_on_CO)
-
-
-# Check that closed orbit is closed
-pstart = closed_orbit[0].copy()
-pstart_st = xline.Particles(**sixdump_CO[0].get_minimal_beam())
-
-print("STsigma, Sigma, Stdelta, delta, Stpx, px")
-for iturn in range(10):
-    line.track(pstart)
-    line.track(pstart_st)
-    print(
-        "%e, %e, %e, %e, %e, %e"
-        % (
-            pstart_st.sigma,
-            pstart.sigma,
-            pstart_st.delta,
-            pstart.delta,
-            pstart_st.px,
-            pstart.px,
-        )
-    )
-
-# Compare closed orbit against sixtrack
-for att in "x px y py delta sigma".split():
-    att_CO = np.array([getattr(pp, att) for pp in closed_orbit])
-    att_CO_at_st_ele = att_CO[iconv]
-    print(
-        "Max C.O. discrepancy in %s %.2e"
-        % (att, np.max(np.abs(att_CO_at_st_ele - getattr(sixdump_CO, att))))
-    )
-
-
-# Compare tracking results
+################
+# Save to json #
+################
 sixdump = sixdump_all[1::2]  # Particle with deviation from CO
-# sixdump = sixdump_all[::2] # Particle on CO
-
-p_in_st = xline.Particles(**sixdump[0].get_minimal_beam())
-p_out_st = xline.Particles(**sixdump[1].get_minimal_beam())
-
-p_in_pyst = p_in_st.copy()
-p_out_pyst = p_in_pyst.copy()
-
-
-for att in "x px y py delta sigma".split():
-    attin = getattr(p_in_st, att)
-    attout = getattr(p_out_st, att)
-    print("SxTr: Change in " + att + ": %e" % (attout - attin))
-
-    attin_pyst = getattr(p_in_pyst, att)
-    attout_pyst = getattr(p_out_pyst, att)
-    print("PyST: Change in " + att + ": %e" % (attout_pyst - attin_pyst))
-
-
-def compare(prun, pbench, pbench_prev):
-    out = []
-    out_rel = []
-    error = False
-    for att in "x px y py delta sigma".split():
-        vrun = getattr(prun, att)
-        vbench = getattr(pbench, att)
-        vbench_prev = getattr(pbench_prev, att)
-        diff = vrun - vbench
-        diffrel = abs(
-            1.0 - abs(vrun - vbench_prev) / abs(vbench - vbench_prev)
-        )
-        out.append(abs(diff))
-        out_rel.append(diffrel)
-        print(
-            f"{att:<5} {vrun:22.13e} {vbench:22.13e}"
-            #f" {diff:22.13g} {diffrel:22.13g}"
-        )
-        if diffrel > 1e-8 or np.isnan(diffrel):
-            if np.abs(diff) > 1e-11:
-                print("Too large discrepancy!")
-                error = True
-    print(f"\nmax {max(out):21.12e} maxrel {max(out_rel):22.12e}")
-    return error
-
-
-print("")
-for ii in range(1, len(iconv)):
-    jja = iconv[ii - 1]
-    jjb = iconv[ii]
-    prun = xline.Particles(**sixdump[ii - 1].get_minimal_beam())
-    pbench_prev = prun.copy()
-    print(f"\n-----sixtrack={ii} sixtracklib={jja} --------------")
-    # print(f"pysixtr {jja}, x={prun.x}, px={prun.px}")
-    for jj in range(jja + 1, jjb + 1):
-        label = line.element_names[jj]
-        elem = line.elements[jj]
-        pin = prun.copy()
-        elem.track(prun)
-        print(f"{jj} {label},{str(elem)[:50]}")
-    pbench = xline.Particles(**sixdump[ii].get_minimal_beam())
-    # print(f"sixdump {ii}, x={pbench.x}, px={pbench.px}")
-    print("-----------------------")
-    error = compare(prun, pbench, pbench_prev)
-    print("-----------------------\n\n")
-
-    if error:
-        print("Error detected")
-        break
-
-#import pickle
-#with open('line_and_particle.pkl', 'wb') as fid:
-#    pickle.dump({
-#        'line': line.to_dict(keepextra=True),
-#        'particle': xline.Particles(
-#                **sixdump[0].get_minimal_beam()).to_dict()},
-#        fid)
-
-part_dict = xline.Particles(
+part_dict = xp.Particles(
         **sixdump[0].get_minimal_beam()).to_dict()
 part_dict['state'] = 1
 
-import json
-class Encoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, np.ndarray):
-            return obj.tolist()
-        elif np.issubdtype(type(obj), np.integer):
-            return int(obj)
-        return json.JSONEncoder.default(self, obj)
 with open('line_and_particle.json', 'w') as fid:
     json.dump({
         'line': line.to_dict(keepextra=True),
         'particle': part_dict},
-        fid, cls=Encoder)
+        fid, cls=xo.JEncoder)
 
