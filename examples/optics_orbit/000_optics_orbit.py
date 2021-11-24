e8b02c3e01084a689a982d1e7b3639f272b8dff2
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2021-11-24 12:18:41 +0100

    This works

--- examples/optics_orbit/000_optics_orbit.py
+++ examples/optics_orbit/000_optics_orbit.py
@@ -5,6 +5,7 @@ import numpy as np
 import xobjects as xo
 import xtrack as xt
 import xpart as xp
+from scipy.constants import c as clight
 
 
 fname_line = '../../test_data/hllhc_14/line_and_particle.json'
@@ -29,11 +30,28 @@ R_matrix = tracker.compute_one_turn_matrix_finite_differences(
 eta = -R_matrix[4, 5]/line.get_length() # minus sign comes from z = s-ct
 alpha_mom_compaction = eta + 1/particle_ref.gamma0[0]**2
 
+T_rev = line.get_length()/(particle_ref.beta0[0]*clight)
+
 freq_list = []
 lag_list_deg = []
 voltage_list = []
+h_list = []
 for ee in line.elements:
     if ee.__class__.__name__ == 'Cavity':
         freq_list.append(ee.frequency)
         lag_list_deg.append(ee.lag)
         voltage_list.append(ee.voltage)
+        h_list.append(ee.frequency*T_rev)
+
+zeta, delta = xp.longitudinal.generate_longitudinal_coordinates(
+        distribution='gaussian',
+        mass0=particle_ref.mass0,
+        q0=particle_ref.q0,
+        gamma0=particle_ref.gamma0,
+        num_particles=10000,
+        circumference=line.get_length(),
+        alpha_momentum_compaction=alpha_mom_compaction,
+        rf_harmonic=h_list,
+        rf_voltage=voltage_list,
+        rf_phase=(np.array(lag_list_deg) - 180)/180*np.pi,
+        sigma_z=10e-2)
