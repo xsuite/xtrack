5f0c180c1b674da89e3af23b317c5a78bd8a93e3
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2022-01-04 12:17:01 -0300

    Cosmetics

--- examples/twiss/000_twiss.py
+++ examples/twiss/000_twiss.py
@@ -24,7 +24,7 @@ import matplotlib.pyplot as plt
 
 plt.close('all')
 
-fig1 = plt.figure(1)
+fig1 = plt.figure(1, figsize=(6.4, 4.8*1.5))
 spbet = plt.subplot(3,1,1)
 spco = plt.subplot(3,1,2, sharex=spbet)
 spdisp = plt.subplot(3,1,3, sharex=spbet)
@@ -37,5 +37,12 @@ spco.plot(tw['s'], tw['y'])
 
 spdisp.plot(tw['s'], tw['dx'])
 spdisp.plot(tw['s'], tw['dy'])
+
+spbet.set_ylabel(r'$\beta_{x,y}$ [m]')
+spco.set_ylabel(r'(Closed orbit)$_{x,y}$ [m]')
+spdisp.set_ylabel(r'$D_{x,y}$ [m]')
+spdisp.set_xlabel('s [m]')
+
+fig1.subplots_adjust(left=.15, right=.92, hspace=.27)
 
 plt.show()
