b3fb085c008cd4e35a6d50a1fbac9c7578546de3
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2022-01-04 21:25:59 -0300

    Quick push

--- examples/twiss/000_twiss.py
+++ examples/twiss/000_twiss.py
@@ -1,23 +1,31 @@
-import pickle
 import json
-import pathlib
 import numpy as np
 
 import xobjects as xo
 import xtrack as xt
 import xpart as xp
 
+###############
+# Load a line #
+###############
+
 fname_line_particles = '../../test_data/hllhc15_noerrors_nobb/line_and_particle.json'
 #fname_line_particles = '../../test_data/sps_w_spacecharge/line_no_spacecharge_and_particle.json' #!skip-doc
 
 with open(fname_line_particles, 'r') as fid:
     input_data = json.load(fid)
-
 line = xt.Line.from_dict(input_data['line'])
 particle_ref = xp.Particles.from_dict(input_data['particle'])
 
-print('Build tracker...')
+#################
+# Build tracker #
+#################
+
 tracker = xt.Tracker(line=line)
+
+#########
+# Twiss #
+#########
 
 tw = tracker.twiss(particle_ref=particle_ref)
 
