63c45275d36fb573d4073ffcd04aa7b29205c5ee
giadarol <giovanni.iadarola@cern.ch>
2022-01-24 14:19:23 +0100

    Added test

--- examples/apertures/004_racetrack.py
+++ examples/apertures/004_racetrack.py
@@ -32,21 +32,21 @@ plt.plot(part_x, part_y, '.', color='red')
 plt.plot(part_x[part_state>0], part_y[part_state>0], '.', color='green')
 
 
-xy_in = np.array([
+xy_out = np.array([
     [-4.8e-2, 3.7e-2],
     [9.6e-2, 3.7e-2],
     [-4.5e-2, -1.8e-2],
     [9.8e-2, -1.8e-2],
     ])
 
-xy_out = np.array([
+xy_in = np.array([
     [-4.2e-2, 3.3e-2],
     [9.4e-2, 3.6e-2],
     [-3.8e-2, -1.8e-2],
     [9.2e-2, -1.8e-2],
     ])
 
-xy_all = np.concatenate([xy_in, xy_out], axis=0)
+xy_all = np.concatenate([xy_out, xy_in], axis=0)
 
 particles = xp.Particles(_context=context,
         p0c=6500e9,
@@ -58,8 +58,14 @@ aper.track(particles)
 part_state = context.nparray_from_context_array(particles.state)
 part_x = context.nparray_from_context_array(particles.x)
 part_y = context.nparray_from_context_array(particles.y)
+part_id = context.nparray_from_context_array(particles.particle_id)
+
+assert np.all(part_state[part_id<4] == 0)
+assert np.all(part_state[part_id>=4] == 1)
 
 plt.plot(part_x, part_y, '.', color='k')
 plt.plot(part_x[part_state>0], part_y[part_state>0], 'x', color='k')
+
+
 
 plt.show()
--- tests/test_apertures.py
+++ tests/test_apertures.py
@@ -39,6 +39,50 @@ def test_rect_ellipse():
 
         assert np.all(flag_re == (flag_r & flag_e))
 
+def test_aperture_racetrack():
+
+    for context in xo.context.get_test_contexts():
+        print(f"Test {context.__class__}")
+
+        part_gen_range = 0.11
+        n_part=100000
+
+        aper = xt.LimitRacetrack(_context=context,
+                                 min_x=-5e-2, max_x=10e-2,
+                                 min_y=-2e-2, max_y=4e-2,
+                                 a=2e-2, b=1e-2)
+
+        xy_out = np.array([
+            [-4.8e-2, 3.7e-2],
+            [9.6e-2, 3.7e-2],
+            [-4.5e-2, -1.8e-2],
+            [9.8e-2, -1.8e-2],
+            ])
+
+        xy_in = np.array([
+            [-4.2e-2, 3.3e-2],
+            [9.4e-2, 3.6e-2],
+            [-3.8e-2, -1.8e-2],
+            [9.2e-2, -1.8e-2],
+            ])
+
+        xy_all = np.concatenate([xy_out, xy_in], axis=0)
+
+        particles = xp.Particles(_context=context,
+                p0c=6500e9,
+                x=xy_all[:, 0],
+                y=xy_all[:, 1])
+
+        aper.track(particles)
+
+        part_state = context.nparray_from_context_array(particles.state)
+        part_id = context.nparray_from_context_array(particles.particle_id)
+
+        assert np.all(part_state[part_id<4] == 0)
+        assert np.all(part_state[part_id>=4] == 1)
+
+
+
 def test_aperture_polygon():
 
     for context in xo.context.get_test_contexts():
