ecc228f37e6b7416fc1001e74c30027b21b8b7b6
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2022-01-27 15:23:19 +0100

    Status

--- examples/twiss/002_dev_fast_twiss.py
+++ examples/twiss/002_dev_fast_twiss.py
@@ -30,8 +30,7 @@ co_search_settings = None
 steps_r_matrix = None
 nemitt_x = 1e-6
 nemitt_y = 1e-6
-
-
+r_sigma = 0.01
 
 context = tracker._buffer.context
 
@@ -41,6 +40,9 @@ part_on_co = tracker.find_closed_orbit(particle_co_guess=particle_co_guess,
 RR = tracker.compute_one_turn_matrix_finite_differences(
                                             steps_r_matrix=steps_r_matrix,
                                             particle_on_co=part_on_co)
+
+gemitt_x = nemitt_x/part_on_co.beta0/part_on_co.gamma0
+gemitt_y = nemitt_y/part_on_co.beta0/part_on_co.gamma0
 
 W, Winv, Rot = xp.compute_linear_normal_form(RR)
 
@@ -65,23 +67,24 @@ s = np.array(tracker.line.get_s_elements())
 #                     scale_with_transverse_norm_emitt=(nemitt_x, nemitt_y),
 #                     )
 
-scale_transverse = 1e-7
+scale_transverse_x = np.sqrt(gemitt_x)*r_sigma
+scale_transverse_y = np.sqrt(gemitt_y)*r_sigma
 part_for_twiss = xp.build_particles(
                     particle_ref=part_on_co, mode='shift',
-                    x=  W[0, :4] * scale_transverse,
-                    px= W[1, :4] * scale_transverse,
-                    y=  W[2, :4] * scale_transverse,
-                    py= W[3, :4] * scale_transverse,
+                    x=  W[0, :4] * scale_transverse_x,
+                    px= W[1, :4] * scale_transverse_x,
+                    y=  W[2, :4] * scale_transverse_y,
+                    py= W[3, :4] * scale_transverse_y,
                     zeta = 0,
                     delta = 0,
                     )
 tracker.track(part_for_twiss, turn_by_turn_monitor='ONE_TURN_EBE')
 
 W4 = np.zeros(shape=(4,4,len(s)), dtype=np.float64)
-W4[0, :, :] = (tracker.record_last_track.x - x_co) / scale_transverse
-W4[1, :, :] = (tracker.record_last_track.px - px_co) / scale_transverse
-W4[2, :, :] = (tracker.record_last_track.y  - y_co) / scale_transverse
-W4[3, :, :] = (tracker.record_last_track.py - py_co) / scale_transverse
+W4[0, :, :] = (tracker.record_last_track.x - x_co) / scale_transverse_x
+W4[1, :, :] = (tracker.record_last_track.px - px_co) / scale_transverse_x
+W4[2, :, :] = (tracker.record_last_track.y  - y_co) / scale_transverse_y
+W4[3, :, :] = (tracker.record_last_track.py - py_co) / scale_transverse_y
 
 betx = W4[0, 0, :]**2 + W4[0, 1, :]**2
 bety = W4[2, 2, :]**2 + W4[2, 3, :]**2
