7fc294b4d12c72b7ed58ba3545f35efdf73dd9da
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2022-01-29 14:47:42 +0100

    Add test for ebe monitor

--- tests/test_tracker.py
+++ tests/test_tracker.py
@@ -1,5 +1,35 @@
+import numpy as np
 import xobjects as xo
 import xtrack as xt
+import xpart as xp
+
+def test_ebe_monitor():
+
+    for context in xo.context.get_test_contexts():
+        print(f"Test {context.__class__}")
+
+        line = xt.Line(elements=[xt.Multipole(knl=[0, 1.]),
+                                xt.Drift(length=0.5),
+                                xt.Multipole(knl=[0, -1]),
+                                xt.Cavity(frequency=400e7, voltage=6e6),
+                                xt.Drift(length=.5),
+                                xt.Drift(length=0)])
+
+        tracker = xt.Tracker(line=line, _context=context)
+
+        particles = xp.Particles(x=[1e-3, -2e-3, 5e-3], y=[2e-3, -4e-3, 3e-3],
+                                zeta=1e-2, p0c=7e12, mass0=xp.PROTON_MASS_EV,
+                                _context=context)
+
+        tracker.track(particles.copy(), turn_by_turn_monitor='ONE_TURN_EBE')
+
+        mon = tracker.record_last_track
+
+        for ii, ee in enumerate(line.elements):
+            for tt, nn in particles._structure['per_particle_vars']:
+                assert np.all(particles.to_dict()[nn] == getattr(mon, nn)[:, ii])
+            ee.track(particles)
+            particles.at_element += 1
 
 def test_cycle():
 
