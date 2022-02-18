38be2de7aac21c52fa79e89e2986cee0e94099ff
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2022-02-18 10:59:47 +0100

    Tracking consistent with emit

--- examples/radiation/004_damping_against_madx.py
+++ examples/radiation/004_damping_against_madx.py
@@ -1,3 +1,4 @@
+import time
 import numpy as np
 
 from cpymad.madx import Madx
@@ -79,3 +80,36 @@ assert np.isclose(tw['partition_numbers'][2],
     met[met.loc[:, 'parameter']=='damping_partion']['mode3'][0],
     rtol=3e-3, atol=0
     )
+
+part_co = tw['particle_on_co']
+particles = xp.build_particles(tracker=tracker,
+    x_norm=[0.1, 0, 0], y_norm=[0, 0.1, 0], zeta=part_co.zeta[0],
+    delta=np.array([0,0,1e-4]) + part_co.delta[0],
+    scale_with_transverse_norm_emitt=(1e-9, 1e-9))
+
+num_turns = 5000
+t1 = time.time()
+tracker.track(particles, num_turns=num_turns, turn_by_turn_monitor=True)
+t2 = time.time()
+print(f'Track time: {(t2-t1)/num_turns:.2e} s/turn')
+mon = tracker.record_last_track
+
+import matplotlib.pyplot as plt
+plt.close('all')
+fig = plt.figure()
+ax1 = fig.add_subplot(311)
+ax2 = fig.add_subplot(312, sharex=ax1)
+ax3 = fig.add_subplot(313, sharex=ax1)
+
+ax1.plot(mon.x.T)
+ax2.plot(mon.y.T)
+ax3.plot(mon.delta.T)
+i_turn = np.arange(num_turns)
+ax1.plot(part_co.x[0]
+    +(mon.x[0,0]-part_co.x[0])*np.exp(-i_turn*tw['damping_constants_turns'][0]))
+ax2.plot(part_co.y[0]
+    +(mon.y[1,0]-part_co.y[0])*np.exp(-i_turn*tw['damping_constants_turns'][1]))
+ax3.plot(part_co.delta[0]
+    +(mon.delta[2,0]-part_co.delta[0])*np.exp(-i_turn*tw['damping_constants_turns'][2]))
+
+plt.show()
