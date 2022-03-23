24936b546d40629ef1001077a4b78304187776d3
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2022-03-23 11:04:40 +0100

    Tested import of multipole

--- examples/mad_import/000_import_test_elements.py
+++ examples/mad_import/000_import_test_elements.py
@@ -12,12 +12,14 @@ mad.input("""
 cav0: rfcavity, freq=10, lag=0.5, volt=6;
 cav1: rfcavity, lag=0.5, volt=6, harmon=8;
 wire1: wire, current=5, l=0, l_phy=1, l_int=2, xma=1e-3, yma=2e-3;
+mult0: multipole, knl={1,2,3}, ksl={4,5,6}, lrad=1.1;
 """)
 
 # Sequence
 mad.input("""
 
 testseq: sequence, l=10;
+m0: mult0 at=0.1;
 c0: cav0, at=0.2, apertype=circle, aperture=0.01;
 c1: cav1, at=0.2, apertype=circle, aperture=0.01;
 
@@ -42,6 +44,15 @@ line.particle_ref = xp.Particles(mass0=xp.PROTON_MASS_EV, gamma0=1.05)
 
 assert len(line.element_names) == len(line.element_dict.keys())
 assert line.get_length() == 10
+
+assert isinstance(line['m0'], xt.Multipole)
+assert line.get_s_position('m0') == 0.1
+assert np.all(line['m0'].knl == np.array([1,2,3]))
+assert np.all(line['m0'].ksl == np.array([4,5,6]))
+assert line['m0'].hxl == 1
+assert line['m0'].hyl == 4
+assert line['m0'].length == 1.1
+
 
 assert isinstance(line['c0'], xt.Cavity)
 assert line.get_s_position('c0') == 0.2
