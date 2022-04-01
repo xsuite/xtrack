a5e1ed212961c566438ac9095f41b1224f675d7c
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2022-04-01 12:33:09 +0200

    This works

--- examples/monitor/000_monitor.py
+++ examples/monitor/000_monitor.py
@@ -1,10 +1,9 @@
 import json
 
+import numpy as np
+
 import xtrack as xt
 import xpart as xp
-
-from cpymad.madx import Madx
-
 
 with open('../../test_data/hllhc15_noerrors_nobb/line_and_particle.json') as f:
     dct = json.load(f)
@@ -19,15 +18,17 @@ particles0 = xp.generate_matched_gaussian_bunch(tracker=tracker,
                                                nemitt_x=2.5e-6,
                                                nemitt_y=2.5e-6,
                                                sigma_z=9e-2)
-#     #####################################
-#     # Save all turns starting from zero #
-#     #####################################
-#     
-#     # (particles.at_turn must be 0 at the beginning)
-#     particles = particles0.copy()
-#     num_turns = 500
-#     tracker.track(particles, num_turns=num_turns, turn_by_turn_monitor=True)
-#     # ---> tracker.record_last_track.x (contains the z x coordinates)
+#####################################
+# Save all turns starting from zero #
+#####################################
+
+# (particles.at_turn must be 0 at the beginning)
+particles = particles0.copy()
+num_turns = 500
+tracker.track(particles, num_turns=num_turns, turn_by_turn_monitor=True)
+# ---> tracker.record_last_track.x (contains the z x coordinates)
+
+mon = tracker.record_last_track
 
 ######################################
 # Save 10 turns starting from turn 5 #
@@ -42,33 +43,13 @@ particles = particles0.copy()
 num_turns = 500
 tracker.track(particles, num_turns=num_turns, turn_by_turn_monitor=monitor)
 
-#     #####################################################
-#     # Save 10 turns starting from turn 5 in ip5 and ip8 #
-#     #####################################################
-#     
-#     # Build a monitor
-#     monitor_ip5 = xt.ParticlesMonitor(start_at_turn=5, stop_at_turn=15,
-#                                       num_particles=num_particles)
-#     monitor_ip8 = xt.ParticlesMonitor(start_at_turn=5, stop_at_turn=15,
-#                                       num_particles=num_particles)
-#     
-#     line_with_monitor = line.copy()
-#     line_with_monitor.insert_element(index='ip5', element=monitor_ip5, name='mymon5')
-#     line_with_monitor.insert_element(index='ip8', element=monitor_ip8, name='mymon8')
-#     
-#     tracker_w_monitor = line_with_monitor.build_tracker()
-#     
-#     particles = particles0.copy()
-#     num_turns = 500
-#     tracker_w_monitor.track(particles, num_turns=num_turns)
-#     
-#     #######################
-#     # Multi-frame monitor #
-#     #######################
+#######################
+# Multi-frame monitor #
+#######################
 
 # Repeated frames
 monitor_multiframe = xt.ParticlesMonitor(start_at_turn=5, stop_at_turn=15,
-                                  n_repetitions=2,
+                                  n_repetitions=3,
                                   repetition_period=100,
                                   num_particles=num_particles)
 
@@ -78,3 +59,16 @@ particles = particles0.copy()
 num_turns = 500
 tracker.track(particles, num_turns=num_turns,
               turn_by_turn_monitor=monitor_multiframe)
+
+assert np.all(mon.x.shape == np.array([50, 500]))
+assert np.all(mon.at_turn[3, :] == np.arange(0, 500))
+assert np.all(mon.particle_id[:, 3] == np.arange(0, num_particles))
+
+assert np.all(monitor.x.shape == np.array([50, 10]))
+assert np.all(monitor.at_turn[3, :] == np.arange(5, 15))
+assert np.all(monitor.particle_id[:, 3] == np.arange(0, num_particles))
+
+assert np.all(monitor_multiframe.x.shape == np.array([3, 50, 10]))
+assert np.all(monitor_multiframe.at_turn[1, 3, :] == np.arange(105, 115))
+assert np.all(monitor_multiframe.particle_id[2, :, 3] == np.arange(0, num_particles))
+
