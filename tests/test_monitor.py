a772f388b756ec250976eca691fcc921c810009d
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2022-04-02 10:03:46 +0200

    Some more asserts

--- tests/test_monitor.py
+++ tests/test_monitor.py
@@ -43,6 +43,7 @@ def test_monitor():
         assert np.all(mon.x.shape == np.array([50, 30]))
         assert np.all(mon.at_turn[3, :] == np.arange(0, num_turns))
         assert np.all(mon.particle_id[:, 3] == np.arange(0, num_particles))
+        assert np.all(mon.at_element[:, :] == 0)
 
         # Test explicit monitor passed to track
         monitor = xt.ParticlesMonitor(_context=context,
@@ -54,6 +55,8 @@ def test_monitor():
         assert np.all(monitor.x.shape == np.array([50, 10]))
         assert np.all(monitor.at_turn[3, :] == np.arange(5, 15))
         assert np.all(monitor.particle_id[:, 3] == np.arange(0, num_particles))
+        assert np.all(monitor.at_element[:, :] == 0)
+
 
         # Test explicit monitor used in in stand-alone mode
         mon2 = xt.ParticlesMonitor(_context=context,
@@ -67,6 +70,7 @@ def test_monitor():
         assert np.all(mon2.x.shape == np.array([50, 10]))
         assert np.all(mon2.at_turn[3, :] == np.arange(5, 15))
         assert np.all(mon2.particle_id[:, 3] == np.arange(0, num_particles))
+        assert np.all(mon2.at_element[:, :] == 0)
 
         # Test monitors with multiple frames
         monitor_multiframe = xt.ParticlesMonitor(_context=context,
@@ -82,3 +86,31 @@ def test_monitor():
         assert np.all(monitor_multiframe.at_turn[1, 3, :] == np.arange(25, 30))
         assert np.all(monitor_multiframe.particle_id[2, :, 3] == np.arange(0,
                                                                  num_particles))
+        assert np.all(monitor_multiframe.at_element[:, :, :] == 0)
+
+        # Test monitors installed in a line
+        monitor_ip5 = xt.ParticlesMonitor(start_at_turn=5, stop_at_turn=15,
+                                          num_particles=num_particles)
+        monitor_ip8 = xt.ParticlesMonitor(start_at_turn=5, stop_at_turn=15,
+                                          num_particles=num_particles)
+        line_with_monitor = line.copy()
+        line_with_monitor.insert_element(index='ip5', element=monitor_ip5, name='mymon5')
+        line_with_monitor.insert_element(index='ip8', element=monitor_ip8, name='mymon8')
+
+        tracker_w_monitor = line_with_monitor.build_tracker(_context=context)
+
+        particles = particles0.copy(_context=context)
+        num_turns = 50
+        tracker_w_monitor.track(particles, num_turns=num_turns)
+
+        assert np.all(monitor_ip5.x.shape == np.array([50, 10]))
+        assert np.all(monitor_ip5.at_turn[3, :] == np.arange(5, 15))
+        assert np.all(monitor_ip5.particle_id[:, 3] == np.arange(0, num_particles))
+        assert np.all(monitor_ip5.at_element[:, :]
+                            == line_with_monitor.element_names.index('ip5') - 1)
+
+        assert np.all(monitor_ip8.x.shape == np.array([50, 10]))
+        assert np.all(monitor_ip8.at_turn[3, :] == np.arange(5, 15))
+        assert np.all(monitor_ip8.particle_id[:, 3] == np.arange(0, num_particles))
+        assert np.all(monitor_ip8.at_element[:, :]
+                            == line_with_monitor.element_names.index('ip8') - 1)
