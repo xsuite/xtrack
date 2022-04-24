2d5774472076040dcc48cb3ea97e0a2a152a896d
giadarol <giovanni.iadarola@cern.ch>
2022-04-24 16:00:51 +0200

    Replace eval with getattr

--- xtrack/loader_mad.py
+++ xtrack/loader_mad.py
@@ -359,13 +359,13 @@ def madx_sequence_to_xtrack_line(
             for m0_i in range(6):
                 att_name = f'kick{m0_i+1}'
                 if hasattr(ee,att_name):
-                    m0[m0_i] = eval(f'ee.{att_name}')
+                    m0[m0_i] = getattr(ee, att_name)
             m1 = np.zeros((6,6),dtype=float)
             for m1_i in range(6):
                 for m1_j in range(6):
                     att_name = f'rm{m1_i+1}{m1_j+1}'
                     if hasattr(ee,att_name):
-                        m1[m1_i,m1_j] = eval(f'ee.{att_name}')
+                        m1[m1_i,m1_j] = getattr(ee, att_name)
 
             newele = classes.FirstOrderTaylorMap(
                 length = length,
@@ -379,13 +379,14 @@ def madx_sequence_to_xtrack_line(
                     _lref[eemame].length = madeval(eepar.L.expr)
                 for m0_i in range(6):
                     att_name = f'kick{m0_i+1}'
-                    if hasattr(ee,att_name) and eval(f'eepar.{att_name}.expr') is not None:
-                        _lref[eename].m0[m0_i] = madeval(eval(f'eepar.{att_name}.expr'))
+                    if hasattr(ee,att_name) and getattr(eepar, att_name).expr is not None:
+                        _lref[eename].m0[m0_i] = madeval(getattr(eepar, att_name).expr)
                 for m1_i in range(6):
                     for m1_j in range(6):
                         att_name = f'rm{m1_i+1}{m1_j+1}'
-                        if hasattr(ee,att_name) and eval(f'eepar.{att_name}.expr') is not None:
-                            _lref[eename].m1[6*m1_i+m1_j]= madeval(eval(f'eepar.{att_name}.expr')) # The array is flattened at instanciation of the element
+                        if hasattr(ee,att_name) and getattr(eepar, att_name).expr is not None:
+                            # The array is flattened at instantiation of the element
+                            _lref[eename].m1[6*m1_i+m1_j]= madeval(getattr(eepar, att_name).expr)
 
 
         else:
