25901634eb74415d6a999d16098561bf5f2d76ae
giadarol <giovanni.iadarola@cern.ch>
2022-04-27 09:05:15 +0200

    Passing all checks in new example

--- examples/tracker/000_tracker_examples.py
+++ examples/tracker/000_tracker_examples.py
@@ -18,11 +18,28 @@ assert tracker.record_last_track.x.shape == (3, 4)
 
 
 # ele_start > 0
-p = xp.Particles(x=[1e-3, 2e-3, 3e-3], p0c=7e12, s=2, at_element=2)
+p = xp.Particles(x=[1e-3, 2e-3, 3e-3], p0c=7e12, s=4., at_element=2)
 tracker.track(p, num_turns=3, ele_start=2, turn_by_turn_monitor=True)
 assert np.all(p.s == 3 * 10 * 2.)
-assert np.all(p.at_turn == 2)
-assert tracker.record_last_track.x.shape == (3,2)
+assert np.all(p.at_turn == 3)
+assert tracker.record_last_track.x.shape == (3,3)
+
+### Behavior:
+
+# For num_turns = 1 (default):
+#  - tracker.track(particles, ele_start=3, ele_stop=5) tracks once from element 3 to
+#    element 5 e. particles.at_turn is not incremented as the particles never pass through
+#    the end of the beam line.
+#  - tracker.track(particles, ele_start=7, ele_stop=3) tracks once from element 5 to
+#    the end of the beamline and then from element 0 to element 3 excluded.
+#    particles.at_turn is incremented to one as the particles pass once
+#    the end of the beam line.
+
+# When indicating num_turns = N with N > 1, additional (N-1) full turns are added to logic
+# above. Therefore:
+# - if ele_stop < ele_start: stops at element ele_stop when particles.at_turn = N - 1
+# - if ele_stop >= ele_start: stops at element ele_stop when particles.at_turn = N
+
 
 
 # 0 <= ele_start < ele_stop
@@ -33,10 +50,18 @@ assert np.all(p.s == 3 * 10 * 2. + 8 * 2)
 assert np.all(p.at_turn == 3)
 assert tracker.record_last_track.x.shape == (3, 4)
 
-# 0 <= ele_start < ele_stop
+# 0 <= ele_start < ele_stop -- num_turns = 1
 p = xp.Particles(x=[1e-3, 2e-3, 3e-3], p0c=7e12, s=5 * 2., at_element=5)
-tracker.track(p, num_turns=4, ele_start=5, ele_stop=3,
+tracker.track(p, num_turns=1, ele_start=5, ele_stop=2,
               turn_by_turn_monitor=True)
-assert np.all(p.s == 4 * 10 * 2. + 3 * 2)
+assert np.all(p.s == 10* 2. + 2*2.)
+assert np.all(p.at_turn == 1)
+assert tracker.record_last_track.x.shape == (3, 2)
+
+# 0 <= ele_start < ele_stop -- num_turns > 1
+p = xp.Particles(x=[1e-3, 2e-3, 3e-3], p0c=7e12, s=5 * 2., at_element=5)
+tracker.track(p, num_turns=4, ele_start=5, ele_stop=2,
+              turn_by_turn_monitor=True)
+assert np.all(p.s == 4 * 10 * 2. + 2 * 2.)
 assert np.all(p.at_turn == 4)
 assert tracker.record_last_track.x.shape == (3, 5)
