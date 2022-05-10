1ca676cd14e9b6c1aa02581cf08e7f958c335887
giadarol <giovanni.iadarola@cern.ch>
2022-05-10 07:36:33 +0200

    a white space

--- xtrack/beam_elements/elements_src/referenceenergyincrease.h
+++ xtrack/beam_elements/elements_src/referenceenergyincrease.h
@@ -8,7 +8,7 @@ void ReferenceEnergyIncrease_track_local_particle(ReferenceEnergyIncreaseData el
     double const Delta_p0c = ReferenceEnergyIncreaseData_get_Delta_p0c(el);
 
     //start_per_particle_block (part0->part)
-	LocalParticle_update_p0c(part, 
+	LocalParticle_update_p0c(part,
 		LocalParticle_get_p0c(part) + Delta_p0c);
     //end_per_particle_block
 
