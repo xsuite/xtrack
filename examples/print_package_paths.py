8f8187318d4477f5f7f3a52f094e4a37e37df2a3
giadarol <giovanni.iadarola@cern.ch>
2022-06-09 08:16:50 +0200

    Cosmetics

--- examples/print_package_paths.py
+++ examples/print_package_paths.py
@@ -6,11 +6,11 @@ import xfields
 
 print("\n-------- Paths: --------")
 
-print('xobjects:\t', xobjects.__path__)
-print('xpart:\t', xpart.__path__)
-print('xdeps:\t', xdeps.__path__)
-print('xtrack\t: ', xtrack.__path__)
-print('xfields\t: ', xfields.__path__)
+print('xobjects: ', xobjects.__path__)
+print('xpart:    ', xpart.__path__)
+print('xdeps:    ', xdeps.__path__)
+print('xtrack:   ', xtrack.__path__)
+print('xfields:  ', xfields.__path__)
 
 
 try:
@@ -25,10 +25,10 @@ try:
     repo_xfields = Repo(Path(xfields.__path__[0]).parent)
 
     print('Xobjects is on branch: ', repo_xobjects.active_branch)
-    print('Xpart is on branch: ', repo_xpart.active_branch)
-    print('Xdeps is on branch: ', repo_xdeps.active_branch)
-    print('Xtrack is on branch: ', repo_xtrack.active_branch)
-    print('Xfields is on branch: ', repo_xfields.active_branch)
+    print('Xpart is on branch:    ', repo_xpart.active_branch)
+    print('Xdeps is on branch:    ', repo_xdeps.active_branch)
+    print('Xtrack is on branch:   ', repo_xtrack.active_branch)
+    print('Xfields is on branch:  ', repo_xfields.active_branch)
 
 except Exception as err:
     print('No git info because of the following exception: ', err)
