be91c780cfbc9f71fe14e3cd0bbe734d0d7849a4
Xavier Buffat <xavier.buffat@cern.ch>
2022-07-15 14:55:18 +0200

    Add internal tag

--- xtrack/pipeline.py
+++ xtrack/pipeline.py
@@ -38,15 +38,14 @@ class PipelineManager:
     def add_element(self,element_name):
         self._elements[element_name] = len(self._elements)
 
-    def get_message_tag(self,element_name,sender_name,reciever_name):
-        tag = self._elements[element_name] + len(self._elements)*self._IDs[sender_name].number+len(self._elements)*len(self._IDs)*self._IDs[reciever_name].number
-        #tag = self.pipeline_number+self.pipeline_max_size*sender.number+self.pipeline_max_size*self.pipeline_max_particles_per_rank*reciever.number
+    def get_message_tag(self,element_name,sender_name,reciever_name,internal_tag=0):
+        tag = self._elements[element_name] + len(self._elements)*self._IDs[sender_name].number + len(self._elements)*len(self._IDs)*self._IDs[reciever_name].number + len(self._elements)*len(self._IDs)*len(self._IDs)*internal_tag
         #if tag > self._max_tag:
         #    print(f'PyPLINEDElement WARNING {self.name}: MPI message tag {tag} is larger than max ({self._max_tag})')
         return tag
 
-    def is_ready_to_send(self,element_name,sender_name,reciever_name,turn):
-        tag = self.get_message_tag(element_name=element_name,sender_name=sender_name,reciever_name=reciever_name)
+    def is_ready_to_send(self,element_name,sender_name,reciever_name,turn,internal_tag=0):
+        tag = self.get_message_tag(element_name=element_name,sender_name=sender_name,reciever_name=reciever_name,internal_tag=internal_tag)
         if tag not in self._last_request_turn.keys():
             return True
         if turn <= self._last_request_turn[tag]:
@@ -56,15 +55,15 @@ class PipelineManager:
         return True
 
     def send_message(self,send_buffer,element_name,sender_name,reciever_name,turn):
-        tag = self.get_message_tag(element_name=element_name,sender_name=sender_name,reciever_name=reciever_name)
+        tag = self.get_message_tag(element_name=element_name,sender_name=sender_name,reciever_name=reciever_name,internal_tag=0)
         self._last_request_turn[tag] = turn
         self._pending_requests[tag] = self._communicator.Issend(send_buffer,dest=self.get_particles_rank(reciever_name),tag=tag)
 
-    def is_ready_to_recieve(self,element_name,sender_name,reciever_name):
+    def is_ready_to_recieve(self,element_name,sender_name,reciever_name,internal_tag=0):
         tag = self.get_message_tag(element_name=element_name,sender_name=sender_name,reciever_name=reciever_name)
         return self._communicator.Iprobe(source=self.get_particles_rank(sender_name), tag=tag)
 
-    def recieve_message(self,recieve_buffer,element_name,sender_name,reciever_name):
+    def recieve_message(self,recieve_buffer,element_name,sender_name,reciever_name,internal_tag=0):
         tag = self.get_message_tag(element_name=element_name,sender_name=sender_name,reciever_name=reciever_name)
         self._communicator.Recv(recieve_buffer,source=self.get_particles_rank(sender_name),tag=tag)
 
