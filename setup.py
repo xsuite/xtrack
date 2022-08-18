4d9bc39532107c7dbf11eaec7da8c94dab11a63b
Giovanni Iadarola <giovanni.iadarola@cern.ch>
2022-08-18 21:37:03 +0200

    Change version number

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ extensions = []
 
 setup(
     name='xtrack',
-    version='0.16.1',
+    version='0.16.2',
     description='Tracking library for particle accelerators',
     long_description='Tracking library for particle accelerators',
     url='https://xsuite.readthedocs.io/',
