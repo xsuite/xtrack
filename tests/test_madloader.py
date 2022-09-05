6dda70b26697381248e697a2a635405e0ba6b7e0
giadarol <giovanni.iadarola@cern.ch>
2022-09-05 23:17:23 +0200

    Fixes

--- tests/test_madloader.py
+++ tests/test_madloader.py
@@ -8,24 +8,24 @@ from cpymad.madx import Madx
 
 def test_non_zero_index():
     lst=[1,2,3,0,0,0]
-    assert xtrack.mad_loader2.non_zero_len([1,2,3,0,0,0])==3
+    assert xtrack.mad_loader.non_zero_len([1,2,3,0,0,0])==3
 
 def test_add_lists():
     a=[1,2,3,1,1,1]
     b=[1,1,1,4,5,6]
-    c=xtrack.mad_loader2.add_lists(a,b,8)
+    c=xtrack.mad_loader.add_lists(a,b,8)
     assert c==[2, 3, 4, 5, 6, 7, 0, 0]
 
 
 def test_add_lists():
     a=[1,2,3,1,1,1]
     b=[1,1,1,4,5,6,7,8]
-    c=xtrack.mad_loader2.add_lists(a,b,8)
+    c=xtrack.mad_loader.add_lists(a,b,8)
     assert c==[2, 3, 4, 5, 6, 7, 7, 8]
 
     a=[1,2,3,1,1,1]
     b=[1,1,1,4,5,6,7,8]
-    c=xtrack.mad_loader2.add_lists(a,b,10)
+    c=xtrack.mad_loader.add_lists(a,b,10)
     assert c==[2, 3, 4, 5, 6, 7, 7, 8,0,0]
 
 
