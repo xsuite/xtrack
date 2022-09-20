dd5c0fe561a3a33211064f26a168d236fe517fff
giadarol <giovanni.iadarola@cern.ch>
2022-09-20 15:32:40 +0200

    Test works on CPU

--- tests/test_dynamic_changes.py
+++ tests/test_dynamic_changes.py
@@ -3,28 +3,30 @@ import numpy as np
 import xobjects as xo
 import xtrack as xt
 
-context = xo.ContextCpu()
+def test_custom_setter_array_element():
 
-line = xt.Line(elements=dict(
-            qf1=xt.Multipole(knl=[0, 0.1]),
-            qf2=xt.Multipole(knl=[0, 0.2]),
-            qf3=xt.Multipole(knl=[0, 0.3]),
-            dr=xt.Drift(length=1.)),
-        element_names=['qf1', 'qf2', 'qf3', 'dr'])
+    for context in xo.context.get_test_contexts():
 
-tracker = line.build_tracker(_context=context)
+        line = xt.Line(elements=dict(
+                    qf1=xt.Multipole(knl=[0, 0.1]),
+                    qf2=xt.Multipole(knl=[0, 0.2]),
+                    qf3=xt.Multipole(knl=[0, 0.3]),
+                    dr=xt.Drift(length=1.)),
+                element_names=['qf1', 'qf2', 'qf3', 'dr'])
 
-elements_to_trim = [nn for nn in line.element_names if nn.startswith('qf')]
+        tracker = line.build_tracker(_context=context)
 
-qf_setter = xt.CustomSetter(tracker, elements_to_trim,
-                            field='knl', index=1 # we want to change knl[1]
-                            )
+        elements_to_trim = [nn for nn in line.element_names if nn.startswith('qf')]
 
-values = qf_setter.get_values()
-assert np.all(values == np.array([0.1, 0.2, 0.3]))
+        qf_setter = xt.CustomSetter(tracker, elements_to_trim,
+                                    field='knl', index=1 # we want to change knl[1]
+                                    )
 
-qf_setter.set_values(np.array([10., 100., 1000.]))
-assert np.all(qf_setter.get_values() == np.array([10., 100., 1000.]))
-assert line['qf1'].knl[1] == 10.
-assert line['qf2'].knl[1] == 100.
-assert line['qf3'].knl[1] == 1000.
+        values = qf_setter.get_values()
+        assert np.all(values == np.array([0.1, 0.2, 0.3]))
+
+        qf_setter.set_values(np.array([10., 100., 1000.]))
+        assert np.all(qf_setter.get_values() == np.array([10., 100., 1000.]))
+        assert line['qf1'].knl[1] == 10.
+        assert line['qf2'].knl[1] == 100.
+        assert line['qf3'].knl[1] == 1000.
