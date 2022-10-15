98e34d4c46520ee568f04e38b4c3f100ce288b5e
giadarol <giovanni.iadarola@cern.ch>
2022-10-15 14:15:50 +0200

    cosmetics

--- examples/print_package_paths.py
+++ examples/print_package_paths.py
@@ -29,11 +29,11 @@ try:
     repo_xtrack = Repo(Path(xtrack.__path__[0]).parent)
     repo_xfields = Repo(Path(xfields.__path__[0]).parent)
 
-    print('Xobjects is on branch: ', repo_xobjects.active_branch)
-    print('Xpart is on branch:    ', repo_xpart.active_branch)
-    print('Xdeps is on branch:    ', repo_xdeps.active_branch)
-    print('Xtrack is on branch:   ', repo_xtrack.active_branch)
-    print('Xfields is on branch:  ', repo_xfields.active_branch)
+    print('xobjects is on branch: ', repo_xobjects.active_branch)
+    print('xpart is on branch:    ', repo_xpart.active_branch)
+    print('xdeps is on branch:    ', repo_xdeps.active_branch)
+    print('xtrack is on branch:   ', repo_xtrack.active_branch)
+    print('xfields is on branch:  ', repo_xfields.active_branch)
 
 except Exception as err:
     print('No git info because of the following exception: ', err)
