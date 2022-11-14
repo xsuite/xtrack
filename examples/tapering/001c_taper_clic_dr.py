faa0c7084dd5066d35849ebec83a4425f0b572ba
giadarol <giovanni.iadarola@cern.ch>
2022-11-14 21:32:03 +0100

    Loading from json

--- examples/tapering/001c_taper_clic_dr.py
+++ examples/tapering/001c_taper_clic_dr.py
@@ -1,51 +1,10 @@
-from cpymad.madx import Madx
-import xtrack as xt
-import xpart as xp
-
+import json
 import numpy as np
-
-# Import a thick sequence
-mad = Madx()
-mad.call('../../test_data/clic_dr/sequence.madx')
-mad.use('ring')
-
-# Makethin
-mad.input(f'''
-select, flag=MAKETHIN, SLICE=4, thick=false;
-select, flag=MAKETHIN, pattern=wig, slice=1;
-MAKETHIN, SEQUENCE=ring, MAKEDIPEDGE=true;
-use, sequence=RING;
-''')
-mad.use('ring')
-
-# Build xtrack line
-print('Build xtrack line...')
-line = xt.Line.from_madx_sequence(mad.sequence['RING'])
-line.particle_ref = xp.Particles(
-        mass0=xp.ELECTRON_MASS_EV,
-        q0=-1,
-        gamma0=15000 # I push up the energy loss
-        )
-
-#line0 = line.copy()
-
-line['rf'].voltage *= 20 # I push up the voltage
-
-line = line.cycle('qdw1..1:38')
-
-c0 = line['rf']
-v0 = c0.voltage
-#c0.frequency /= 100
-s0 = line.get_s_position('rf')
+import xtrack as xt
 
 
-line.insert_element(at_s=41., element=c0.copy(), name='rf1')
-line.insert_element(at_s=line.get_length()-s0, element=c0.copy(), name='rf2a')
-line.insert_element(at_s=line.get_length()-s0, element=c0.copy(), name='rf2b')
-line.insert_element(at_s=line.get_length()-41, element=c0.copy(), name='rf3')
-
-line['rf2a'].voltage *= 0.6 # I split the voltage unevenly to test the partitioning
-line['rf2b'].voltage *= 0.4
+with open('../../test_data/clic_dr/line_for_taper.json', 'r') as f:
+    line = xt.Line.from_dict(json.load(f))
 
 tracker = line.build_tracker()
 
