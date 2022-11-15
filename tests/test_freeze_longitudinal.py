ee299ef0f19aac54fbd2258d9a581ceb6716c564
giadarol <giovanni.iadarola@cern.ch>
2022-11-15 21:44:27 +0100

    Fix in test

--- tests/test_freeze_longitudinal.py
+++ tests/test_freeze_longitudinal.py
@@ -17,18 +17,61 @@ def test_freeze_longitudinal_explicit():
     # import a line and add reference particle
     with open(fname_line) as fid:
         line_dict = json.load(fid)
-    line = xt.Line.from_dict(line_dict['line'])
-    line.particle_ref = xp.Particles.from_dict(line_dict['particle'])
 
     for context in xo.context.get_test_contexts():
-            print(f"Test {context.__class__}")
 
-            # Build the tracker
-            tracker = line.build_tracker(_context=context)
+        print(f"Test {context.__class__}")
 
-            # Freeze longitudinal coordinates
-            tracker.freeze_longitudinal()
+        line = xt.Line.from_dict(line_dict['line'])
+        line.particle_ref = xp.Particles.from_dict(line_dict['particle'])
+        # Build the tracker
+        tracker = line.build_tracker(_context=context)
 
+        # Freeze longitudinal coordinates
+        tracker.freeze_longitudinal()
+
+        # Track some particles with frozen longitudinal coordinates
+        particles = tracker.build_particles(delta=1e-3, x=[-1e-3, 0, 1e-3])
+        tracker.track(particles, num_turns=10)
+        particles.move(_context=xo.context_default)
+        assert np.allclose(particles.delta, 1e-3, rtol=0, atol=1e-12)
+        assert np.allclose(particles.zeta, 0, rtol=9, atol=1e-12)
+
+        # Twiss with frozen longitudinal coordinates (needs to be 4d)
+        twiss = tracker.twiss(mode='4d')
+        assert twiss.slip_factor == 0
+
+        # Unfreeze longitudinal coordinates
+        tracker.freeze_longitudinal(False)
+
+        # Track some particles with unfrozen longitudinal coordinates
+        particles = tracker.build_particles(delta=1e-3, x=[-1e-3, 0, 1e-3])
+        tracker.track(particles, num_turns=10)
+        particles.move(_context=xo.context_default)
+        assert np.allclose(particles.delta, 0.00099218, rtol=0, atol=1e-6)
+
+        # Twiss with unfrozen longitudinal coordinates (can be 6d)
+        twiss = tracker.twiss(mode='6d')
+        assert np.isclose(twiss.slip_factor, 0.00032151, rtol=0, atol=1e-6)
+
+def test_freeze_longitudinal_context_manager():
+
+    fname_line = test_data_folder / 'lhc_no_bb/line_and_particle.json'
+
+    # import a line and add reference particle
+    with open(fname_line) as fid:
+        line_dict = json.load(fid)
+
+    for context in xo.context.get_test_contexts():
+        print(f"Test {context.__class__}")
+
+        line = xt.Line.from_dict(line_dict['line'])
+        line.particle_ref = xp.Particles.from_dict(line_dict['particle'])
+
+        # Build the tracker
+        tracker = line.build_tracker(_context=context)
+
+        with xt.freeze_longitudinal(tracker):
             # Track some particles with frozen longitudinal coordinates
             particles = tracker.build_particles(delta=1e-3, x=[-1e-3, 0, 1e-3])
             tracker.track(particles, num_turns=10)
@@ -40,56 +83,15 @@ def test_freeze_longitudinal_explicit():
             twiss = tracker.twiss(mode='4d')
             assert twiss.slip_factor == 0
 
-            # Unfreeze longitudinal coordinates
-            tracker.freeze_longitudinal(False)
+        # Track some particles with unfrozen longitudinal coordinates
+        particles = tracker.build_particles(delta=1e-3, x=[-1e-3, 0, 1e-3])
+        tracker.track(particles, num_turns=10)
+        particles.move(_context=xo.context_default)
+        assert np.allclose(particles.delta, 0.00099218, rtol=0, atol=1e-6)
 
-            # Track some particles with unfrozen longitudinal coordinates
-            particles = tracker.build_particles(delta=1e-3, x=[-1e-3, 0, 1e-3])
-            tracker.track(particles, num_turns=10)
-            particles.move(_context=xo.context_default)
-            assert np.allclose(particles.delta, 0.00099218, rtol=0, atol=1e-6)
-
-            # Twiss with unfrozen longitudinal coordinates (can be 6d)
-            twiss = tracker.twiss(mode='6d')
-            assert np.isclose(twiss.slip_factor, 0.00032151, rtol=0, atol=1e-6)
-
-def test_freeze_longitudinal_context_manager():
-
-    fname_line = test_data_folder / 'lhc_no_bb/line_and_particle.json'
-
-    # import a line and add reference particle
-    with open(fname_line) as fid:
-        line_dict = json.load(fid)
-    line = xt.Line.from_dict(line_dict['line'])
-    line.particle_ref = xp.Particles.from_dict(line_dict['particle'])
-
-    for context in xo.context.get_test_contexts():
-            print(f"Test {context.__class__}")
-
-            # Build the tracker
-            tracker = line.build_tracker(_context=context)
-
-            with xt.freeze_longitudinal(tracker):
-                # Track some particles with frozen longitudinal coordinates
-                particles = tracker.build_particles(delta=1e-3, x=[-1e-3, 0, 1e-3])
-                tracker.track(particles, num_turns=10)
-                particles.move(_context=xo.context_default)
-                assert np.allclose(particles.delta, 1e-3, rtol=0, atol=1e-12)
-                assert np.allclose(particles.zeta, 0, rtol=9, atol=1e-12)
-
-                # Twiss with frozen longitudinal coordinates (needs to be 4d)
-                twiss = tracker.twiss(mode='4d')
-                assert twiss.slip_factor == 0
-
-            # Track some particles with unfrozen longitudinal coordinates
-            particles = tracker.build_particles(delta=1e-3, x=[-1e-3, 0, 1e-3])
-            tracker.track(particles, num_turns=10)
-            particles.move(_context=xo.context_default)
-            assert np.allclose(particles.delta, 0.00099218, rtol=0, atol=1e-6)
-
-            # Twiss with unfrozen longitudinal coordinates (can be 6d)
-            twiss = tracker.twiss(mode='6d')
-            assert np.isclose(twiss.slip_factor, 0.00032151, rtol=0, atol=1e-6)
+        # Twiss with unfrozen longitudinal coordinates (can be 6d)
+        twiss = tracker.twiss(mode='6d')
+        assert np.isclose(twiss.slip_factor, 0.00032151, rtol=0, atol=1e-6)
 
 def test_freeze_longitudinal_individual_methods():
 
@@ -98,32 +100,33 @@ def test_freeze_longitudinal_individual_methods():
     # import a line and add reference particle
     with open(fname_line) as fid:
         line_dict = json.load(fid)
-    line = xt.Line.from_dict(line_dict['line'])
-    line.particle_ref = xp.Particles.from_dict(line_dict['particle'])
 
     for context in xo.context.get_test_contexts():
-            print(f"Test {context.__class__}")
+        print(f"Test {context.__class__}")
 
-            # Build the tracker
-            tracker = line.build_tracker(_context=context)
+        line = xt.Line.from_dict(line_dict['line'])
+        line.particle_ref = xp.Particles.from_dict(line_dict['particle'])
 
-            # Track some particles with frozen longitudinal coordinates
-            particles = tracker.build_particles(delta=1e-3, x=[-1e-3, 0, 1e-3])
-            tracker.track(particles, num_turns=10, freeze_longitudinal=True)
-            particles.move(_context=xo.context_default)
-            assert np.allclose(particles.delta, 1e-3, rtol=0, atol=1e-12)
-            assert np.allclose(particles.zeta, 0, rtol=9, atol=1e-12)
+        # Build the tracker
+        tracker = line.build_tracker(_context=context)
 
-            # Twiss with frozen longitudinal coordinates (needs to be 4d)
-            twiss = tracker.twiss(mode='4d', freeze_longitudinal=True)
-            assert twiss.slip_factor == 0
+        # Track some particles with frozen longitudinal coordinates
+        particles = tracker.build_particles(delta=1e-3, x=[-1e-3, 0, 1e-3])
+        tracker.track(particles, num_turns=10, freeze_longitudinal=True)
+        particles.move(_context=xo.context_default)
+        assert np.allclose(particles.delta, 1e-3, rtol=0, atol=1e-12)
+        assert np.allclose(particles.zeta, 0, rtol=9, atol=1e-12)
 
-            # Track some particles with unfrozen longitudinal coordinates
-            particles = tracker.build_particles(delta=1e-3, x=[-1e-3, 0, 1e-3])
-            tracker.track(particles, num_turns=10)
-            particles.move(_context=xo.context_default)
-            assert np.allclose(particles.delta, 0.00099218, rtol=0, atol=1e-6)
+        # Twiss with frozen longitudinal coordinates (needs to be 4d)
+        twiss = tracker.twiss(mode='4d', freeze_longitudinal=True)
+        assert twiss.slip_factor == 0
 
-            # Twiss with unfrozen longitudinal coordinates (can be 6d)
-            twiss = tracker.twiss(mode='6d')
-            assert np.isclose(twiss.slip_factor, 0.00032151, rtol=0, atol=1e-6)
+        # Track some particles with unfrozen longitudinal coordinates
+        particles = tracker.build_particles(delta=1e-3, x=[-1e-3, 0, 1e-3])
+        tracker.track(particles, num_turns=10)
+        particles.move(_context=xo.context_default)
+        assert np.allclose(particles.delta, 0.00099218, rtol=0, atol=1e-6)
+
+        # Twiss with unfrozen longitudinal coordinates (can be 6d)
+        twiss = tracker.twiss(mode='6d')
+        assert np.isclose(twiss.slip_factor, 0.00032151, rtol=0, atol=1e-6)
