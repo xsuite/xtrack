8870fbf1f2bbd385e711dffd04ef00e484224b46
giadarol <giovanni.iadarola@cern.ch>
2022-11-17 17:12:07 +0100

    All asserts in example are passing

--- examples/tapering/000_taper_clic_dr.py
+++ examples/tapering/000_taper_clic_dr.py
@@ -20,7 +20,7 @@ tracker.configure_radiation(model='mean')
 tracker.compensate_radiation_energy_loss()
 
 # Twiss(es) with radiation
-tw_real_tracking = tracker.twiss(method='6d', matrix_stability_tol=3.,
+tw = tracker.twiss(method='6d', matrix_stability_tol=3.,
                     eneloss_and_damping=True)
 tw_sympl = tracker.twiss(radiation_method='kick_as_co', method='6d')
 tw_scale_as_co = tracker.twiss(
@@ -28,11 +28,13 @@ tw_scale_as_co = tracker.twiss(
                         method='6d',
                         matrix_stability_tol=0.5)
 
+p0corr = 1 + tracker.delta_taper
+
 import matplotlib.pyplot as plt
 plt.close('all')
 
 print('Non sympltectic tracker:')
-print(f'Tune error =  error_qx: {abs(tw_real_tracking.qx - tw_no_rad.qx):.3e} error_qy: {abs(tw_real_tracking.qy - tw_no_rad.qy):.3e}')
+print(f'Tune error =  error_qx: {abs(tw.qx - tw_no_rad.qx):.3e} error_qy: {abs(tw.qy - tw_no_rad.qy):.3e}')
 print('Sympltectic tracker:')
 print(f'Tune error =  error_qx: {abs(tw_sympl.qx - tw_no_rad.qx):.3e} error_qy: {abs(tw_sympl.qy - tw_no_rad.qy):.3e}')
 print ('Preserve angles:')
@@ -40,17 +42,15 @@ print(f'Tune error =  error_qx: {abs(tw_scale_as_co.qx - tw_no_rad.qx):.3e} erro
 plt.figure(2)
 
 plt.subplot(2,1,1)
-plt.plot(tw_no_rad.s, tw_sympl.betx/tw_no_rad.betx - 1)
-plt.plot(tw_no_rad.s, tw_scale_as_co.betx/tw_no_rad.betx - 1)
-#tw.betx *= (1 + delta_beta_corr)
-#plt.plot(tw_no_rad.s, tw.betx/tw_no_rad.betx - 1)
+plt.plot(tw.s, tw.betx*p0corr/tw_no_rad.betx-1)
+plt.plot(tw_no_rad.s, tw_sympl.betx*p0corr/tw_no_rad.betx - 1)
+plt.plot(tw_no_rad.s, tw_scale_as_co.betx*p0corr/tw_no_rad.betx - 1)
 plt.ylabel(r'$\Delta \beta_x / \beta_x$')
 
 plt.subplot(2,1,2)
-plt.plot(tw_no_rad.s, tw_sympl.bety/tw_no_rad.bety - 1)
-plt.plot(tw_no_rad.s, tw_scale_as_co.bety/tw_no_rad.bety - 1)
-#tw.bety *= (1 + delta_beta_corr)
-#plt.plot(tw_no_rad.s, tw.bety/tw_no_rad.bety - 1)
+plt.plot(tw.s, tw.bety*p0corr/tw_no_rad.bety-1)
+plt.plot(tw_no_rad.s, tw_sympl.bety*p0corr/tw_no_rad.bety - 1)
+plt.plot(tw_no_rad.s, tw_scale_as_co.bety*p0corr/tw_no_rad.bety - 1)
 plt.ylabel(r'$\Delta \beta_y / \beta_y$')
 
 plt.figure(10)
@@ -69,54 +69,58 @@ plt.plot(tw_no_rad.s, tw_scale_as_co.y, 'g')
 plt.figure(3)
 plt.subplot()
 plt.plot(tw_no_rad.s, tracker.delta_taper)
-plt.plot(tw_real_tracking.s, tw_real_tracking.delta)
+plt.plot(tw.s, tw.delta)
 
 assert np.isclose(tracker.delta_taper[0], 0, rtol=0, atol=1e-10)
 assert np.isclose(tracker.delta_taper[-1], 0, rtol=0, atol=1e-10)
 assert np.isclose(np.max(tracker.delta_taper), 0.00568948, rtol=1e-4, atol=0)
 assert np.isclose(np.min(tracker.delta_taper), -0.00556288, rtol=1e-4, atol=0)
 
-assert np.allclose(tw_real_tracking.delta, tracker.delta_taper, rtol=0, atol=1e-6)
+assert np.allclose(tw.delta, tracker.delta_taper, rtol=0, atol=1e-6)
 assert np.allclose(tw_sympl.delta, tracker.delta_taper, rtol=0, atol=1e-6)
 assert np.allclose(tw_scale_as_co.delta, tracker.delta_taper, rtol=0, atol=1e-6)
 
-assert np.isclose(tw_real_tracking.qx, tw_no_rad.qx, rtol=0, atol=5e-4)
+assert np.isclose(tw.qx, tw_no_rad.qx, rtol=0, atol=5e-4)
 assert np.isclose(tw_sympl.qx, tw_no_rad.qx, rtol=0, atol=5e-4)
 assert np.isclose(tw_scale_as_co.qx, tw_no_rad.qx, rtol=0, atol=5e-4)
 
-assert np.isclose(tw_real_tracking.qy, tw_no_rad.qy, rtol=0, atol=5e-4)
+assert np.isclose(tw.qy, tw_no_rad.qy, rtol=0, atol=5e-4)
 assert np.isclose(tw_sympl.qy, tw_no_rad.qy, rtol=0, atol=5e-4)
 assert np.isclose(tw_scale_as_co.qy, tw_no_rad.qy, rtol=0, atol=5e-4)
 
-assert np.isclose(tw_real_tracking.dqx, tw_no_rad.dqx, rtol=0, atol=0.2)
+assert np.isclose(tw.dqx, tw_no_rad.dqx, rtol=0, atol=0.2)
 assert np.isclose(tw_sympl.dqx, tw_no_rad.dqx, rtol=0, atol=0.2)
 assert np.isclose(tw_scale_as_co.dqx, tw_no_rad.dqx, rtol=0, atol=0.2)
 
-assert np.isclose(tw_real_tracking.dqy, tw_no_rad.dqy, rtol=0, atol=0.2)
+assert np.isclose(tw.dqy, tw_no_rad.dqy, rtol=0, atol=0.2)
 assert np.isclose(tw_sympl.dqy, tw_no_rad.dqy, rtol=0, atol=0.2)
 assert np.isclose(tw_scale_as_co.dqy, tw_no_rad.dqy, rtol=0, atol=0.2)
 
-assert np.allclose(tw_real_tracking.x, tw_no_rad.x, rtol=0, atol=1e-7)
+assert np.allclose(tw.x, tw_no_rad.x, rtol=0, atol=1e-7)
 assert np.allclose(tw_sympl.x, tw_no_rad.x, rtol=0, atol=1e-7)
 assert np.allclose(tw_scale_as_co.x, tw_no_rad.x, rtol=0, atol=1e-7)
 
-assert np.allclose(tw_real_tracking.y, tw_no_rad.y, rtol=0, atol=1e-7)
+assert np.allclose(tw.y, tw_no_rad.y, rtol=0, atol=1e-7)
 assert np.allclose(tw_sympl.y, tw_no_rad.y, rtol=0, atol=1e-7)
 assert np.allclose(tw_scale_as_co.y, tw_no_rad.y, rtol=0, atol=1e-7)
 
-assert np.allclose(tw_sympl.betx, tw_no_rad.betx, rtol=0.02, atol=0)
-assert np.allclose(tw_scale_as_co.betx, tw_no_rad.betx, rtol=0.003, atol=0)
+assert np.allclose(tw.betx*p0corr, tw_no_rad.betx, rtol=0.02, atol=0)
+assert np.allclose(tw_sympl.betx*p0corr, tw_no_rad.betx, rtol=0.02, atol=0)
+assert np.allclose(tw_scale_as_co.betx, tw_no_rad.betx, rtol=0.02, atol=0)
 
-assert np.allclose(tw_sympl.bety, tw_no_rad.bety, rtol=0.04, atol=0)
-assert np.allclose(tw_scale_as_co.bety, tw_no_rad.bety, rtol=0.003, atol=0)
+assert np.allclose(tw.bety*p0corr, tw_no_rad.bety, rtol=0.02, atol=0)
+assert np.allclose(tw_sympl.bety*p0corr, tw_no_rad.bety, rtol=0.02, atol=0)
+assert np.allclose(tw_scale_as_co.bety, tw_no_rad.bety, rtol=0.02, atol=0)
 
+assert np.allclose(tw.dx, tw.dx, rtol=0.00, atol=0.1e-3)
 assert np.allclose(tw_sympl.dx, tw_no_rad.dx, rtol=0.00, atol=0.1e-3)
 assert np.allclose(tw_scale_as_co.dx, tw_no_rad.dx, rtol=0.00, atol=0.1e-3)
 
+assert np.allclose(tw.dy, tw.dy, rtol=0.00, atol=0.1e-3)
 assert np.allclose(tw_sympl.dy, tw_no_rad.dy, rtol=0.00, atol=0.1e-3)
 assert np.allclose(tw_scale_as_co.dy, tw_no_rad.dy, rtol=0.00, atol=0.1e-3)
 
-eneloss_real_tracking = tw_real_tracking.eneloss_turn
+eneloss_real_tracking = tw.eneloss_turn
 assert np.isclose(line['rf'].voltage*np.sin(line['rf'].lag/180*np.pi), eneloss_real_tracking/4, rtol=1e-5)
 assert np.isclose(line['rf1'].voltage*np.sin(line['rf1'].lag/180*np.pi), eneloss_real_tracking/4, rtol=1e-5)
 assert np.isclose(line['rf2a'].voltage*np.sin(line['rf2a'].lag/180*np.pi), eneloss_real_tracking/4*0.6, rtol=1e-5)
