4aa9a4e2e610c8997d67dddc5b45a9d5000db6d0
giadarol <giovanni.iadarola@cern.ch>
2022-11-20 07:19:52 +0100

    Cleaning up match example

--- examples/twiss/003_match_tune_chroma.py
+++ examples/twiss/003_match_tune_chroma.py
@@ -3,36 +3,50 @@ import time
 import xtrack as xt
 import xpart as xp
 
+###################################
+# Load a line and build a tracker #
+####################################
+
 with open('../../test_data/hllhc15_noerrors_nobb/line_w_knobs_and_particle.json') as f:
     dct = json.load(f)
 
 line = xt.Line.from_dict(dct['line'])
 line.particle_ref = xp.Particles.from_dict(dct['particle'])
-
 tracker=line.build_tracker()
 
-print('\nInitial twiss parameters')
+#########
+# Twiss #
+#########
+
+print('\nInitial twiss parameters')                                             #!skip-doc
 tw_before = tracker.twiss()
+
+# Initial tune and chromaticity values
 print(f"Qx = {tw_before['qx']:.5f} Qy = {tw_before['qy']:.5f} "
       f"Q'x = {tw_before['dqx']:.5f} Q'y = {tw_before['dqy']:.5f}")
 
+# Initial value of the knobs correcting tunes an chromaticities
 print(f"kqtf.b1 = {line.vars['kqtf.b1']._value}")
 print(f"kqtd.b1 = {line.vars['kqtd.b1']._value}")
 print(f"ksf.b1 = {line.vars['ksf.b1']._value}")
 print(f"ksd.b1 = {line.vars['ksd.b1']._value}")
 
-t1 = time.time()
+#####################################################
+# Match tunes and chromaticities to assigned values #
+#####################################################
+
+t1 = time.time()                                                                #!skip-doc
 tracker.match(vary=['kqtf.b1', 'kqtd.b1','ksf.b1', 'ksd.b1'],
     targets = [
         ('qx', 62.315),
         (lambda tw: tw['qx'] - tw['qy'], 1.99), # equivalent to ('qy', 60.325)
         ('dqx', 10.0),
         ('dqy', 12.0),])
-t2 = time.time()
-print('\nTime fsolve: ', t2-t1)
+t2 = time.time()                                                                #!skip-doc
+print('\nTime fsolve: ', t2-t1)                                                 #!skip-doc
 
-tw_final = tracker.twiss()
-print('\nFinal twiss parameters')
+tw_final = tracker.twiss()                                                      #!skip-doc
+print('\nFinal twiss parameters')                                               #!skip-doc
 print(f"Qx = {tw_final['qx']:.5f} Qy = {tw_final['qy']:.5f} "
       f"Q'x = {tw_final['dqx']:.5f} Q'y = {tw_final['dqy']:.5f}")
 print(f"kqtf.b1 = {line.vars['kqtf.b1']._value}")
@@ -40,29 +54,6 @@ print(f"kqtd.b1 = {line.vars['kqtd.b1']._value}")
 print(f"ksf.b1 = {line.vars['ksf.b1']._value}")
 print(f"ksd.b1 = {line.vars['ksd.b1']._value}")
 
-t1 = time.time()
-tracker.match(vary=['kqtf.b1', 'kqtd.b1','ksf.b1', 'ksd.b1'],
-    targets = [
-        ('qx', 62.27),
-        ('qy', 60.28),
-        ('dqx', -5.0),
-        ('dqy', -7.0),])
-t2 = time.time()
-print('\nTime fsolve: ', t2-t1)
-
-tw_final = tracker.twiss()
-print('\nFinal twiss parameters')
-print(f"Qx = {tw_final['qx']:.5f} Qy = {tw_final['qy']:.5f} "
-      f"Q'x = {tw_final['dqx']:.5f} Q'y = {tw_final['dqy']:.5f}")
-print(f"kqtf.b1 = {line.vars['kqtf.b1']._value}")
-print(f"kqtd.b1 = {line.vars['kqtd.b1']._value}")
-print(f"ksf.b1 = {line.vars['ksf.b1']._value}")
-print(f"ksd.b1 = {line.vars['ksd.b1']._value}")
-
-# 4d matching
-for ee in line.elements:
-    if isinstance(ee, xt.Cavity):
-        ee.voltage = 0.0
 
 t1 = time.time()
 tracker.match(method='4d', # <-- 4d matching
@@ -83,3 +74,25 @@ print(f"kqtf.b1 = {line.vars['kqtf.b1']._value}")
 print(f"kqtd.b1 = {line.vars['kqtd.b1']._value}")
 print(f"ksf.b1 = {line.vars['ksf.b1']._value}")
 print(f"ksd.b1 = {line.vars['ksd.b1']._value}")
+
+#!end-doc-part
+
+t1 = time.time()
+tracker.match(vary=['kqtf.b1', 'kqtd.b1','ksf.b1', 'ksd.b1'],
+    targets = [
+        ('qx', 62.27),
+        ('qy', 60.28),
+        ('dqx', -5.0),
+        ('dqy', -7.0),])
+t2 = time.time()
+print('\nTime fsolve: ', t2-t1)
+
+tw_final = tracker.twiss()
+print('\nFinal twiss parameters')
+print(f"Qx = {tw_final['qx']:.5f} Qy = {tw_final['qy']:.5f} "
+      f"Q'x = {tw_final['dqx']:.5f} Q'y = {tw_final['dqy']:.5f}")
+print(f"kqtf.b1 = {line.vars['kqtf.b1']._value}")
+print(f"kqtd.b1 = {line.vars['kqtd.b1']._value}")
+print(f"ksf.b1 = {line.vars['ksf.b1']._value}")
+print(f"ksd.b1 = {line.vars['ksd.b1']._value}")
+
