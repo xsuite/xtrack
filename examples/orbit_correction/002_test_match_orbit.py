f1792f3f2bebb3a5ee214db1303d24ff21462385
giadarol <giovanni.iadarola@cern.ch>
2023-01-30 21:59:15 +0100

    Take into account experimental magnets in checks

--- examples/orbit_correction/002_test_match_orbit.py
+++ examples/orbit_correction/002_test_match_orbit.py
@@ -4,106 +4,19 @@ import numpy as np
 import xtrack as xt
 import xobjects as xo
 
-with open('../../test_data/hllhc14_no_errors_with_coupling_knobs/line_b1.json',
-            'r') as fid:
-    dct_b1 = json.load(fid)
-line = xt.Line.from_dict(dct_b1)
+# Load line and line_co_ref from json
+with open('line_with_orbit_ref.json', 'r') as fid:
+    dct = json.load(fid)
+line = xt.Line.from_dict(dct['line'])
+line_co_ref = xt.Line.from_dict(dct['line_co_ref'])
+
 tracker = line.build_tracker()
-
-# Load line with knobs on correctors only
-from cpymad.madx import Madx
-mad = Madx()
-mad.call('../../test_data/hllhc14_no_errors_with_coupling_knobs/lhcb1_seq.madx')
-mad.use(sequence='lhcb1')
-line_co_ref = xt.Line.from_madx_sequence(mad.sequence.lhcb1,
-    deferred_expressions=True,
-    expressions_for_element_types=('kicker', 'hkicker', 'vkicker'))
-
 tracker_co_ref = line_co_ref.build_tracker()
-tracker_co_ref.particle_ref = tracker.particle_ref
 
 # Bind variables in the two lines
 from shared_knobs import VarSharing
 var_sharing = VarSharing(lines = [line, line_co_ref],
                          names=['lhcb1', '_orbit_ref_lhcb1'])
-
-
-# Wipe out orbit correction from pymask
-for kk in line._var_management['data']['var_values']:
-    if kk.startswith('corr_acb'):
-        line.vars[kk] = 0
-
-# Switch off crossing angles and separations
-line.vars['on_x1'] = 0
-line.vars['on_x2'] = 0
-line.vars['on_x5'] = 0
-line.vars['on_x8'] = 0
-line.vars['on_sep1'] = 0
-line.vars['on_sep2'] = 0
-line.vars['on_sep5'] = 0
-line.vars['on_sep8'] = 0
-
-# Switch off experimental magnets
-line.vars['on_alice'] = 0
-line.vars['on_lhcb'] = 0
-line.vars['on_sol_alice'] = 0
-line.vars['on_sol_lhcb'] = 0
-line.vars['on_sol_atlas'] = 0
-line.vars['on_sol_cms'] = 0
-
-# Check that in both machines the orbit is flat at the ips
-for lll in [line_co_ref, line]:
-    tw = lll.tracker.twiss(method='4d', zeta0=0, delta0=0)
-    for ip in ['ip1', 'ip2', 'ip5', 'ip8']:
-        assert np.isclose(tw[ip, 'x'], 0, 1e-10)
-        assert np.isclose(tw[ip, 'px'], 0, 1e-10)
-        assert np.isclose(tw[ip, 'y'], 0, 1e-10)
-        assert np.isclose(tw[ip, 'py'], 0, 1e-10)
-
-# Check that the tune knobs work only on line and not on line_co_ref
-tw0 = tracker.twiss(method='4d', zeta0=0, delta0=0)
-tw0_co_ref = tracker_co_ref.twiss(method='4d', zeta0=0, delta0=0)
-tracker.vars['kqtf.b1'] = 1e-5
-tracker_co_ref.vars['kqtf.b1'] = 1e-5
-tw1 = tracker.twiss(method='4d', zeta0=0, delta0=0)
-tw1_co_ref = tracker_co_ref.twiss(method='4d', zeta0=0, delta0=0)
-assert tw1.qx != tw0.qx
-assert tw1_co_ref.qx == tw0_co_ref.qx
-
-# Add correction term to all dipole correctors
-line.vars['on_corr_co'] = 1
-for kk in list(line._var_management['data']['var_values'].keys()):
-    if kk.startswith('acb'):
-        line.vars['corr_co_'+kk] = 0
-        line.vars[kk] += line.vars['corr_co_'+kk] * line.vars['on_corr_co']
-
-# Set some orbit knobs in both machines
-line.vars['on_x1'] = 250
-line.vars['on_x2'] = 250
-line.vars['on_x5'] = 250
-line.vars['on_x8'] = 250
-line.vars['on_disp'] = 1
-
-# Introduce dip kick in all triplets (only in line)
-line['mqxfb.b2l1..11'].knl[0] = 1e-6
-line['mqxfb.b2l1..11'].ksl[0] = 1.5e-6
-line['mqxfb.b2r1..11'].knl[0] = 2e-6
-line['mqxfb.b2r1..11'].ksl[0] = 1e-6
-
-line['mqxb.b2l2..11'].knl[0] = 1e-6
-line['mqxb.b2l2..11'].ksl[0] = 1.5e-6
-line['mqxb.b2r2..11'].knl[0] = 2e-6
-line['mqxb.b2r2..11'].ksl[0] = 1e-6
-
-line['mqxfb.b2l5..11'].knl[0] = 1e-6
-line['mqxfb.b2l5..11'].ksl[0] = 1.5e-6
-line['mqxfb.b2r5..11'].knl[0] = 2e-6
-line['mqxfb.b2r5..11'].ksl[0] = 1e-6
-
-line['mqxb.b2l8..11'].knl[0] = 1e-6
-line['mqxb.b2l8..11'].ksl[0] = 1.5e-6
-line['mqxb.b2r8..11'].knl[0] = 2e-6
-line['mqxb.b2r8..11'].ksl[0] = 1e-6
 
 tw_before = tracker.twiss()
 
@@ -140,13 +53,21 @@ for corr_name, corr in correction_setup.items():
 tw = tracker.twiss()
 
 assert np.isclose(tw['ip1', 'px'], 250e-6, atol=1e-8)
-assert np.isclose(tw['ip2', 'py'], 250e-6, atol=1e-8)
-assert np.isclose(tw['ip5', 'py'], 250e-6, atol=1e-8)
-assert np.isclose(tw['ip8', 'px'], 250e-6, atol=1e-8)
 assert np.isclose(tw['ip1', 'py'], 0, atol=1e-8)
-assert np.isclose(tw['ip2', 'px'], 0, atol=1e-8)
+
 assert np.isclose(tw['ip5', 'px'], 0, atol=1e-8)
-assert np.isclose(tw['ip8', 'py'], 0, atol=1e-8)
+assert np.isclose(tw['ip5', 'py'], 250e-6, atol=1e-8)
+
+assert tw['ip2', 'px'] > 1e-7 # effect of the spectrometer tilt
+assert tw['ip2', 'py'] > 255e-6 # effect of the spectrometer
+assert np.isclose(tw['bpmsw.1r2.b1', 'px'], 0, atol=1e-8)  # external angle
+assert np.isclose(tw['bpmsw.1r2.b1', 'py'], 250e-6, atol=1e-8) # external angle
+
+assert tw['ip8', 'px'] > 255e-6 # effect of the spectrometer
+assert tw['ip8', 'py'] > 1e-6 # effect of the spectrometer tilt
+assert np.isclose(tw['bpmsw.1r8.b1', 'px'], 250e-6, atol=1e-8) # external angle
+assert np.isclose(tw['bpmsw.1r8.b1', 'py'], 0, atol=1e-8) # external angle
+
 
 places_to_check = [
  'e.ds.r8.b1',
