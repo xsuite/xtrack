bdcb914d4ed0ef86407d6bb90a2afd24a19e60c4
giadarol <giovanni.iadarola@cern.ch>
2023-02-06 12:15:40 +0100

    Drafted octupoles, untested

--- examples/orbit_correction/004_dev_mask_functions.py
+++ examples/orbit_correction/004_dev_mask_functions.py
@@ -8,6 +8,7 @@ import json
 import numpy as np
 import xtrack as xt
 import xobjects as xo
+from scipy.constants import c as clight
 
 with open('../../test_data/hllhc14_no_errors_with_coupling_knobs/line_b1.json',
             'r') as fid:
@@ -27,3 +28,20 @@ assert np.isclose(line.twiss().c_minus, 1e-3, atol=1e-4, rtol=0)
 line.vars['c_minus_re_b1'] = 0
 line.vars['c_minus_im_b1'] = 1e-3
 assert np.isclose(line.twiss().c_minus, 1e-3, atol=1e-4, rtol=0)
+
+# Switch on octupoles
+beamn = 1
+line.vars[f'p0c_b{beamn}'] = line.particle_ref.p0c[0]
+line.vars[f'q0_b{beamn}'] = line.particle_ref.q0
+line.vars[f'brho0_{beamn}'] = line.vars['p0c'] / line.vars['q0'] / clight
+
+line.vars[f'i_oct_b{beamn}'] = 0
+for ss in '12 23 34 45 56 67 78 81'.split():
+    line.vars[f'kof.a{ss}b{beamn}'] = (
+        line.vars['kmax_mo']
+        * line.vars[f'i_oct_b{beamn}'] / line.vars['imax_mo']
+        / line.vars[f'brho0_{beamn}'])
+    line.vars[f'kod.a{ss}b{beamn}'] = (
+        line.vars['kmax_mo']
+        * line.vars[f'i_oct_b{beamn}'] / line.vars['imax_mo']
+        / line.vars[f'brho0_{beamn}'])
