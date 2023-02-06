6309952c0c8b7ab25f89c9adb6286a77eb3b7c39
giadarol <giovanni.iadarola@cern.ch>
2023-02-06 13:38:27 +0100

    Checked new octupole knobs

--- examples/orbit_correction/004_dev_mask_functions.py
+++ examples/orbit_correction/004_dev_mask_functions.py
@@ -33,15 +33,27 @@ assert np.isclose(line.twiss().c_minus, 1e-3, atol=1e-4, rtol=0)
 beamn = 1
 line.vars[f'p0c_b{beamn}'] = line.particle_ref.p0c[0]
 line.vars[f'q0_b{beamn}'] = line.particle_ref.q0
-line.vars[f'brho0_{beamn}'] = line.vars['p0c'] / line.vars['q0'] / clight
+line.vars[f'brho0_b{beamn}'] = (line.vars[f'p0c_b{beamn}']
+                               / line.vars[f'q0_b{beamn}'] / clight)
 
 line.vars[f'i_oct_b{beamn}'] = 0
 for ss in '12 23 34 45 56 67 78 81'.split():
     line.vars[f'kof.a{ss}b{beamn}'] = (
         line.vars['kmax_mo']
         * line.vars[f'i_oct_b{beamn}'] / line.vars['imax_mo']
-        / line.vars[f'brho0_{beamn}'])
+        / line.vars[f'brho0_b{beamn}'])
     line.vars[f'kod.a{ss}b{beamn}'] = (
         line.vars['kmax_mo']
         * line.vars[f'i_oct_b{beamn}'] / line.vars['imax_mo']
-        / line.vars[f'brho0_{beamn}'])
+        / line.vars[f'brho0_b{beamn}'])
+
+# Check against these references
+i_ioct_ref = -235.
+k_ref = -6.928009
+knl_ref = -2.21696289
+
+line.vars['i_oct_b1'] = i_ioct_ref
+assert np.isclose(line.vars['kof.a12b1']._value, k_ref, atol=0, rtol=1e-5)
+assert np.isclose(line.vars['kod.a45b1']._value, k_ref, atol=0, rtol=1e-5)
+assert np.isclose(line['mo.24r2.b1'].knl[3], knl_ref, atol=0, rtol=1e-5)
+
