a7c2554bdbccb61821a2935eab7e4bab0f2996b1
Frederik Van der Veken <frederik@cern.ch>
2023-02-15 13:49:09 +0100

    Updated random generator tests to new context scan.

--- tests/test_random_gen_gauss.py
+++ tests/test_random_gen_gauss.py
@@ -8,66 +8,65 @@ from pathlib import Path
 import numpy as np
 
 import xobjects as xo
+from xobjects.test_helpers import for_all_test_contexts
 import xtrack as xt
 import xpart as xp
 
-def test_random_generation():
-    for ctx in xo.context.get_test_contexts():
-        print(f'{ctx}')
+@for_all_test_contexts
+def test_random_generation(test_context):
 
-        part = xp.Particles(_context=ctx, p0c=6.5e12, x=[1,2,3])
-        part._init_random_number_generator()
+    part = xp.Particles(_context=test_context, p0c=6.5e12, x=[1,2,3])
+    part._init_random_number_generator()
 
-        class TestElement(xt.BeamElement):
-            _xofields={
-                'dummy': xo.Float64,
+    class TestElement(xt.BeamElement):
+        _xofields={
+            'dummy': xo.Float64,
+            }
+
+        _depends_on = [xt.RandomNormal]
+
+        _extra_c_sources = [
+            '''
+                /*gpufun*/
+                void TestElement_track_local_particle(
+                        TestElementData el, LocalParticle* part0){
+                    //start_per_particle_block (part0->part)
+                        double rr = RandomNormal_generate(part);
+                        LocalParticle_set_x(part, rr);
+                    //end_per_particle_block
                 }
+            '''
+        ]
 
-            _depends_on = [xt.RandomNormal]
+    telem = TestElement(_context=test_context)
 
-            _extra_c_sources = [
-                '''
-                    /*gpufun*/
-                    void TestElement_track_local_particle(
-                            TestElementData el, LocalParticle* part0){
-                        //start_per_particle_block (part0->part)
-                            double rr = RandomNormal_generate(part);
-                            LocalParticle_set_x(part, rr);
-                        //end_per_particle_block
-                    }
-                '''
-            ]
+    telem.track(part)
 
-        telem = TestElement(_context=ctx)
+    # Use turn-by turin monitor to acquire some statistics
 
-        telem.track(part)
+    tracker = xt.Tracker(_buffer=telem._buffer,
+            line=xt.Line(elements=[telem]))
 
-        # Use turn-by turin monitor to acquire some statistics
+    tracker.track(part, num_turns=1e6, turn_by_turn_monitor=True)
 
-        tracker = xt.Tracker(_buffer=telem._buffer,
-                line=xt.Line(elements=[telem]))
-
-        tracker.track(part, num_turns=1e6, turn_by_turn_monitor=True)
-
-        for i_part in range(part._capacity):
-            x = tracker.record_last_track.x[i_part, :]
-            hstgm, bin_edges = np.histogram(x,  bins=50, range=(-3, 3), density=True)
-            bin_centers = (bin_edges[:-1]+bin_edges[1:])/2
-            gauss = np.exp(-bin_centers**2/2)/np.sqrt(2.0*np.pi)
-            assert np.allclose(hstgm, gauss, rtol=1e-10, atol=1E-2)
+    for i_part in range(part._capacity):
+        x = tracker.record_last_track.x[i_part, :]
+        hstgm, bin_edges = np.histogram(x,  bins=50, range=(-3, 3), density=True)
+        bin_centers = (bin_edges[:-1]+bin_edges[1:])/2
+        gauss = np.exp(-bin_centers**2/2)/np.sqrt(2.0*np.pi)
+        assert np.allclose(hstgm, gauss, rtol=1e-10, atol=1E-2)
 
 
-def test_direct_sampling():
-    for ctx in xo.context.get_test_contexts():
-        print(f'{ctx}')
-        n_seeds = 3
-        n_samples = 3e6
-        ran = xt.RandomNormal()
-        samples, _ = ran.generate(n_samples=n_samples, n_seeds=n_seeds)
+@for_all_test_contexts
+def test_direct_sampling(test_context):
+    n_seeds = 3
+    n_samples = 3e6
+    ran = xt.RandomNormal(_context=test_context)
+    samples, _ = ran.generate(n_samples=n_samples, n_seeds=n_seeds)
 
-        for i_part in range(n_seeds):
-            hstgm, bin_edges = np.histogram(samples[i_part],  bins=50, range=(-3, 3), density=True)
-            bin_centers = (bin_edges[:-1]+bin_edges[1:])/2
-            gauss = np.exp(-bin_centers**2/2)/np.sqrt(2.0*np.pi)
-            assert np.allclose(hstgm, gauss, rtol=1e-10, atol=1E-2)
+    for i_part in range(n_seeds):
+        hstgm, bin_edges = np.histogram(samples[i_part],  bins=50, range=(-3, 3), density=True)
+        bin_centers = (bin_edges[:-1]+bin_edges[1:])/2
+        gauss = np.exp(-bin_centers**2/2)/np.sqrt(2.0*np.pi)
+        assert np.allclose(hstgm, gauss, rtol=1e-10, atol=1E-2)
 
--- tests/test_random_gen_ruth.py
+++ tests/test_random_gen_ruth.py
@@ -8,6 +8,7 @@ from pathlib import Path
 import numpy as np
 
 import xobjects as xo
+from xobjects.test_helpers import for_all_test_contexts
 import xtrack as xt
 import xpart as xp
 
@@ -20,82 +21,80 @@ rA = 0.0012306225579197868
 rB = 53.50625
 iterations = 20
 
-def test_random_generation():
-    for ctx in xo.context.get_test_contexts():
-        print(f'{ctx}')
+@for_all_test_contexts
+def test_random_generation(test_context):
 
-        part = xp.Particles(_context=ctx, p0c=6.5e12, x=[1,2,3])
-        part._init_random_number_generator()
+    part = xp.Particles(_context=test_context, p0c=6.5e12, x=[1,2,3])
+    part._init_random_number_generator()
 
-        class TestElement(xt.BeamElement):
-            _xofields={
-                'dummy': xo.Float64,
-                'rng':   xt.RandomRutherford
+    class TestElement(xt.BeamElement):
+        _xofields={
+            'dummy': xo.Float64,
+            'rng':   xt.RandomRutherford
+            }
+
+        _extra_c_sources = [
+            '''
+                /*gpufun*/
+                void TestElement_track_local_particle(
+                        TestElementData el, LocalParticle* part0){
+                    RandomRutherfordData rng = TestElementData_getp_rng(el);
+                    //start_per_particle_block (part0->part)
+                        double rr = RandomRutherford_generate(rng, part);
+                        LocalParticle_set_x(part, rr);
+                    //end_per_particle_block
                 }
+            '''
+        ]
 
-            _extra_c_sources = [
-                '''
-                    /*gpufun*/
-                    void TestElement_track_local_particle(
-                            TestElementData el, LocalParticle* part0){
-                        RandomRutherfordData rng = TestElementData_getp_rng(el);
-                        //start_per_particle_block (part0->part)
-                            double rr = RandomRutherford_generate(rng, part);
-                            LocalParticle_set_x(part, rr);
-                        //end_per_particle_block
-                    }
-                '''
-            ]
+        def __init__(self, **kwargs):
+            if '_xobject' not in kwargs:
+                kwargs.setdefault('rng', xt.RandomRutherford())
+            super().__init__(**kwargs)
 
-            def __init__(self, **kwargs):
-                if '_xobject' not in kwargs:
-                    kwargs.setdefault('rng', xt.RandomRutherford())
-                super().__init__(**kwargs)
+    telem = TestElement(_context=test_context)
+    telem.rng.A = rA
+    telem.rng.B = rB
+    telem.rng.lower_val = t0
+    telem.rng.upper_val = t1
+    telem.rng.Newton_iterations = iterations
 
-        telem = TestElement(_context=ctx)
-        telem.rng.A = rA
-        telem.rng.B = rB
-        telem.rng.lower_val = t0
-        telem.rng.upper_val = t1
-        telem.rng.Newton_iterations = iterations
+    telem.track(part)
 
-        telem.track(part)
+    # Use turn-by turin monitor to acquire some statistics
 
-        # Use turn-by turin monitor to acquire some statistics
+    tracker = xt.Tracker(_buffer=telem._buffer,
+            line=xt.Line(elements=[telem]))
 
-        tracker = xt.Tracker(_buffer=telem._buffer,
-                line=xt.Line(elements=[telem]))
+    tracker.track(part, num_turns=1e6, turn_by_turn_monitor=True)
 
-        tracker.track(part, num_turns=1e6, turn_by_turn_monitor=True)
-
-        for i_part in range(part._capacity):
-            x = tracker.record_last_track.x[i_part, :]
-            assert np.all(x[i_part]>=t0)
-            assert np.all(x[i_part]<=t1)
-            hstgm, bin_edges = np.histogram(x[0],  bins=100, range=(t0, t1), density=True)
-            bin_centers = (bin_edges[:-1]+bin_edges[1:])/2
-            ruth = np.array([ruth_PDF(t, rA, rB) for t in bin_centers ])
-            np.allclose(hstgm[:-10], ruth[:-10], rtol=5e-2, atol=1)
+    for i_part in range(part._capacity):
+        x = tracker.record_last_track.x[i_part, :]
+        assert np.all(x[i_part]>=t0)
+        assert np.all(x[i_part]<=t1)
+        hstgm, bin_edges = np.histogram(x[0],  bins=100, range=(t0, t1), density=True)
+        bin_centers = (bin_edges[:-1]+bin_edges[1:])/2
+        ruth = np.array([ruth_PDF(t, rA, rB) for t in bin_centers ])
+        np.allclose(hstgm[:-10], ruth[:-10], rtol=5e-2, atol=1)
 
 
-def test_direct_sampling():
-    for ctx in xo.context.get_test_contexts():
-        print(f'{ctx}')
-        n_seeds = 3
-        n_samples = 3e6
-        ran = xt.RandomRutherford()
-        ran.A = rA
-        ran.B = rB
-        ran.lower_val = t0
-        ran.upper_val = t1
-        ran.Newton_iterations = iterations
-        samples, _ = ran.generate(n_samples=n_samples, n_seeds=n_seeds)
+@for_all_test_contexts
+def test_direct_sampling(test_context):
+    n_seeds = 3
+    n_samples = 3e6
+    ran = xt.RandomRutherford(_context=test_context)
+    ran.A = rA
+    ran.B = rB
+    ran.lower_val = t0
+    ran.upper_val = t1
+    ran.Newton_iterations = iterations
+    samples, _ = ran.generate(n_samples=n_samples, n_seeds=n_seeds)
 
-        for i_part in range(n_seeds):
-            assert np.all(samples[i_part]>=t0)
-            assert np.all(samples[i_part]<=t1)
-            hstgm, bin_edges = np.histogram(samples[0],  bins=100, range=(t0, t1), density=True)
-            bin_centers = (bin_edges[:-1]+bin_edges[1:])/2
-            ruth = np.array([ruth_PDF(t, rA, rB) for t in bin_centers ])
-            np.allclose(hstgm[:-10], ruth[:-10], rtol=5e-2, atol=1)
+    for i_part in range(n_seeds):
+        assert np.all(samples[i_part]>=t0)
+        assert np.all(samples[i_part]<=t1)
+        hstgm, bin_edges = np.histogram(samples[0],  bins=100, range=(t0, t1), density=True)
+        bin_centers = (bin_edges[:-1]+bin_edges[1:])/2
+        ruth = np.array([ruth_PDF(t, rA, rB) for t in bin_centers ])
+        np.allclose(hstgm[:-10], ruth[:-10], rtol=5e-2, atol=1)
 
