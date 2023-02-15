1db016119fe8aed291723b22de1b5051d1f9c9f0
Frederik Van der Veken <frederik@cern.ch>
2023-02-15 14:34:05 +0100

    Renamed XT_LOST_ALL_E_IN_SYNC into XT_LOST_ALL_E_IN_SYNRAD.

--- xtrack/headers/particle_states.h
+++ xtrack/headers/particle_states.h
@@ -8,7 +8,7 @@
 
 #define  XT_LOST_ON_APERTURE     0
 #define  XT_LOST_ON_LONG_CUT    -2
-#define  XT_LOST_ALL_E_IN_SYNC -10
+#define  XT_LOST_ALL_E_IN_SYNRAD -10
 #define  RNG_ERR_SEEDS_NOT_SET -20
 #define  RNG_ERR_INVALID_TRACK -21
 #define  RNG_ERR_RUTH_NOT_SET  -22
