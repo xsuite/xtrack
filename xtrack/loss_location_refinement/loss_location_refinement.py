de575cbf52981811daa6975b8f087d3939810795
Frederik Van der Veken <frederik@cern.ch>
2023-02-22 12:37:06 +0100

    Fixed error in logic to check if backtracking through element type is allowed.

--- xtrack/loss_location_refinement/loss_location_refinement.py
+++ xtrack/loss_location_refinement/loss_location_refinement.py
@@ -202,7 +202,7 @@ def refine_loss_location_single_aperture(particles, i_aper_1, i_start_thin_0,
     for nn in interp_tracker._original_tracker.line.element_names[
                                              i_aper_1 - n_backtrack : i_aper_1]:
         ee = interp_tracker._original_tracker.line.element_dict[nn]
-        if not _allow_backtrack(ee) or not isinstance(ee, tuple(allowed_backtrack_types)):
+        if not _allow_backtrack(ee) and not isinstance(ee, tuple(allowed_backtrack_types)):
             if _skip_in_loss_location_refinement(ee):
                 return 'skipped'
             raise TypeError(
