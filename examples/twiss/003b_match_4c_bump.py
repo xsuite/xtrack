ea2e276e6169492dbab37de98d267f5ef07a52ce
giadarol <giovanni.iadarola@cern.ch>
2023-02-26 15:36:48 +0100

    clean up example

--- examples/twiss/003b_match_4c_bump.py
+++ examples/twiss/003b_match_4c_bump.py
@@ -10,17 +10,10 @@ with open('../../test_data/hllhc14_no_errors_with_coupling_knobs/line_b1.json',
 line = xt.Line.from_dict(dct)
 
 line.build_tracker()
-'''
-mq.28l4.b1
-acbv30.l4b1
-acbv28.l4b1
-acbv26.l4b1
-acbv24.l4b1
-'''
 
 tw_before = line.twiss()
 
-res1 =line.match(
+line.match(
     ele_start='mq.33l8.b1',
     ele_stop='mq.23l8.b1',
     twiss_init=tw_before.get_twiss_init(at_element='mq.33l8.b1'),
@@ -39,24 +32,6 @@ res1 =line.match(
         xt.Target('py', at='mq.23l8.b1', value=tw_before['mq.23l8.b1', 'py'], tol=1e-7, scale=1000),
     ]
 )
-
-# res2 =line.match(verbose=True,
-#     ele_start='mq.33l8.b1',
-#     ele_stop='mq.23l8.b1',
-#     twiss_init=tw_before.get_twiss_init(at_element='mq.33l8.b1'),
-#     vary=[
-#         #xt.Vary(name='acbv32.l8b1', limits=[-100e-6, 100e-6], step=1e-10),
-#         #xt.Vary(name='acbv30.l8b1', limits=[-100e-6, 100e-6], step=1e-10),
-
-#     ],
-#     targets=[
-#         # I want the orbit to be 1 mm at mq.28l8.b1 with zero angle
-#         #xt.Target('y', at='mb.b28l8.b1', value=1e-3, tol=1e-5, scale=1),
-#         #xt.Target('py', at='mb.b28l8.b1', value=0, tol=1e-7, scale=1000),
-#         # I want the bump to be closed
-
-#     ]
-# )
 
 tw = line.twiss()
 
