636a997c6e0ac8e87e0caf798e41b6f2855d30cc
giadarol <giovanni.iadarola@cern.ch>
2023-03-03 18:13:19 +0100

    Fix (charge was zero)

--- test_data/psb_injection/000_generate_xsuite_line.py
+++ test_data/psb_injection/000_generate_xsuite_line.py
@@ -11,7 +11,7 @@ mad.use('psb')
 mad.twiss()
 
 line = xt.Line.from_madx_sequence(mad.sequence['psb'])
-line.particle_ref = xp.Particles(mass0=xp.PROTON_MASS_EV, q0=0,
+line.particle_ref = xp.Particles(mass0=xp.PROTON_MASS_EV, q0=1.,
                                  gamma0=mad.sequence['psb'].beam.gamma)
 
 with open('line_and_particle.json', 'w') as fid:
