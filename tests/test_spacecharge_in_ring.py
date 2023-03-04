00917ad23e89608ecbc059fc0db366762f6d280e
giadarol <giovanni.iadarola@cern.ch>
2023-03-04 13:30:13 +0100

    Removed unnecessary build_tracker

--- tests/test_spacecharge_in_ring.py
+++ tests/test_spacecharge_in_ring.py
@@ -56,7 +56,6 @@ def test_ring_with_spacecharge(test_context):
     ##################
     line_temp = line0_no_sc.filter_elements(
         exclude_types_starting_with='SpaceCh')
-    line_temp.build_tracker(_context=test_context)
     import warnings
     warnings.filterwarnings('ignore')
     particle_probe = xp.build_particles(
@@ -145,7 +144,6 @@ def test_ring_with_spacecharge(test_context):
         ###############################
 
         line_no_sc = line.filter_elements(exclude_types_starting_with='SpaceCh')
-        line_no_sc.build_tracker(_context=test_context)
         tw = line_no_sc.twiss(
                 particle_ref=particle_ref,  at_elements=[0])
 
