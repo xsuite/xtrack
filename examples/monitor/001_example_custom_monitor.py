15d7069a39fc4494e74ef9ea7f9e0d1ce0f23b75
giadarol <giovanni.iadarola@cern.ch>
2023-03-04 11:48:29 +0100

    Adapter examples to use line instead of tracker

--- examples/monitor/001_example_custom_monitor.py
+++ examples/monitor/001_example_custom_monitor.py
@@ -16,10 +16,10 @@ with open('../../test_data/hllhc15_noerrors_nobb/line_and_particle.json') as f:
 line = xt.Line.from_dict(dct['line'])
 line.particle_ref = xp.Particles.from_dict(dct['particle'])
 
-tracker = line.build_tracker()
+line.build_tracker()
 
 num_particles = 50
-particles = xp.generate_matched_gaussian_bunch(tracker=tracker,
+particles = xp.generate_matched_gaussian_bunch(line=line,
                                                num_particles=num_particles,
                                                nemitt_x=2.5e-6,
                                                nemitt_y=2.5e-6,
@@ -29,12 +29,12 @@ num_turns = 30
 monitor = xt.ParticlesMonitor(_context=context,
                               start_at_turn=5, stop_at_turn=15,
                               num_particles=num_particles)
-tracker.track(particles, num_turns=num_turns,
+line.track(particles, num_turns=num_turns,
               turn_by_turn_monitor=monitor
              )
-# tracker.record_last_track contains the measured data. For example,
-# tracker.record_last_track.x contains the x coordinate for all particles
-# and the selected turns, e.g. tracker.record_last_track.x[3, 5] gives the
+# line.record_last_track contains the measured data. For example,
+# line.record_last_track.x contains the x coordinate for all particles
+# and the selected turns, e.g. line.record_last_track.x[3, 5] gives the
 # x coordinates for the particle having particle_id = 3 and for the fifth
 # recorded turn. The turn indeces that are recorded can be inspected in
-# tracker.record_last_track.at_turn
+# line.record_last_track.at_turn
