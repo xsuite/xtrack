1376ce0a1e5888a998fd4f100d436aef473e7ff7
giadarol <giovanni.iadarola@cern.ch>
2023-03-04 09:33:10 +0100

    Adapted tests to use line istead of tracker

--- tests/test_element_internal_record.py
+++ tests/test_element_internal_record.py
@@ -79,18 +79,19 @@ def test_record_single_table(test_context):
 
     n_kicks0 = 5
     n_kicks1 = 3
-    tracker = xt.Tracker(_context=test_context, line=xt.Line(elements = [
-        TestElement(n_kicks=n_kicks0), TestElement(n_kicks=n_kicks1)]))
-    tracker.line._needs_rng = True
+    line=xt.Line(elements = [
+        TestElement(n_kicks=n_kicks0), TestElement(n_kicks=n_kicks1)])
+    line._needs_rng = True
+    line.build_tracker(_context=test_context)
 
-    record = tracker.start_internal_logging_for_elements_of_type(
+    record = line.start_internal_logging_for_elements_of_type(
                                                         TestElement, capacity=10000)
 
     part = xp.Particles(_context=test_context, p0c=6.5e12, x=[1e-3,2e-3,3e-3])
     num_turns0 = 10
     num_turns1 = 3
-    tracker.track(part, num_turns=num_turns0)
-    tracker.track(part, num_turns=num_turns1)
+    line.track(part, num_turns=num_turns0)
+    line.track(part, num_turns=num_turns1)
 
     num_recorded = record._index.num_recorded
     num_turns = num_turns0 + num_turns1
@@ -108,30 +109,30 @@ def test_record_single_table(test_context):
                                                             * (n_kicks0 + n_kicks1))
 
     # Check reached capacity
-    record = tracker.start_internal_logging_for_elements_of_type(
+    record = line.start_internal_logging_for_elements_of_type(
                                                         TestElement, capacity=20)
 
     part = xp.Particles(_context=test_context, p0c=6.5e12, x=[1e-3,2e-3,3e-3])
     num_turns0 = 10
     num_turns1 = 3
-    tracker.track(part, num_turns=num_turns0)
-    tracker.track(part, num_turns=num_turns1)
+    line.track(part, num_turns=num_turns0)
+    line.track(part, num_turns=num_turns1)
 
     num_recorded = record._index.num_recorded
     assert num_recorded == 20
 
 
     # Check stop
-    record = tracker.start_internal_logging_for_elements_of_type(
+    record = line.start_internal_logging_for_elements_of_type(
                                                         TestElement, capacity=10000)
 
     part = xp.Particles(_context=test_context, p0c=6.5e12, x=[1e-3,2e-3,3e-3])
     num_turns0 = 10
     num_turns1 = 3
     num_particles = len(part.x)
-    tracker.track(part, num_turns=num_turns0)
-    tracker.stop_internal_logging_for_elements_of_type(TestElement)
-    tracker.track(part, num_turns=num_turns1)
+    line.track(part, num_turns=num_turns0)
+    line.stop_internal_logging_for_elements_of_type(TestElement)
+    line.track(part, num_turns=num_turns1)
 
     num_recorded = record._index.num_recorded
     num_turns = num_turns0
@@ -155,18 +156,19 @@ def test_record_single_table(test_context):
     elements = [
         TestElement(n_kicks=n_kicks0, _context=test_context), TestElement(n_kicks=n_kicks1)]
     elements[0].iscollective = True
-    tracker = xt.Tracker(_context=test_context, line=xt.Line(elements=elements))
-    tracker.line._needs_rng = True
+    line=xt.Line(elements=elements)
+    line.build_tracker(_context=test_context)
+    line._needs_rng = True
 
-    record = tracker.start_internal_logging_for_elements_of_type(
+    record = line.start_internal_logging_for_elements_of_type(
                                                         TestElement, capacity=10000)
 
     part = xp.Particles(_context=test_context, p0c=6.5e12, x=[1e-3,2e-3,3e-3])
     num_turns0 = 10
     num_turns1 = 3
-    tracker.track(part, num_turns=num_turns0)
-    tracker.stop_internal_logging_for_elements_of_type(TestElement)
-    tracker.track(part, num_turns=num_turns1)
+    line.track(part, num_turns=num_turns0)
+    line.stop_internal_logging_for_elements_of_type(TestElement)
+    line.track(part, num_turns=num_turns1)
 
     # Checks
     part.move(_context=xo.ContextCpu())
@@ -300,18 +302,19 @@ def test_record_multiple_tables(test_context):
     # Checks
     n_kicks0 = 5
     n_kicks1 = 3
-    tracker = xt.Tracker(_context=test_context, line=xt.Line(elements = [
-        TestElement(n_kicks=n_kicks0), TestElement(n_kicks=n_kicks1)]))
-    tracker.line._needs_rng = True
+    line=xt.Line(elements = [
+        TestElement(n_kicks=n_kicks0), TestElement(n_kicks=n_kicks1)])
+    line._needs_rng = True
+    line.build_tracker(_context=test_context)
 
-    record = tracker.start_internal_logging_for_elements_of_type(TestElement,
+    record = line.start_internal_logging_for_elements_of_type(TestElement,
                                 capacity={'table1': 10000, 'table2': 10000})
 
     part = xp.Particles(_context=test_context, p0c=6.5e12, x=[1e-3,2e-3,3e-3])
     num_turns0 = 10
     num_turns1 = 3
-    tracker.track(part, num_turns=num_turns0)
-    tracker.track(part, num_turns=num_turns1)
+    line.track(part, num_turns=num_turns0)
+    line.track(part, num_turns=num_turns1)
 
     part.move(_context=xo.ContextCpu())
     record.move(_context=xo.ContextCpu())
@@ -338,15 +341,15 @@ def test_record_multiple_tables(test_context):
         assert np.sum((table2.at_turn[:num_recorded_tab2] == i_turn)) == (num_particles
                                                             * (n_kicks0 + n_kicks1))
     # Check reached capacity
-    record = tracker.start_internal_logging_for_elements_of_type(
+    record = line.start_internal_logging_for_elements_of_type(
                                                         TestElement,
                                         capacity={'table1': 20, 'table2': 15})
 
     part = xp.Particles(_context=test_context, p0c=6.5e12, x=[1e-3,2e-3,3e-3])
     num_turns0 = 10
     num_turns1 = 3
-    tracker.track(part, num_turns=num_turns0)
-    tracker.track(part, num_turns=num_turns1)
+    line.track(part, num_turns=num_turns0)
+    line.track(part, num_turns=num_turns1)
 
     table1 = record.table1
     table2 = record.table2
@@ -358,7 +361,7 @@ def test_record_multiple_tables(test_context):
 
 
     # Check stop
-    record = tracker.start_internal_logging_for_elements_of_type(
+    record = line.start_internal_logging_for_elements_of_type(
                                         TestElement,
                                         capacity={'table1': 1000, 'table2': 1000})
 
@@ -367,9 +370,9 @@ def test_record_multiple_tables(test_context):
     num_turns1 = 3
     num_particles = len(part.x)
 
-    tracker.track(part, num_turns=num_turns0)
-    tracker.stop_internal_logging_for_elements_of_type(TestElement)
-    tracker.track(part, num_turns=num_turns1)
+    line.track(part, num_turns=num_turns0)
+    line.stop_internal_logging_for_elements_of_type(TestElement)
+    line.track(part, num_turns=num_turns1)
 
     part.move(_context=xo.ContextCpu())
     record.move(_context=xo.ContextCpu())
@@ -401,19 +404,20 @@ def test_record_multiple_tables(test_context):
     elements = [
         TestElement(n_kicks=n_kicks0, _context=test_context), TestElement(n_kicks=n_kicks1)]
     elements[0].iscollective = True
-    tracker = xt.Tracker(_context=test_context, line=xt.Line(elements=elements))
-    tracker.line._needs_rng = True
+    line = xt.Line(elements=elements)
+    line.build_tracker(_context=test_context)
+    line.line._needs_rng = True
 
-    record = tracker.start_internal_logging_for_elements_of_type(
+    record = line.start_internal_logging_for_elements_of_type(
                                         TestElement,
                                         capacity={'table1': 1000, 'table2': 1000})
 
     part = xp.Particles(_context=test_context, p0c=6.5e12, x=[1e-3,2e-3,3e-3])
     num_turns0 = 10
     num_turns1 = 3
-    tracker.track(part, num_turns=num_turns0)
-    tracker.stop_internal_logging_for_elements_of_type(TestElement)
-    tracker.track(part, num_turns=num_turns1)
+    line.track(part, num_turns=num_turns0)
+    line.stop_internal_logging_for_elements_of_type(TestElement)
+    line.track(part, num_turns=num_turns1)
 
     # Checks
     part.move(_context=xo.ContextCpu())
--- tests/test_freeze_longitudinal.py
+++ tests/test_freeze_longitudinal.py
@@ -25,33 +25,33 @@ def test_freeze_longitudinal_explicit(test_context):
     line = xt.Line.from_dict(line_dict['line'])
     line.particle_ref = xp.Particles.from_dict(line_dict['particle'])
     # Build the tracker
-    tracker = line.build_tracker(_context=test_context)
+    line.build_tracker(_context=test_context)
 
     # Freeze longitudinal coordinates
-    tracker.freeze_longitudinal()
+    line.freeze_longitudinal()
 
     # Track some particles with frozen longitudinal coordinates
-    particles = tracker.build_particles(delta=1e-3, x=[-1e-3, 0, 1e-3])
-    tracker.track(particles, num_turns=10)
+    particles = line.build_particles(delta=1e-3, x=[-1e-3, 0, 1e-3])
+    line.track(particles, num_turns=10)
     particles.move(_context=xo.context_default)
     assert np.allclose(particles.delta, 1e-3, rtol=0, atol=1e-12)
     assert np.allclose(particles.zeta, 0, rtol=9, atol=1e-12)
 
     # Twiss with frozen longitudinal coordinates (needs to be 4d)
-    twiss = tracker.twiss(method='4d')
+    twiss = line.twiss(method='4d')
     assert twiss.slip_factor == 0
 
     # Unfreeze longitudinal coordinates
-    tracker.freeze_longitudinal(False)
+    line.freeze_longitudinal(False)
 
     # Track some particles with unfrozen longitudinal coordinates
-    particles = tracker.build_particles(delta=1e-3, x=[-1e-3, 0, 1e-3])
-    tracker.track(particles, num_turns=10)
+    particles = line.build_particles(delta=1e-3, x=[-1e-3, 0, 1e-3])
+    line.track(particles, num_turns=10)
     particles.move(_context=xo.context_default)
     assert np.allclose(particles.delta, 0.00099218, rtol=0, atol=1e-6)
 
     # Twiss with unfrozen longitudinal coordinates (can be 6d)
-    twiss = tracker.twiss(method='6d')
+    twiss = line.twiss(method='6d')
     assert np.isclose(twiss.slip_factor, 0.00032151, rtol=0, atol=1e-6)
 
 
@@ -70,28 +70,28 @@ def test_freeze_longitudinal_context_manager(test_context):
     line.particle_ref = xp.Particles.from_dict(line_dict['particle'])
 
     # Build the tracker
-    tracker = line.build_tracker(_context=test_context)
+    line.build_tracker(_context=test_context)
 
-    with xt.freeze_longitudinal(tracker):
+    with xt.freeze_longitudinal(line):
         # Track some particles with frozen longitudinal coordinates
-        particles = tracker.build_particles(delta=1e-3, x=[-1e-3, 0, 1e-3])
-        tracker.track(particles, num_turns=10)
+        particles = line.build_particles(delta=1e-3, x=[-1e-3, 0, 1e-3])
+        line.track(particles, num_turns=10)
         particles.move(_context=xo.context_default)
         assert np.allclose(particles.delta, 1e-3, rtol=0, atol=1e-12)
         assert np.allclose(particles.zeta, 0, rtol=9, atol=1e-12)
 
         # Twiss with frozen longitudinal coordinates (needs to be 4d)
-        twiss = tracker.twiss(method='4d')
+        twiss = line.twiss(method='4d')
         assert twiss.slip_factor == 0
 
     # Track some particles with unfrozen longitudinal coordinates
-    particles = tracker.build_particles(delta=1e-3, x=[-1e-3, 0, 1e-3])
-    tracker.track(particles, num_turns=10)
+    particles = line.build_particles(delta=1e-3, x=[-1e-3, 0, 1e-3])
+    line.track(particles, num_turns=10)
     particles.move(_context=xo.context_default)
     assert np.allclose(particles.delta, 0.00099218, rtol=0, atol=1e-6)
 
     # Twiss with unfrozen longitudinal coordinates (can be 6d)
-    twiss = tracker.twiss(method='6d')
+    twiss = line.twiss(method='6d')
     assert np.isclose(twiss.slip_factor, 0.00032151, rtol=0, atol=1e-6)
 
 
@@ -110,25 +110,25 @@ def test_freeze_longitudinal_individual_methods(test_context):
     line.particle_ref = xp.Particles.from_dict(line_dict['particle'])
 
     # Build the tracker
-    tracker = line.build_tracker(_context=test_context)
+    line.build_tracker(_context=test_context)
 
     # Track some particles with frozen longitudinal coordinates
-    particles = tracker.build_particles(delta=1e-3, x=[-1e-3, 0, 1e-3])
-    tracker.track(particles, num_turns=10, freeze_longitudinal=True)
+    particles = line.build_particles(delta=1e-3, x=[-1e-3, 0, 1e-3])
+    line.track(particles, num_turns=10, freeze_longitudinal=True)
     particles.move(_context=xo.context_default)
     assert np.allclose(particles.delta, 1e-3, rtol=0, atol=1e-12)
     assert np.allclose(particles.zeta, 0, rtol=9, atol=1e-12)
 
     # Twiss with frozen longitudinal coordinates (needs to be 4d)
-    twiss = tracker.twiss(method='4d', freeze_longitudinal=True)
+    twiss = line.twiss(method='4d', freeze_longitudinal=True)
     assert twiss.slip_factor == 0
 
     # Track some particles with unfrozen longitudinal coordinates
-    particles = tracker.build_particles(delta=1e-3, x=[-1e-3, 0, 1e-3])
-    tracker.track(particles, num_turns=10)
+    particles = line.build_particles(delta=1e-3, x=[-1e-3, 0, 1e-3])
+    line.track(particles, num_turns=10)
     particles.move(_context=xo.context_default)
     assert np.allclose(particles.delta, 0.00099218, rtol=0, atol=1e-6)
 
     # Twiss with unfrozen longitudinal coordinates (can be 6d)
-    twiss = tracker.twiss(method='6d')
+    twiss = line.twiss(method='6d')
     assert np.isclose(twiss.slip_factor, 0.00032151, rtol=0, atol=1e-6)
