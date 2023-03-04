46581dd55565cf40369f585c456a3589a1e86a80
giadarol <giovanni.iadarola@cern.ch>
2023-03-04 11:42:24 +0100

    Adapted examples to use line instead of tracker

--- examples/knobs/002_from_json.py
+++ examples/knobs/002_from_json.py
@@ -8,19 +8,15 @@ import numpy as np
 
 import xtrack as xt
 
-with open('status.json', 'r') as fid:
-    dct = json.load(fid)
-
-line = xt.Line.from_dict(dct)
-
-tracker = xt.Tracker(line=line)
+line = xt.Line.from_json('status.json')
+line.build_tracker()
 
 line.vars['on_x1'] = 250
-assert np.isclose(tracker.twiss(at_elements=['ip1'])['px'][0], 250e-6,
+assert np.isclose(line.twiss(at_elements=['ip1'])['px'][0], 250e-6,
                   atol=1e-6, rtol=0)
 
 line.vars['on_x1'] = -300
-assert np.isclose(tracker.twiss(at_elements=['ip1'])['px'][0], -300e-6,
+assert np.isclose(line.twiss(at_elements=['ip1'])['px'][0], -300e-6,
                   atol=1e-6, rtol=0)
 
 manager = line._var_management['manager']
