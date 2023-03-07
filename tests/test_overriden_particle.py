51d91204af711bb4f41e79bf22c044ddb454d836
Szymon Łopaciuk <szymon@lopaciuk.eu>
2023-03-07 17:10:37 +0100

    complete the cavity test for purely longitudinal studies
    Signed-off-by: Szymon Łopaciuk <szymon@lopaciuk.eu>

--- tests/test_overriden_particle.py
+++ tests/test_overriden_particle.py
@@ -13,8 +13,19 @@ from xobjects.test_helpers import for_all_test_contexts
 
 
 @for_all_test_contexts
-def test_overriden_particle(test_context):
-    p = xp.ParticlesFixed()
+def test_purely_longitudinal(test_context):
+    p_fixed = xp.ParticlesFixed(p0c=[1, 2, 3], delta=[3, 2, 1])
+    p = xp.Particles(p0c=[1, 2, 3], delta=[3, 2, 1], x=[1, 2, 3])
+
     l = xt.Line(elements=[xt.Cavity(voltage=1e6, frequency=1e6)])
     t = xt.Tracker(line=l, compile=False, _context=test_context)
+
+    t.track(p_fixed)
     t.track(p)
+
+    d_fixed = p_fixed.to_dict()
+    d = {k: v for k, v in p.to_dict().items() if k in d_fixed}
+
+    assert d.keys() == d_fixed.keys()
+    for k in d.keys():
+        assert np.allclose(d[k], d_fixed[k])
