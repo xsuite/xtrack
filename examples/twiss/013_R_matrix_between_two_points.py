9e3900597b093e4033cffa235a0d4165000b9b22
giadarol <giovanni.iadarola@cern.ch>
2023-03-09 12:06:17 +0100

    A check

--- examples/twiss/013_R_matrix_between_two_points.py
+++ examples/twiss/013_R_matrix_between_two_points.py
@@ -20,11 +20,12 @@ tw = line.twiss()
 R_IP3_IP6 = tw.get_R_matrix(ele_start=0, ele_stop='ip6')
 R_IP6_IP3 = tw.get_R_matrix(ele_start='ip6', ele_stop=len(tw.name)-1)
 
+# Checks
 R_prod = R_IP6_IP3 @ R_IP3_IP6
 
-# Checks
 from xtrack.linear_normal_form import compute_linear_normal_form
 eig = np.linalg.eig
+norm = np.linalg.norm
 
 R_matrix = tw.R_matrix
 
@@ -34,6 +35,47 @@ W_prod, invW_prod, Rot_prod = compute_linear_normal_form(R_prod, symplectify=Tru
 for i_mode in range(3):
     lam_ref = eig(Rot_ref[2*i_mode:2*i_mode+2, 2*i_mode:2*i_mode+2])[0][0]
     lam_prod = eig(Rot_prod[2*i_mode:2*i_mode+2, 2*i_mode:2*i_mode+2])[0][0]
+
     assert np.isclose(np.abs(np.angle(lam_ref)) / 2 / np.pi,
                       np.abs(np.angle(lam_prod)) / 2 / np.pi,
                       rtol=0, atol=1e-6)
+
+    assert np.isclose(
+        norm(W_prod[:, 2*i_mode] - W_ref[:, 2*i_mode], ord=2)/norm(W_ref[:, 2*i_mode], ord=2),
+        0, rtol=0, atol=2e-4)
+
+# Check method=4d
+
+tw4d = line.twiss(method='4d')
+
+R_IP3_IP6_4d = tw4d.get_R_matrix(ele_start=0, ele_stop='ip6')
+R_IP6_IP3_4d = tw4d.get_R_matrix(ele_start='ip6', ele_stop=len(tw4d.name)-1)
+
+R_prod_4d = R_IP6_IP3_4d @ R_IP3_IP6_4d
+
+# Checks
+from xtrack.linear_normal_form import compute_linear_normal_form
+eig = np.linalg.eig
+norm = np.linalg.norm
+
+R_matrix_4d = tw4d.R_matrix
+
+W_ref_4d, invW_ref_4d, Rot_ref_4d = compute_linear_normal_form(
+    R_matrix_4d, symplectify=True, only_4d_block=True)
+W_prod_4d, invW_prod_4d, Rot_prod_4d = compute_linear_normal_form(
+    R_prod_4d, symplectify=True, only_4d_block=True)
+
+for i_mode in range(3):
+    lam_ref_4d = eig(Rot_ref_4d[2*i_mode:2*i_mode+2, 2*i_mode:2*i_mode+2])[0][0]
+    lam_prod_4d = eig(Rot_prod_4d[2*i_mode:2*i_mode+2, 2*i_mode:2*i_mode+2])[0][0]
+
+    assert np.isclose(np.abs(np.angle(lam_ref_4d)) / 2 / np.pi,
+                      np.abs(np.angle(lam_prod_4d)) / 2 / np.pi,
+                      rtol=0, atol=1e-6)
+
+    assert np.isclose(
+        norm(W_prod_4d[:, 2*i_mode] - W_ref_4d[:, 2*i_mode], ord=2)/norm(W_ref_4d[:, 2*i_mode], ord=2),
+        0, rtol=0, atol=2e-4)
+
+
+
