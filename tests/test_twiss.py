addfe3e90fb81a2474d020b2f41394b0c6d4c2ff
giadarol <giovanni.iadarola@cern.ch>
2023-03-18 13:39:20 +0100

    Added test

--- tests/test_twiss.py
+++ tests/test_twiss.py
@@ -352,3 +352,31 @@ def test_get_R_matrix():
             0, rtol=0, atol=5e-5)
 
 
+def test_hide_thin_groups():
+
+    line = xt.Line.from_json(test_data_folder /
+                                        'lhc_no_bb/line_and_particle.json')
+    line.particle_ref = xp.Particles(
+                        mass0=xp.PROTON_MASS_EV, q0=1, energy0=7e12)
+    line.build_tracker()
+
+    tw = line.twiss()
+    tw_htg = line.twiss(hide_thin_groups=True)
+
+    for nn in ('x y px py zeta delta ptau '
+            'betx bety alfx alfy gamx gamy dx dy dpx dpy').split():
+        assert np.isnan(tw_htg[nn]).sum() == 2281
+        assert np.isnan(tw[nn]).sum() == 0
+
+        # Check in presence of a srotation
+        assert tw.name[11197] == 'mbxws.1r8_pretilt'
+        assert tw.name[11198] == 'mbxws.1r8'
+        assert tw.name[11199] == 'mbxws.1r8_posttilt'
+
+        assert tw_htg[nn][11197] == tw[nn][11197]
+        assert np.isnan(tw_htg[nn][11198])
+        assert np.isnan(tw_htg[nn][11199])
+        assert tw_htg[nn][11200] == tw[nn][11200]
+
+
+
