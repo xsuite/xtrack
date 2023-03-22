feeaf11a940a775f1f6cd4b38df0d6579eeabd13
giadarol <giovanni.iadarola@cern.ch>
2023-03-22 21:52:20 +0100

    Check on linear rescale

--- examples/footprint/001_footpring_checks.py
+++ examples/footprint/001_footpring_checks.py
@@ -116,17 +116,17 @@ assert np.allclose(np.diff(fp1_jgrid.Jx_grid), np.diff(fp1_jgrid.Jx_grid)[0],
 assert np.allclose(np.diff(fp1_jgrid.Jy_grid), np.diff(fp1_jgrid.Jy_grid)[0],
                      rtol=0, atol=1e-10)
 
-assert np.isclose(fp1_jgrid.x_norm_2d[0, 0], 0.1, rtol=0, atol=1e-10)
-assert np.isclose(fp1_jgrid.y_norm_2d[0, 0], 0.1, rtol=0, atol=1e-10)
+assert np.isclose(fp1_jgrid.x_norm_2d[0, 0], 0.01, rtol=0, atol=1e-10)
+assert np.isclose(fp1_jgrid.y_norm_2d[0, 0], 0.01, rtol=0, atol=1e-10)
 assert np.isclose(fp1_jgrid.qx[0, 0], 0.31, rtol=0, atol=5e-5)
 assert np.isclose(fp1_jgrid.qy[0, 0], 0.32, rtol=0, atol=5e-5)
 
 assert np.isclose(fp1_jgrid.x_norm_2d[0, -1], 6, rtol=0, atol=1e-10)
-assert np.isclose(fp1_jgrid.y_norm_2d[0, -1], 0.1, rtol=0, atol=1e-10)
+assert np.isclose(fp1_jgrid.y_norm_2d[0, -1], 0.01, rtol=0, atol=1e-10)
 assert np.isclose(fp1_jgrid.qx[0, -1], 0.3121, rtol=0, atol=1e-4)
 assert np.isclose(fp1_jgrid.qy[0, -1], 0.3189, rtol=0, atol=1e-4)
 
-assert np.isclose(fp1_jgrid.x_norm_2d[-1, 0], 0.1, rtol=0, atol=1e-10)
+assert np.isclose(fp1_jgrid.x_norm_2d[-1, 0], 0.01, rtol=0, atol=1e-10)
 assert np.isclose(fp1_jgrid.y_norm_2d[-1, 0], 6, rtol=0, atol=1e-10)
 assert np.isclose(fp1_jgrid.qx[-1, 0], 0.3089, rtol=0, atol=1e-4)
 assert np.isclose(fp1_jgrid.qy[-1, 0], 0.3221, rtol=0, atol=1e-4)
@@ -136,6 +136,48 @@ assert np.isclose(fp1_jgrid.y_norm_2d[-1, -1], 6, rtol=0, atol=1e-10)
 assert np.isclose(fp1_jgrid.qx[-1, -1], 0.3111, rtol=0, atol=1e-4)
 assert np.isclose(fp1_jgrid.qy[-1, -1], 0.3210, rtol=0, atol=1e-4)
 
+assert np.isclose(np.max(fp1_jgrid.qx[:]) - np.min(fp1_jgrid.qx[:]), 0.0032,
+                    rtol=0, atol=1e-4)
+assert np.isclose(np.max(fp1_jgrid.qy[:]) - np.min(fp1_jgrid.qy[:]), 0.0032,
+                    rtol=0, atol=1e-4)
+
+x_norm_range = [1, 6]
+y_norm_range = [1, 6]
+line.vars['i_oct_b1'] = 50000 # Particles are lost for such high octupole current
+fp50k = line.get_footprint(nemitt_x=nemitt_x, nemitt_y=nemitt_y,
+                            x_norm_range=x_norm_range, y_norm_range=y_norm_range,
+                            n_x_norm=9, n_y_norm=8,
+                            mode='uniform_action_grid',
+                            linear_rescale_on_knobs=xt.LinearRescale(
+                                knob_name='i_oct_b1', v0=500, dv=100))
+
+
+line.vars['i_oct_b1'] = 60000 # Particles are lost for such high octupole current
+fp60k = line.get_footprint(nemitt_x=nemitt_x, nemitt_y=nemitt_y,
+                            x_norm_range=x_norm_range, y_norm_range=y_norm_range,
+                            n_x_norm=9, n_y_norm=8,
+                            mode='uniform_action_grid',
+                            linear_rescale_on_knobs=xt.LinearRescale(
+                            knob_name='i_oct_b1', v0=500, dv=100))
+
+line.vars['i_oct_b1'] = 500
+fp500 = line.get_footprint(nemitt_x=nemitt_x, nemitt_y=nemitt_y,
+                            x_norm_range=x_norm_range, y_norm_range=y_norm_range,
+                            n_x_norm=9, n_y_norm=8,
+                            mode='uniform_action_grid')
+
+line.vars['i_oct_b1'] = 600
+fp600 = line.get_footprint(nemitt_x=nemitt_x, nemitt_y=nemitt_y,
+                            x_norm_range=x_norm_range, y_norm_range=y_norm_range,
+                            n_x_norm=9, n_y_norm=8,
+                            mode='uniform_action_grid')
+
+assert np.allclose((fp60k.qx - fp50k.qx)/(fp600.qx-fp500.qx), 100, rtol=0, atol=1e-2)
+assert np.allclose((fp60k.qy - fp50k.qy)/(fp600.qy-fp500.qy), 100, rtol=0, atol=1e-2)
+
+
+
+fp5k.plot(color='r', label='I_oct=500')
 
 
 plt.legend()
