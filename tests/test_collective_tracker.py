ea12142a8c2da931ea5e3ae71230616546fe0984
giadarol <giovanni.iadarola@cern.ch>
2023-04-01 15:53:15 +0200

    Move check to test suite

--- tests/test_collective_tracker.py
+++ tests/test_collective_tracker.py
@@ -110,3 +110,66 @@ def test_collective_tracker(test_context):
               f'    xtrack: {xt_value: .7e}\n')
         if not passed:
             raise ValueError
+
+@for_all_test_contexts
+def test_get_non_collective_line(test_context):
+
+    line = xt.Line(
+        elements=[xt.Drift(length=1) for i in range(8)],
+        element_names=[f'e{i}' for i in range(8)]
+    )
+    line['e3'].iscollective = True
+    e3_buffer = line['e3']._buffer
+    e3 = line['e3']
+
+    try:
+        line.iscollective
+    except RuntimeError:
+        pass
+    else:
+        raise ValueError('This should have failed')
+
+    try:
+        line._buffer
+    except RuntimeError:
+        pass
+    else:
+        raise ValueError('This should have failed')
+
+    line.build_tracker(_context=test_context)
+
+    assert line.iscollective == True
+    assert line['e0']._buffer is line._buffer
+    assert line['e7']._buffer is line._buffer
+    assert line['e3']._buffer is not line._buffer
+    assert line['e3']._buffer is e3_buffer
+    assert line['e3'] is e3
+    assert line.tracker.line is line
+
+    nc_line = line._get_non_collective_line()
+
+    # Check that the original line is untouched
+    assert line.iscollective == True
+    assert line['e0']._buffer is line._buffer
+    assert line['e7']._buffer is line._buffer
+    assert line['e3']._buffer is not line._buffer
+    assert line['e3']._buffer is e3_buffer
+    assert line['e3'] is e3
+    assert line.tracker.line is line
+
+    assert nc_line.iscollective == False
+    assert nc_line._buffer is line._buffer
+    assert nc_line['e0']._buffer is line._buffer
+    assert nc_line['e7']._buffer is line._buffer
+    assert nc_line['e3']._buffer is line._buffer
+    assert nc_line['e3'] is not e3
+    assert nc_line['e0'] is line['e0']
+    assert nc_line['e7'] is line['e7']
+    assert nc_line.tracker.line is nc_line
+
+    assert np.allclose(nc_line.get_s_elements(), line.get_s_elements(),
+                    rtol=0, atol=1e-15)
+
+    assert nc_line.tracker is not line.tracker
+    assert nc_line.tracker._tracker_data is line.tracker._tracker_data
+    assert line.tracker._track_kernel is nc_line.tracker._track_kernel
