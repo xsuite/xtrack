2e294c30b23ca71f6d4c59a57081e7bc85932697
giadarol <giovanni.iadarola@cern.ch>
2023-04-15 20:56:00 +0200

    Add checks on longitudinal quantities

--- examples/transfer_matrix/002_check_with_twiss.py
+++ examples/transfer_matrix/002_check_with_twiss.py
@@ -18,8 +18,10 @@ dx = [10, 0]
 dy = [0, 20]
 dpx = [0.7, -0.3]
 dpy = [0.4, -0.6]
+beta_s = 1e-3
 
-segm_1 = xt.LinearTransferMatrix(Q_x=0.4, Q_y=0.3, Q_s=0.0001, length=0.1,
+segm_1 = xt.LinearTransferMatrix(Q_x=0.4, Q_y=0.3, Q_s=0.0001, 
+                                 beta_s = beta_s, length=0.1,
                                  beta_x_0=betx[0], beta_x_1=betx[1],
                                  beta_y_0=bety[0], beta_y_1=bety[1],
                                  alpha_x_0=alfx[0], alpha_x_1=alfx[1],
@@ -32,7 +34,8 @@ segm_1 = xt.LinearTransferMatrix(Q_x=0.4, Q_y=0.3, Q_s=0.0001, length=0.1,
                                  px_ref_0=px_co[0], px_ref_1=px_co[1],
                                  y_ref_0=y_co[0], y_ref_1=y_co[1],
                                  py_ref_0=py_co[0], py_ref_1=py_co[1])
-segm_2 = xt.LinearTransferMatrix(Q_x=0.21, Q_y=0.32, Q_s=0.0003, length=0.2,
+segm_2 = xt.LinearTransferMatrix(Q_x=0.21, Q_y=0.32, Q_s=0.0003,
+                                 beta_s = beta_s, length=0.2,
                                  chroma_x=2., chroma_y=3.,
                                  beta_x_0=betx[1], beta_x_1=betx[0],
                                  beta_y_0=bety[1], beta_y_1=bety[0],
@@ -52,6 +55,9 @@ line.build_tracker()
 
 tw4d = line.twiss(method='4d')
 tw6d = line.twiss()
+
+assert np.isclose(tw6d.qs, 0.0004, atol=1e-7, rtol=0)
+assert np.isclose(tw6d.betz0, 1e-3, atol=1e-7, rtol=0)
 
 for tw in [tw4d, tw6d]:
 
