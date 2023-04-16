56ea607f56707e124add911bf3e7f4e9c9fa2263
giadarol <giovanni.iadarola@cern.ch>
2023-04-16 11:41:49 +0200

    Preparing examples for doc

--- examples/twiss/011_tune_vs_delta.py
+++ examples/twiss/011_tune_vs_delta.py
@@ -20,23 +20,25 @@ line.build_tracker()
 tw = line.twiss()
 
 delta_values = np.linspace(-5e-3, 5e-3, 100)
+
 qx_values = delta_values * 0
 qy_values = delta_values * 0
-
-qx_values_mad = delta_values * 0
-qy_values_mad = delta_values * 0
-
 for i, delta in enumerate(delta_values):
-    print(f'Working on {i} of {len(delta_values)}')
+    print(f'Xsuite working on {i} of {len(delta_values)}  ', end='\r', flush=True)
     tt = line.twiss(method='4d', delta0=delta)
+
     qx_values[i] = tt.qx
     qy_values[i] = tt.qy
 
-    #mad.twiss(deltap=delta)
+#!end-doc-part
+
+qx_values_mad = delta_values * 0
+qy_values_mad = delta_values * 0
+for i, delta in enumerate(delta_values):
+    print(f'Working on {i} of {len(delta_values)}   ' , end='\r', flush=True)
     mad.input(f'twiss, deltap={delta};')
     qx_values_mad[i] = mad.table.summ.q1
     qy_values_mad[i] = mad.table.summ.q2
-
 
 import matplotlib.pyplot as plt
 plt.close('all')
@@ -54,6 +56,17 @@ plt.plot(delta_values, qy_values_mad, label='madx')
 plt.xlabel(r'$\delta$')
 plt.ylabel(r'$Q_y$')
 plt.legend()
+
+# Figure with Xsuite only
+fig2 = plt.figure(2)
+plt.subplot(211)
+plt.plot(delta_values, qx_values, '.-', label='xtrack')
+plt.ylabel(r'$Q_x$')
+
+plt.subplot(212)
+plt.plot(delta_values, qy_values, '.-', label='xtrack')
+plt.xlabel(r'$\delta$')
+plt.ylabel(r'$Q_y$')
 
 plt.show()
 
