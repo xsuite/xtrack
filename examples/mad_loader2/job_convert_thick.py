7da9bfd373472639a52f7f8c91348c87656873be
Szymon Łopaciuk <szymon@lopaciuk.eu>
2023-05-02 16:47:39 +0200

    update example
    Signed-off-by: Szymon Łopaciuk <szymon@lopaciuk.eu>

--- examples/mad_loader2/job_convert_thick.py
+++ examples/mad_loader2/job_convert_thick.py
@@ -11,16 +11,40 @@ import xpart as xp
 
 
 
-from xtrack.mad_loader import MadLoader, MadElem
+from xtrack.mad_loader import MadLoader, MadElem, TeapotSlicing
 
 ml = MadLoader(mad.sequence.lhcb1)
-ml.slicing_strategies=[(None,None,xt.mad_loader.UniformSlicing(10))]
+
+ml.slicing_strategies = [
+    ml.make_slicing_strategy(
+        name_regex=r'(mqt|mqtli|mqtlh)\..*',
+        slicing_strategy=TeapotSlicing(2),
+    ),
+    ml.make_slicing_strategy(
+        name_regex=r'(mbx|mbrb|mbrc|mbrs|mbh|mqwa|mqwb|mqy|mqm|mqmc|mqml)\..*',
+        slicing_strategy=TeapotSlicing(4),
+    ),
+    ml.make_slicing_strategy(
+        madx_type='mqxb',
+        slicing_strategy=TeapotSlicing(16),
+    ),
+    ml.make_slicing_strategy(
+        madx_type='mqxa',
+        slicing_strategy=TeapotSlicing(16),
+    ),
+    ml.make_slicing_strategy(
+        madx_type='mq',
+        slicing_strategy=TeapotSlicing(2),
+    ),
+    ml.make_slicing_strategy(
+        madx_type='mb',
+        slicing_strategy=TeapotSlicing(2),
+    ),
+    ml.make_slicing_strategy(TeapotSlicing(1)),  # Default catch-all as in MAD-X
+]
 line=ml.make_line()
 
 line.particle_ref = xp.Particles(mass0=xp.PROTON_MASS_EV, q0=1, energy0=7e12)
 
 line.build_tracker()
 tw = line.twiss(method='4d')
-
-list(line.element_dict.items())[:200]
-
