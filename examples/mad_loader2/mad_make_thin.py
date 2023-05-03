427975b5d7a11e7d61ead0100a07bdd6135ed29d
Szymon Łopaciuk <szymon@lopaciuk.eu>
2023-05-03 11:40:51 +0200

    save examples

--- examples/mad_loader2/mad_make_thin.py
+++ examples/mad_loader2/mad_make_thin.py
@@ -1,6 +1,8 @@
 import xtrack as xt
 import xpart as xp
 from cpymad.madx import Madx
+import matplotlib.pyplot as plt
+from xtrack.mad_loader import MadLoader
 
 mad = Madx()
 mad.call('lhc_sequence.madx')
@@ -9,9 +11,13 @@ mad.call('slice.madx')
 mad.beam()
 mad.sequence.lhcb1.use()
 
-line = xt.Line.from_madx_sequence(mad.sequence.lhcb1)
+ml = MadLoader(mad.sequence.lhcb1)
+ml.slicing_strategies = []
+line = ml.make_line()
 line.particle_ref = xp.Particles(mass0=xp.PROTON_MASS_EV, q0=1, energy0=7e12)
 
 line.build_tracker()
 tw = line.twiss(method='4d')
 
+plt.plot(tw.s, tw.x)
+plt.show()
