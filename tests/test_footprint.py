89f09ab2cee8b4bc656b064798c3198bfc800f8b
giadarol <giovanni.iadarola@cern.ch>
2023-05-06 10:59:41 +0200

    Test for off-momentum footprint

--- tests/test_footprint.py
+++ tests/test_footprint.py
@@ -185,3 +185,35 @@ def test_footprint(test_context, freeze_longitudinal):
 
     assert np.allclose((fp60k.qx - fp50k.qx)/(fp600.qx-fp500.qx), 100, rtol=0, atol=1e-2)
     assert np.allclose((fp60k.qy - fp50k.qy)/(fp600.qy-fp500.qy), 100, rtol=0, atol=1e-2)
+
+@for_all_test_contexts
+def test_footprint_delta0(test_context):
+
+    nemitt_x = 1e-6
+    nemitt_y = 1e-6
+
+    line = xt.Line.from_json(test_data_folder /
+                        'hllhc15_noerrors_nobb/line_w_knobs_and_particle.json')
+    line.particle_ref = xp.Particles(mass0=xp.PROTON_MASS_EV, p0c=7e12)
+    line.build_tracker(_context=test_context)
+
+    # Some octupoles and chromaticity to see the footprint moving
+    line.vars['i_oct_b1'] = 500
+    line.match(
+        targets=[xt.TargetList(['dqx', 'dqy'], value=10, tol=0.01)],
+        vary=[xt.VaryList(['ksf.b1', 'ksd.b1'], step=1e-3)])
+
+    # Compute and plot footprint on momentum
+    fp1 = line.get_footprint(nemitt_x=nemitt_x, nemitt_y=nemitt_y,
+                            freeze_longitudinal=True)
+
+    # Compute and plot footprint off momentum
+    fp2 = line.get_footprint(nemitt_x=nemitt_x, nemitt_y=nemitt_y,
+                            freeze_longitudinal=True, delta0=1e-4)
+
+    assert np.allclose(line.record_last_track.delta[:], 1e-4, atol=1e-12, rtol=0)
+
+    # It is 12, 9 instead of 10, 10 because of non-linear chromaticity
+    assert np.isclose((np.max(fp2.qx) - np.max(fp1.qx))/1e-4, 12, atol=0.5, rtol=0)
+    assert np.isclose((np.max(fp2.qy) - np.max(fp1.qy))/1e-4, 9, atol=0.5, rtol=0)
+
