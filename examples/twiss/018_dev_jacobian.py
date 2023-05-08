83f5b3abb528d825eed776d9a45fff4e6ebb3fa7
giadarol <giovanni.iadarola@cern.ch>
2023-05-08 11:26:17 +0200

    Tune and chromaticity!

--- examples/twiss/018_dev_jacobian.py
+++ examples/twiss/018_dev_jacobian.py
@@ -22,6 +22,7 @@ print('\nInitial twiss parameters')                                             
 tw_before = line.twiss()
 
 print(f"Qx = {tw_before['qx']:.5f} Qy = {tw_before['qy']:.5f} ")
+print(f"Q'x = {tw_before['dqx']:.5f} Q'y = {tw_before['dqy']:.5f}")
 
 # Initial value of the knobs correcting tunes an chromaticities
 print(f"kqtf.b1 = {line.vars['kqtf.b1']._value}")
@@ -35,10 +36,15 @@ line.match(
     vary=[
         xt.Vary('kqtf.b1', step=1e-8),
         xt.Vary('kqtd.b1', step=1e-8),
+        xt.Vary('ksf.b1', step=1e-8),
+        xt.Vary('ksd.b1', step=1e-8),
     ],
     targets = [
         xt.Target('qx', 62.315, tol=1e-4),
-        xt.Target('qy', 60.325, tol=1e-4)])
+        xt.Target('qy', 60.325, tol=1e-4),
+        xt.Target('dqx', 10.0, tol=0.05),
+        xt.Target('dqy', 12.0, tol=0.05)]
+    )
 t2 = time.time()                                                                #!skip-doc
 print('\nTime match: ', t2 - t1)
 
@@ -46,6 +52,7 @@ tw_after = line.twiss()
 
 print('\nFinal twiss parameters')                                               #!skip-doc
 print(f"Qx = {tw_after['qx']:.5f} Qy = {tw_after['qy']:.5f} ")
+print(f"Q'x = {tw_after['dqx']:.5f} Q'y = {tw_after['dqy']:.5f}")
 
 # Initial value of the knobs correcting tunes an chromaticities
 print(f"kqtf.b1 = {line.vars['kqtf.b1']._value}")
