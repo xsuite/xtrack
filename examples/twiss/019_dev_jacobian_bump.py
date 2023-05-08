ace6ebd33045aa68fe0437ad04c249ab269af85f
giadarol <giovanni.iadarola@cern.ch>
2023-05-08 11:54:10 +0200

    First rectangular problem

--- examples/twiss/019_dev_jacobian_bump.py
+++ examples/twiss/019_dev_jacobian_bump.py
@@ -17,24 +17,23 @@ line.match(
     solver='jacobian',
     # Portion of the beam line to be modified and initial conditions
     ele_start='mq.33l8.b1',
-    ele_stop='mq.23l8.b1',
-    twiss_init=tw_before.get_twiss_init(at_element='mq.33l8.b1'),
+    ele_stop='mq.21l8.b1',
+    twiss_init='preserve',
     # Dipole corrector strengths to be varied
     vary=[
-        xt.Vary(name='acbv30.l8b1', step=1e-10),
+        xt.Vary(name='acbv32.l8b1', step=1e-10),
         xt.Vary(name='acbv28.l8b1', step=1e-10),
         xt.Vary(name='acbv26.l8b1', step=1e-10),
         xt.Vary(name='acbv24.l8b1', step=1e-10),
+        xt.Vary(name='acbv22.l8b1', step=1e-10),
     ],
     targets=[
         # I want the vertical orbit to be at 3 mm at mq.28l8.b1 with zero angle
-        xt.Target('y', at='mb.b28l8.b1', value=3e-3, tol=1e-4, scale=1),
-        xt.Target('py', at='mb.b28l8.b1', value=0, tol=1e-6, scale=1000),
+        xt.Target('y', at='mb.b26l8.b1', value=3e-3, tol=1e-4, scale=1),
+        xt.Target('py', at='mb.b26l8.b1', value=0, tol=1e-6, scale=1000),
         # I want the bump to be closed
-        xt.Target('y', at='mq.23l8.b1', value=tw_before['y', 'mq.23l8.b1'],
-                  tol=1e-6, scale=1),
-        xt.Target('py', at='mq.23l8.b1', value=tw_before['py', 'mq.23l8.b1'],
-                  tol=1e-7, scale=1000),
+        xt.Target('y', at='mq.21l8.b1', value='preserve', tol=1e-6, scale=1),
+        xt.Target('py', at='mq.21l8.b1', value='preserve', tol=1e-7, scale=1000),
     ]
 )
 
@@ -48,17 +47,21 @@ fig = plt.figure(1, figsize=(6.4*1.2, 4.8*0.8))
 ax = fig.add_subplot(111)
 ax.plot(tw_before.s, tw_before.y*1000, label='y')
 ax.plot(tw.s, tw.y*1000, label='y')
-ax.axvline(x=line.get_s_position('mb.b28l8.b1'), color='r', linestyle='--', alpha=0.5)
-ax.axvline(x=line.get_s_position('mcbv.30l8.b1'), color='k', linestyle='--', alpha=0.5)
+# Target
+ax.axvline(x=line.get_s_position('mb.b26l8.b1'), color='r', linestyle='--', alpha=0.5)
+# Correctors
+ax.axvline(x=line.get_s_position('mcbv.32l8.b1'), color='k', linestyle='--', alpha=0.5)
 ax.axvline(x=line.get_s_position('mcbv.28l8.b1'), color='k', linestyle='--', alpha=0.5)
 ax.axvline(x=line.get_s_position('mcbv.26l8.b1'), color='k', linestyle='--', alpha=0.5)
 ax.axvline(x=line.get_s_position('mcbv.24l8.b1'), color='k', linestyle='--', alpha=0.5)
+ax.axvline(x=line.get_s_position('mcbv.22l8.b1'), color='k', linestyle='--', alpha=0.5)
+# Boundaries
 ax.axvline(x=line.get_s_position('mq.33l8.b1'), color='g', linestyle='--', alpha=0.5)
-ax.axvline(x=line.get_s_position('mq.23l8.b1'), color='g', linestyle='--', alpha=0.5)
+ax.axvline(x=line.get_s_position('mq.21l8.b1'), color='g', linestyle='--', alpha=0.5)
 ax.set_xlim(line.get_s_position('mq.33l8.b1') - 10,
-            line.get_s_position('mq.23l8.b1') + 10)
+            line.get_s_position('mq.21l8.b1') + 10)
 ax.set_xlabel('s [m]')
 ax.set_ylabel('y [mm]')
-ax.set_ylim(-0.5, 10)
+ax.set_ylim(-10, 10)
 plt.subplots_adjust(bottom=.152, top=.9, left=.1, right=.95)
 plt.show()
