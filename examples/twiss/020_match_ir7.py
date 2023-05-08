6d7dd6bab6cb0bed67b2f053e958cf1a78d52ee1
giadarol <giovanni.iadarola@cern.ch>
2023-05-08 22:55:50 +0200

    Prepare for match ir7

---
+++ examples/twiss/020_match_ir7.py
@@ -0,0 +1,30 @@
+import xtrack as xt
+import xpart as xp
+
+# Load the line
+line = xt.Line.from_json(
+    '../../test_data/hllhc15_noerrors_nobb/line_w_knobs_and_particle.json')
+line.particle_ref = xp.Particles(p0c=7e12, mass=xp.PROTON_MASS_EV)
+line.build_tracker()
+
+tw_ref = line.twiss()
+
+ele_start_match = 's.ds.l7.b1'
+ele_end_match = 'e.ds.r7.b1'
+tw_init = tw_ref.get_twiss_init(ele_start_match)
+
+betx_end_match = tw_ref['betx', ele_end_match]
+bety_end_match = tw_ref['bety', ele_end_match]
+alfx_end_match = tw_ref['alfx', ele_end_match]
+alfy_end_match = tw_ref['alfy', ele_end_match]
+dx_end_match = tw_ref['dx', ele_end_match]
+dpx_end_match = tw_ref['dpx', ele_end_match]
+mux_end_match = tw_ref['mux', ele_end_match]
+muy_end_match = tw_ref['muy', ele_end_match]
+
+betx_at_ip7 = tw_ref['betx', 'ip7']
+bety_at_ip7 = tw_ref['bety', 'ip7']
+alfx_at_ip7 = tw_ref['alfx', 'ip7']
+alfy_at_ip7 = tw_ref['alfy', 'ip7']
+dx_at_ip7 = tw_ref['dx', 'ip7']
+dpx_at_ip7 = tw_ref['dpx', 'ip7']
