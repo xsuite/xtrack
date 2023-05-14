a9f1c7e8926766bf37579ed657c8135f4bc701c5
giadarol <giovanni.iadarola@cern.ch>
2023-05-14 15:56:23 +0200

    Clean up

--- examples/twiss/019a_dev_jacobian_bump_backtwiss.py
+++ examples/twiss/019a_dev_jacobian_bump_backtwiss.py
@@ -56,6 +56,7 @@ fig = plt.figure(1, figsize=(6.4*1.2, 4.8*0.8))
 ax = fig.add_subplot(111)
 ax.plot(tw_before.s, tw_before.y*1000, label='y')
 ax.plot(tw.s, tw.y*1000, label='y')
+
 # Target
 ax.axvline(x=line.get_s_position('mb.b26l8.b1'), color='r', linestyle='--', alpha=0.5)
 # Correctors
@@ -69,6 +70,7 @@ ax.axvline(x=line.get_s_position('mcbv.18l8.b1'), color='k', linestyle='--', alp
 ax.axvline(x=line.get_s_position('mq.33l8.b1'), color='g', linestyle='--', alpha=0.5)
 ax.axvline(x=line.get_s_position('mq.17l8.b1'), color='g', linestyle='--', alpha=0.5)
 ax.axhline(y=-1, color='b', linestyle='--', alpha=0.5)
+
 ax.set_xlim(line.get_s_position('mq.33l8.b1') - 10,
             line.get_s_position('mq.17l8.b1') + 10)
 ax.set_xlabel('s [m]')
