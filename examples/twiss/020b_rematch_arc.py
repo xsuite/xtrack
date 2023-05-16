5fa82bc0a8462c8d0a3ddc11b17dceb2fe4b88f6
giadarol <giovanni.iadarola@cern.ch>
2023-05-16 08:48:10 +0200

    Working on arc match

--- examples/twiss/020b_rematch_arc.py
+++ examples/twiss/020b_rematch_arc.py
@@ -14,15 +14,38 @@ line.particle_ref = xp.Particles(p0c=7e12, mass=xp.PROTON_MASS_EV)
 collider = xt.Multiline(lines={'lhcb1': line})
 collider.build_trackers()
 
-tw = collider.twiss()
+tw = collider.lhcb1.twiss()
+
+start_cell = 's.cell.67.b1'
+end_cell = 'e.cell.67.b1'
+start_arc = 'e.ds.r6.b1'
+end_arc = 'e.ds.l7.b1'
+
+mux_arc_target = tw['mux', end_arc] - tw['mux', start_arc]
+muy_arc_target = tw['muy', end_arc] - tw['muy', start_arc]
 
 tw_cell = collider.lhcb1.twiss(
-    ele_start='s.cell.67.b1',
-    ele_stop='e.cell.67.b1',
+    ele_start=start_cell,
+    ele_stop=end_cell,
     twiss_init='preserve')
 
 tw_cell_periodic = collider.lhcb1.twiss(
     method='4d',
-    ele_start='s.cell.67.b1',
-    ele_stop='e.cell.67.b1',
+    ele_start=start_cell,
+    ele_stop=end_cell,
     twiss_init='periodic')
+
+twinit_start_cell = tw_cell_periodic.get_twiss_init(start_cell)
+
+tw_to_end_arc = collider.lhcb1.twiss(
+    ele_start=start_cell,
+    ele_stop=end_arc,
+    twiss_init=twinit_start_cell)
+
+tw_to_start_arc = collider.lhcb1.twiss(
+    ele_start=start_arc,
+    ele_stop=start_cell,
+    twiss_init=twinit_start_cell)
+
+mux_arc_from_cell = tw_to_end_arc['mux', end_arc] - tw_to_start_arc['mux', start_arc]
+muy_arc_from_cell = tw_to_end_arc['muy', end_arc] - tw_to_start_arc['muy', start_arc]
