d878adcf8b85025c5a82a0c44d582288feba1257
giadarol <giovanni.iadarola@cern.ch>
2023-05-18 19:19:16 +0200

    Periodic twiss also for b2

--- examples/twiss/020b_rematch_arc.py
+++ examples/twiss/020b_rematch_arc.py
@@ -9,24 +9,34 @@ import xtrack as xt
 collider = xt.Multiline.from_json(
     '../../test_data/hllhc15_collider/collider_00_from_mad.json')
 collider.build_trackers()
+
 collider.lhcb1.twiss_default['method'] = '4d'
+collider.lhcb2.twiss_default['method'] = '4d'
+collider.lhcb2.twiss_default['reverse'] = True
 
-start_cell = 's.cell.67.b1'
-end_cell = 'e.cell.67.b1'
-start_arc = 'e.ds.r6.b1'
-end_arc = 'e.ds.l7.b1'
+# line = collider.lhcb1
+# start_cell = 's.cell.67.b1'
+# end_cell = 'e.cell.67.b1'
+# start_arc = 'e.ds.r6.b1'
+# end_arc = 'e.ds.l7.b1'
 
-tw = collider.lhcb1.twiss()
+line = collider.lhcb2
+start_cell = 's.cell.67.b2'
+end_cell = 'e.cell.67.b2'
+start_arc = 'e.ds.r6.b2'
+end_arc = 'e.ds.l7.b2'
+
+tw = line.twiss()
 
 mux_arc_target = tw['mux', end_arc] - tw['mux', start_arc]
 muy_arc_target = tw['muy', end_arc] - tw['muy', start_arc]
 
-tw_cell = collider.lhcb1.twiss(
+tw_cell = line.twiss(
     ele_start=start_cell,
     ele_stop=end_cell,
     twiss_init='preserve')
 
-tw_cell_periodic = collider.lhcb1.twiss(
+tw_cell_periodic = line.twiss(
     method='4d',
     ele_start=start_cell,
     ele_stop=end_cell,
@@ -34,12 +44,12 @@ tw_cell_periodic = collider.lhcb1.twiss(
 
 twinit_start_cell = tw_cell_periodic.get_twiss_init(start_cell)
 
-tw_to_end_arc = collider.lhcb1.twiss(
+tw_to_end_arc = line.twiss(
     ele_start=start_cell,
     ele_stop=end_arc,
     twiss_init=twinit_start_cell)
 
-tw_to_start_arc = collider.lhcb1.twiss(
+tw_to_start_arc = line.twiss(
     ele_start=start_arc,
     ele_stop=start_cell,
     twiss_init=twinit_start_cell)
