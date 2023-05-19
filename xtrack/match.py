9593f6d061de708b8a3c64ab6347ac11c7c5e3c2
giadarol <giovanni.iadarola@cern.ch>
2023-05-19 11:52:57 +0200

    Preallocated monitor handled directly in the twiss

--- xtrack/match.py
+++ xtrack/match.py
@@ -233,16 +233,7 @@ class ActionTwiss(Action):
                 'twiss_init' in kwargs and kwargs['twiss_init'] is not None)
 
         tw0 = line.twiss(_keep_initial_particles=_keep_initial_particles,
-                        _keep_tracking_data=True, **kwargs)
-
-        if isinstance(line, xt.Multiline):
-            ebe_monitor = []
-            for llnn in tw0._line_names:
-                if tw0[llnn].tracking_data is not None:
-                    ebe_monitor.append(tw0[llnn].tracking_data)
-        else:
-            ebe_monitor = tw0.tracking_data
-        kwargs['_ebe_monitor'] = ebe_monitor
+                         **kwargs)
 
         if 'twiss_init' in kwargs and kwargs['twiss_init'] is not None: # open line mode
             if isinstance(line, xt.Multiline):
