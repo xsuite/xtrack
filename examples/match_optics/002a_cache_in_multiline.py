508b5b7b06ae011143d71bfb85829d67250e7fc8
giadarol <giovanni.iadarola@cern.ch>
2023-05-24 10:14:42 +0200

    Code checked for collider and line within collider

--- examples/match_optics/002a_cache_in_multiline.py
+++ examples/match_optics/002a_cache_in_multiline.py
@@ -25,8 +25,10 @@ collider.vars['on_x5'] = 55
 assert collider.vars['on_x1']._value == 11
 assert collider.vars['on_x5']._value == 55
 
-assert np.isclose(collider['lhcb1'].twiss()['px', 'ip1'], 11e-6, atol=1e-9, rtol=0)
-assert np.isclose(collider['lhcb1'].twiss()['py', 'ip5'], 55e-6, atol=1e-9, rtol=0)
+assert np.isclose(
+    collider['lhcb1'].twiss()['px', 'ip1'], 11e-6, atol=1e-9, rtol=0)
+assert np.isclose(
+    collider['lhcb1'].twiss()['py', 'ip5'], 55e-6, atol=1e-9, rtol=0)
 
 vsharing = collider._var_sharing
 collider._var_sharing = None
@@ -37,8 +39,10 @@ collider.vars['on_x5'] = 123
 assert collider.vars['on_x1']._value == 234
 assert collider.vars['on_x5']._value == 123
 
-assert np.isclose(collider['lhcb1'].twiss()['px', 'ip1'], 234e-6, atol=1e-9, rtol=0)
-assert np.isclose(collider['lhcb1'].twiss()['py', 'ip5'], 123e-6, atol=1e-9, rtol=0)
+assert np.isclose(
+    collider['lhcb1'].twiss()['px', 'ip1'], 234e-6, atol=1e-9, rtol=0)
+assert np.isclose(
+    collider['lhcb1'].twiss()['py', 'ip5'], 123e-6, atol=1e-9, rtol=0)
 
 try:
     collider.vars['on_x2'] = 234
@@ -50,5 +54,59 @@ else:
 collider._var_sharing = vsharing
 collider.vars['on_x2'] = 234
 
-assert np.isclose(collider['lhcb1'].twiss()['py', 'ip2'], 234e-6, atol=1e-9, rtol=0)
+assert np.isclose(
+    collider['lhcb1'].twiss()['py', 'ip2'], 234e-6, atol=1e-9, rtol=0)
 
+# Same check on single line within collider
+
+# Load the line
+collider = xt.Multiline.from_json(
+    '../../test_data/hllhc15_collider/collider_00_from_mad.json')
+collider.build_trackers()
+
+collider.lhcb1.twiss_default['method'] = '4d'
+collider.lhcb2.twiss_default['method'] = '4d'
+collider.lhcb2.twiss_default['reverse'] = True
+
+line = collider['lhcb1']
+
+line.vars['on_x2'] = 123
+line.vars.cache_active = True
+
+line.vars['on_x1'] = 11
+line.vars['on_x5'] = 55
+
+assert line.vars['on_x1']._value == 11
+assert line.vars['on_x5']._value == 55
+
+assert np.isclose(
+    line.twiss()['px', 'ip1'], 11e-6, atol=1e-9, rtol=0)
+assert np.isclose(
+    line.twiss()['py', 'ip5'], 55e-6, atol=1e-9, rtol=0)
+
+vsharing = collider._var_sharing
+collider._var_sharing = None
+
+line.vars['on_x1'] = 234
+line.vars['on_x5'] = 123
+
+assert line.vars['on_x1']._value == 234
+assert line.vars['on_x5']._value == 123
+
+assert np.isclose(
+    line.twiss()['px', 'ip1'], 234e-6, atol=1e-9, rtol=0)
+assert np.isclose(
+    line.twiss()['py', 'ip5'], 123e-6, atol=1e-9, rtol=0)
+
+try:
+    line.vars['on_x2'] = 234
+except RuntimeError:
+    pass
+else:
+    raise ValueError('Should have raised RuntimeError')
+
+collider._var_sharing = vsharing
+line.vars['on_x2'] = 234
+
+assert np.isclose(
+    line.twiss()['py', 'ip2'], 234e-6, atol=1e-9, rtol=0)
