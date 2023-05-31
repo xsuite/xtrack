b6809585773fc3a019518b5937ba929888548af7
giadarol <giovanni.iadarola@cern.ch>
2023-05-31 15:06:37 +0200

    Status

--- examples/thick_bend/000_play.py
+++ examples/thick_bend/000_play.py
@@ -3,18 +3,35 @@ import numpy as np
 import xtrack as xt
 import xpart as xp
 
+rho = 20
 
-theta_array = np.linspace(0, 2*np.pi, 100)
+theta_array = np.linspace(1e-10, 2*np.pi, 100)
 x_array = np.zeros_like(theta_array)
 s_array = np.zeros_like(theta_array)
+X0_array = np.zeros_like(theta_array)
+Z0_array = np.zeros_like(theta_array)
+
 for ii, tt in enumerate(theta_array):
 
-    rho = 20
-    theta = 0.1
-    bend = xt.TrueBend(length=rho*theta, h=1e-10, k0=1/rho)
+    theta = tt
+    bend = xt.TrueBend(length=rho*theta, h=1e-6, k0=1/rho)
 
     p = xp.Particles(p0c=1e9)
     bend.track(p)
 
+    X0 = -rho*(1-np.cos(theta))
+    Z0 = rho*np.sin(theta)
+
+    X0_array[ii] = X0
+    Z0_array[ii] = Z0
+
     x_array[ii] = p.x[0]
     s_array[ii] = p.s[0]
+
+import matplotlib.pyplot as plt
+plt.close('all')
+plt.figure(1)
+plt.plot(X0_array, Z0_array)
+plt.axis('equal')
+
+plt.show()
