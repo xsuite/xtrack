38c166a6dff52a00354a892996f2c8aa21ecda29
giadarol <giovanni.iadarola@cern.ch>
2023-05-31 15:21:25 +0200

    Status

--- examples/thick_bend/000_play.py
+++ examples/thick_bend/000_play.py
@@ -11,16 +11,24 @@ s_array = np.zeros_like(theta_array)
 X0_array = np.zeros_like(theta_array)
 Z0_array = np.zeros_like(theta_array)
 
+X_array = np.zeros_like(theta_array)
+Z_array = np.zeros_like(theta_array)
+
 for ii, tt in enumerate(theta_array):
 
     theta = tt
-    bend = xt.TrueBend(length=rho*theta, h=1e-6, k0=1/rho)
+    bend = xt.TrueBend(length=rho*theta, h=1/rho, k0=1/rho)
 
     p = xp.Particles(p0c=1e9)
+    p.x += 10
+    p.px += 0.1
     bend.track(p)
 
     X0 = -rho*(1-np.cos(theta))
     Z0 = rho*np.sin(theta)
+
+    ex_X = np.cos(theta)
+    ex_Z = np.sin(theta)
 
     X0_array[ii] = X0
     Z0_array[ii] = Z0
@@ -28,10 +36,14 @@ for ii, tt in enumerate(theta_array):
     x_array[ii] = p.x[0]
     s_array[ii] = p.s[0]
 
+    X_array[ii] = X0 + p.x[0]*ex_X
+    Z_array[ii] = Z0 + p.x[0]*ex_Z
+
 import matplotlib.pyplot as plt
 plt.close('all')
 plt.figure(1)
-plt.plot(X0_array, Z0_array)
+plt.plot(X0_array, Z0_array, 'b')
+plt.plot(X_array, Z_array, 'r')
 plt.axis('equal')
 
 plt.show()
