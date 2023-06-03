8097b8b04879cd0c208ad6b7998b9a455f0ba139
giadarol <giovanni.iadarola@cern.ch>
2023-06-03 13:04:19 +0200

    Reintroduced LinearTransferMatrix with deprecation warning

--- xtrack/beam_elements/elements.py
+++ xtrack/beam_elements/elements.py
@@ -11,7 +11,7 @@ import xpart as xp
 
 from ..base_element import BeamElement
 from ..random import RandomUniform, RandomExponential, RandomNormal
-from ..general import _pkg_root
+from ..general import _pkg_root, _print
 from ..internal_record import RecordIndex, RecordIdentifier
 
 
@@ -1265,3 +1265,211 @@ def _angle_from_trig(cos=None, sin=None, tan=None):
 
     angle = np.arctan2(sin, cos)
     return angle, cos, sin, tan
+
+class LinearTransferMatrix(BeamElement):
+    _xofields={
+        'no_detuning': xo.Int64,
+        'q_x': xo.Float64,
+        'q_y': xo.Float64,
+        'cos_s': xo.Float64,
+        'sin_s': xo.Float64,
+        'beta_x_0': xo.Float64,
+        'beta_y_0': xo.Float64,
+        'beta_ratio_x': xo.Float64,
+        'beta_prod_x': xo.Float64,
+        'beta_ratio_y': xo.Float64,
+        'beta_prod_y': xo.Float64,
+        'alpha_x_0': xo.Float64,
+        'alpha_x_1': xo.Float64,
+        'alpha_y_0': xo.Float64,
+        'alpha_y_1': xo.Float64,
+        'disp_x_0': xo.Float64,
+        'disp_x_1': xo.Float64,
+        'disp_y_0': xo.Float64,
+        'disp_y_1': xo.Float64,
+        'beta_s': xo.Float64,
+        'energy_ref_increment': xo.Float64,
+        'energy_increment': xo.Float64,
+        'chroma_x': xo.Float64,
+        'chroma_y': xo.Float64,
+        'detx_x': xo.Float64,
+        'detx_y': xo.Float64,
+        'dety_y': xo.Float64,
+        'dety_x': xo.Float64,
+        'x_ref_0': xo.Float64,
+        'px_ref_0': xo.Float64,
+        'y_ref_0': xo.Float64,
+        'py_ref_0': xo.Float64,
+        'x_ref_1': xo.Float64,
+        'px_ref_1': xo.Float64,
+        'y_ref_1': xo.Float64,
+        'py_ref_1': xo.Float64,
+        'length': xo.Float64,
+        'uncorrelated_rad_damping': xo.Int64,
+        'damping_factor_x':xo.Float64,
+        'damping_factor_y':xo.Float64,
+        'damping_factor_s':xo.Float64,
+        'uncorrelated_gauss_noise': xo.Int64,
+        'gauss_noise_ampl_x':xo.Float64,
+        'gauss_noise_ampl_px':xo.Float64,
+        'gauss_noise_ampl_y':xo.Float64,
+        'gauss_noise_ampl_py':xo.Float64,
+        'gauss_noise_ampl_zeta':xo.Float64,
+        'gauss_noise_ampl_delta':xo.Float64,
+        }
+
+    _depends_on = [RandomNormal]
+    isthick = True
+
+    _extra_c_sources = [
+        _pkg_root.joinpath('headers/constants.h'),
+        _pkg_root.joinpath('beam_elements/elements_src/lineartransfermatrix.h')]
+
+    def __init__(self, Q_x=0, Q_y=0,
+                     beta_x_0=1.0, beta_x_1=1.0, beta_y_0=1.0, beta_y_1=1.0,
+                     alpha_x_0=0.0, alpha_x_1=0.0, alpha_y_0=0.0, alpha_y_1=0.0,
+                     disp_x_0=0.0, disp_x_1=0.0, disp_y_0=0.0, disp_y_1=0.0,
+                     Q_s=0.0, beta_s=1.0,
+                     chroma_x=0.0, chroma_y=0.0,
+                     detx_x=0.0, detx_y=0.0, dety_y=0.0, dety_x=0.0,
+                     energy_increment=0.0, energy_ref_increment=0.0,
+                     x_ref_0 = 0.0, px_ref_0 = 0.0, x_ref_1 = 0.0, px_ref_1 = 0.0,
+                     y_ref_0 = 0.0, py_ref_0 = 0.0, y_ref_1 = 0.0, py_ref_1 = 0.0,
+                     damping_rate_x = 0.0, damping_rate_y = 0.0, damping_rate_s = 0.0,
+                     equ_emit_x = 0.0, equ_emit_y = 0.0, equ_emit_s = 0.0,
+                     gauss_noise_ampl_x=0.0,gauss_noise_ampl_px=0.0,gauss_noise_ampl_y=0.0,gauss_noise_ampl_py=0.0,gauss_noise_ampl_zeta=0.0,gauss_noise_ampl_delta=0.0,
+                     **nargs):
+
+        _print('Warning: `LinearTransferMatrix` is deprecated and will be removed in the future. '
+               'Please use `SimplifiedAcceleratorSegment` instead.')
+        if (chroma_x==0 and chroma_y==0
+            and detx_x==0 and detx_y==0 and dety_y==0 and dety_x==0):
+
+            cos_x = np.cos(2.0*np.pi*Q_x)
+            sin_x = np.sin(2.0*np.pi*Q_x)
+            cos_y = np.cos(2.0*np.pi*Q_y)
+            sin_y = np.sin(2.0*np.pi*Q_y)
+
+            nargs['no_detuning']  =  True
+            nargs['q_x'] = sin_x
+            nargs['q_y'] = sin_y
+            nargs['chroma_x'] = cos_x
+            nargs['chroma_y'] = cos_y
+            nargs['detx_x'] = 0.
+            nargs['detx_y'] = 0.
+            nargs['dety_y'] = 0.
+            nargs['dety_x'] = 0.
+        else:
+            nargs['no_detuning']  =  False
+            nargs['q_x'] = Q_x
+            nargs['q_y'] = Q_y
+            nargs['chroma_x'] = chroma_x
+            nargs['chroma_y'] = chroma_y
+            nargs['detx_x'] = detx_x
+            nargs['detx_y'] = detx_y
+            nargs['dety_y'] = dety_y
+            nargs['dety_x'] = dety_x
+
+        if Q_s is not None:
+            nargs['cos_s'] = np.cos(2.0*np.pi*Q_s)
+            nargs['sin_s'] = np.sin(2.0*np.pi*Q_s)
+        else:
+            nargs['cos_s'] = 999
+            nargs['sin_s'] = 0.
+
+        nargs['beta_x_0'] = beta_x_0
+        nargs['beta_y_0'] = beta_y_0
+        nargs['beta_ratio_x'] = np.sqrt(beta_x_1/beta_x_0)
+        nargs['beta_prod_x'] = np.sqrt(beta_x_1*beta_x_0)
+        nargs['beta_ratio_y'] = np.sqrt(beta_y_1/beta_y_0)
+        nargs['beta_prod_y'] = np.sqrt(beta_y_1*beta_y_0)
+        nargs['alpha_x_0'] = alpha_x_0
+        nargs['alpha_x_1'] = alpha_x_1
+        nargs['alpha_y_0'] = alpha_y_0
+        nargs['alpha_y_1'] = alpha_y_1
+        nargs['disp_x_0'] = disp_x_0
+        nargs['disp_x_1'] = disp_x_1
+        nargs['disp_y_0'] = disp_y_0
+        nargs['disp_y_1'] = disp_y_1
+        nargs['beta_s'] = beta_s
+        nargs['x_ref_0'] = x_ref_0
+        nargs['x_ref_1'] = x_ref_1
+        nargs['px_ref_0'] = px_ref_0
+        nargs['px_ref_1'] = px_ref_1
+        nargs['y_ref_0'] = y_ref_0
+        nargs['y_ref_1'] = y_ref_1
+        nargs['py_ref_0'] = py_ref_0
+        nargs['py_ref_1'] = py_ref_1
+        # acceleration with change of reference momentum
+        nargs['energy_ref_increment'] = energy_ref_increment
+        # acceleration without change of reference momentum
+        nargs['energy_increment'] = energy_increment
+
+        if damping_rate_x < 0.0 or damping_rate_y < 0.0 or damping_rate_s < 0.0:
+            raise ValueError('Damping rates cannot be negative')
+        if damping_rate_x > 0.0 or damping_rate_y > 0.0 or damping_rate_s > 0.0:
+            nargs['uncorrelated_rad_damping'] = True
+            nargs['damping_factor_x'] = 1.0-damping_rate_x/2.0
+            nargs['damping_factor_y'] = 1.0-damping_rate_y/2.0
+            nargs['damping_factor_s'] = 1.0-damping_rate_s/2.0
+        else:
+            nargs['uncorrelated_rad_damping'] = False
+
+        if equ_emit_x < 0.0 or equ_emit_y < 0.0 or equ_emit_s < 0.0:
+            raise ValueError('Equilibrium emittances cannot be negative')
+        nargs['uncorrelated_gauss_noise'] = False
+        nargs['gauss_noise_ampl_x'] = 0.0
+        nargs['gauss_noise_ampl_px'] = 0.0
+        nargs['gauss_noise_ampl_y'] = 0.0
+        nargs['gauss_noise_ampl_py'] = 0.0
+        nargs['gauss_noise_ampl_zeta'] = 0.0
+        nargs['gauss_noise_ampl_delta'] = 0.0
+
+        assert equ_emit_x >= 0.0
+        assert equ_emit_y >= 0.0
+        assert equ_emit_s >= 0.0
+
+        if equ_emit_x > 0.0:
+            assert alpha_x_1 == 0
+            nargs['uncorrelated_gauss_noise'] = True
+            nargs['gauss_noise_ampl_px'] = np.sqrt(equ_emit_x*damping_rate_x/beta_x_1)
+            nargs['gauss_noise_ampl_x'] = beta_x_1*nargs['gauss_noise_ampl_px']
+        if equ_emit_y > 0.0:
+            assert alpha_y_1 == 0
+            nargs['uncorrelated_gauss_noise'] = True
+            nargs['gauss_noise_ampl_py'] = np.sqrt(equ_emit_y*damping_rate_y/beta_y_1)
+            nargs['gauss_noise_ampl_y'] = beta_y_1*nargs['gauss_noise_ampl_py']
+        if equ_emit_s > 0.0:
+            nargs['uncorrelated_gauss_noise'] = True
+            nargs['gauss_noise_ampl_delta'] = np.sqrt(equ_emit_s*damping_rate_s/beta_s)
+            nargs['gauss_noise_ampl_zeta'] = beta_s*nargs['gauss_noise_ampl_delta']
+
+        assert gauss_noise_ampl_x >= 0.0
+        assert gauss_noise_ampl_px >= 0.0
+        assert gauss_noise_ampl_y >= 0.0
+        assert gauss_noise_ampl_py >= 0.0
+        assert gauss_noise_ampl_zeta >= 0.0
+        assert gauss_noise_ampl_delta >= 0.0
+
+        if gauss_noise_ampl_x > 0.0 or gauss_noise_ampl_px > 0.0 or gauss_noise_ampl_y > 0.0 or gauss_noise_ampl_py > 0.0 or gauss_noise_ampl_zeta > 0.0 or gauss_noise_ampl_delta > 0.0:
+            nargs['uncorrelated_gauss_noise'] = True
+            nargs['gauss_noise_ampl_x'] = np.sqrt(nargs['gauss_noise_ampl_x']**2+gauss_noise_ampl_x**2)
+            nargs['gauss_noise_ampl_px'] = np.sqrt(nargs['gauss_noise_ampl_px']**2+gauss_noise_ampl_px**2)
+            nargs['gauss_noise_ampl_y'] = np.sqrt(nargs['gauss_noise_ampl_y']**2+gauss_noise_ampl_y**2)
+            nargs['gauss_noise_ampl_py'] = np.sqrt(nargs['gauss_noise_ampl_py']**2+gauss_noise_ampl_py**2)
+            nargs['gauss_noise_ampl_zeta'] = np.sqrt(nargs['gauss_noise_ampl_zeta']**2+gauss_noise_ampl_zeta**2)
+            nargs['gauss_noise_ampl_delta'] = np.sqrt(nargs['gauss_noise_ampl_delta']**2+gauss_noise_ampl_delta**2)
+
+        super().__init__(**nargs)
+
+    @property
+    def Q_s(self):
+        return np.arccos(self.cos_s) / (2*np.pi)
+
+    @property
+    def beta_x_1(self):
+        return self.beta_prod_x*self.beta_ratio_x
+
+    @property
+    def beta_y_1(self):
+        return self.beta_prod_y*self.beta_ratio_y
