1eaf83c85c68f77cc9cefa0dfccb03e4fda86534
giadarol <giovanni.iadarola@cern.ch>
2023-06-06 23:09:10 +0200

    Plotting also closed twissses

--- examples/crab_dispersion/000_crab_dispersion.py
+++ examples/crab_dispersion/000_crab_dispersion.py
@@ -20,6 +20,10 @@ tw_minus = line.twiss(method='4d', ele_start=0, ele_stop=len(line) - 1,
                                                 line=line, zeta=-d_zeta))
 dx_zeta_rf_off_crab_off = (tw_plus.x - tw_minus.x)/(tw_plus.zeta - tw_minus.zeta)
 
+tw_plus_closed = line.twiss(method='4d', zeta0=d_zeta)
+tw_minus_closed = line.twiss(method='4d', zeta0=-d_zeta)
+cl_dx_zeta_rf_off_crab_off = (tw_plus_closed.x - tw_minus_closed.x)/(tw_plus_closed.zeta - tw_minus_closed.zeta)
+
 line.vars['vrf400'] = 16
 
 tw_plus = line.twiss(method='4d', ele_start=0, ele_stop=len(line) - 1,
@@ -29,6 +33,10 @@ tw_minus = line.twiss(method='4d', ele_start=0, ele_stop=len(line) - 1,
                         twiss_init=xt.TwissInit(element_name=line.element_names[0],
                                                 line=line, zeta=-d_zeta))
 dx_zeta_4d_rf_on_crab_off = (tw_plus.x - tw_minus.x)/(tw_plus.zeta - tw_minus.zeta)
+
+tw_plus_closed = line.twiss(method='4d', zeta0=d_zeta)
+tw_minus_closed = line.twiss(method='4d', zeta0=-d_zeta)
+cl_dx_zeta_4d_rf_on_crab_off = (tw_plus_closed.x - tw_minus_closed.x)/(tw_plus_closed.zeta - tw_minus_closed.zeta)
 
 line.vars['on_crab1'] = -190
 
@@ -40,10 +48,29 @@ tw_minus = line.twiss(method='4d', ele_start=0, ele_stop=len(line) - 1,
                                                 line=line, zeta=-d_zeta))
 dx_zeta_4d_rf_on_crab_on = (tw_plus.x - tw_minus.x)/(tw_plus.zeta - tw_minus.zeta)
 
+tw_plus_closed = line.twiss(method='4d', zeta0=d_zeta)
+tw_minus_closed = line.twiss(method='4d', zeta0=-d_zeta)
+cl_dx_zeta_4d_rf_on_crab_on = (tw_plus_closed.x - tw_minus_closed.x)/(tw_plus_closed.zeta - tw_minus_closed.zeta)
+
+line.vars['vrf400'] = 0
+tw_plus = line.twiss(method='4d', ele_start=0, ele_stop=len(line) - 1,
+                        twiss_init=xt.TwissInit(element_name=line.element_names[0],
+                                                line=line, zeta=d_zeta))
+tw_minus = line.twiss(method='4d', ele_start=0, ele_stop=len(line) - 1,
+                        twiss_init=xt.TwissInit(element_name=line.element_names[0],
+                                                line=line, zeta=-d_zeta))
+dx_zeta_rf_off_crab_on = (tw_plus.x - tw_minus.x)/(tw_plus.zeta - tw_minus.zeta)
+
+tw_plus_closed = line.twiss(method='4d', zeta0=d_zeta)
+tw_minus_closed = line.twiss(method='4d', zeta0=-d_zeta)
+cl_dx_zeta_rf_off_crab_on = (tw_plus_closed.x - tw_minus_closed.x)/(tw_plus_closed.zeta - tw_minus_closed.zeta)
+
+
 import matplotlib.pyplot as plt
 plt.close('all')
 plt.figure(1)
 plt.plot(tw_plus.s, dx_zeta_4d_rf_on_crab_on, label='rf on, crab on', color='g')
+plt.plot(tw_plus.s, dx_zeta_rf_off_crab_on, label='rf off, crab on', color='m')
 plt.plot(tw_plus.s, dx_zeta_4d_rf_on_crab_off, label='rf on, crab off', color='b')
 plt.plot(tw_plus.s, dx_zeta_rf_off_crab_off, label='rf off, crab off', color='r')
 plt.axvline(x=line.get_s_position('ip4'), color='k', linestyle='--', label='ip4')
@@ -51,6 +78,18 @@ plt.legend()
 plt.xlabel('s [m]')
 plt.ylabel('dx/dzeta')
 
+plt.figure(10)
+plt.plot(tw_plus.s, cl_dx_zeta_4d_rf_on_crab_on, label='rf on, crab on', color='g')
+plt.plot(tw_plus.s, cl_dx_zeta_rf_off_crab_on, label='rf off, crab on', color='m')
+plt.plot(tw_plus.s, cl_dx_zeta_4d_rf_on_crab_off, label='rf on, crab off', color='b')
+plt.plot(tw_plus.s, cl_dx_zeta_rf_off_crab_off, label='rf off, crab off', color='r')
+plt.legend()
+plt.xlabel('s [m]')
+plt.ylabel('dx/dzeta')
+
+
+
+line.vars['vrf400'] = 16
 
 line.vars['on_crab1'] = 0
 tw_crab_off = line.twiss()
