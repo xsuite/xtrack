ef653ce65328932bae1cf232700253240844477c
giadarol <giovanni.iadarola@cern.ch>
2023-06-14 10:26:36 +0200

    knob controlled by function works, but json round trip fails

--- examples/functions_in_knobs/000_dev.py
+++ examples/functions_in_knobs/000_dev.py
@@ -14,8 +14,24 @@ line.particle_ref = xp.Particles(mass0=xp.PROTON_MASS_EV, q0=1,
                                  gamma0=mad.sequence.lhcb1.beam.gamma)
 line.build_tracker()
 
-line.functions['ramp_on_x8'] = xd.FunctionPieceWiseLinear(x=[0, 1, 3, 5],
+line.functions['ramp_on_x1'] = xd.FunctionPieceWiseLinear(x=[0.5, 1, 3, 5],
                                                           y=[0, 100, 100, 0])
+line.vars['t_turn_s'] = 0
+line.vars['on_x1'] = line.functions.ramp_on_x1(line.vars['t_turn_s'])
+
+assert np.isclose(line.twiss()['px', 'ip1'], 0, atol=1e-7, rtol = 0)
+line.vars['t_turn_s'] = 1
+assert np.isclose(line.twiss()['px', 'ip1'], 100e-6, atol=1e-7, rtol = 0)
+line.vars['t_turn_s'] = 2
+assert np.isclose(line.twiss()['px', 'ip1'], 100e-6, atol=1e-7, rtol = 0)
+line.vars['t_turn_s'] = 3
+assert np.isclose(line.twiss()['px', 'ip1'], 100e-6, atol=1e-7, rtol = 0)
+line.vars['t_turn_s'] = 4
+assert np.isclose(line.twiss()['px', 'ip1'], 50e-6, atol=1e-7, rtol = 0)
+line.vars['t_turn_s'] = 5
+assert np.isclose(line.twiss()['px', 'ip1'], 0, atol=1e-7, rtol = 0)
+line.vars['t_turn_s'] = 6
+assert np.isclose(line.twiss()['px', 'ip1'], 0, atol=1e-7, rtol = 0)
 
 # Test a knob involving functions
 line.vars['on_x8'] = 100
@@ -40,7 +56,20 @@ line2.vars['phi_ir8'] = 90
 assert np.isclose(line2.twiss()['px', 'ip8'], 0, atol=1e-7, rtol = 0)
 assert np.isclose(line2.twiss()['py', 'ip8'], 100e-6, atol=1e-7, rtol = 0)
 
+assert np.isclose(line2.twiss()['px', 'ip1'], 0, atol=1e-7, rtol = 0)
+line2.vars['t_turn_s'] = 1
+assert np.isclose(line2.twiss()['px', 'ip1'], 100e-6, atol=1e-7, rtol = 0)
+line2.vars['t_turn_s'] = 2
+assert np.isclose(line2.twiss()['px', 'ip1'], 100e-6, atol=1e-7, rtol = 0)
+line2.vars['t_turn_s'] = 3
+assert np.isclose(line2.twiss()['px', 'ip1'], 100e-6, atol=1e-7, rtol = 0)
+line2.vars['t_turn_s'] = 4
+assert np.isclose(line2.twiss()['px', 'ip1'], 50e-6, atol=1e-7, rtol = 0)
+line2.vars['t_turn_s'] = 5
+assert np.isclose(line2.twiss()['px', 'ip1'], 0, atol=1e-7, rtol = 0)
+line2.vars['t_turn_s'] = 6
+assert np.isclose(line2.twiss()['px', 'ip1'], 0, atol=1e-7, rtol = 0)
 
 
-# Remember to test multiline
+# Remember to test multiline2
 
