38fb96f4ec7093a5ad31b81e5fb19aa9371aa8e8
giadarol <giovanni.iadarola@cern.ch>
2023-06-15 16:04:50 +0200

    makethin

--- examples/psb/005e_makethin.py
+++ examples/psb/005e_makethin.py
@@ -7,8 +7,37 @@ import xtrack as xt
 import xpart as xp
 import xdeps as xd
 
+from xtrack.slicing import Teapot, Strategy
+
 import matplotlib.pyplot as plt
 
 line = xt.Line.from_json('psb_03_with_chicane_corrected.json')
-line.insert_element(element=xt.Marker(), name='mker_match', at_s=79.874)
 line.build_tracker()
+
+line.vars['on_chicane_k0'] = 0
+line.vars['on_chicane_k2'] = 0
+line.vars['on_chicane_beta_corr'] = 1
+line.vars['on_chicane_tune_corr'] = 1
+
+line_thick = line.copy()
+line_thick.build_tracker()
+
+line.discard_tracker()
+
+slicing_strategies = [
+    Strategy(slicing=Teapot(1)),  # Default
+    Strategy(slicing=Teapot(50), element_type=xt.TrueBend),
+    Strategy(slicing=Teapot(50), element_type=xt.CombinedFunctionMagnet),
+]
+
+print("Slicing thick elements...")
+line.slice_in_place(slicing_strategies)
+line.build_tracker()
+
+tw_thin = line.twiss()
+tw_thick = line_thick.twiss()
+
+print(f'Qx: thick {tw_thin.qx:.4f} thin {tw_thick.qx:.4f}, diff {tw_thin.qx-tw_thick.qx:.4e}')
+print(f'Qy: thick {tw_thin.qy:.4f} thin {tw_thick.qy:.4f}, diff {tw_thin.qy-tw_thick.qy:.4e}')
+print(f"Q'x: thick {tw_thin.dqx:.4f} thin {tw_thick.dqx:.4f}, diff {tw_thin.dqx-tw_thick.dqx:.4f}")
+print(f"Q'y: thick {tw_thin.dqy:.4f} thin {tw_thick.dqy:.4f}, diff {tw_thin.dqy-tw_thick.dqy:.4f}")
