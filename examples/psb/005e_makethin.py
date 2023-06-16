928d388df834f3a795c79a94bbc88a6b7d0f8d99
giadarol <giovanni.iadarola@cern.ch>
2023-06-16 08:53:20 +0200

    Check also non-linear q' behavior

--- examples/psb/005e_makethin.py
+++ examples/psb/005e_makethin.py
@@ -45,3 +45,41 @@ print(f"Q'y: thick {tw_thin.dqy:.4f} thin {tw_thick.dqy:.4f}, diff {tw_thin.dqy-
 
 bety_interp = np.interp(tw_thick.s, tw_thin.s, tw_thin.bety)
 print(f"Max beta beat: {np.max(np.abs(tw_thick.bety/bety_interp - 1)):.4e}")
+
+plt.close('all')
+
+delta_values = np.linspace(-0.001, 0.001, 100)
+qy_thick_list = 0 * delta_values
+qx_thick_list = 0 * delta_values
+qy_thin_list = 0 * delta_values
+qx_thin_list = 0 * delta_values
+
+for ii, dd in enumerate(delta_values):
+    tt_thin = line.twiss(delta0=dd)
+    tt_thick = line_thick.twiss(delta0=dd)
+
+    qy_thick_list[ii] = tt_thick.qy
+    qx_thick_list[ii] = tt_thick.qx
+    qy_thin_list[ii] = tt_thin.qy
+    qx_thin_list[ii] = tt_thin.qx
+
+plt.close('all')
+plt.figure(1)
+
+plt.plot(delta_values, qx_thin_list, label='qy thin')
+plt.plot(delta_values, qx_thick_list, label='qy thick')
+plt.plot(delta_values, qy_thin_list, label='qx thin')
+plt.plot(delta_values, qy_thick_list, label='qx thick')
+
+plt.xlabel('delta')
+plt.ylabel('tune')
+plt.show()
+
+# plot difference
+plt.figure(2)
+plt.plot(delta_values, qx_thick_list - qx_thin_list, label='qx thick - thin')
+plt.plot(delta_values, qy_thick_list - qy_thin_list, label='qy thick - thin')
+plt.xlabel('delta')
+plt.ylabel('tune')
+plt.legend()
+plt.show()
