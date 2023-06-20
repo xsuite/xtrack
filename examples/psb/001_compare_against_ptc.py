51076dcc547a76b18ee89855abc2ea3040cac255
giadarol <giovanni.iadarola@cern.ch>
2023-06-20 23:13:48 +0200

    Improve plot

--- examples/psb/001_compare_against_ptc.py
+++ examples/psb/001_compare_against_ptc.py
@@ -121,17 +121,27 @@ t_ptc = xd.Table(mad.table.ptc_twiss)
 
 line_thick = xt.Line.from_json('psb_03_with_chicane_corrected.json')
 line_thick.build_tracker()
-
 line_thick.vars['on_chicane_beta_corr'] = 0
 line_thick.vars['on_chicane_tune_corr'] = 0
 
+line_thin = xt.Line.from_json('psb_04_with_chicane_corrected_thin.json')
+line_thin.build_tracker()
+line_thin.vars['on_chicane_beta_corr'] = 0
+line_thin.vars['on_chicane_tune_corr'] = 0
+
 t_test = np.linspace(0, 6e-3, 20)
 
-qx = []
-qy = []
-dqx = []
-dqy = []
-bety_at_scraper = []
+qx_thick = []
+qy_thick = []
+dqx_thick = []
+dqy_thick = []
+bety_at_scraper_thick = []
+qx_thin = []
+qy_thin = []
+dqx_thin = []
+dqy_thin = []
+bety_at_scraper_thin = []
+
 qx_ptc = []
 qy_ptc = []
 dqx_ptc = []
@@ -140,13 +150,21 @@ bety_at_scraper_ptc = []
 for ii, tt in enumerate(t_test):
     print(f'Twiss at t = {tt*1e3:.2f} ms   ', end='\r', flush=True)
     line_thick.vars['t_turn_s'] = tt
+    line_thin.vars['t_turn_s'] = tt
 
-    tw = line_thick.twiss()
-    bety_at_scraper.append(tw['bety', 'br.stscrap22'])
-    qx.append(tw.qx)
-    qy.append(tw.qy)
-    dqx.append(tw.dqx)
-    dqy.append(tw.dqy)
+    tw_thick = line_thick.twiss()
+    bety_at_scraper_thick.append(tw_thick['bety', 'br.stscrap22'])
+    qx_thick.append(tw_thick.qx)
+    qy_thick.append(tw_thick.qy)
+    dqx_thick.append(tw_thick.dqx)
+    dqy_thick.append(tw_thick.dqy)
+
+    tw_thin = line_thin.twiss()
+    bety_at_scraper_thin.append(tw_thin['bety', 'br.stscrap22'])
+    qx_thin.append(tw_thin.qx)
+    qy_thin.append(tw_thin.qy)
+    dqx_thin.append(tw_thin.dqx)
+    dqy_thin.append(tw_thin.dqy)
 
     mad.globals.bsw_k0l = line_thick.vars['bsw_k0l']._value
     mad.globals.bsw_k2l = line_thick.vars['bsw_k2l']._value
@@ -189,12 +207,20 @@ for ii, tt in enumerate(t_test):
 
     bety_at_scraper_ptc.append(tw_ptc['bety', 'br.stscrap22:1'])
 
-qx = np.array(qx)
-qy = np.array(qy)
-bety_at_scraper = np.array(bety_at_scraper)
+qx_thick = np.array(qx_thick)
+qy_thick = np.array(qy_thick)
+dqx_thick = np.array(dqx_thick)
+dqy_thick = np.array(dqy_thick)
+bety_at_scraper_thick = np.array(bety_at_scraper_thick)
+qx_thin = np.array(qx_thin)
+qy_thin = np.array(qy_thin)
+dqx_thin = np.array(dqx_thin)
+dqy_thin = np.array(dqy_thin)
+bety_at_scraper_thin = np.array(bety_at_scraper_thin)
 qx_ptc = np.array(qx_ptc)
 qy_ptc = np.array(qy_ptc)
 bety_at_scraper_ptc = np.array(bety_at_scraper_ptc)
+
 
 import matplotlib.pyplot as plt
 plt.close('all')
@@ -202,7 +228,7 @@ plt.close('all')
 fig1 = plt.figure(1)
 sp1 = plt.subplot(2,1,1)
 
-plt.plot(t_test*1e3, qy, label='qy')
+plt.plot(t_test*1e3, qy_thick, label='qy')
 plt.plot(t_test*1e3, qy_ptc, label='qy ptc')
 plt.ylabel('tune')
 
@@ -210,27 +236,31 @@ plt.ylabel('tune')
 plt.legend()
 
 sp2 = plt.subplot(2,1,2, sharex=sp1)
-plt.plot(t_test*1e3, bety_at_scraper, label='bety at scraper')
-plt.plot(t_test*1e3, bety_at_scraper_ptc, label='bety at scraper ptc')
+plt.plot(t_test*1e3, bety_at_scraper_thick, label='xsuite thick')
+plt.plot(t_test*1e3, bety_at_scraper_thin, label='xsuite thin')
+plt.plot(t_test*1e3, bety_at_scraper_ptc, label='ptc')
 plt.ylim(bottom=0)
+
 plt.legend()
 
 plt.xlabel('time [ms]')
-plt.ylabel('beta [m]')
+plt.ylabel(r'$\beta_y$ at scraper [m]')
 
 plt.figure(2)
 sp1 = plt.subplot(2,1,1, sharex=sp1)
-plt.plot(t_test*1e3, dqx, label='xsuite')
-plt.plot(t_test*1e3, dqx_ptc, label='xsuite')
-plt.ylim(top=0)
-plt.ylabel('dqx')
+plt.plot(t_test*1e3, dqx_thick, '-', color='C0', label='xsuite thick')
+plt.plot(t_test*1e3, dqx_thin, '-.', color='C1', label='xsuite thin')
+plt.plot(t_test*1e3, dqx_ptc, '--', color='C2', label='ptc')
+plt.ylim(bottom=-9, top=0)
+plt.ylabel(r"$Q'_x$")
 plt.legend()
 
 sp2 = plt.subplot(2,1,2, sharex=sp1)
-plt.plot(t_test*1e3, dqy, label='dqy')
-plt.plot(t_test*1e3, dqy_ptc, label='dqy ptc')
-plt.ylabel('dqy')
-plt.ylim(top=0)
+plt.plot(t_test*1e3, dqy_thick, '-', color='C0', label='xsuite thick')
+plt.plot(t_test*1e3, dqy_thin, '-.', color='C1', label='xsuite thin')
+plt.plot(t_test*1e3, dqy_ptc, '--', color='C2', label='ptc')
+plt.ylabel(r"$Q'_y$")
+plt.ylim(bottom=-9,top=0)
 plt.legend()
 
 plt.xlabel('time [ms]')
