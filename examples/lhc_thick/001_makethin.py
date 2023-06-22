febffa0010a54bcf52c81ab288a006d6e513989a
giadarol <giovanni.iadarola@cern.ch>
2023-06-22 09:44:07 +0200

    Check a match

--- examples/lhc_thick/001_makethin.py
+++ examples/lhc_thick/001_makethin.py
@@ -1,3 +1,4 @@
+import time
 import numpy as np
 
 import xtrack as xt
@@ -15,7 +16,7 @@ slicing_strategies = [
     Strategy(slicing=Teapot(20), element_type=xt.Quadrupole),
     Strategy(slicing=Teapot(2), name=r'^mb\..*'),
     Strategy(slicing=Teapot(5), name=r'^mq\..*'),
-    Strategy(slicing=Teapot(3), name=r'^mqt.*'),
+    Strategy(slicing=Teapot(2), name=r'^mqt.*'),
     Strategy(slicing=Teapot(60), name=r'^mqx.*'),
 ]
 
@@ -60,3 +61,55 @@ assert np.isclose(tw['px', 'ip1'], 50e-6, rtol=0, atol=5e-7)
 assert np.isclose(tw['py', 'ip1'], 0, rtol=0, atol=5e-7)
 assert np.isclose(tw['px', 'ip5'], 0, rtol=0, atol=5e-7)
 assert np.isclose(tw['py', 'ip5'], 60e-6, rtol=0, atol=5e-7)
+
+t1 = time.time()                                                                #!skip-doc
+line_thick.match(
+    vary=[
+        xt.Vary('kqtf.b1', step=1e-8),
+        xt.Vary('kqtd.b1', step=1e-8),
+        xt.Vary('ksf.b1', step=1e-8),
+        xt.Vary('ksd.b1', step=1e-8),
+    ],
+    targets = [
+        xt.Target('qx', 62.27, tol=1e-4),
+        xt.Target('qy', 60.29, tol=1e-4),
+        xt.Target('dqx', 10.0, tol=0.05),
+        xt.Target('dqy', 12.0, tol=0.05)])
+t2 = time.time()                                                                #!skip-doc
+print('\nTime match thick: ', t2-t1)
+
+line.match(
+    vary=[
+        xt.Vary('kqtf.b1', step=1e-8),
+        xt.Vary('kqtd.b1', step=1e-8),
+        xt.Vary('ksf.b1', step=1e-8),
+        xt.Vary('ksd.b1', step=1e-8),
+    ],
+    targets = [
+        xt.Target('qx', 62.27, tol=1e-4),
+        xt.Target('qy', 60.29, tol=1e-4),
+        xt.Target('dqx', 10.0, tol=0.05),
+        xt.Target('dqy', 12.0, tol=0.05)])
+t2 = time.time()
+print('\nTime match thin: ', t2-t1)
+
+tw = line.twiss()
+tw_thick = line_thick.twiss()
+
+assert np.isclose(tw_thick.qx, 62.27, rtol=0, atol=1e-4)
+assert np.isclose(tw_thick.qy, 60.29, rtol=0, atol=1e-4)
+assert np.isclose(tw_thick.dqx, 10.0, rtol=0, atol=0.05)
+assert np.isclose(tw_thick.dqy, 12.0, rtol=0, atol=0.05)
+assert np.isclose(tw.qx, 62.27, rtol=0, atol=1e-4)
+assert np.isclose(tw.qy, 60.29, rtol=0, atol=1e-4)
+assert np.isclose(tw.dqx, 10.0, rtol=0, atol=0.05)
+assert np.isclose(tw.dqy, 12.0, rtol=0, atol=0.05)
+
+assert np.isclose(line.vars['kqtf.b1']._value, line_thick.vars['kqtf.b1']._value,
+                    rtol=0.03, atol=0)
+assert np.isclose(line.vars['kqtd.b1']._value, line_thick.vars['kqtd.b1']._value,
+                    rtol=0.03, atol=0)
+assert np.isclose(line.vars['ksf.b1']._value, line_thick.vars['ksf.b1']._value,
+                    rtol=0.01, atol=0)
+assert np.isclose(line.vars['ksd.b1']._value, line_thick.vars['ksd.b1']._value,
+                    rtol=0.01, atol=0)
