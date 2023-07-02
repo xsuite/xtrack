144cdd433361f1316af3e9f57f2fdda883770ed0
giadarol <giovanni.iadarola@cern.ch>
2023-07-02 14:38:22 +0200

    Add a check

--- examples/sps_thick/000_dev.py
+++ examples/sps_thick/000_dev.py
@@ -10,13 +10,6 @@ mad.call('../../test_data/sps_thick/lhc_q20.str')
 mad.use(sequence='sps')
 twmad = mad.twiss()
 
-mad_sbend = Madx()
-mad_sbend.call('../../test_data/sps_thick/sps_sbends.seq')
-mad_sbend.input('beam, particle=proton, pc=26;')
-mad_sbend.call('../../test_data/sps_thick/lhc_q20.str')
-mad_sbend.use(sequence='sps')
-twmad_sbend = mad_sbend.twiss()
-
 deferred_expressions = True
 
 line = xt.Line.from_madx_sequence(
@@ -28,18 +21,15 @@ line.particle_ref = xp.Particles(mass0=xp.PROTON_MASS_EV,
 line.build_tracker()
 line.twiss_default['method'] = '4d'
 
+# Check a bend
+assert line.element_names[52] == 'mbb.10150_entry'
+assert line.element_names[53] == 'mbb.10150_den'
+assert line.element_names[54] == 'mbb.10150'
+assert line.element_names[55] == 'mbb.10150_dex'
+assert line.element_names[56] == 'mbb.10150_exit'
+
+
+
 tw = line.twiss()
-
-
-line_sbend = xt.Line.from_madx_sequence(
-    sequence=mad_sbend.sequence.sps,
-    deferred_expressions=deferred_expressions,
-    allow_thick=True)
-line_sbend.particle_ref = xp.Particles(mass0=xp.PROTON_MASS_EV,
-                                 gamma0=mad.sequence.sps.beam.gamma)
-line_sbend.build_tracker()
-line_sbend.twiss_default['method'] = '4d'
-
-tw_sbend = line_sbend.twiss()
 
 
