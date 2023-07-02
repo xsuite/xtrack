ef3c9b375ce201a8c5bdfbd8c9de539100566d7d
giadarol <giovanni.iadarola@cern.ch>
2023-07-02 16:55:08 +0200

    Some more checks

--- examples/sps_thick/000_dev.py
+++ examples/sps_thick/000_dev.py
@@ -42,6 +42,7 @@ assert line['mbb.10150_dex'].model == 'linear'
 assert line['mbb.10150_dex'].side == 'exit'
 assert line['mbb.10150_den']._linear_mode == 0
 assert line['mbb.10150_dex']._linear_mode == 0
+assert line['mbb.10150'].model == 'expanded'
 
 ang = line['mbb.10150'].k0 * line['mbb.10150'].length
 assert np.isclose(line['mbb.10150_den'].e1, ang / 2, atol=1e-11, rtol=0)
@@ -50,7 +51,15 @@ assert np.isclose(line['mbb.10150_dex'].e1, ang / 2, atol=1e-11, rtol=0)
 tw0 = line.twiss()
 assert np.isclose(twmad.s[-1], tw0.s[-1], atol=1e-11, rtol=0)
 
-line.configure_bend_model(edge='full')
+line.configure_bend_model(edge='full', core='full')
+
+assert line['mbb.10150_den'].model == 'full'
+assert line['mbb.10150_den'].side == 'entry'
+assert line['mbb.10150_dex'].model == 'full'
+assert line['mbb.10150_dex'].side == 'exit'
+assert line['mbb.10150_den']._linear_mode == 0
+assert line['mbb.10150_dex']._linear_mode == 0
+assert line['mbb.10150'].model == 'full'
 
 tw1 = line.twiss()
 
