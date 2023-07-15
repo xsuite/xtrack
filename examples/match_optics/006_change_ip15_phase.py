bcc656f1764413efe9b381c5514e0612ecda75ad
giadarol <giovanni.iadarola@cern.ch>
2023-07-15 15:56:38 +0200

    Tough to match ip2b2

--- examples/match_optics/006_change_ip15_phase.py
+++ examples/match_optics/006_change_ip15_phase.py
@@ -21,45 +21,51 @@ opt = lm.change_phase_non_ats_arcs(collider,
     d_mux_15_b2=d_mux_15_b2, d_muy_15_b2=d_muy_15_b2,
     solve=True)
 
-tw_sq_ip1_b1 = lm.propagate_optics_from_beta_star(collider, ip_name='ip1',
-        line_name='lhcb1', ele_start='s.ds.r8.b1', ele_stop='e.ds.l2.b1',
-        beta_star_x=collider.varval['betxip1b1'],
-        beta_star_y=collider.varval['betyip1b1'])
+for bn in ['b1', 'b2']:
 
-mux_compensate = (tw_sq_ip1_b1['mux', 's.ds.l2.b1'] - tw_sq_ip1_b1['mux', 'ip1']
-                   - collider.varval['muxip1b1_r'] - collider.varval['mux12b1'])
-mux_ir2_target = collider.varval['muxip2b1'] - mux_compensate
-muy_compensate = (tw_sq_ip1_b1['muy', 's.ds.l2.b1'] - tw_sq_ip1_b1['muy', 'ip1']
-                  - collider.varval['muyip1b1_r'] - collider.varval['muy12b1'])
-muy_ir2_target = collider.varval['muyip2b1'] - muy_compensate
+    print(f"Matching IR2 {bn}")
 
-arc_periodic_solution = lm.get_arc_periodic_solution(collider)
+    tw_sq_ip1 = lm.propagate_optics_from_beta_star(collider, ip_name='ip1',
+            line_name=f'lhc{bn}', ele_start=f's.ds.r8.{bn}', ele_stop=f'e.ds.l2.{bn}',
+            beta_star_x=collider.varval[f'betxip1{bn}'],
+            beta_star_y=collider.varval[f'betyip1{bn}'])
 
-opt = collider.lhcb1.match(
-    solve=False,
-    ele_start='s.ds.l2.b1', ele_stop='e.ds.r2.b1',
-    # Left boundary
-    twiss_init='preserve_start', table_for_twiss_init=tw_sq_ip1_b1,
-    targets=[
-        # IP optics
-        xt.TargetList(('alfx', 'alfy', 'dx', 'dpx'), value=0, at='ip2'),
-        xt.Target('betx', value=collider.varval['betxip2b1'], at='ip2'),
-        xt.Target('bety', value=collider.varval['betyip2b1'], at='ip2'),
-        # Right boundary
-        xt.TargetList(('betx', 'bety', 'alfx', 'alfy', 'dx', 'dpx'),
-                value=arc_periodic_solution['lhcb1']['23'], at='e.ds.r2.b1'),
-        xt.TargetRelPhaseAdvance('mux', mux_ir2_target),
-        xt.TargetRelPhaseAdvance('muy', muy_ir2_target),
-    ],
-    vary=xt.VaryList(
-        ['kq4.l2b1', 'kq5.l2b1',  'kq6.l2b1', 'kq7.l2b1', 'kq8.l2b1',
-         'kq4.r2b1', 'kq5.r2b1',  'kq6.r2b1', 'kq7.r2b1', 'kq8.r2b1',
-         'kq9.l2b1', 'kq10.l2b1', 'kqtl11.l2b1', 'kqt12.l2b1', 'kqt13.l2b1',
-         'kq9.r2b1', 'kq10.r2b1', 'kqtl11.r2b1', 'kqt12.r2b1', 'kqt13.r2b1']
+    mux_compensate = (tw_sq_ip1['mux', f's.ds.l2.{bn}'] - tw_sq_ip1['mux', 'ip1']
+                    - collider.varval[f'muxip1{bn}_r'] - collider.varval[f'mux12{bn}'])
+    mux_ir2_target = collider.varval[f'muxip2{bn}'] - mux_compensate
+
+    muy_compensate = (tw_sq_ip1['muy', f's.ds.l2.{bn}'] - tw_sq_ip1['muy', 'ip1']
+                    - collider.varval[f'muyip1{bn}_r'] - collider.varval[f'muy12{bn}'])
+    muy_ir2_target = collider.varval[f'muyip2{bn}'] - muy_compensate
+
+    arc_periodic_solution = lm.get_arc_periodic_solution(collider, arc_name=['23'])
+
+    opt = collider[f'lhc{bn}'].match(
+        solve=False,
+        solver_options={'n_bisections': 5},
+        ele_start=f's.ds.l2.{bn}', ele_stop=f'e.ds.r2.{bn}',
+        # Left boundary
+        twiss_init='preserve_start', table_for_twiss_init=tw_sq_ip1,
+        targets=[
+            # IP optics
+            xt.TargetList(('alfx', 'alfy', 'dx', 'dpx'), value=0, at='ip2'),
+            xt.Target('betx', value=collider.varval[f'betxip2{bn}'], at='ip2'),
+            xt.Target('bety', value=collider.varval[f'betyip2{bn}'], at='ip2'),
+            # Right boundary
+            xt.TargetList(('betx', 'bety', 'alfx', 'alfy', 'dx', 'dpx'),
+                    value=arc_periodic_solution[f'lhc{bn}']['23'], at=f'e.ds.r2.{bn}'),
+            xt.TargetRelPhaseAdvance('mux', mux_ir2_target),
+            xt.TargetRelPhaseAdvance('muy', muy_ir2_target),
+        ],
+        vary=xt.VaryList(
+            [f'kq4.l2{bn}', f'kq5.l2{bn}',  f'kq6.l2{bn}',    f'kq7.l2{bn}',   f'kq8.l2{bn}',
+             f'kq4.r2{bn}', f'kq5.r2{bn}',  f'kq6.r2{bn}',    f'kq7.r2{bn}',   f'kq8.r2{bn}',
+             f'kq9.l2{bn}', f'kq10.l2{bn}', f'kqtl11.l2{bn}', f'kqt12.l2{bn}', f'kqt13.l2{bn}',
+             f'kq9.r2{bn}', f'kq10.r2{bn}', f'kqtl11.r2{bn}', f'kqt12.r2{bn}', f'kqt13.r2{bn}']
+        )
     )
-)
 
-opt.solve()
+    opt.solve()
 
 # Check
 tw_sq_check = lm.propagate_optics_from_beta_star(collider, ip_name='ip1',
