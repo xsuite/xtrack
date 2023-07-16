c90d25b9325611047043b5de823b5d700e902f82
giadarol <giovanni.iadarola@cern.ch>
2023-07-16 19:12:52 +0200

    Prepare for matching ir4

--- examples/match_optics/006_change_ip15_phase.py
+++ examples/match_optics/006_change_ip15_phase.py
@@ -29,27 +29,37 @@ arc_periodic_solution = lm.get_arc_periodic_solution(collider)
 
 for bn in ['b1', 'b2']:
 
-    print(f"Matching IR2 {bn}")
-
-    tw_sq_ip1 = lm.propagate_optics_from_beta_star(collider, ip_name='ip1',
+    tw_sq_a81_ip1_a12 = lm.propagate_optics_from_beta_star(collider, ip_name='ip1',
             line_name=f'lhc{bn}', ele_start=f's.ds.r8.{bn}', ele_stop=f'e.ds.l2.{bn}',
             beta_star_x=collider.varval[f'betxip1{bn}'],
             beta_star_y=collider.varval[f'betyip1{bn}'])
 
-    mux_compensate = (tw_sq_ip1['mux', f's.ds.l2.{bn}'] - tw_sq_ip1['mux', 'ip1']
+    tw_sq_a45_ip5_a56 = lm.propagate_optics_from_beta_star(collider, ip_name='ip5',
+            line_name=f'lhc{bn}', ele_start=f's.ds.r4.{bn}', ele_stop=f'e.ds.l6.{bn}',
+            beta_star_x=collider.varval[f'betxip5{bn}'],
+            beta_star_y=collider.varval[f'betyip5{bn}'])
+
+    mux_compensate_ir2 = (tw_sq_a81_ip1_a12['mux', f's.ds.l2.{bn}'] - tw_sq_a81_ip1_a12['mux', 'ip1']
                     - collider.varval[f'muxip1{bn}_r'] - collider.varval[f'mux12{bn}'])
-    mux_ir2_target = collider.varval[f'muxip2{bn}'] - mux_compensate
-
-    muy_compensate = (tw_sq_ip1['muy', f's.ds.l2.{bn}'] - tw_sq_ip1['muy', 'ip1']
+    mux_ir2_target = collider.varval[f'muxip2{bn}'] - mux_compensate_ir2
+    muy_compensate_ir2 = (tw_sq_a81_ip1_a12['muy', f's.ds.l2.{bn}'] - tw_sq_a81_ip1_a12['muy', 'ip1']
                     - collider.varval[f'muyip1{bn}_r'] - collider.varval[f'muy12{bn}'])
-    muy_ir2_target = collider.varval[f'muyip2{bn}'] - muy_compensate
+    muy_ir2_target = collider.varval[f'muyip2{bn}'] - muy_compensate_ir2
 
+    mux_compensate_ir4 = (tw_sq_a45_ip5_a56['mux', 'ip5'] - tw_sq_a45_ip5_a56['mux', f'e.ds.r4.{bn}']
+                    - collider.varval[f'muxip5{bn}_l'] - collider.varval[f'mux45{bn}'])
+    mux_ir4_target = collider.varval[f'muxip4{bn}'] - mux_compensate_ir4
+    muy_compensate_ir4 = (tw_sq_a45_ip5_a56['muy', 'ip5'] - tw_sq_a45_ip5_a56['muy', f'e.ds.r4.{bn}']
+                    - collider.varval[f'muyip5{bn}_l'] - collider.varval[f'muy45{bn}'])
+    muy_ir4_target = collider.varval[f'muyip4{bn}'] - muy_compensate_ir4
+
+    print(f"Matching IR2 {bn}")
 
     betx_ip2 = collider.varval[f'betxip2{bn}']
     bety_ip2 = collider.varval[f'betyip2{bn}']
 
     opt = lm.rematch_ir2(collider, line_name=f'lhc{bn}',
-                boundary_conditions_left=tw_sq_ip1,
+                boundary_conditions_left=tw_sq_a81_ip1_a12,
                 boundary_conditions_right=arc_periodic_solution[f'lhc{bn}']['23'],
                 mux_ir2=mux_ir2_target, muy_ir2=muy_ir2_target,
                 betx_ip2=betx_ip2, bety_ip2=bety_ip2,
