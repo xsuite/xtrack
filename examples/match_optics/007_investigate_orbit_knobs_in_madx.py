305242a337749c8dafcb18a46170dfe93c1c70f7
giadarol <giovanni.iadarola@cern.ch>
2023-07-17 12:09:08 +0200

    Have a reasonable stripped-down madx to start from

--- examples/match_optics/007_investigate_orbit_knobs_in_madx.py
+++ examples/match_optics/007_investigate_orbit_knobs_in_madx.py
@@ -16,15 +16,17 @@ mad.twiss()
 mad.call('../../../hllhc15/toolkit/macro.madx')
 mad.call('make_one_crossing_knob.madx') # only macro definitions
 
+# Create empty tables
 mad.input('''
 ! reset values
 exec,mktable_orbit8h(orbit_ir8h);
 exec,mktable_orbit8v(orbit_ir8v);
 ''')
+
+# Clean up (?)
 mad.input('''
 setvars_const,table=orbit_ir8h;
 setvars_const,table=orbit_ir8v;
-
 ''')
 
 mad.input('''
@@ -67,7 +69,6 @@ setvars_knob,table=knob_on_x8v  ,knob=on_x8v;
 
 ''')
 
-
 mad.use('lhcb1')
 mad.use('lhcb2')
 
@@ -86,5 +87,30 @@ plt.plot(twb1.s, twb1.x, label='x')
 plt.plot(twb1.s, twb1.y, label='y')
 plt.plot(twb2.s, twb2.x, label='x')
 plt.plot(twb2.s, twb2.y, label='y')
+
+
+# Inspect knob expressions
+all_knobs_ip2ip8 = ['acbxh3.r2', 'acbchs5.r2b1', 'pxip2b1', 'acbxh2.l8',
+    'acbyhs4.r8b2', 'pyip2b1', 'acbxv1.l8', 'acbyvs4.l2b1', 'acbxh1.l8',
+    'acbxv2.r8', 'pxip8b2', 'yip8b1', 'pxip2b2', 'acbcvs5.r2b1', 'acbyhs4.l8b1',
+    'acbyvs4.l8b1', 'acbxh2.l2', 'acbxh3.l2', 'acbxv1.r8', 'acbxv1.r2',
+    'acbyvs4.r2b2', 'acbyvs4.l2b2', 'yip8b2', 'xip2b2', 'acbxh2.r2',
+    'acbyhs4.l2b2', 'acbxv2.r2', 'acbyhs5.r8b1', 'acbxh2.r8', 'acbxv3.r8',
+    'acbyvs5.r8b2', 'acbyvs5.l2b2', 'yip2b1', 'acbxv2.l2', 'acbyhs4.r2b2',
+    'acbyhs4.r2b1', 'xip8b2', 'acbyvs5.l2b1', 'acbyvs4.r8b1', 'acbyvs4.r8b2',
+    'acbyvs5.r8b1', 'acbxh1.r8', 'acbyvs4.l8b2', 'acbyhs5.l2b1', 'acbyvs4.r2b1',
+    'acbcvs5.r2b2', 'acbcvs5.l8b2', 'acbyhs4.r8b1', 'pxip8b1', 'acbxv1.l2',
+    'yip2b2', 'acbyhs4.l8b2', 'acbxv3.r2', 'xip8b1', 'acbchs5.r2b2', 'acbxh3.l8',
+    'acbxh3.r8', 'acbyhs5.r8b2', 'acbxv2.l8', 'acbxh1.l2', 'pyip8b1', 'pyip8b2',
+    'acbxv3.l8', 'xip2b1', 'acbyhs5.l2b2', 'acbchs5.l8b2', 'acbcvs5.l8b1',
+    'pyip2b2', 'acbxv3.l2', 'acbchs5.l8b1', 'acbyhs4.l2b1', 'acbxh1.r2']
+all_knobs_ip2ip8 = sorted(all_knobs_ip2ip8)
+all_knobs_ip2 = [kk for kk in all_knobs_ip2ip8 if '.l2' in kk or '.r2' in kk or 'ip2' in kk]
+all_knobs_ip8 = [kk for kk in all_knobs_ip2ip8 if '.l8' in kk or '.r8' in kk or 'ip8' in kk]
+
+for kk in all_knobs_ip2:
+    mad.show(kk)
+for kk in all_knobs_ip8:
+    mad.show(kk)
 
 plt.show()
