01af42c72a4e760478942cbddfa16c2d5e52b21c
giadarol <giovanni.iadarola@cern.ch>
2023-07-25 08:11:20 +0200

    Repr

--- xtrack/match.py
+++ xtrack/match.py
@@ -38,6 +38,8 @@ Action = xd.Action
 class _LOC:
     def __init__(self, name=None):
         self.name = name
+    def __repr__(self):
+        return self.name
 
 START = _LOC('START')
 END = _LOC('END')
