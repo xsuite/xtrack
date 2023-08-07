a0d738d4de9438362eb23bc9e6124287c6359ea7
giadarol <giovanni.iadarola@cern.ch>
2023-08-07 16:43:00 +0200

    Still a problem on s

--- examples/twiss/025_try_markers_only.py
+++ examples/twiss/025_try_markers_only.py
@@ -8,6 +8,8 @@ collider.lhcb1.twiss_default['method'] = '4d'
 collider.lhcb2.twiss_default['method'] = '4d'
 collider.lhcb2.twiss_default['reverse'] = True
 
+# Check on b1 (no reverse)
+
 line = collider.lhcb1
 
 tw_init_ip5 = line.twiss().get_twiss_init('s.ds.l5.b1')
@@ -19,7 +21,6 @@ tw_mk = line.twiss(ele_start='s.ds.l5.b1', ele_stop='e.ds.r5.b1', twiss_init=tw_
                    only_markers=True)
 tw2_mk = line.twiss(ele_start='s.ds.l5.b1', ele_stop='e.ds.r5.b1', twiss_init=tw_init_ip5,
                     only_markers=True)
-
 
 # Check names are the right ones
 ltable = line.get_table()
@@ -46,14 +47,10 @@ for tt in [tw, tw2, tw_mk, tw2_mk]:
             continue
         assert np.allclose(tt[kk], tw.rows[tt.name][kk], rtol=0, atol=1e-15)
 
-# Remember to check the Ws
-
-
 line = collider.lhcb2
 
 tw_init_ip5 = line.twiss().get_twiss_init('s.ds.l5.b2')
 
-import pdb; pdb.set_trace()
 tw = line.twiss(ele_start='s.ds.l5.b2', ele_stop='e.ds.r5.b2', twiss_init=tw_init_ip5)
 tw2 = line.twiss(ele_start='s.ds.l5.b2', ele_stop='e.ds.r5.b2', twiss_init=tw_init_ip5)
 
@@ -62,5 +59,28 @@ tw_mk = line.twiss(ele_start='s.ds.l5.b2', ele_stop='e.ds.r5.b2', twiss_init=tw_
 tw2_mk = line.twiss(ele_start='s.ds.l5.b2', ele_stop='e.ds.r5.b2', twiss_init=tw_init_ip5,
                     only_markers=True)
 
+# Check on b2 (with reverse)
+# Check names are the right ones
+ltable = line.get_table()
+expected_names = np.concatenate([
+    ltable.rows[ltable.element_type == 'Marker'].rows['e.ds.r5.b2':'s.ds.l5.b2'].name[::-1],
+    ['_end_point']])
 
-assert tw_mk['s', 'e.ds.r5.b2'] == tw['s', 'e.ds.r5.b2']
+assert np.all(tw_mk.name == expected_names)
+assert np.all(tw2_mk.name == expected_names)
+assert np.all(tw2.name == tw.name)
+
+# Consistency checks on other columns
+for tt in [tw, tw2, tw_mk, tw2_mk]:
+    assert tw.name[0] == 's.ds.l5.b2'
+    assert tw.name[-1] == '_end_point'
+    assert tw.name[-2] == 'e.ds.r5.b2'
+
+    assert tt['s', 'e.ds.r5.b2'] == line.get_length() - line.get_s_position('e.ds.r5.b2')
+    assert tt['s', 'e.ds.r5.b2'] == tt['s', '_end_point']
+    assert tt['s', 's.ds.l5.b2'] == line.get_length() - line.get_s_position('s.ds.l5.b2')
+
+    for kk in tw._col_names:
+        if kk == 'name':
+            continue
+        assert np.allclose(tt[kk], tw.rows[tt.name][kk], rtol=0, atol=1e-15)
