f04688b6000bcb867ae105be9376001f8a60c31c
giadarol <giovanni.iadarola@cern.ch>
2023-08-08 08:53:50 +0200

    Still issue...

--- examples/match_optics/009_write_madx_optics_file.py
+++ examples/match_optics/009_write_madx_optics_file.py
@@ -10,19 +10,21 @@ collider.vars.load_madx_optics_file(
     "../../test_data/hllhc15_thick/opt_round_150_1500.madx")
 
 fun_container = xt.line.Functions()
-_functions = {}
+_functions = []
 for ff in fun_container._mathfunctions.keys():
-    _functions['f.' + ff] = ff
+    _functions.append(ff)
 
 def get_mad_str_expr(var_expr):
     str_expr = str(var_expr)
     mad_str_expr = str_expr.replace("vars['", '').replace("']", '')
-    for ff in _functions.keys():
-        mad_str_expr = mad_str_expr.replace(ff, _functions[ff])
+    for ff in _functions:
+        mad_str_expr = mad_str_expr.replace('f.' + ff, ff)
     return mad_str_expr
 
 def extract_val_or_madexpr(var, dct_expr, dct_val):
     var_name = var._key
+    if var_name in _functions:
+        return
     if var._expr is not None and var_name not in dct_expr:
         dct_expr[var_name] = get_mad_str_expr(var._expr)
         for vv in var._expr._get_dependencies():
@@ -36,11 +38,25 @@ def extract_val_or_madexpr(var, dct_expr, dct_val):
 dct_expr = {}
 dct_val = {}
 
-vv = collider.vars['acbxh1.l8']
-extract_val_or_madexpr(vv, dct_expr, dct_val)
+vtable = collider.vars.get_table()
+vsave = vtable.rows[
+    vtable.mask[vtable.mask['acb.*'] | vtable.mask['kq.*'] | vtable.mask['ks.*']]]
+for nn in vsave.name:
+    vv = collider.vars[nn]
+    extract_val_or_madexpr(vv, dct_expr, dct_val)
 
+out_lines = []
 
+for nn in sorted(dct_val.keys()):
+    out_lines.append(nn + ' = ' + str(dct_val[nn]) + ';')
 
+out_lines.append('')
+
+for nn in sorted(dct_expr.keys()):
+    out_lines.append(nn + ' := ' + dct_expr[nn] + ';')
+
+with open('opt_round_150_1500_xs.madx', 'w') as fid:
+    fid.write('\n'.join(out_lines))
 
 
 
