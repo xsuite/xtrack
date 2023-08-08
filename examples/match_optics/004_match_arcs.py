607844f1f9781c45902d716870c9aa97eaebe994
giadarol <giovanni.iadarola@cern.ch>
2023-08-08 16:36:33 +0200

    Rematch arc phases

--- examples/match_optics/004_match_arcs.py
+++ examples/match_optics/004_match_arcs.py
@@ -25,3 +25,14 @@ for aa in lm.ARC_NAMES:
     collider.varval[f'kqtd.a{aa}b2'] *= 1.15
     collider.varval[f'kqd.a{aa}'] *= 1.03
     collider.varval[f'kqf.a{aa}'] *= 1.02
+
+for aa in lm.ARC_NAMES:
+    print(f"Matching arc {aa}")
+    target_mux_b1 = collider.varval[f'mux{aa}b1']
+    target_muy_b1 = collider.varval[f'muy{aa}b1']
+    target_mux_b2 = collider.varval[f'mux{aa}b2']
+    target_muy_b2 = collider.varval[f'muy{aa}b2']
+
+    opt = lm.match_arc_phase_advance(collider=collider, arc_name=aa,
+                    target_mux_b1=target_mux_b1, target_muy_b1=target_muy_b1,
+                    target_mux_b2=target_mux_b2, target_muy_b2=target_muy_b2)
