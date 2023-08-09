3871ed64b8259c8700b39864c54aa21834399f71
giadarol <giovanni.iadarola@cern.ch>
2023-08-09 15:50:17 +0200

    Clean up

--- examples/match_optics/004_write_madx_optics_file.py
+++ examples/match_optics/004_write_madx_optics_file.py
@@ -129,3 +129,4 @@ assert np.isclose(mad.table.twb1.summary.dq1, 10.0, rtol=0, atol=0.3)
 assert np.isclose(mad.table.twb1.summary.dq2, 12.0, rtol=0, atol=0.3)
 assert np.isclose(mad.table.twb2.summary.dq1, 9.0, rtol=0, atol=0.3)
 assert np.isclose(mad.table.twb2.summary.dq2, 11.0, rtol=0, atol=0.3)
+
