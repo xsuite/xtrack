528d58697c0d8fc152654912497a0b7e428ee347
giadarol <giovanni.iadarola@cern.ch>
2023-08-10 12:22:10 +0200

    Move set of limits into a file

--- examples/match_optics/001_change_ip15_phase.py
+++ examples/match_optics/001_change_ip15_phase.py
@@ -6,13 +6,15 @@ import xtrack._temp.lhc_match as lm
 
 default_tol = {None: 1e-8, 'betx': 1e-6, 'bety': 1e-6} # to have no rematching w.r.t. madx
 
-collider = xt.Multiline.from_json('collider_01_with_knob_steps_and_limits.json')
+collider = xt.Multiline.from_json('collider_00_from_madx.json')
 collider.build_trackers()
 collider.vars.load_madx_optics_file(
     "../../test_data/hllhc15_thick/opt_round_150_1500.madx")
 
 collider.lhcb1.twiss_default['only_markers'] = True
 collider.lhcb2.twiss_default['only_markers'] = True
+
+lm.set_var_limits_and_steps(collider)
 
 tw0 = collider.twiss()
 
--- xtrack/_temp/lhc_match/__init__.py
+++ xtrack/_temp/lhc_match/__init__.py
@@ -1,1 +1,2 @@
 from .lhc_match import *
+from .var_limits import set_var_limits_and_steps
