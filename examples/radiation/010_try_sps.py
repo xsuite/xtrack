822929a685bb6c6bda8382f184da6d5bd2c4f66d
giadarol <giovanni.iadarola@cern.ch>
2023-08-12 15:18:58 +0200

    Also hofmann, vertical still off

--- examples/radiation/010_try_sps.py
+++ examples/radiation/010_try_sps.py
@@ -7,16 +7,14 @@ import xpart as xp
 mad = Madx()
 mad.call('../../test_data/sps_thick/sps.seq')
 
-mad.options.rbarc = True
-
 # mad.input('beam, particle=proton, pc=26;')
 # mad.input('beam, particle=electron, pc=20;')
 
-# mad.input('beam, particle=electron, pc=20;')
-# v_mv = 25
+mad.input('beam, particle=electron, pc=20;')
+v_mv = 25
 
-mad.input('beam, particle=electron, pc=50;')
-v_mv = 250
+# mad.input('beam, particle=electron, pc=50;')
+# v_mv = 250
 
 mad.call('../../test_data/sps_thick/lhc_q20.str')
 
@@ -29,7 +27,7 @@ mad.sequence.sps.elements['actcse.31632'].freq = 350 / 10  # having the same qs
 mad.sequence.sps.elements['actcse.31632'].lag = 0.5
 
 # # Some vertical orbit
-mad.sequence.sps.elements['mdv.10107'].kick = 10e-6
+mad.sequence.sps.elements['mdv.10107'].kick = 100e-6
 
 mad.input('twiss, table=tw6d;')
 twm6d = mad.table.tw6d
@@ -88,11 +86,11 @@ hy[mask] = (np.diff(tw.py)[mask] + hyl[mask]) / dl[mask]
 hh = np.sqrt(hx**2 + hy**2)
 
 integ_x = np.sum(dl
-    * np.abs(hh)**3 * np.squeeze(tw_rad.W_matrix[:-1, 4, 0]**2 + tw_rad.W_matrix[:-1, 4, 1]**2))
+    * np.abs(hh)**3 * np.squeeze(tw.W_matrix[:-1, 4, 0]**2 + tw.W_matrix[:-1, 4, 1]**2))
 integ_y = np.sum(dl
-    * np.abs(hh)**3 * np.squeeze(tw_rad.W_matrix[:-1, 4, 2]**2 + tw_rad.W_matrix[:-1, 4, 3]**2))
+    * np.abs(hh)**3 * np.squeeze(tw.W_matrix[:-1, 4, 2]**2 + tw.W_matrix[:-1, 4, 3]**2))
 integ_z = np.sum(dl
-    * np.abs(hh)**3 * np.squeeze(tw_rad.W_matrix[:-1, 4, 4]**2 + tw_rad.W_matrix[:-1, 4, 5]**2))
+    * np.abs(hh)**3 * np.squeeze(tw.W_matrix[:-1, 4, 4]**2 + tw.W_matrix[:-1, 4, 5]**2))
 
 mass0 = line.particle_ref.mass0
 q0 = line.particle_ref.q0
@@ -111,8 +109,23 @@ ez = clg * integ_z / alpha_damp_z
 
 line.configure_radiation(model='quantum')
 p = line.build_particles(num_particles=30)
-line.track(p, num_turns=400, time=True, turn_by_turn_monitor=True)
+line.track(p, num_turns=2000, time=True, turn_by_turn_monitor=True)
+print(f'Tracking time: {line.time_last_track}')
 
+twe = tw.rows[:-1]
+cur_H_x = twe.gamx * twe.dx**2 + 2 * twe.alfx * twe.dx * twe.dpx + twe.betx * twe.dpx**2
+I5_x  = np.sum(cur_H_x * hh**3 * dl)
+I2_x = np.sum(hh**2 * dl)
+I4_x = np.sum(twe.dx * hh**3 * dl) # to be generalized for combined function magnets
+
+cur_H_y = twe.gamy * twe.dy**2 + 2 * twe.alfy * twe.dy * twe.dpy + twe.bety * twe.dpy**2
+I5_y  = np.sum(cur_H_y * hh**3 * dl)
+I2_y = np.sum(hh**2 * dl)
+I4_y = np.sum(twe.dy * hh**3 * dl) # to be generalized for combined function magnets
+
+lam_comp = 2.436e-12 # [m]
+ex_hof = 55 * np.sqrt(3) / 96 * lam_comp / 2 / np.pi * gamma0**2 * I5_x / (I2_x - I4_x)
+ey_hof = 55 * np.sqrt(3) / 96 * lam_comp / 2 / np.pi * gamma0**2 * I5_y / (I2_y - I4_y)
 
 mon = line.record_last_track
 
@@ -122,10 +135,12 @@ fig = plt.figure(1)
 spx = fig. add_subplot(3, 1, 1)
 spx.plot(np.std(mon.x, axis=0))
 spx.axhline(np.sqrt(ex * tw.betx[0] + (np.std(p.delta) * tw.dx[0])**2), color='red')
+spx.axhline(np.sqrt(ex_hof * tw.betx[0] + (np.std(p.delta) * tw.dx[0])**2), color='green')
 
 spy = fig. add_subplot(3, 1, 2, sharex=spx)
 spy.plot(np.std(mon.y, axis=0))
 spy.axhline(np.sqrt(ey * tw.bety[0] + (np.std(p.delta) * tw.dy[0])**2), color='red')
+spy.axhline(np.sqrt(ey_hof * tw.bety[0] + (np.std(p.delta) * tw.dy[0])**2), color='green')
 
 spz = fig. add_subplot(3, 1, 3, sharex=spx)
 spz.plot(np.std(mon.zeta, axis=0))
