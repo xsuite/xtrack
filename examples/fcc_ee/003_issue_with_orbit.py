1bb3199013da2ade2a35312e3b74072c462564a3
giadarol <giovanni.iadarola@cern.ch>
2023-08-20 16:18:27 +0200

    Lots of wigglers

--- examples/fcc_ee/003_issue_with_orbit.py
+++ examples/fcc_ee/003_issue_with_orbit.py
@@ -7,54 +7,62 @@ varvals = line._xdeps_vref._owner.copy()
 
 # Add longer wigglers
 
-tt = line.get_table()
-tt_insertion = tt.rows['qrfr1.1_entry':'qrfl1.1_exit']
-mask_drift_q = tt_insertion.mask['drift_q.*']
-tt_wig_drifts = tt_insertion.rows[~mask_drift_q].rows['drift_.*']
+section_list = [
+    ('qrfr1.1_entry', 'qrfl1.1_exit'),
+    # ('qrfr1.2_entry', 'qrfl1.2_exit'), # taken by the RF
+    ('qrfr1.3_entry', 'qrfl1.3_exit'),
+    ('qrfr1.4_entry', 'qrfl1.4_exit'),
+]
 
 line.vars['k0l_long_wig'] = 0
+for iss, section in enumerate(section_list):
 
-for ii, nn in enumerate(tt_wig_drifts.name):
-    s_start_wig = tt['s', nn]
-    s_end_wig = tt['s', nn] + line[nn].length
+    tt = line.get_table()
+    tt_insertion = tt.rows[section[0]: section[1]]
+    mask_drift_q = tt_insertion.mask['drift_q.*']
+    tt_wig_drifts = tt_insertion.rows[~mask_drift_q].rows['drift_.*']
 
-    tag_wig = f'w{ii}'
+    for ii, nn in enumerate(tt_wig_drifts.name):
+        s_start_wig = tt['s', nn]
+        s_end_wig = tt['s', nn] + line[nn].length
 
-    s_wig_kicks = np.linspace(s_start_wig, s_end_wig, 30)
-    s_wig_plus = s_wig_kicks[:-1:2]
-    s_wig_minus = s_wig_kicks[1::2]
+        tag_wig = f'w{iss}_{ii}'
 
-    line.discard_tracker()
+        s_wig_kicks = np.linspace(s_start_wig, s_end_wig, 30)
+        s_wig_plus = s_wig_kicks[:-1:2]
+        s_wig_minus = s_wig_kicks[1::2]
 
-    kk_wig = f'k0l_wig.{tag_wig}'
-    line.vars[kk_wig] = line.vars['k0l_long_wig']
-    for ii, (ss_p, ss_m) in enumerate(zip(s_wig_plus, s_wig_minus)):
-        print(f'Wig {tag_wig} period {ii}/{len(s_wig_plus)}    ', end='\r', flush=True)
-        wmg_plus = xt.Multipole(knl=[0])
-        wmg_minus = xt.Multipole(knl=[0])
-        wmg_plus.length = ss_m - ss_p
-        wmg_minus.length = ss_m - ss_p
-        if ii == 0:
-            ss_p += 0.5 * wmg_plus.length
-        if ii == len(s_wig_plus) - 1:
-            ss_m -= 0.5 * wmg_plus.length
-        nn_plus = f'mwg.{tag_wig}.plus.{ii}'
-        nn_minus = f'mwg.{tag_wig}.minus.{ii}'
-        line.insert_element(nn_plus, wmg_plus, at_s=ss_p)
-        line.insert_element(nn_minus, wmg_minus, at_s=ss_m)
-        line.element_refs[nn_plus].ksl[0] = line.vars[kk_wig]
-        line.element_refs[nn_minus].ksl[0] = -line.vars[kk_wig]
-        if ii == 0:
-            line.element_refs[nn_plus].ksl[0] = line.vars[kk_wig] * 0.5
-        if ii == len(s_wig_plus) - 1:
-            line.element_refs[nn_minus].ksl[0] = -line.vars[kk_wig] * 0.5
+        line.discard_tracker()
+
+        kk_wig = f'k0l_wig.{tag_wig}'
+        line.vars[kk_wig] = line.vars['k0l_long_wig']
+        for ii, (ss_p, ss_m) in enumerate(zip(s_wig_plus, s_wig_minus)):
+            print(f'Wig {tag_wig} period {ii}/{len(s_wig_plus)}    ', end='\r', flush=True)
+            wmg_plus = xt.Multipole(knl=[0])
+            wmg_minus = xt.Multipole(knl=[0])
+            wmg_plus.length = ss_m - ss_p
+            wmg_minus.length = ss_m - ss_p
+            if ii == 0:
+                ss_p += 0.5 * wmg_plus.length
+            if ii == len(s_wig_plus) - 1:
+                ss_m -= 0.5 * wmg_plus.length
+            nn_plus = f'mwg.{tag_wig}.plus.{ii}'
+            nn_minus = f'mwg.{tag_wig}.minus.{ii}'
+            line.insert_element(nn_plus, wmg_plus, at_s=ss_p)
+            line.insert_element(nn_minus, wmg_minus, at_s=ss_m)
+            line.element_refs[nn_plus].ksl[0] = line.vars[kk_wig]
+            line.element_refs[nn_minus].ksl[0] = -line.vars[kk_wig]
+            if ii == 0:
+                line.element_refs[nn_plus].ksl[0] = line.vars[kk_wig] * 0.5
+            if ii == len(s_wig_plus) - 1:
+                line.element_refs[nn_minus].ksl[0] = -line.vars[kk_wig] * 0.5
 
 line.to_json('fccee_p_ring_thin_wig_long.json')
 
 line = line.from_json('fccee_p_ring_thin_wig_long.json')
 line.build_tracker()
 
-line.vars['k0l_long_wig'] = 1e-3
+line.vars['k0l_long_wig'] = 1e-4
 
 tw = line.twiss(method='4d')
 
