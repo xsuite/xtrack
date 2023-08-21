7749002d36421e4d44461385d2a031c53dff3836
giadarol <giovanni.iadarola@cern.ch>
2023-08-21 13:09:25 +0200

    Dogleg

--- examples/fcc_ee/003_issue_with_orbit.py
+++ examples/fcc_ee/003_issue_with_orbit.py
@@ -9,12 +9,14 @@ varvals = line._xdeps_vref._owner.copy()
 section_list = [
     ('qrfr1.1_entry', 'qrfl1.1_exit'),
     # ('qrfr1.2_entry', 'qrfl1.2_exit'), # taken by the RF
-    ('qrfr1.3_entry', 'qrfl1.3_exit'),
-    ('qrfr1.4_entry', 'qrfl1.4_exit'),
+    # ('qrfr1.3_entry', 'qrfl1.3_exit'),
+    # ('qrfr1.4_entry', 'qrfl1.4_exit'),
 ]
 
+
+n_slices = 5
 line.vars['k0l_long_wig'] = 0
-line.vars['wig_drift_fraction'] = 0.4
+line.vars['wig_drift_fraction'] = 0.7
 for iss, section in enumerate(section_list):
 
     tt = line.get_table()
@@ -23,12 +25,13 @@ for iss, section in enumerate(section_list):
     tt_wig_drifts = tt_insertion.rows[~mask_drift_q].rows['drift_.*']
 
     for ii, nn in enumerate(tt_wig_drifts.name):
+        if ii > 0: break # debug
         s_start_wig = tt['s', nn]
         s_end_wig = tt['s', nn] + line[nn].length
 
         tag_wig = f'w{iss}_{ii}'
 
-        s_wig_kicks = np.linspace(s_start_wig, s_end_wig, 60)
+        s_wig_kicks = np.linspace(s_start_wig, s_end_wig, 4) #60)
         s_wig_plus = s_wig_kicks[:-1:2]
         s_wig_minus = s_wig_kicks[1::2]
 
@@ -37,32 +40,40 @@ for iss, section in enumerate(section_list):
         kk_wig = f'k0l_wig.{tag_wig}'
         line.vars[kk_wig] = line.vars['k0l_long_wig']
         for ii, (ss_p, ss_m) in enumerate(zip(s_wig_plus, s_wig_minus)):
-            print(f'Wig {tag_wig} period {ii}/{len(s_wig_plus)}    ', end='\r', flush=True)
-            wmg_plus = xt.Multipole(knl=[0])
-            wmg_minus = xt.Multipole(knl=[0])
+            half_period = ss_m - ss_p
             if ii == 0:
-                ss_p += 0.5 * wmg_plus.length
+                ss_p += 0.5 * half_period
             if ii == len(s_wig_plus) - 1:
-                ss_m -= 0.5 * wmg_plus.length
-            nn_plus = f'mwg.{tag_wig}.plus.{ii}'
-            nn_minus = f'mwg.{tag_wig}.minus.{ii}'
-            line.insert_element(nn_plus, wmg_plus, at_s=ss_p)
-            line.insert_element(nn_minus, wmg_minus, at_s=ss_m)
-            line.element_refs[nn_plus].ksl[0] = line.vars[kk_wig]
-            line.element_refs[nn_minus].ksl[0] = -line.vars[kk_wig]
-            line.element_refs[nn_plus].length = (ss_m - ss_p) * (1 - line.vars['wig_drift_fraction'])
-            line.element_refs[nn_minus].length = (ss_m - ss_p) * (1 - line.vars['wig_drift_fraction'])
-            if ii == 0:
-                line.element_refs[nn_plus].ksl[0] = line.vars[kk_wig] * 0.5
-            if ii == len(s_wig_plus) - 1:
-                line.element_refs[nn_minus].ksl[0] = -line.vars[kk_wig] * 0.5
+                ss_m -= 0.5 * half_period
+
+            for i_slice in range(n_slices):
+                print(
+                    f'Wig {tag_wig} period {ii}/{len(s_wig_plus)} slice {i_slice}{n_slices}   ',
+                    end='\r', flush=True)
+                wmg_plus = xt.Multipole(knl=[0])
+                wmg_minus = xt.Multipole(knl=[0])
+
+                nn_plus = f'mwg.{tag_wig}.plus.{ii}..{i_slice}'
+                nn_minus = f'mwg.{tag_wig}.minus.{ii}..{i_slice}'
+                line.insert_element(nn_plus, wmg_plus,
+                    at_s=ss_p + i_slice * (1 - line.varval['wig_drift_fraction']) * half_period / n_slices)
+                line.insert_element(nn_minus, wmg_minus,
+                    at_s=ss_m + i_slice * (1 - line.varval['wig_drift_fraction']) * half_period / n_slices)
+                line.element_refs[nn_plus].ksl[0] = line.vars[kk_wig] / n_slices
+                line.element_refs[nn_minus].ksl[0] = -line.vars[kk_wig] / n_slices
+                line.element_refs[nn_plus].length = half_period * (1 - line.vars['wig_drift_fraction']) / n_slices
+                line.element_refs[nn_minus].length = half_period * (1 - line.vars['wig_drift_fraction']) / n_slices
+                if ii == 0:
+                    line.element_refs[nn_plus].ksl[0] = line.vars[kk_wig] / n_slices * 0.5
+                if ii == len(s_wig_plus) - 1:
+                    line.element_refs[nn_minus].ksl[0] = -line.vars[kk_wig] / n_slices * 0.5
 
 line.to_json('fccee_p_ring_thin_wig_long.json')
 
 line = line.from_json('fccee_p_ring_thin_wig_long.json')
 line.build_tracker()
 
-line.vars['k0l_long_wig'] = 1e-4
+line.vars['k0l_long_wig'] = 3e-4
 
 tw = line.twiss(method='4d')
 
@@ -74,15 +85,26 @@ ex = tw_rad.nemitt_x_rad / (tw_rad.gamma0 * tw_rad.beta0)
 ey = tw_rad.nemitt_y_rad / (tw_rad.gamma0 * tw_rad.beta0)
 ez = tw_rad.nemitt_zeta_rad / (tw_rad.gamma0 * tw_rad.beta0)
 
+print(f'ex = {ex:.2e} m, ey = {ey:.2e} m, ez = {ez:.2e} m')
+print('damping times: ',
+        tw_rad.damping_constants_s[0],
+        tw_rad.damping_constants_s[1],
+        tw_rad.damping_constants_s[2])
+
 import matplotlib.pyplot as plt
 plt.close('all')
-sp1 = plt.subplot(3,1,1)
+plt.figure(1, figsize=(6.4, 4.8*1.5))
+sp1 = plt.subplot(4,1,1)
 sp1.plot(tw.s, tw_rad.betx, label='betx')
 sp1.plot(tw.s, tw_rad.bety, label='bety')
 
-sp2 = plt.subplot(3,1,2, sharex=sp1)
+sp2 = plt.subplot(4,1,2, sharex=sp1)
 sp2.plot(tw.s, tw_rad.dx, label='dx')
 
-sp3 = plt.subplot(3,1,3, sharex=sp1)
-sp3.plot(tw.s, tw_rad.delta, label='delta')
+sp3 = plt.subplot(4,1,3, sharex=sp1)
+sp3.plot(tw.s, tw_rad.dy, label='dy')
+
+sp4 = plt.subplot(4,1,4, sharex=sp1)
+sp4.plot(tw.s, tw_rad.delta, label='delta')
+plt.subplots_adjust(hspace=0.33, bottom=0.07)
 plt.show()
