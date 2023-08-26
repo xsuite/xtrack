209bf5cec424f24d42a2212b7270f7c167f1c7de
giadarol <giovanni.iadarola@cern.ch>
2023-08-26 13:37:37 +0200

    Add docstring

--- xtrack/beam_elements/elements.py
+++ xtrack/beam_elements/elements.py
@@ -213,6 +213,20 @@ class Elens(BeamElement):
 
 
 class NonLinearLens(BeamElement):
+    '''
+    Beam element modeling a non-linear lens with elliptic potential.
+    See the corresponding element in MAD-X documentation.
+
+    Parameters
+    ----------
+    knll : float
+        Integrated strength of lens (m). The strength is parametrized so that
+        the quadrupole term of the multipole expansion is k1=2*knll/cnll^2.
+    cnll : float
+        Focusing strength (m).
+        The dimensional parameter of lens (m).
+        The singularities of the potential are located at x=-cnll, +cnll and y=0.
+    '''
 
     _xofields={
             'knll': xo.Float64,
