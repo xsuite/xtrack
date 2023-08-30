f4762dab71c3ba309a6ff31b8b42f86b161360a9
giadarol <giovanni.iadarola@cern.ch>
2023-08-30 22:27:00 +0200

    Update example

--- examples/fcc_ee/000_build_xsuite_model.py
+++ examples/fcc_ee/000_build_xsuite_model.py
@@ -49,7 +49,7 @@ slicing_strategies = [
     Strategy(slicing=Teapot(10), name=r'^qy.*'),
     Strategy(slicing=Teapot(50), name=r'^qa.*'),
     Strategy(slicing=Teapot(50), name=r'^qc.*'),
-    # Strategy(slicing=Teapot(20), name=r'^sy\..*'), # Not taken into account for now!!!!
+    Strategy(slicing=Teapot(20), name=r'^sy\..*'),
 ]
 
 line.slice_thick_elements(slicing_strategies=slicing_strategies)
