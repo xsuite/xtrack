064495b052634a83f4f4abdb9b415c9aa1776072
giadarol <giovanni.iadarola@cern.ch>
2023-09-22 10:35:06 +0200

    Status

--- examples/transfer_matrix/005_radiation.py
+++ examples/transfer_matrix/005_radiation.py
@@ -99,12 +99,12 @@ for turn in range(n_turns):
     emit_s[turn] = 0.5 * (part.zeta[0] ** 2 / beta_s + beta_s * part.delta[0] ** 2)
     el.track(part)
 
-plt.figure(0)
-plt.plot(x, px, "x")
-plt.figure(1)
-plt.plot(y, py, "x")
-plt.figure(2)
-plt.plot(z, delta, "x")
+# plt.figure(0)
+# plt.plot(x, px, "x")
+# plt.figure(1)
+# plt.plot(y, py, "x")
+# plt.figure(2)
+# plt.plot(z, delta, "x")
 
 turns = np.arange(n_turns)
 fit_range = 1000
@@ -126,8 +126,8 @@ length_0 = np.std(z[averga_start:])
 
 # Definition of damping rate
 eps_x_expected = emit_x[0] * np.exp(-damping_rate_x * turns)
-eps_y_expected = emit_y[0] * np.exp(-damping_rate_x * turns)
-eps_s_expected = emit_s[0] * np.exp(-damping_rate_x * turns)
+eps_y_expected = emit_y[0] * np.exp(-damping_rate_y * turns)
+eps_s_expected = emit_s[0] * np.exp(-damping_rate_s * turns)
 
 
 print(
@@ -136,17 +136,31 @@ print(
     emit_s_0 / equ_emit_s,
     length_0 / equ_length,
 )
+
+
 plt.figure(10)
+sp1 = plt.subplot(311)
 plt.plot(turns, emit_x)
-plt.plot(turns, np.exp(fit_x.intercept + fit_x.slope * turns), "--k")
-plt.plot(turns, eps_x_expected, "--r")
-plt.plot([turns[0], turns[-1]], [emit_x_0, emit_x_0], "--k")
+plt.plot(turns, np.exp(fit_x.intercept + fit_x.slope * turns), "--k", label="fit")
+plt.plot(turns, eps_x_expected, "--r", label="expected")
+plt.plot([turns[0], turns[-1]], [emit_x_0, emit_x_0], "--g", label="equilibrium")
+plt.ylabel(r"$\epsilon_x$ [m]")
+plt.legend()
+
+
+plt.subplot(312, sharex=sp1)
 plt.plot(turns, emit_y)
 plt.plot(turns, np.exp(fit_y.intercept + fit_y.slope * turns), "--k")
-plt.plot([turns[0], turns[-1]], [emit_y_0, emit_y_0], "--k")
-plt.figure(11)
+plt.plot(turns, eps_y_expected, "--r")
+plt.plot([turns[0], turns[-1]], [emit_y_0, emit_y_0], "--g")
+plt.ylabel(r"$\epsilon_y$ [m]")
+
+plt.subplot(313, sharex=sp1)
 plt.plot(turns, emit_s)
 plt.plot(turns, np.exp(fit_s.intercept + fit_s.slope * turns), "--k")
-plt.plot([turns[0], turns[-1]], [emit_s_0, emit_s_0], "--k")
+plt.plot(turns, eps_s_expected, "--r")
+plt.plot([turns[0], turns[-1]], [emit_s_0, emit_s_0], "--g")
+plt.ylabel(r"$\epsilon_s$ [m]")
+plt.xlabel("turn")
 
 plt.show()
