a019ba6ab48a8b3e2f7975c0ffbdc6579743df5b
giadarol <giovanni.iadarola@cern.ch>
2023-10-05 10:59:05 +0200

    Not too bad...

--- examples/taylor_map/000_taylor_map.py
+++ examples/taylor_map/000_taylor_map.py
@@ -5,20 +5,50 @@ line = xt.Line.from_json('../../test_data/hllhc15_thick/lhc_thick_with_knobs.jso
 line.vars['vrf400'] = 16
 line.vars['lagrf400.b1'] = 0.5
 
+line.vars['acbh22.l7b1'] = 10e-6
+line.vars['acbv21.l7b1'] = 10e-6
+
 tw = line.twiss()
 
 ele_start = 'ip3'
 ele_stop = 'ip4'
 twinit = tw.get_twiss_init(ele_start)
+twinit_out = tw.get_twiss_init(ele_stop)
 RR = line.compute_one_turn_matrix_finite_differences(
     ele_start=ele_start, ele_stop=ele_stop, particle_on_co=twinit.particle_on_co
     )['R_matrix']
 TT = line.compute_T_matrix(ele_start=ele_start, ele_stop=ele_stop,
                             particle_on_co=twinit.particle_on_co)
 
-smap = xt.SecondOrderTaylorMap(R=RR, T=TT)
+x_co_in = np.array([
+    twinit.particle_on_co.x[0],
+    twinit.particle_on_co.px[0],
+    twinit.particle_on_co.y[0],
+    twinit.particle_on_co.py[0],
+    twinit.particle_on_co.zeta[0],
+    twinit.particle_on_co.pzeta[0],
+])
 
-p_ref = line.build_particles(x=1e-4, px=2e-6, y=3e-4, py=4e-6, zeta=1e-4, delta=1e-4)
+x_co_out = np.array([
+    twinit_out.particle_on_co.x[0],
+    twinit_out.particle_on_co.px[0],
+    twinit_out.particle_on_co.y[0],
+    twinit_out.particle_on_co.py[0],
+    twinit_out.particle_on_co.zeta[0],
+    twinit_out.particle_on_co.pzeta[0],
+])
+
+R_T_fd = np.einsum('ijk,k->ij', TT, x_co_in)
+K_T_fd = R_T_fd @ x_co_in
+
+K_hat = x_co_out - RR @ x_co_in + K_T_fd
+
+RR_hat = RR - R_T_fd
+
+smap = xt.SecondOrderTaylorMap(R=RR_hat, T=TT, k=K_hat)
+
+p_ref = line.build_particles(
+    x=1e-4, px=2e-6, y=3e-4, py=4e-6, zeta=1e-4, delta=1e-4)
 p_map = p_ref.copy()
 p0 = p_ref.copy()
 
