433ddc8c350c6ef6a2abd7d752da33b9035fe21a
giadarol <giovanni.iadarola@cern.ch>
2023-10-12 15:01:45 +0200

    There seems to be a problem with time shift

--- examples/psb/004b_check_energy_program.py
+++ examples/psb/004b_check_energy_program.py
@@ -30,13 +30,35 @@ line.build_tracker()
 ep = xt.EnergyProgram(t_s=t_s, kinetic_energy0=E_kin_GeV*1e9)
 line.energy_program = ep
 
+line['br1.acwf7l1.1'].voltage = 2e6
+line['br1.acwf7l1.1'].frequency = 1e3
+
 
 p_test = line.build_particles()
 line.enable_time_dependent_vars = True
-n_turn_test = 10000
+n_turn_test = 5000
 monitor = xt.ParticlesMonitor(num_particles=len(p_test.zeta), start_at_turn=0,
                               stop_at_turn=n_turn_test)
 for ii in range(n_turn_test):
     if ii % 10 == 0:
         print(f'Tracking turn {ii}/{n_turn_test}     ', end='\r', flush=True)
     line.track(p_test, turn_by_turn_monitor=monitor)
+
+beta_at_turn = monitor.beta0[0, :]
+
+t_turn_ref = np.cumsum(line.get_length()/clight/beta_at_turn)
+E_kin_turn = line.particle_ref.mass0 * (monitor.gamma0[0, :] - 1)
+
+t_check = np.linspace(0, 20e-3, 1000)
+E_check = np.interp(t_check, t_turn_ref, E_kin_turn)
+E_check_ref = np.interp(t_check, t_s, E_kin_GeV*1e9)
+
+import matplotlib.pyplot as plt
+plt.close('all')
+plt.plot(beta_at_turn, '.')
+
+plt.figure()
+plt.plot(t_turn_ref, E_kin_turn)
+plt.plot(t_s, E_kin_GeV*1e9)
+
+plt.show()
