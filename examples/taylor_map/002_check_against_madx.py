4996431deeddd67dc697581da0df478980cbf99e
giadarol <giovanni.iadarola@cern.ch>
2023-10-14 11:44:29 +0200

    This passes!

--- examples/taylor_map/002_check_against_madx.py
+++ examples/taylor_map/002_check_against_madx.py
@@ -20,7 +20,7 @@ call,file="../../test_data/hllhc15_thick/hllhc_sequence.madx";
 seqedit,sequence=lhcb1;flatten;cycle,start=IP7;flatten;endedit;
 seqedit,sequence=lhcb2;flatten;cycle,start=IP7;flatten;endedit;
 beam, sequence=lhcb1, particle=proton, pc=7000;
-beam, sequence=lhcb2, particle=proton, pc=7000;
+beam, sequence=lhcb2, particle=proton, pc=7000, bv=-1;
 call,file="../../test_data/hllhc15_thick/opt_round_150_1500.madx";
 """)
 mad.globals.update(orbit_settings)
@@ -34,9 +34,6 @@ collider = xt.Multiline.from_json(
 collider.vars.update(orbit_settings)
 collider['lhcb1'].twiss_default['method'] = '4d'
 collider['lhcb2'].twiss_default['method'] = '4d'
-
-ele_start='ip3'
-ele_stop='ip4'
 
 mad.input('''
 select, flag=sectormap, pattern='ip';
@@ -55,58 +52,89 @@ twiss, sectormap, sectorpure, sectortable=secttab_b2;
 ''')
 sectmad_b2  = xd.Table(mad.table.secttab_b2)
 
-xs_map = xt.SecondOrderTaylorMap.from_line(
+xs_map_b1 = xt.SecondOrderTaylorMap.from_line(
     line=collider.lhcb1, ele_start='ip3', ele_stop='ip4')
 
-TT = xs_map.T
-RR = xs_map.R
-k = xs_map.k
+xs_map_b2 = xt.SecondOrderTaylorMap.from_line(
+    line=collider.lhcb2, ele_start='ip4', ele_stop='ip3')
 
-tw = collider.lhcb1.twiss()
-
-nemitt_x = 2.5e-6
-nemitt_y = 2.5e-6
-scale_in = [
-    np.sqrt(tw['betx', ele_start] * nemitt_x / tw['gamma0']),
-    np.sqrt(tw['gamx', ele_start] * nemitt_x / tw['gamma0']),
-    np.sqrt(tw['bety', ele_start] * nemitt_y / tw['gamma0']),
-    np.sqrt(tw['gamy', ele_start] * nemitt_y / tw['gamma0']),
-    0.05,
-    1e-3]
-
-scale_out = [
-    np.sqrt(tw['betx', ele_stop] * nemitt_x / tw['gamma0']),
-    np.sqrt(tw['gamx', ele_stop] * nemitt_x / tw['gamma0']),
-    np.sqrt(tw['bety', ele_stop] * nemitt_y / tw['gamma0']),
-    np.sqrt(tw['gamy', ele_stop] * nemitt_y / tw['gamma0']),
-    0.05,
-    1e-3]
-
-# Check T
+scale_factors = np.array([-1, -1, 1, 1, 1, 1])
 for ii in range(6):
-    for jj in range(6):
-        for kk in range(6):
-            scaled_tt = (TT[ii, jj, kk]
-                         / scale_out[ii] * scale_in[jj] * scale_in[kk])
-            scaled_tt_mad = (sectmad_b1[f't{ii+1}{jj+1}{kk+1}', ele_stop]
-                             / scale_out[ii] * scale_in[jj] * scale_in[kk])
-            # The following means that a change of one sigma in jj, kk results
+    xs_map_b2.T[ii, :, :] *= scale_factors[ii]
+    xs_map_b2.R[ii, :] *= scale_factors[ii]
+    xs_map_b2.k[ii] *= scale_factors[ii]
+
+for jj in range(6):
+    xs_map_b2.T[:, jj, :] *= scale_factors[jj]
+    xs_map_b2.R[:, jj] *= scale_factors[jj]
+
+for kk in range(6):
+    xs_map_b2.T[:, :, kk] *= scale_factors[kk]
+
+for line_name in ['lhcb1', 'lhcb2']:
+
+    if line_name == 'lhcb1':
+        xs_map = xs_map_b1
+        sectmad = sectmad_b1
+        ele_start = 'ip3'
+        ele_stop = 'ip4'
+    elif line_name == 'lhcb2':
+        xs_map = xs_map_b2
+        sectmad = sectmad_b2
+        ele_start = 'ip4'
+        ele_stop = 'ip3'
+    else:
+        raise ValueError(f'Unknown line_name: {line_name}')
+
+    TT = xs_map.T
+    RR = xs_map.R
+    k = xs_map.k
+
+    tw = collider.lhcb1.twiss()
+
+    nemitt_x = 2.5e-6
+    nemitt_y = 2.5e-6
+    scale_in = [
+        np.sqrt(tw['betx', ele_start] * nemitt_x / tw['gamma0']),
+        np.sqrt(tw['gamx', ele_start] * nemitt_x / tw['gamma0']),
+        np.sqrt(tw['bety', ele_start] * nemitt_y / tw['gamma0']),
+        np.sqrt(tw['gamy', ele_start] * nemitt_y / tw['gamma0']),
+        0.05,
+        1e-3]
+
+    scale_out = [
+        np.sqrt(tw['betx', ele_stop] * nemitt_x / tw['gamma0']),
+        np.sqrt(tw['gamx', ele_stop] * nemitt_x / tw['gamma0']),
+        np.sqrt(tw['bety', ele_stop] * nemitt_y / tw['gamma0']),
+        np.sqrt(tw['gamy', ele_stop] * nemitt_y / tw['gamma0']),
+        0.05,
+        1e-3]
+
+    # Check T
+    for ii in range(6):
+        for jj in range(6):
+            for kk in range(6):
+                scaled_tt = (TT[ii, jj, kk]
+                            / scale_out[ii] * scale_in[jj] * scale_in[kk])
+                scaled_tt_mad = (sectmad[f't{ii+1}{jj+1}{kk+1}', ele_stop]
+                                / scale_out[ii] * scale_in[jj] * scale_in[kk])
+                # The following means that a change of one sigma in jj, kk results
+                # in an error of less than 5e-4 sigmas on ii
+                assert np.isclose(scaled_tt, scaled_tt_mad, atol=5e-4, rtol=0)
+
+    # Check R
+    for ii in range(6):
+        for jj in range(6):
+            scaled_rr = RR[ii, jj] / scale_out[ii] * scale_in[jj]
+            scaled_rr_mad = sectmad[f'r{ii+1}{jj+1}', ele_stop] * (
+                                scale_in[jj] / scale_out[ii])
+            # The following means that a change of one sigma in jj results
             # in an error of less than 5e-4 sigmas on ii
-            assert np.isclose(scaled_tt, scaled_tt_mad, atol=5e-4, rtol=0)
+            assert np.isclose(scaled_rr, scaled_rr_mad, atol=5e-4, rtol=0)
 
-# Check R
-for ii in range(6):
-    for jj in range(6):
-        scaled_rr = RR[ii, jj] / scale_out[ii] * scale_in[jj]
-        scaled_rr_mad = sectmad_b1[f'r{ii+1}{jj+1}', ele_stop] * (
-                            scale_in[jj] / scale_out[ii])
-        # The following means that a change of one sigma in jj results
-        # in an error of less than 5e-4 sigmas on ii
-        assert np.isclose(scaled_rr, scaled_rr_mad, atol=5e-4, rtol=0)
-
-# Check k
-for ii in range(6):
-    scaled_k = k[ii] / scale_out[ii]
-    scaled_k_mad = sectmad_b1[f'k{ii+1}', ele_stop] / scale_out[ii]
-    # The following means that a the orbit kick is the same within 5e-5 sigmas
-    assert np.isclose(scaled_k, scaled_k_mad, atol=5e-5, rtol=0)
+    # Check k
+    for ii in range(6):
+        scaled_k = k[ii] / scale_out[ii]
+        scaled_k_mad = sectmad[f'k{ii+1}', ele_stop] / scale_out[ii]
+        # The following means that a the orbit kick is the same within 5e-5 sigmas
+        assert np.isclose(scaled_k, scaled_k_mad, atol=5e-5, rtol=0)
