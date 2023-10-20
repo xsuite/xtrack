b63b61723192315d365bb00b16bf5542c40bfa0a
giadarol <giovanni.iadarola@cern.ch>
2023-10-20 21:47:30 +0200

    To be cleaned up

--- examples/da_example/000_da_example.py
+++ examples/da_example/000_da_example.py
@@ -2,23 +2,36 @@ import numpy as np
 
 import xtrack as xt
 import xpart as xp
+import xobjects as xo
 
 line = xt.Line.from_json(
     '../../test_data/hllhc14_no_errors_with_coupling_knobs/line_b1.json')
 line.build_tracker()
 
+for arc in ['12', '23', '34', '45', '56', '67', '78', '81']:
+    line.vars[f'kod.a{arc}b1'] = 2.0
+    line.vars[f'kof.a{arc}b1'] = 2.0
+
+n_r = 50
+n_theta = 60
 x_normalized, y_normalized, r_xy, theta_xy = xp.generate_2D_polar_grid(
-    r_range=(0, 50.), # beam sigmas
-    theta_range=(0, np.pi/2), nr=50, ntheta=50)
+    r_range=(0, 40.), # beam sigmas
+    theta_range=(0, np.pi/2),
+    nr=n_r, ntheta=n_theta)
 
 delta_init = 0 # In case off-momentum DA is needed
 
 particles = line.build_particles(x_norm=x_normalized, px_norm=0,
                                  y_norm=y_normalized, py_norm=0,
-                                 nemitt_x=2e-6, nemitt_y=2e-6,
+                                 nemitt_x=3e-6, nemitt_y=3e-6,
                                  delta=delta_init)
 
-line.track(particles, num_turns=100, time=True)
+line.discard_tracker()
+line.build_tracker(_context=xo.ContextCpu(omp_num_threads='auto'))
+
+line.track(particles, num_turns=200, time=True)
+
+print(f'Tracked in {line.time_last_track} seconds')
 
 particles.sort(interleave_lost_particles=True)
 
@@ -26,3 +39,11 @@ import matplotlib.pyplot as plt
 plt.close('all')
 plt.figure(1)
 plt.scatter(x_normalized, y_normalized, c=particles.at_turn)
+
+plt.figure(2)
+plt.pcolormesh(
+    x_normalized.reshape(n_r, n_theta), y_normalized.reshape(n_r, n_theta),
+    particles.at_turn.reshape(n_r, n_theta), shading='gouraud')
+plt.colorbar()
+
+plt.show()
