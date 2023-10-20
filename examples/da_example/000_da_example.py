2367524ffb995dd887c92cd054428e5b25bffd76
giadarol <giovanni.iadarola@cern.ch>
2023-10-20 21:52:04 +0200

    Comments

--- examples/da_example/000_da_example.py
+++ examples/da_example/000_da_example.py
@@ -8,10 +8,12 @@ line = xt.Line.from_json(
     '../../test_data/hllhc14_no_errors_with_coupling_knobs/line_b1.json')
 line.build_tracker()
 
+# Switch on LHC octupole circuits to have a "low" dynamic aperture
 for arc in ['12', '23', '34', '45', '56', '67', '78', '81']:
     line.vars[f'kod.a{arc}b1'] = 2.0
     line.vars[f'kof.a{arc}b1'] = 2.0
 
+# Generate normalized particle cooridnates on a polar grid
 n_r = 50
 n_theta = 60
 x_normalized, y_normalized, r_xy, theta_xy = xp.generate_2D_polar_grid(
@@ -19,22 +21,28 @@ x_normalized, y_normalized, r_xy, theta_xy = xp.generate_2D_polar_grid(
     theta_range=(0, np.pi/2),
     nr=n_r, ntheta=n_theta)
 
+# Set initial momentum deviation
 delta_init = 0 # In case off-momentum DA is needed
 
+# Match particles to the machine optics and orbit
 particles = line.build_particles(x_norm=x_normalized, px_norm=0,
                                  y_norm=y_normalized, py_norm=0,
                                  nemitt_x=3e-6, nemitt_y=3e-6,
                                  delta=delta_init)
 
+# Activate multi-core CPU parallelization
 line.discard_tracker()
 line.build_tracker(_context=xo.ContextCpu(omp_num_threads='auto'))
 
+# Track
 line.track(particles, num_turns=200, time=True)
 
 print(f'Tracked in {line.time_last_track} seconds')
 
+# Sort particles to initial order
 particles.sort(interleave_lost_particles=True)
 
+# Plot
 import matplotlib.pyplot as plt
 plt.close('all')
 plt.figure(1)
