97237c8dab0e001f4b813525a95a9adf42d5d9f5
giadarol <giovanni.iadarola@cern.ch>
2023-11-09 21:13:00 +0100

    Generated first knobs

--- examples/match_optics/001b_match_on_disp.py
+++ examples/match_optics/001b_match_on_disp.py
@@ -35,31 +35,70 @@ h_correctors_ip5_b2 = ['acbh12.r5b2', 'acbh14.r5b2', 'acbh16.l6b2',
 v_correctors_ip5_b2 = ['acbv16.r4b2', 'acbv14.l5b2', 'acbv12.l5b2',
                        'acbv13.r5b2', 'acbv15.r5b2', 'acbv15.l6b2']
 
+correctors = {}
+correctors['ip1_x_b1'] = h_correctors_ip1_b1
+correctors['ip1_y_b1'] = v_correctors_ip1_b1
+correctors['ip5_x_b1'] = h_correctors_ip5_b1
+correctors['ip5_y_b1'] = v_correctors_ip5_b1
+correctors['ip1_x_b2'] = h_correctors_ip1_b2
+correctors['ip1_y_b2'] = v_correctors_ip1_b2
+correctors['ip5_x_b2'] = h_correctors_ip5_b2
+correctors['ip5_y_b2'] = v_correctors_ip5_b2
+
+# Wipe corrector strengths
+for kk in correctors:
+    for cc in correctors[kk]:
+        collider.vars[cc] = 0
+
 acb_limits = (-800.e-6, 800e-6)
 
+knobs_to_compensate = {
+    'on_x5vl': dict(value=295, ip='ip5', plane='y'),
+    'on_x5hl': dict(value=295, ip='ip5', plane='x'),
+}
 
+for line_name in ['lhcb1', 'lhcb2']:
+    line = collider[line_name]
+    line.cycle('ip3', inplace=True)
 
-line = collider.lhcb1
-tw_ref = line.twiss()
-opt = line.match_knob(
-    run=False,
-    knob_name='on_dx5vl',
-    ele_start='ip4', ele_stop='ip6',
-    twiss_init='preserve_start', table_for_twiss_init=tw_ref,
-    vary=[
-        xt.VaryList(v_correctors_ip5_b1, step=1e-10, limits=acb_limits),
-        ],
-    targets=[
-        # Constraints on dispersion
-        xt.Target('dy', value=tw_ref, at='ip5', tol=1e-6),
-        xt.Target('dy', value=tw_ref, at='ip6', tol=1e-6),
-        # Constraints on orbit
-        xt.TargetSet(['y', 'py'], value=tw_ref, at='e.ds.l5.b1'),
-        xt.TargetSet(['y', 'py'], value=tw_ref, at='e.ds.l6.b1'),
-    ],
-)
+for kk in knobs_to_compensate:
 
-temp_expr = line.vars['on_x5vl']._expr
-line.vars['on_x5vl'] = 295.
-opt.solve()
-line.vars['on_x5vl'] = temp_expr
+    disp_knob_name = kk.replace('on_', 'on_d')
+
+    plane = knobs_to_compensate[kk]['plane']
+    ip = knobs_to_compensate[kk]['ip']
+    ref_val = knobs_to_compensate[kk]['value']
+
+    for line_name in ['lhcb1', 'lhcb2']:
+        line = collider[line_name]
+        beam_name = line_name[-2:]
+
+        left_ip = {'ip1': 'ip8', 'ip5': 'ip4'}[ip]
+        right_ip = {'ip1': 'ip2', 'ip5': 'ip6'}[ip]
+
+        tw_ref = line.twiss()
+        opt = line.match_knob(
+            run=False,
+            knob_name=disp_knob_name,
+            knob_value_start=0, knob_value_end=ref_val,
+            ele_start=left_ip, ele_stop=right_ip,
+            twiss_init='preserve_start', table_for_twiss_init=tw_ref,
+            vary=[
+                xt.VaryList(correctors[f'{ip}_{plane}_{beam_name}'],
+                            step=1e-10, limits=acb_limits),
+                ],
+            targets=[
+                # Constraints on dispersion
+                xt.Target('d' + plane, value=tw_ref, at='ip5', tol=1e-6),
+                xt.Target('d' + plane, value=tw_ref, at='ip6', tol=1e-6),
+                # Constraints on orbit
+                xt.TargetSet([plane, 'p' + plane], value=tw_ref, at='e.ds.l5.' + beam_name),
+                xt.TargetSet([plane, 'p' + plane], value=tw_ref, at='e.ds.l6.' + beam_name),
+            ],
+        )
+
+        temp_expr = line.vars[kk]._expr
+        line.vars[kk] = ref_val
+        opt.solve()
+        line.vars[kk] = temp_expr
+        opt.generate_knob()
