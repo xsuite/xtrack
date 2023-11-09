5121594c92cddd1e71ed7ea598932ea24fc45403
giadarol <giovanni.iadarola@cern.ch>
2023-11-09 17:02:26 +0100

    Same number of vary and constraints

--- examples/match_optics/001b_match_on_disp.py
+++ examples/match_optics/001b_match_on_disp.py
@@ -41,9 +41,38 @@ v_correctors_ip5_b1 = ['acbv15.r4b1', 'acbv15.l5b1', 'acbv13.l5b1',
 
 h_correctors_ip1_b2 = ['acbh15.r8b2', 'acbh15.l1b2', 'acbh13.l1b2',
                        'acbh12.r1b2', 'acbh14.r1b2', 'acbh16.l2b2']
-v_correctors_ip1_b2 = ['acbv16.r8b2', 'acbv14.l1b2', 'acbv12.l1b2'
+v_correctors_ip1_b2 = ['acbv16.r8b2', 'acbv14.l1b2', 'acbv12.l1b2',
                        'acbv13.r1b2', 'acbv15.r1b2', 'acbv15.l2b2']
 h_correctors_ip5_b2 = ['acbh12.r5b2', 'acbh14.r5b2', 'acbh16.l6b2',
                        'acbh15.r4b2', 'acbh15.l5b2', 'acbh13.l5b2']
-v_correctors_ip5_b2 = ['acbv16.r4b2', 'acbv14.l5b2', 'acbv12.l5b2'
+v_correctors_ip5_b2 = ['acbv16.r4b2', 'acbv14.l5b2', 'acbv12.l5b2',
                        'acbv13.r5b2', 'acbv15.r5b2', 'acbv15.l6b2']
+
+acb_limits = (-800.e-6, 800e-6)
+
+tw_ref = collider.twiss()
+
+opt_dx5vl = collider.match_knob(
+    run=False,
+    knob_name='on_dx5vl',
+    ele_start='ip4', ele_stop='ip6',
+    twiss_init='preserve_start', table_for_twiss_init=[tw_ref.lhcb1, tw_ref.lhcb2],
+    vary=[
+        xt.VaryList(v_correctors_ip5_b1, step=1e-10, limits=acb_limits),
+        xt.VaryList(v_correctors_ip5_b1, step=1e-10, limits=acb_limits),
+        xt.VaryList(v_correctors_ip5_b2, step=1e-10, limits=acb_limits),
+        xt.VaryList(v_correctors_ip5_b2, step=1e-10, limits=acb_limits),
+        ],
+    targets=[
+        # Constraints on dispersion
+        xt.TargetSet(['dx', 'dy'], line='lhcb1', value=tw_ref.lhcb1, at='ip5'),
+        xt.TargetSet(['dx', 'dy'], line='lhcb2', value=tw_ref.lhcb2, at='ip5'),
+        xt.TargetSet(['dx', 'dy'], line='lhcb1', value=tw_ref.lhcb1, at=xt.END),
+        xt.TargetSet(['dx', 'dy'], line='lhcb2', value=tw_ref.lhcb2, at=xt.END),
+        # Constraints on orbit
+        xt.TargetSet(['x', 'px', 'y', 'py'], line='lhcb1', value=tw_ref.lhcb1, at='e.ds.l5.b1'),
+        xt.TargetSet(['x', 'px', 'y', 'py'], line='lhcb2', value=tw_ref.lhcb2, at='e.ds.l5.b2'),
+        xt.TargetSet(['x', 'px', 'y', 'py'], line='lhcb1', value=tw_ref.lhcb1, at='e.ds.l6.b1'),
+        xt.TargetSet(['x', 'px', 'y', 'py'], line='lhcb2', value=tw_ref.lhcb2, at='e.ds.l6.b2'),
+    ],
+)
