89b495a52e40bb9d0f61456be71b57b3409eafc3
giadarol <giovanni.iadarola@cern.ch>
2023-11-13 07:04:39 +0100

    Finalize example

--- examples/lhc_thick/003_insert_thin_elements_after_makethin.py
+++ examples/lhc_thick/003_insert_thin_elements_after_makethin.py
@@ -19,5 +19,17 @@ for ii, (nn, ss) in enumerate(zip(tt_active.name, tt_active.s)):
     mm = xt.Marker()
     insertions.append((ss, [(f'aper_{nn}', aa), (f'marker_{nn}', mm)]))
 
+# Generate an aperture and a marker to be added every 100 m
+for ss in np.arange(100, tt.s[-1], 100):
+    aa = xt.LimitRect(min_x=-0.2, max_x=0.2, min_y=-0.2, max_y=0.2)
+    mm = xt.Marker()
+    insertions.append((ss, [(f'aper_{ss}', aa), (f'marker_{ss}', mm)]))
+
 line.discard_tracker()
+
+# Insert all
 line._insert_thin_elements_at_s(insertions)
+
+# Inspect:
+tt_after = line.get_table()
+tt_after.rows['marker_20000.0%%-30': 'marker_20000.0%%+30'].show()
