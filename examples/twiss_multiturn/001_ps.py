5f772fa3c12167194378b1290b63cb8b3906dfe2
giadarol <giovanni.iadarola@cern.ch>
2023-11-27 11:41:32 +0100

    PS loaded

--- examples/twiss_multiturn/001_ps.py
+++ examples/twiss_multiturn/001_ps.py
@@ -1,5 +1,24 @@
 from cpymad.madx import Madx
+import xtrack as xt
 
 mad = Madx()
-mad.call('ps_hs_sftpro.madx_job')
+mad.input("""
+beam, particle=proton, pc = 14.0;
+BRHO      = BEAM->PC * 3.3356;
+""")
+mad.call("ps.seq")
+mad.call("ps_hs_sftpro.str")
+mad.use('ps')
+twm = mad.twiss()
 
+line = xt.Line.from_madx_sequence(mad.sequence.ps, allow_thick=True,
+                                  deferred_expressions=True,
+                                  replace_in_expr={'->':'__madarrow__'})
+for kk in line.vars.keys():
+    if '__madarrow__' in kk:
+        mad_expr = kk.replace('__madarrow__', '->')
+        mad.input(f'{kk} = {mad_expr}')
+        line.vars[kk] = mad.globals[kk]
+
+line.particle_ref = xt.Particles(mass0=xt.PROTON_MASS_EV,
+                                    q0=1, gamma0=mad.sequence.ps.beam.gamma)
