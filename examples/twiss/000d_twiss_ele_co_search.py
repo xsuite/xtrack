4c12028aa11051f7b377a9d2b01de4e577d01595
giadarol <giovanni.iadarola@cern.ch>
2023-12-17 09:00:26 +0100

    Clean up example

--- examples/twiss/000d_twiss_ele_co_search.py
+++ examples/twiss/000d_twiss_ele_co_search.py
@@ -16,17 +16,19 @@ collider.lhcb1.twiss_default['method'] = '4d'
 collider.lhcb2.twiss_default['method'] = '4d'
 collider.lhcb2.twiss_default['reverse'] = True
 
-# Twiss B1 and B2 from IP1
-two = collider.twiss(ele_start='ip7', ele_stop='ip3', ele_init='ip1',
+line = collider.lhcb2
+line_name = 'lhcb2'
+
+line = collider.lhcb1
+line_name = 'lhcb1'
+
+two = line.twiss(ele_start='ip7', ele_stop='ip3', ele_init='ip1',
                  betx=.15, bety=.15)
 
-# Twiss B1 only from IP1
-two1 = collider.lhcb1.twiss(ele_start='ip7', ele_stop='ip3', ele_init='ip1',
-                            betx=.15, bety=.15)
 
 import matplotlib.pyplot as plt
 plt.close('all')
 plt.figure(1)
-plt.plot(two.lhcb1.s, two.lhcb1.betx, label='betx')
-plt.plot(two.lhcb2.s, two.lhcb2.betx, label='betx')
+plt.plot(two.s, two.betx, label='betx')
+plt.plot(two.s, two.bety, label='bety')
 plt.show()
