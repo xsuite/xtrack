7143948a02fdac241b242cd194b8051d536aca2a
giadarol <giovanni.iadarola@cern.ch>
2023-12-21 14:34:51 +0100

    There seems to be a bug

--- examples/match_basics/002_match_bump_basic.py
+++ examples/match_basics/002_match_bump_basic.py
@@ -5,13 +5,34 @@ line = xt.Line.from_json('../../test_data/hllhc15_thick/lhc_thick_with_knobs.jso
 line.build_tracker()
 
 opt = line.match(
-    ele_start='mq.33l8.b1', ele_stop='mq.23l8.b1',
-    method='4d',
-    vary=[
-        xt.VaryList(['kqtf.b1', 'kqtd.b1'], step=1e-8, tag='quad'),
-        xt.VaryList(['ksf.b1', 'ksd.b1'], step=1e-4, limits=[-0.1, 0.1], tag='sext'),
-    ],
+    ele_start='mq.33l8.b1', ele_stop='mq.23l8.b1', ele_init='mq.33l8.b1',
+    betx=1, bety=1, y=0, py=0, # conditions at ele_init
+    vary=xt.VaryList(['acbv30.l8b1', 'acbv28.l8b1', 'acbv26.l8b1', 'acbv24.l8b1'],
+                    step=1e-10, limits=[-1e-3, 1e-3]),
     targets = [
-        xt.TargetSet(qx=62.315, qy=60.325, tol=1e-6, tag='tune'),
-        xt.TargetSet(dqx=10.0, dqy=12.0, tol=0.01, tag='chrom'),
+        xt.TargetSet(y=3e-3, py=0, at='mb.b28l8.b1'),
+        xt.TargetSet(y=0, py=0, at=xt.END)
     ])
+
+import matplotlib.pyplot as plt
+
+tw = line.twiss()
+
+plt.close('all')
+fig = plt.figure(1, figsize=(6.4*1.2, 4.8*0.8))
+ax = fig.add_subplot(111)
+ax.plot(tw.s, tw.y*1000, label='y')
+ax.axvline(x=line.get_s_position('mb.b28l8.b1'), color='r', linestyle='--', alpha=0.5)
+ax.axvline(x=line.get_s_position('mcbv.30l8.b1'), color='k', linestyle='--', alpha=0.5)
+ax.axvline(x=line.get_s_position('mcbv.28l8.b1'), color='k', linestyle='--', alpha=0.5)
+ax.axvline(x=line.get_s_position('mcbv.26l8.b1'), color='k', linestyle='--', alpha=0.5)
+ax.axvline(x=line.get_s_position('mcbv.24l8.b1'), color='k', linestyle='--', alpha=0.5)
+ax.axvline(x=line.get_s_position('mq.33l8.b1'), color='g', linestyle='--', alpha=0.5)
+ax.axvline(x=line.get_s_position('mq.23l8.b1'), color='g', linestyle='--', alpha=0.5)
+ax.set_xlim(line.get_s_position('mq.33l8.b1') - 10,
+            line.get_s_position('mq.23l8.b1') + 10)
+ax.set_xlabel('s [m]')
+ax.set_ylabel('y [mm]')
+ax.set_ylim(-0.5, 10)
+plt.subplots_adjust(bottom=.152, top=.9, left=.1, right=.95)
+plt.show()
