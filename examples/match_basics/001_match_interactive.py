23d56067d7428526a548cf54cb872574790cbb59
giadarol <giovanni.iadarola@cern.ch>
2023-12-21 13:35:22 +0100

    Update example

--- examples/match_basics/001_match_interactive.py
+++ examples/match_basics/001_match_interactive.py
@@ -68,7 +68,6 @@ opt.solve()
 
 opt.target_status()
 # prints:
-#
 # Target status:
 # id state tag   tol_met      residue current_val target_val description
 #  0 ON    tune     True -5.62885e-10      62.315     62.315 'qx', val=62.315, tol=1e-06, weight=10)
@@ -82,7 +81,6 @@ opt.targets[1].tol = 1e-10
 
 opt.target_status()
 # prints:
-#
 # Target status:
 # id state tag   tol_met      residue current_val target_val description
 #  0 ON    tune     True -5.62885e-10      62.315     62.315 'qx', val=62.315, tol=1e-06, weight=10)
@@ -95,7 +93,6 @@ opt.step(2)
 
 opt.target_status()
 # prints (two steps were not enough to reach convergence):
-#
 # Target status:
 # id state tag   tol_met      residue current_val target_val description
 #  0 ON    tune     True  4.55631e-08      62.315     62.315 'qx', val=62.315, tol=1e-06, weight=10)
@@ -115,3 +112,28 @@ opt.target_status()
 #  1 ON    tune     True  1.42109e-14       60.05      60.05 'qy', val=60.05, tol=1e-10, weight=10)
 #  2 ON    chrom    True  3.19579e-07          10         10 'dqx', val=10, tol=0.01, weight=1)
 #  3 ON    chrom    True -1.30694e-09          12         12 'dqy', val=12, tol=0.01, weight=1)
+
+# Tag present configuration
+opt.add_point_to_log(tag='my_tag')
+
+# Reload initial configuration
+opt.reload(iteration=0)
+opt.target_status()
+# prints:
+#
+# Target status:
+# id state tag   tol_met     residue current_val target_val description
+#  0 ON    tune    False -0.00499997       62.31     62.315 'qx', val=62.315, tol=1e-06, weight=10
+#  1 ON    tune    False        0.27       60.32      60.05 'qy', val=60.05, tol=1e-10, weight=10
+#  2 ON    chrom   False    -8.09005     1.90995         10 'dqx', val=10, tol=0.01, weight=1
+#  3 ON    chrom   False     -10.057     1.94297         12 'dqy', val=12, tol=0.01, weight=1
+
+# Reload configuration tagged configuration
+opt.reload(tag='my_tag')
+opt.target_status()
+# Target status:
+# id state tag   tol_met      residue current_val target_val description
+#  0 ON    tune     True -4.00533e-11      62.315     62.315 'qx', val=62.315, tol=1e-06, weight=10
+#  1 ON    tune     True  1.42109e-14       60.05      60.05 'qy', val=60.05, tol=1e-10, weight=10
+#  2 ON    chrom    True  3.19579e-07          10         10 'dqx', val=10, tol=0.01, weight=1
+#  3 ON    chrom    True -1.30694e-09          12         12 'dqy', val=12, tol=0.01, weight=1
