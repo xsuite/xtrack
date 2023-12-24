ffa14e472e2967dc2f3d0885fcbe59900454fc16
giadarol <giovanni.iadarola@cern.ch>
2023-12-24 14:59:18 +0100

    Rematching particles at each iteration

--- examples/match_basics/006_match_action.py
+++ examples/match_basics/006_match_action.py
@@ -9,27 +9,35 @@ class ActionMeasAmplDet(xt.Action):
     def __init__(self, line, nemitt_x, nemitt_y, num_turns=256,
                  a0_sigmas=0.01, a1_sigmas=0.1, a2_sigmas=0.2):
 
+        self.a0_sigmas = a0_sigmas
+        self.a1_sigmas = a1_sigmas
+        self.a2_sigmas = a2_sigmas
+
         self.line = line
         self.num_turns = num_turns
         self.nemitt_x = nemitt_x
         self.nemitt_y = nemitt_y
 
-        self.Jx_0 = a0_sigmas**2 * nemitt_x / 2
-        self.Jx_1 = a1_sigmas**2 * nemitt_x / 2
-        self.Jx_2 = a2_sigmas**2 * nemitt_x / 2
-        self.Jy_0 = a0_sigmas**2 * nemitt_y / 2
-        self.Jy_1 = a1_sigmas**2 * nemitt_y / 2
-        self.Jy_2 = a2_sigmas**2 * nemitt_y / 2
 
-        self.particles0 = line.build_particles(
+    def run(self):
+
+        a0_sigmas = self.a0_sigmas
+        a1_sigmas = self.a1_sigmas
+        a2_sigmas = self.a2_sigmas
+
+        Jx_0 = a0_sigmas**2 * self.nemitt_x / 2
+        Jx_1 = a1_sigmas**2 * self.nemitt_x / 2
+        Jx_2 = a2_sigmas**2 * self.nemitt_x / 2
+        Jy_0 = a0_sigmas**2 * self.nemitt_y / 2
+        Jy_1 = a1_sigmas**2 * self.nemitt_y / 2
+        Jy_2 = a2_sigmas**2 * self.nemitt_y / 2
+
+        particles = self.line.build_particles(
                             method='4d',
                             zeta=0, delta=0,
                             x_norm=[a1_sigmas, a2_sigmas, a0_sigmas, a0_sigmas],
                             y_norm=[a0_sigmas, a0_sigmas, a1_sigmas, a2_sigmas],
-                            nemitt_x=nemitt_x, nemitt_y=nemitt_y)
-
-    def run(self):
-        particles = self.particles0.copy()
+                            nemitt_x=self.nemitt_x, nemitt_y=self.nemitt_y)
 
         self.line.track(particles,
                         num_turns=self.num_turns, time=True,
@@ -45,10 +53,10 @@ class ActionMeasAmplDet(xt.Action):
             qx[ii] = nl.get_tune(mon.x[ii, :])
             qy[ii] = nl.get_tune(mon.y[ii, :])
 
-        det_xx = (qx[1] - qx[0]) / (self.Jx_2 - self.Jx_1)
-        det_yy = (qy[3] - qy[2]) / (self.Jy_2 - self.Jy_1)
-        det_xy = (qx[3] - qx[2]) / (self.Jy_2 - self.Jy_1)
-        det_yx = (qy[1] - qy[0]) / (self.Jx_2 - self.Jx_1)
+        det_xx = (qx[1] - qx[0]) / (Jx_2 - Jx_1)
+        det_yy = (qy[3] - qy[2]) / (Jy_2 - Jy_1)
+        det_xy = (qx[3] - qx[2]) / (Jy_2 - Jy_1)
+        det_yx = (qy[1] - qy[0]) / (Jx_2 - Jx_1)
 
         return {'det_xx': det_xx, 'det_yy': det_yy,
                 'det_xy': det_xy, 'det_yx': det_yx}
