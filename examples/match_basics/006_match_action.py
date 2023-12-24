20b5539a6f9a2d4c5a65163ecb0702de102e02aa
giadarol <giovanni.iadarola@cern.ch>
2023-12-24 09:06:08 +0100

    Measured reasanable amplitude detuning

--- examples/match_basics/006_match_action.py
+++ examples/match_basics/006_match_action.py
@@ -8,15 +8,15 @@ line.build_tracker()
 nemitt_x = 2.5e-6
 nemitt_y = 2.5e-6
 
-line.vars['kof.a23b1'] = 1e4
-line.vars['kof.a34b1'] = 1e4
-line.vars['kof.a67b1'] = 1e4
-line.vars['kof.a78b1'] = 1e4
+line.vars['kof.a23b1'] = 1e3
+line.vars['kof.a34b1'] = 1e3
+line.vars['kof.a67b1'] = 1e3
+line.vars['kof.a78b1'] = 1e3
 
-line.vars['kod.a23b2'] = 1e4
-line.vars['kod.a34b2'] = 1e4
-line.vars['kod.a67b2'] = 1e4
-line.vars['kod.a78b2'] = 1e4
+line.vars['kod.a23b1'] = 1e3
+line.vars['kod.a34b1'] = 1e3
+line.vars['kod.a67b1'] = 1e3
+line.vars['kod.a78b1'] = 1e3
 
 a1_sigmas = .5
 a2_sigmas = .6
@@ -30,12 +30,13 @@ Jy_2 = a2_sigmas**2 * nemitt_y / 2
 particles = line.build_particles(
                     method='4d',
                     zeta=0, delta=0,
-                    x_norm=[a1_sigmas, a2_sigmas, a1_sigmas, a1_sigmas],
-                    y_norm=[a1_sigmas, a1_sigmas, a1_sigmas, a2_sigmas],
+                    x_norm=[a1_sigmas, a2_sigmas, 0.001, 0.001],
+                    y_norm=[0.001, 0.001, a1_sigmas, a2_sigmas],
                     nemitt_x=nemitt_x, nemitt_y=nemitt_y)
 
 line.track(particles, num_turns=num_turns, time=True, turn_by_turn_monitor=True)
 
+assert np.all(particles.state > 0)
 # self.x_norm_2d = np.sqrt(2 * self.Jx_2d / nemitt_x)
 # self.y_norm_2d = np.sqrt(2 * self.Jy_2d / nemitt_y)
 
@@ -53,3 +54,5 @@ alpha_xx = (qx[1] - qx[0]) / (Jx_2 - Jx_1)
 alpha_yy = (qy[3] - qy[2]) / (Jy_2 - Jy_1)
 alpha_xy = (qx[3] - qx[2]) / (Jy_2 - Jy_1)
 alpha_yx = (qy[1] - qy[0]) / (Jx_2 - Jx_1)
+
+
