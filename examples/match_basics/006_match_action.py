0627be1c91532702053c653967a7a3b14fec6637
giadarol <giovanni.iadarola@cern.ch>
2023-12-24 12:40:22 +0100

    Matrix less symmetric

--- examples/match_basics/006_match_action.py
+++ examples/match_basics/006_match_action.py
@@ -18,8 +18,8 @@ line.vars['kod.a34b1'] = 1e3
 line.vars['kod.a67b1'] = 1e3
 line.vars['kod.a78b1'] = 1e3
 
-a1_sigmas = .5
-a2_sigmas = .6
+a1_sigmas = .1
+a2_sigmas = .2
 num_turns = 256
 
 Jx_1 = a1_sigmas**2 * nemitt_x / 2
@@ -30,8 +30,8 @@ Jy_2 = a2_sigmas**2 * nemitt_y / 2
 particles = line.build_particles(
                     method='4d',
                     zeta=0, delta=0,
-                    x_norm=[a1_sigmas, a2_sigmas, 0.001, 0.001],
-                    y_norm=[0.001, 0.001, a1_sigmas, a2_sigmas],
+                    x_norm=[a1_sigmas, a2_sigmas, a1_sigmas],
+                    y_norm=[a1_sigmas, a1_sigmas, a2_sigmas],
                     nemitt_x=nemitt_x, nemitt_y=nemitt_y)
 
 line.track(particles, num_turns=num_turns, time=True, turn_by_turn_monitor=True)
@@ -50,9 +50,17 @@ fft_y = np.fft.rfft(mon.y, n=n_fft)
 qx = freq_axis[np.argmax(np.abs(fft_x), axis=1)]
 qy = freq_axis[np.argmax(np.abs(fft_y), axis=1)]
 
-alpha_xx = (qx[1] - qx[0]) / (Jx_2 - Jx_1)
-alpha_yy = (qy[3] - qy[2]) / (Jy_2 - Jy_1)
-alpha_xy = (qx[3] - qx[2]) / (Jy_2 - Jy_1)
-alpha_yx = (qy[1] - qy[0]) / (Jx_2 - Jx_1)
+Dq = np.array([[qx[1] - qx[0], qx[2] - qx[0]],
+               [qy[1] - qy[0], qy[2] - qy[0]]])
+
+DJ = np.array([[Jx_2 - Jx_1 , 0            ],
+                [0          , Jy_2 - Jy_1 ]])
+
+DD = Dq @ np.linalg.inv(DJ)
+
+# alpha_xx = (qx[1] - qx[0]) / (Jx_2 - Jx_1)
+# alpha_yy = (qy[3] - qy[2]) / (Jy_2 - Jy_1)
+# alpha_xy = (qx[3] - qx[2]) / (Jy_2 - Jy_1)
+# alpha_yx = (qy[1] - qy[0]) / (Jx_2 - Jx_1)
 
 
