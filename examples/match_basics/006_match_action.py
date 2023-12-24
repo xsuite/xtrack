9c0bf3141c9a9449076bea56abfd52a1c8363d94
giadarol <giovanni.iadarola@cern.ch>
2023-12-24 14:22:52 +0100

    Checked that it is more stable

--- examples/match_basics/006_match_action.py
+++ examples/match_basics/006_match_action.py
@@ -1,6 +1,6 @@
 import numpy as np
 import xtrack as xt
-import NAFFlib as pnf
+import NAFFlib as nl
 
 # Load a line and build a tracker
 line = xt.Line.from_json('../../test_data/hllhc15_thick/lhc_thick_with_knobs.json')
@@ -15,64 +15,53 @@ alpha_yy_arr = np.empty_like(k_oct_arr)
 alpha_xy_arr = np.empty_like(k_oct_arr)
 alpha_yx_arr = np.empty_like(k_oct_arr)
 
-for k_oct in k_oct_arr:
+k_oct = 1e3
 
-    print(f'k_oct = {k_oct}', end='\r', flush=True)
+line.vars['kof.a23b1'] = k_oct
+line.vars['kof.a34b1'] = k_oct
+line.vars['kof.a67b1'] = k_oct
+line.vars['kof.a78b1'] = k_oct
 
-    line.vars['kof.a23b1'] = k_oct
-    line.vars['kof.a34b1'] = k_oct
-    line.vars['kof.a67b1'] = k_oct
-    line.vars['kof.a78b1'] = k_oct
+line.vars['kod.a23b1'] = k_oct
+line.vars['kod.a34b1'] = k_oct
+line.vars['kod.a67b1'] = k_oct
+line.vars['kod.a78b1'] = k_oct
 
-    line.vars['kod.a23b1'] = k_oct
-    line.vars['kod.a34b1'] = k_oct
-    line.vars['kod.a67b1'] = k_oct
-    line.vars['kod.a78b1'] = k_oct
+a0_sigmas = .01
+a1_sigmas = 0.3
+a2_sigmas = 0.2
+num_turns = 256
 
-    a0_sigmas = .01
-    a1_sigmas = 0.1
-    a2_sigmas = 0.15
-    num_turns = 256
+Jx_0 = a0_sigmas**2 * nemitt_x / 2
+Jx_1 = a1_sigmas**2 * nemitt_x / 2
+Jx_2 = a2_sigmas**2 * nemitt_x / 2
+Jy_0 = a0_sigmas**2 * nemitt_y / 2
+Jy_1 = a1_sigmas**2 * nemitt_y / 2
+Jy_2 = a2_sigmas**2 * nemitt_y / 2
 
-    Jx_0 = a0_sigmas**2 * nemitt_x / 2
-    Jx_1 = a1_sigmas**2 * nemitt_x / 2
-    Jx_2 = a2_sigmas**2 * nemitt_x / 2
-    Jy_0 = a0_sigmas**2 * nemitt_y / 2
-    Jy_1 = a1_sigmas**2 * nemitt_y / 2
-    Jy_2 = a2_sigmas**2 * nemitt_y / 2
+particles = line.build_particles(
+                    method='4d',
+                    zeta=0, delta=0,
+                    x_norm=[a1_sigmas, a2_sigmas, a0_sigmas, a0_sigmas],
+                    y_norm=[a0_sigmas, a0_sigmas, a1_sigmas, a2_sigmas],
+                    nemitt_x=nemitt_x, nemitt_y=nemitt_y)
 
-    particles = line.build_particles(
-                        method='4d',
-                        zeta=0, delta=0,
-                        x_norm=[a1_sigmas, a2_sigmas, a0_sigmas, a0_sigmas],
-                        y_norm=[a0_sigmas, a0_sigmas, a1_sigmas, a2_sigmas],
-                        nemitt_x=nemitt_x, nemitt_y=nemitt_y)
+line.track(particles, num_turns=num_turns, time=True, turn_by_turn_monitor=True)
+mon = line.record_last_track
 
-    line.track(particles, num_turns=num_turns, time=True, turn_by_turn_monitor=True)
+assert np.all(particles.state > 0)
 
-    assert np.all(particles.state > 0)
+qx = np.zeros(4)
+qy = np.zeros(4)
 
-    n_fft = 2**20
-    freq_axis = np.fft.rfftfreq(n_fft)
+for ii in range(len(qx)):
+    qx[ii] = nl.get_tune(mon.x[ii, :])
+    qy[ii] = nl.get_tune(mon.y[ii, :])
 
-    mon = line.record_last_track
-    fft_x = np.fft.rfft(mon.x, n=n_fft)
-    fft_y = np.fft.rfft(mon.y, n=n_fft)
+alpha_xx = (qx[1] - qx[0]) / (Jx_2 - Jx_1)
+alpha_yy = (qy[3] - qy[2]) / (Jy_2 - Jy_1)
+alpha_xy = (qx[3] - qx[2]) / (Jy_2 - Jy_1)
+alpha_yx = (qy[1] - qy[0]) / (Jx_2 - Jx_1)
 
-    qx = freq_axis[np.argmax(np.abs(fft_x), axis=1)]
-    qy = freq_axis[np.argmax(np.abs(fft_y), axis=1)]
-
-    for ii in range(len(qx)):
-        qx[ii] = pnf.get_tune(mon.x[ii, :])
-        qy[ii] = pnf.get_tune(mon.y[ii, :])
-
-    alpha_xx = (qx[1] - qx[0]) / (Jx_2 - Jx_1)
-    alpha_yy = (qy[3] - qy[2]) / (Jy_2 - Jy_1)
-    alpha_xy = (qx[3] - qx[2]) / (Jy_2 - Jy_1)
-    alpha_yx = (qy[1] - qy[0]) / (Jx_2 - Jx_1)
-
-    alpha_xx_arr[k_oct_arr==k_oct] = alpha_xx
-    alpha_yy_arr[k_oct_arr==k_oct] = alpha_yy
-    alpha_xy_arr[k_oct_arr==k_oct] = alpha_xy
-    alpha_yx_arr[k_oct_arr==k_oct] = alpha_yx
-
+print(f'alpha_xx = {alpha_xx}, alpha_xy = {alpha_xy}')
+print(f'alpha_yx = {alpha_yx}, alpha_yy = {alpha_yy}')
