7e7c336633f17712a9f5b56c59488235206d181f
giadarol <giovanni.iadarola@cern.ch>
2023-12-24 14:47:25 +0100

    Using a single arc

--- examples/match_basics/006_match_action.py
+++ examples/match_basics/006_match_action.py
@@ -2,6 +2,57 @@ import numpy as np
 import xtrack as xt
 import NAFFlib as nl
 
+
+
+class ActionMeasAmplDet(xt.Action):
+
+    def __init__(self, line, nemitt_x, nemitt_y, num_turns=256,
+                 a0_sigmas=0.01, a1_sigmas=0.1, a2_sigmas=0.2):
+
+        self.line = line
+        self.num_turns = num_turns
+        self.nemitt_x = nemitt_x
+        self.nemitt_y = nemitt_y
+
+        self.Jx_0 = a0_sigmas**2 * nemitt_x / 2
+        self.Jx_1 = a1_sigmas**2 * nemitt_x / 2
+        self.Jx_2 = a2_sigmas**2 * nemitt_x / 2
+        self.Jy_0 = a0_sigmas**2 * nemitt_y / 2
+        self.Jy_1 = a1_sigmas**2 * nemitt_y / 2
+        self.Jy_2 = a2_sigmas**2 * nemitt_y / 2
+
+        self.particles0 = line.build_particles(
+                            method='4d',
+                            zeta=0, delta=0,
+                            x_norm=[a1_sigmas, a2_sigmas, a0_sigmas, a0_sigmas],
+                            y_norm=[a0_sigmas, a0_sigmas, a1_sigmas, a2_sigmas],
+                            nemitt_x=nemitt_x, nemitt_y=nemitt_y)
+
+    def run(self):
+        particles = self.particles0.copy()
+
+        self.line.track(particles,
+                        num_turns=self.num_turns, time=True,
+                        turn_by_turn_monitor=True)
+        mon = line.record_last_track
+
+        assert np.all(particles.state > 0)
+
+        qx = np.zeros(4)
+        qy = np.zeros(4)
+
+        for ii in range(len(qx)):
+            qx[ii] = nl.get_tune(mon.x[ii, :])
+            qy[ii] = nl.get_tune(mon.y[ii, :])
+
+        det_xx = (qx[1] - qx[0]) / (self.Jx_2 - self.Jx_1)
+        det_yy = (qy[3] - qy[2]) / (self.Jy_2 - self.Jy_1)
+        det_xy = (qx[3] - qx[2]) / (self.Jy_2 - self.Jy_1)
+        det_yx = (qy[1] - qy[0]) / (self.Jx_2 - self.Jx_1)
+
+        return {'det_xx': det_xx, 'det_yy': det_yy,
+                'det_xy': det_xy, 'det_yx': det_yx}
+
 # Load a line and build a tracker
 line = xt.Line.from_json('../../test_data/hllhc15_thick/lhc_thick_with_knobs.json')
 line.build_tracker()
@@ -9,59 +60,10 @@ line.build_tracker()
 nemitt_x = 2.5e-6
 nemitt_y = 2.5e-6
 
-k_oct_arr = np.arange(-250, 250, 1e1)
-alpha_xx_arr = np.empty_like(k_oct_arr)
-alpha_yy_arr = np.empty_like(k_oct_arr)
-alpha_xy_arr = np.empty_like(k_oct_arr)
-alpha_yx_arr = np.empty_like(k_oct_arr)
+action = ActionMeasAmplDet(line=line, nemitt_x=nemitt_x, nemitt_y=nemitt_y)
 
-k_oct = 1e3
+action.run()
 
-line.vars['kof.a23b1'] = k_oct
-line.vars['kof.a34b1'] = k_oct
-line.vars['kof.a67b1'] = k_oct
-line.vars['kof.a78b1'] = k_oct
-
-line.vars['kod.a23b1'] = k_oct
-line.vars['kod.a34b1'] = k_oct
-line.vars['kod.a67b1'] = k_oct
-line.vars['kod.a78b1'] = k_oct
-
-a0_sigmas = .01
-a1_sigmas = 0.3
-a2_sigmas = 0.2
-num_turns = 256
-
-Jx_0 = a0_sigmas**2 * nemitt_x / 2
-Jx_1 = a1_sigmas**2 * nemitt_x / 2
-Jx_2 = a2_sigmas**2 * nemitt_x / 2
-Jy_0 = a0_sigmas**2 * nemitt_y / 2
-Jy_1 = a1_sigmas**2 * nemitt_y / 2
-Jy_2 = a2_sigmas**2 * nemitt_y / 2
-
-particles = line.build_particles(
-                    method='4d',
-                    zeta=0, delta=0,
-                    x_norm=[a1_sigmas, a2_sigmas, a0_sigmas, a0_sigmas],
-                    y_norm=[a0_sigmas, a0_sigmas, a1_sigmas, a2_sigmas],
-                    nemitt_x=nemitt_x, nemitt_y=nemitt_y)
-
-line.track(particles, num_turns=num_turns, time=True, turn_by_turn_monitor=True)
-mon = line.record_last_track
-
-assert np.all(particles.state > 0)
-
-qx = np.zeros(4)
-qy = np.zeros(4)
-
-for ii in range(len(qx)):
-    qx[ii] = nl.get_tune(mon.x[ii, :])
-    qy[ii] = nl.get_tune(mon.y[ii, :])
-
-alpha_xx = (qx[1] - qx[0]) / (Jx_2 - Jx_1)
-alpha_yy = (qy[3] - qy[2]) / (Jy_2 - Jy_1)
-alpha_xy = (qx[3] - qx[2]) / (Jy_2 - Jy_1)
-alpha_yx = (qy[1] - qy[0]) / (Jx_2 - Jx_1)
-
-print(f'alpha_xx = {alpha_xx}, alpha_xy = {alpha_xy}')
-print(f'alpha_yx = {alpha_yx}, alpha_yy = {alpha_yy}')
+opt = line.match(vary=xt.VaryList(['kof.a23b1', 'kod.a23b1'], step=1.),
+                 targets=[action.target('det_xx', 1e3, tol=1e1),
+                          action.target('det_yy', 2e3, tol=1e1)])
