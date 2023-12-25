3c9733e98fe7bf6d662567db411e68120b8327c6
giadarol <giovanni.iadarola@cern.ch>
2023-12-25 22:15:54 +0100

    Update example

--- examples/match_basics/006_match_action.py
+++ examples/match_basics/006_match_action.py
@@ -17,14 +17,25 @@ class ActionMeasAmplDet(xt.Action):
     def run(self):
 
         det_coefficients = self.line.get_amplitude_detuning_coefficients(
-                                                    num_turns=self.num_turns)
-        return det_coefficients
+                                nemitt_x=self.nemitt_x, nemitt_y=self.nemitt_y,
+                                num_turns=self.num_turns)
+
+        out = {'d_xx': det_coefficients['det_xx'],
+               'd_yy': det_coefficients['det_yy']}
+
+        return out
 
 action = ActionMeasAmplDet(line=line, nemitt_x=2.5e-6, nemitt_y=2.5e-6,
                            num_turns=128)
 
 opt = line.match(vary=xt.VaryList(['kof.a23b1', 'kod.a23b1'], step=1.),
-                 targets=[action.target('det_xx', 1000., tol=0.1),
-                          action.target('det_yy', 2000., tol=0.1)])
+                 targets=[action.target('d_xx', 1000., tol=0.1),
+                          action.target('d_yy', 2000., tol=0.1)])
 
 opt.target_status()
+# prints:
+#
+# Target status:
+# id state tag tol_met     residue current_val target_val description
+#  0 ON           True   0.0844456     1000.08       1000 'd_xx', val=1000, ...
+#  1 ON           True -0.00209987        2000       2000 'd_yy', val=2000, ...
