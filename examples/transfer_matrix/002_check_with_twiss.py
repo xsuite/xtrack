31df3aab9ccac18b0b6c92176a4ac25265cab03b
giadarol <giovanni.iadarola@cern.ch>
2023-12-29 19:02:35 -0300

    Update examples

--- examples/transfer_matrix/002_check_with_twiss.py
+++ examples/transfer_matrix/002_check_with_twiss.py
@@ -1,7 +1,6 @@
 import numpy as np
 
 import xtrack as xt
-import xpart as xp
 
 x_co = [1e-3, 2e-3]
 px_co = [2e-6, -3e-6]
@@ -49,7 +48,7 @@ segm_2 = xt.LineSegmentMap(
         y_ref=[y_co[1], y_co[0]],
         py_ref=[py_co[1], py_co[0]])
 
-line = xt.Line(elements=[segm_1, segm_2], particle_ref=xp.Particles(p0c=1e9))
+line = xt.Line(elements=[segm_1, segm_2], particle_ref=xt.Particles(p0c=1e9))
 line.build_tracker()
 
 tw4d = line.twiss(method='4d')
