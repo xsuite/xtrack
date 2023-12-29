70d2c86fb69de11ceec851fad337ebd1bf25caa5
giadarol <giovanni.iadarola@cern.ch>
2023-12-29 18:51:32 -0300

    Update examples

--- examples/radiation/004_damping_against_madx.py
+++ examples/radiation/004_damping_against_madx.py
@@ -9,7 +9,6 @@ import numpy as np
 from cpymad.madx import Madx
 
 import xtrack as xt
-import xpart as xp
 import xobjects as xo
 
 # Import thick sequence
@@ -49,8 +48,8 @@ mad.twiss()
 # Build xtrack line
 print('Build xtrack line...')
 line = xt.Line.from_madx_sequence(mad.sequence['RING'])
-line.particle_ref = xp.Particles(
-        mass0=xp.ELECTRON_MASS_EV,
+line.particle_ref = xt.Particles(
+        mass0=xt.ELECTRON_MASS_EV,
         q0=-1,
         gamma0=mad.sequence.ring.beam.gamma)
 
--- examples/radiation/007_record_photons.py
+++ examples/radiation/007_record_photons.py
@@ -9,8 +9,6 @@ import numpy as np
 from cpymad.madx import Madx
 
 import xtrack as xt
-import xpart as xp
-import xobjects as xo
 
 # Import a thick sequence
 mad = Madx()
@@ -29,8 +27,8 @@ mad.use('ring')
 # Build xtrack line
 print('Build xtrack line...')
 line = xt.Line.from_madx_sequence(mad.sequence['RING'])
-line.particle_ref = xp.Particles(
-        mass0=xp.ELECTRON_MASS_EV,
+line.particle_ref = xt.Particles(
+        mass0=xt.ELECTRON_MASS_EV,
         q0=-1,
         gamma0=mad.sequence.ring.beam.gamma)
 
@@ -42,7 +40,7 @@ line.configure_radiation(model='quantum')
 record = line.start_internal_logging_for_elements_of_type(
                                                 xt.Multipole, capacity=100000)
 
-particles = xp.build_particles(line=line, x=[0,0,0,0])
+particles = line.build_particles(x=[0,0,0,0])
 
 line.track(particles, num_turns=10)
 
