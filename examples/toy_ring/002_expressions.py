1d99b8bbd33c270c5509f4c227c400c727bda397
giadarol <giovanni.iadarola@cern.ch>
2023-12-30 14:12:08 -0300

    Working on variables example

--- examples/toy_ring/002_expressions.py
+++ examples/toy_ring/002_expressions.py
@@ -1,6 +1,7 @@
 import numpy as np
 import xtrack as xt
 
+# We build a simple ring
 pi = np.pi
 lbend = 3
 lquad = 0.3
@@ -25,12 +26,10 @@ elements = {
     'mb2.2': xt.Bend(length=lbend, k0=pi / 2 / lbend, h=pi / 2 / lbend),
     'd4.2':  xt.Drift(length=1),
 }
-
-# Build the ring
 line = xt.Line(elements=elements, element_names=list(elements.keys()))
 line.particle_ref = xt.Particles(p0c=1.2e9, mass0=xt.PROTON_MASS_EV)
 
-# For each quadrupole create a variable controlling its integrated strength
+# For each quadrupole we create a variable controlling its integrated strength
 line.vars['k1l.qf.1'] = 0
 line.element_refs['mqf.1'].k1 = line.vars['k1l.qf.1'] / lquad
 line.vars['k1l.qd.1'] = 0
@@ -47,22 +46,68 @@ line['mqf.1'].k1 # is 0.333, i.e. 0.1 / lquad
 
 # We can create a variable controlling the integrated strength of the two
 # focusing quadrupoles
-line.vars['k1l.qf'] = 0.1
-line.vars['k1l.qf.1'] = line.vars['k1l.qf']
-line.vars['k1l.qf.2'] = line.vars['k1l.qf']
+line.vars['k1lf'] = 0.1
+line.vars['k1l.qf.1'] = line.vars['k1lf']
+line.vars['k1l.qf.2'] = line.vars['k1lf']
 # and a variable controlling the integrated strength of the two defocusing quadrupoles
-line.vars['k1l.qd'] = -0.7
-line.vars['k1l.qd.1'] = line.vars['k1l.qd']
-line.vars['k1l.qd.2'] = line.vars['k1l.qd']
+line.vars['k1ld'] = -0.7
+line.vars['k1l.qd.1'] = line.vars['k1ld']
+line.vars['k1l.qd.2'] = line.vars['k1ld']
 
-# Changes on this variable are propagated to the two controlled variables and
+# Changes on the controlling variable are propagated to the two controlled ones and
 # to the corresponding element properties:
-line.vars['k1l.qf'] = 0.2
-line.vars['k1l.qf.1']._value # is 0.
-line.vars['k1l.qf.2']._value # is 0.
+line.vars['k1lf'] = 0.2
+line.vars['k1l.qf.1']._get_value() # is 0.2
+line.vars['k1l.qf.2']._get_value() # is 0.2
 line['mqf.1'].k1 # is 0.666, i.e. 0.2 / lquad
 line['mqf.2'].k1 # is 0.666, i.e. 0.2 / lquad
 
+# The `_info()` method of a variable provides information on the existing relations
+# between the variables. For example:
+line.vars['k1l.qf.1']._info()
+# prints:
+##  vars['k1l.qf.1']._get_value()
+#   vars['k1l.qf.1'] = 0.2
+#
+##  vars['k1l.qf.1']._expr
+#   vars['k1l.qf.1'] = vars['k1lf']
+#
+##  vars['k1l.qf.1']._expr._get_dependencies()
+#   vars['k1lf'] = 0.2
+#
+##  vars['k1l.qf.1']._find_dependant_targets()
+#   element_refs['mqf.1'].k1
 
+line.vars['k1lf']._info()
+# prints:
+##  vars['k1lf']._get_value()
+#   vars['k1lf'] = 0.2
+#
+##  vars['k1lf']._expr is None
+#
+##  vars['k1lf']._find_dependant_targets()
+#   vars['k1l.qf.2']
+#   element_refs['mqf.2'].k1
+#   vars['k1l.qf.1']
+#   element_refs['mqf.1'].k1
 
+line.element_refs['mqf.1'].k1._info()
+# prints:
+##  element_refs['mqf.1'].k1._get_value()
+#   element_refs['mqf.1'].k1 = 0.6666666666666667
+#
+##  element_refs['mqf.1'].k1._expr
+#   element_refs['mqf.1'].k1 = (vars['k1l.qf.1'] / 0.3)
+#
+##  element_refs['mqf.1'].k1._expr._get_dependencies()
+#   vars['k1l.qf.1'] = 0.2
+#
+##  element_refs['mqf.1'].k1 does not influence any target
+
+# Expressions can include multiple variables and mathematical operations. For example
+line.vars['a'] = 3 * line.functions.sqrt(line.vars['k1lf']) + 2 * line.vars['k1ld']
+
+# As seen above, line.vars['varname'] returns a reference object that
+# can be used to build further references, or to inpect its properties.
+# To get the current value of the variable, one needs to åuse `._get_value()`
 
