f7ef522ea07001130b424b72c6f08ad6dca98c8f
giadarol <giovanni.iadarola@cern.ch>
2024-01-01 14:15:30 -0300

    Update example

--- examples/toy_ring/000_toy_ring.py
+++ examples/toy_ring/000_toy_ring.py
@@ -26,8 +26,16 @@ elements = {
 }
 
 # Build the ring
-line = xt.Line(elements=elements, element_names=list(elements.keys()))
+line = xt.Line(elements=elements,
+               element_names=['mqf.1', 'd1.1', 'mb1.1', 'd2.1',
+                              'mqd.1', 'd3.1', 'mb2.1', 'd4.1',
+                              'mqf.2', 'd1.2', 'mb1.2', 'd2.2',
+                              'mqd.2', 'd3.2', 'mb2.2', 'd4.2'])
+# Define reference particle
 line.particle_ref = xt.Particles(p0c=1.2e9, mass0=xt.PROTON_MASS_EV)
+
+#!end-doc-part
+
 line.configure_bend_model(core='full', edge=None)
 tw0 = line.twiss(method='4d')
 
