c3c1d07dedd47868accb8ea3e6ee3e4abebfd49c
giadarol <giovanni.iadarola@cern.ch>
2024-01-07 12:47:14 -0300

    Clean up example

--- examples/toy_ring/006_dynamic_bump.py
+++ examples/toy_ring/006_dynamic_bump.py
@@ -1,5 +1,6 @@
 import numpy as np
 import xtrack as xt
+import xdeps as xd
 
 # Define elements
 pi = np.pi
@@ -49,8 +50,7 @@ kin_energy_0 = 50e6 # 50 MeV
 line.particle_ref = xt.Particles(energy0=kin_energy_0 + xt.PROTON_MASS_EV, # total energy
                                  mass0=xt.PROTON_MASS_EV)
 
-line.build_tracker()
-
+# Power the correctors to make a closed orbit bump
 line.vars['bumper_strength'] = 0.
 line.element_refs['bumper_0'].k0 = -line.vars['bumper_strength']
 line.element_refs['bumper_1'].k0 = 2 * line.vars['bumper_strength']
@@ -60,20 +60,22 @@ line.vars['bumper_strength'] = 0.1
 
 tw = line.twiss(method='4d')
 
-
-
 # Control the bumpers with a sinusoidal function of time
-T_sin = 100e-6
+line.functions['pulse'] = xd.FunctionPieceWiseLinear(
+    x=np.array([10, 50, 150, 190]) * 1e-6, # s
+    y=np.array([0,   1,   1,   0])         # knob value
+)
+
+T_sin = 10e-6
 sin = line.functions.sin
-line.vars['bumper_strength'] = 0.1 * sin(2 * np.pi / T_sin * line.vars['t_turn_s'])
+line.vars['bumper_strength'] = (0.1 * line.functions['pulse'](line.vars['t_turn_s'])
+                                * sin(2 * np.pi / T_sin * line.vars['t_turn_s']))
 
+# --- Track particles with time-dependent bumpers ---
 num_particles = 100
-particles = line.build_particles(
-                        x=np.random.uniform(-0.1e-3, 0.1e-3, num_particles),
-                        px=np.random.uniform(-0.1e-3, 0.1e-3, num_particles))
-
 num_turns = 1000
 
+# Install monitor in the middle of the bump
 monitor = xt.ParticlesMonitor(num_particles=num_particles,
                               start_at_turn=0,
                               stop_at_turn =num_turns)
@@ -81,13 +83,26 @@ line.discard_tracker()
 line.insert_element('monitor', monitor, index='bumper_1')
 line.build_tracker()
 
+# Generate particles
+particles = line.build_particles(
+                        x=np.random.uniform(-0.1e-3, 0.1e-3, num_particles),
+                        px=np.random.uniform(-0.1e-3, 0.1e-3, num_particles))
+
+# Enable time-dependent variables
 line.enable_time_dependent_vars = True
+
+# Track
 line.track(particles, num_turns=num_turns, with_progress=True)
 
 import matplotlib.pyplot as plt
 plt.close('all')
-plt.plot(monitor.x.T, lw=1, color='k', alpha=0.005)
-plt.plot(monitor.x.mean(axis=0), lw=2, color='k')
+
+plt.plot(1e6 * monitor.at_turn.T * tw.T_rev0, 1e3 * monitor.x.T,
+         lw=1, color='r', alpha=0.05)
+plt.plot(1e6 * monitor.at_turn[0, :] * tw.T_rev0, 1e3 * monitor.x.mean(axis=0),
+         lw=2, color='k')
+plt.xlabel('Time [us]')
+plt.ylabel('x [mm]')
 
 plt.show()
 
