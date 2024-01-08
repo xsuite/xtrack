3e11d48f4da1a02bd57ab59c0e738b27ac482454
giadarol <giovanni.iadarola@cern.ch>
2024-01-08 14:01:33 -0300

    Adapt backtrack kernel

--- examples/tracker/002_check_backtrack_with_flag.py
+++ examples/tracker/002_check_backtrack_with_flag.py
@@ -4,17 +4,27 @@ line = xt.Line.from_json(
     '../../test_data/hllhc15_noerrors_nobb/line_w_knobs_and_particle.json')
 line.build_tracker()
 
-line.vars['on_crab1'] = -190
-line.vars['on_crab5'] = -190
-line.vars['on_x1'] = 130
-line.vars['on_x5'] = 130
-
 p = xt.Particles(
     p0c=7000e9, x=1e-4, px=1e-6, y=2e-4, py=3e-6, zeta=0.01, delta=1e-4)
+p.get_table().show(digits=3)
+# prints:
+#
+# particle_id s      x    px      y    py zeta  delta chi ...
+#           0 0 0.0001 1e-06 0.0002 3e-06 0.01 0.0001   1
 
-line.track(p, turn_by_turn_monitor='ONE_TURN_EBE')
-mon_forward = line.record_last_track
+# Track one turn
+line.track(p)
+p.get_table().show()
+# prints:
+#
+# particle_id s        x        px        y        py    zeta  delta chi ...
+#           0 0 0.000324 -1.07e-05 1.21e-05 -1.42e-06 0.00909 0.0001   1
 
-line.track(p, backtrack=True, turn_by_turn_monitor='ONE_TURN_EBE')
-mon_backtrack = line.record_last_track
-
+# Track back one turn
+line.track(p, backtrack=True)
+# The particle is back to the initial coordinates
+p.get_table().show(digits=3)
+# prints:
+#
+# particle_id s      x    px      y    py zeta  delta chi ...
+#           0 0 0.0001 1e-06 0.0002 3e-06 0.01 0.0001   1
