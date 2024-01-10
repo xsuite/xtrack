9cee6be2e49935ac07762062f8fd1c4d73eba4f4
giadarol <giovanni.iadarola@cern.ch>
2024-01-10 16:55:50 -0300

    Ongoing investigation

--- examples/elena/000_model.py
+++ examples/elena/000_model.py
@@ -1,3 +1,4 @@
+import numpy as np
 from cpymad.madx import Madx
 import xtrack as xt
 
@@ -10,8 +11,6 @@ mad.call(folder + '/highenergy.str')
 mad.call(folder + '/highenergy.beam')
 
 mad.use('elena')
-mad.input('twiss, chrom, table=twchr;')
-mad.input('twiss, chrom=false, table=twnochr;')
 
 seq = mad.sequence.elena
 line = xt.Line.from_madx_sequence(seq)
@@ -22,6 +21,11 @@ line.particle_ref = xt.Particles(gamma0=seq.beam.gamma,
 # tquads = tt.rows[tt.element_type == 'Quadrupole']
 # for nn in tquads.name:
 #     line[nn].k1 = 0
+#     mad.input(f'{nn}, k1 = 0;')
+
+mad.input('twiss, chrom, table=twchr;')
+mad.input('twiss, chrom=false, table=twnochr;')
+
 
 line.slice_thick_elements(
         slicing_strategies=[
@@ -58,6 +62,21 @@ plt.legend(loc='best')
 plt.xlabel('s [m]')
 plt.ylabel(r'$W_x$')
 
+plt.figure(11, figsize=(4.8*1.8, 4.8))
+ax1 = plt.subplot(1, 1, 1)
+plt.plot(tw_el.s, tw_el.wy_chrom, label='core: expanded, edge: linear')
+plt.plot(tw_fl.s, tw_fl.wy_chrom, label='core: full, edge: linear')
+plt.plot(tw_ef.s, tw_ef.wy_chrom, label='core: expanded, edge: full')
+plt.plot(tw_ff.s, tw_ff.wy_chrom, label='core: full, edge: full', lw=2, color='k')
+
+twmad = mad.table.twchr
+plt.plot(twmad.s, twmad.wy*tw_ff.beta0, '--', label='madx', color='r', alpha=0.9)
+
+plt.legend(loc='best')
+plt.xlabel('s [m]')
+plt.ylabel(r'$W_y$')
+
+
 # Same for betx
 plt.figure(2, figsize=(4.8*1.8, 4.8))
 ax2 = plt.subplot(1, 1, 1)
@@ -72,19 +91,19 @@ plt.legend(loc='best')
 plt.xlabel('s [m]')
 plt.ylabel(r'$\beta_x$ [m]')
 
-tw_ff = line.twiss(method='4d')
+tw= line.twiss(method='4d')
 plt.figure(3, figsize=(4.8*1.8, 4.8))
 ax3 = plt.subplot(1, 1, 1)
-plt.plot(tw_ff.s, tw_ff.betx, label='betx')
-plt.plot(tw_ff.s, tw_ff.bety, label='bety')
+plt.plot(tw.s, tw.betx, label='betx')
+plt.plot(tw.s, tw.bety, label='bety')
 plt.legend(loc='best')
 plt.xlabel('s [m]')
 plt.ylabel(r'$\beta_{x,y}$ [m]')
 
 plt.figure(4, figsize=(4.8*1.8, 4.8))
 ax4 = plt.subplot(1, 1, 1)
-plt.plot(tw_ff.s, tw_ff.wx_chrom, label='wx')
-plt.plot(tw_ff.s, tw_ff.wy_chrom, label='wy')
+plt.plot(tw.s, tw.wx_chrom, label='wx')
+plt.plot(tw.s, tw.wy_chrom, label='wy')
 plt.legend(loc='best')
 plt.xlabel('s')
 plt.ylabel(r'$W_{x,y}$')
@@ -118,12 +137,41 @@ mad.input(f'''
                summary_table=ptc_twiss_summary_pdp, slice_magnets=true;
     ptc_twiss, closed_orbit, icase=56, no=2, deltap={-delta_chrom:e}, table=ptc_twiss_mdp,
                summary_table=ptc_twiss_summary_mdp, slice_magnets=true;
+
+    #### TEEEEST
+    ptc_twiss, closed_orbit, icase=56, no=2, deltap={delta_chrom:e}, table=ptc_twiss_pdp_o,
+               alfx=0, betx=1, alfy=0, bety=1, betz=1, dx=0, dy=0, dpx=0, dpy=0,
+               summary_table=ptc_twiss_summary_pdp_o, slice_magnets=true;
   ptc_end;
 ''')
+
 qx_ptc = mad.table.ptc_twiss.mu1[-1]
 qy_ptc = mad.table.ptc_twiss.mu2[-1]
 dq1_ptc = (mad.table.ptc_twiss_pdp.mu1[-1] - mad.table.ptc_twiss_mdp.mu1[-1]) / (2 * delta_chrom)
 dq2_ptc = (mad.table.ptc_twiss_pdp.mu2[-1] - mad.table.ptc_twiss_mdp.mu2[-1]) / (2 * delta_chrom)
+
+tptc = mad.table.ptc_twiss
+tptc_p = mad.table.ptc_twiss_pdp
+tptc_m = mad.table.ptc_twiss_mdp
+
+betx = 0.5 * (tptc_p.beta11 + tptc_m.beta11)
+alfx = 0.5 * (tptc_p.alfa11 + tptc_m.alfa11)
+d_betx = (tptc_p.beta11 - tptc_m.beta11)/ (2 * delta_chrom)
+d_alfx = (tptc_p.alfa11 - tptc_m.alfa11)/ (2 * delta_chrom)
+
+bx_ptc = d_betx / betx
+ax_ptc = d_alfx - d_betx * alfx / betx
+wx_ptc = np.sqrt(ax_ptc**2 + bx_ptc**2)
+
+bety = 0.5 * (tptc_p.beta22 + tptc_m.beta22)
+alfy = 0.5 * (tptc_p.alfa22 + tptc_m.alfa22)
+d_bety = (tptc_p.beta22 - tptc_m.beta22)/ (2 * delta_chrom)
+d_alfy = (tptc_p.alfa22 - tptc_m.alfa22)/ (2 * delta_chrom)
+
+by_ptc = d_bety / bety
+ay_ptc = d_alfy - alfy * by_ptc
+wy_ptc = np.sqrt(ay_ptc**2 + by_ptc**2)
+
 
 print(f'qx xsuite:          {tw_ff.qx}')
 print(f'qx ptc:             {qx_ptc}')
@@ -137,5 +185,78 @@ print(f'dqx mad (chrom=F):  {twmad_nc.summary.dq1 * seq.beam.beta}')
 print(f'dqy xsuite:         {tw_ff.dqy}')
 print(f'dqy ptc:            {dq2_ptc}')
 print(f'dqy mad (chrom=F):  {twmad_nc.summary.dq2 * seq.beam.beta}')
+
+
+plt.figure(101, figsize=(6.4, 4.8 * 1.5))
+
+ax1 = plt.subplot(4,1,1)
+plt.plot(tptc.s, ax_ptc, label='ptc')
+plt.plot(tw.s, tw.ax_chrom, label='xsuite')
+plt.ylabel(r'$A_x$')
+plt.legend(loc='best')
+
+ax2 = plt.subplot(4,1,2, sharex=ax1)
+plt.plot(tptc.s, bx_ptc)
+plt.plot(tw.s, tw.bx_chrom)
+plt.ylabel(r'$B_x$')
+
+ax3 = plt.subplot(4,1,3, sharex=ax1)
+plt.plot(tptc.s, ay_ptc)
+plt.plot(tw.s, tw.ay_chrom)
+plt.ylabel(r'$A_y$')
+
+ax4 = plt.subplot(4,1,4, sharex=ax1)
+plt.plot(tptc.s, by_ptc)
+plt.plot(tw.s, tw.by_chrom)
+plt.ylabel(r'$B_y$')
+plt.xlabel('s [m]')
+
+for ax in [ax1, ax2, ax3, ax4]:
+    tt = line.get_table()
+    tbends = tt.rows[tt.element_type == 'Bend']
+    tquads = tt.rows[tt.element_type == 'Quadrupole']
+    for nn in tbends.name:
+        ax.axvspan(tbends['s', nn], tbends['s', nn] + line[nn].length, color='b',
+                    alpha=0.2, lw=0)
+    for nn in tquads.name:
+        ax.axvspan(tquads['s', nn], tquads['s', nn] + line[nn].length, color='r',
+                    alpha=0.2, lw=0)
+
+two = line.twiss(method='4d', start=line.element_names[0], end='_end_point',
+                 betx=tptc.beta11[0], bety=tptc.beta22[0],
+                 alfx=tptc.alfa11[0], alfy=tptc.alfa22[0],
+                 dx=tptc.dx[0], dy=tptc.dy[0],
+                 dpx=tptc.dpx[0], dpy=tptc.dpy[0],
+                 ax_chrom=ax_ptc[0], bx_chrom=bx_ptc[0],
+                 ay_chrom=ay_ptc[0], by_chrom=by_ptc[0],
+                 compute_chromatic_properties=True)
+
+twp = line.twiss(method='4d', delta0=delta_chrom)
+tptc_p_o = mad.table.ptc_twiss_pdp_o
+
+# Same for beta and orbit
+plt.figure(102, figsize=(6.4, 4.8 * 1.5))
+
+ax1 = plt.subplot(4,1,1)
+plt.plot(tptc.s, tptc.beta11, label='ptc')
+plt.plot(tw.s, tw.betx, label='xsuite')
+plt.ylabel(r'$\beta_x$')
+plt.legend(loc='best')
+
+plt.subplot(4,1,2, sharex=ax1)
+plt.plot(tptc.s, tptc.beta22)
+plt.plot(tw.s, tw.bety)
+plt.ylabel(r'$\beta_y$')
+
+plt.subplot(4,1,3, sharex=ax1)
+plt.plot(tptc.s, tptc.x)
+plt.plot(tw.s, tw.x)
+plt.ylabel(r'$x$')
+
+plt.subplot(4,1,4, sharex=ax1)
+plt.plot(tptc.s, tptc.y)
+plt.plot(tw.s, tw.y)
+plt.ylabel(r'$y$')
+plt.xlabel('s [m]')
 
 plt.show()
