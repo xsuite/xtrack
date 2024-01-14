c25cfbcff1b1bf3c8a3ecc40fe29d2603e862535
giadarol <giovanni.iadarola@cern.ch>
2024-01-14 07:41:17 -0300

    Plot in examples

--- examples/radial_steering/000_radial_steering.py
+++ examples/radial_steering/000_radial_steering.py
@@ -3,6 +3,7 @@
 # Copyright (c) CERN, 2021.                 #
 # ######################################### #
 
+import numpy as np
 import xtrack as xt
 
 # Load a line and build a tracker
@@ -12,7 +13,7 @@ line.particle_ref = xt.Particles(mass0=xt.PROTON_MASS_EV, q0=1, energy0=7e12)
 line.build_tracker()
 
 # Frequency trim to make
-df_hz = 180 # Frequency trim
+df_hz = 50 # Frequency trim
 
 # Twiss
 tw0 = line.twiss()
@@ -45,5 +46,29 @@ df_rev = df_hz / h_rf
 eta = tw0.slip_factor
 delta_expected = -df_rev / f_rev / eta
 
-tw1.delta[0] # is -0.00128763
-delta_expected        # is -0.00128893
+tw1.delta[0]    # is -0.00035789
+delta_expected  # is -0.00035803
+
+# Check that particle stays off momentum in multi-turn tracking
+p = tw1.particle_on_co.copy()
+
+p_test = line.build_particles(x_norm=0,
+            delta=np.linspace(delta_expected - 8e-4, delta_expected + 8e-4, 60))
+# p_test = line.build_particles(x_norm=0, delta=delta_expected,
+#                               zeta=np.linspace(0, 0.5, 101))
+
+# Track
+line.track(p_test, num_turns=1000, turn_by_turn_monitor=True, with_progress=True)
+mon = line.record_last_track
+
+# Plot
+import matplotlib.pyplot as plt
+plt.close('all')
+plt.figure()
+plt.plot(mon.zeta[:, :].T, mon.delta[:, :].T * 1e4, color='C0')
+plt.xlabel(r'$\zeta$ [m]')
+plt.ylabel('$\delta$ [$10^{-4}$]')
+plt.xlim(-.8, .8)
+plt.ylim(delta_expected * 1e4 - 4, delta_expected * 1e4 + 4)
+plt.axhline(delta_expected * 1e4, color='r', linestyle='--', label='$\delta$ expected')
+plt.show()
