17ef16888efed129040236ab3dac96dae95240ba
giadarol <giovanni.iadarola@cern.ch>
2024-01-24 00:15:41 +0100

    Trying anharm, not working

--- examples/mad_writer/000_dev.py
+++ examples/mad_writer/000_dev.py
@@ -71,7 +71,7 @@ line.build_tracker()
 #                                     mass0=seq.beam.mass * 1e9,
 #                                     q0=seq.beam.charge)
 
-mad_seq = line.to_madx_sequence(sequence_name='sps')
+mad_seq = line.to_madx_sequence(sequence_name='myseq')
 
 # mad_seq = """
 # squad: quadrupole, k1s:=0.5, l=1, at=0.5;
@@ -82,8 +82,6 @@ mad_seq = line.to_madx_sequence(sequence_name='sps')
 
 # """
 
-
-
 temp_fname = 'temp4madng'
 with open(temp_fname+'.madx', 'w') as fid:
     fid.write(mad_seq)
@@ -91,17 +89,42 @@ with open(temp_fname+'.madx', 'w') as fid:
 from pymadng import MAD
 mad = MAD()
 mad.MADX.load(f'"{temp_fname}.madx"', f"'{temp_fname}.madng'")
-mad["sps"] = mad.MADX.sps
-mad.sps.beam = mad.beam()
-mad["mytwtable", 'mytwflow'] = mad.twiss(sequence=mad.sps, method=4, mapdef=2, implicit=True, nslice=3, save="'atbody'")
-
-
-# # os.remove(temp_fname)
+mad["myseq"] = mad.MADX.myseq
+mad.myseq.beam = mad.beam()
+mad["mytwtable", 'mytwflow'] = mad.twiss(
+    sequence=mad.myseq, method=4, mapdef=2, implicit=True, nslice=3, save="'atbody'")
 
 mad2 = Madx()
 mad2.input(mad_seq)
 mad2.beam()
-mad2.use('sps')
+mad2.use('myseq')
+
+mad.send('''
+        local track in MAD  -- like "from MAD import track"
+        local mytrktable, mytrkflow = MAD.track{sequence=MADX.myseq, method=4,
+                                                mapdef=4, nslice=3}
+
+         -- print(MAD.typeid.is_damap(mytrkflow[1]))
+
+        local normal in MAD.gphys  -- like "from MAD.gphys import normal"
+        local my_norm_for = normal(mytrkflow[1]):analyse('anh') -- anh stands for anharmonicity
+
+        local nf = my_norm_for
+        py:send({
+                nf:q1{1}, -- qx from the normal form (fractional part)
+                nf:q2{1}, -- qy
+                nf:dq1{1}, -- dqx / d delta
+                nf:dq2{1}, -- dqy / d delta
+                nf:dq1{2}, -- d2 qx / d delta2
+                nf:dq2{2}, -- d2 qy / d delta2
+                nf:anhx{1, 0}, -- dqx / djx
+                nf:anhy{0, 1}, -- dqy / djy
+                nf:anhx{0, 1}, -- dqx / djy
+                nf:anhy{1, 0}, -- dqy / djx
+             })
+    ''')
+
+out = mad.recv()
 
 # # tw = line.twiss(method='4d')
 # # twmad2 = mad2.twiss()
