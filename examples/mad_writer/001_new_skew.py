38249619e3a5b1f7c240b8ae79fbacd5c03ea0ee
giadarol <giovanni.iadarola@cern.ch>
2024-01-23 14:05:59 +0100

    There seems to be a discrpancy

--- examples/mad_writer/001_new_skew.py
+++ examples/mad_writer/001_new_skew.py
@@ -5,13 +5,17 @@ mad = Madx()
 mad.input("""
 
 a = 1.;
-quad: quadrupole, k1:=a*3, l=1;
-squad: quadrupole, k1s:=a*2, l=1;
+
 
 testseq: sequence, l=10;
-qn: quad, at=2., l=1;
-qs: squad, at=5.;
-m1: marker, at=6.;
+
+squad1: quadrupole, k1s:=a*0.5, l=1, at=1;
+
+quad: quadrupole, k1s:=1, l=1, at=3;
+
+squad2: quadrupole, k1s:=-a*0.2, l=1, at=4;
+
+m1: marker, at=10.;
 endsequence;
 beam;
 use, sequence=testseq;
@@ -22,12 +26,12 @@ line = xt.Line.from_madx_sequence(sequence=mad.sequence.testseq,
                                     deferred_expressions=True)
 line.particle_ref = xt.Particles(p0c=7000e9, mass0=0.9382720813e9)
 
-tw = line.twiss(betx=1, bety=2, start=line.element_names[0],
+tw = line.twiss(betx=1, bety=1, start=line.element_names[0],
                                  end=line.element_names[-1])
 
-twmad = mad.twiss(betx=1, bety=2, ripken=True)
+twmad = mad.twiss(betx=1, bety=1, ripken=True)
 
 print('betx2 mad:    ', twmad.beta12[-1])
 print('betx2 xtrack: ', tw.betx2[-1])
-print('bety2 mad:    ', twmad.beta22[-1])
-print('bety2 xtrack: ', tw.bety2[-1])
+print('bety2 mad:    ', twmad.beta21[-1])
+print('bety2 xtrack: ', tw.bety1[-1])
