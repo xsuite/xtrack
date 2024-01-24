1c393a60f4c5017c959014e29e8a7dee4ab2b92d
giadarol <giovanni.iadarola@cern.ch>
2024-01-24 22:39:05 +0100

    Normal form

--- examples/mad_writer/004_xsuite_madng.py
+++ examples/mad_writer/004_xsuite_madng.py
@@ -46,3 +46,31 @@ mng2["mytwtable", 'mytwflow'] = mng2.twiss(
 
 print(mng2["mytwtable"].mu1[-1])
 assert np.isclose(mng2["mytwtable"].mu1[-1][0], 62.31, atol=1e-6, rtol=0)
+
+
+mng2.send('''
+    local track in MAD  -- like "from MAD import track"
+    local mytrktable, mytrkflow = MAD.track{sequence=MADX.lhcb1, method=4,
+                                            mapdef=4, nslice=3}
+
+        -- print(MAD.typeid.is_damap(mytrkflow[1]))
+
+    local normal in MAD.gphys  -- like "from MAD.gphys import normal"
+    local my_norm_for = normal(mytrkflow[1]):analyse('anh') -- anh stands for anharmonicity
+
+    local nf = my_norm_for
+    py:send({
+            nf:q1{1}, -- qx from the normal form (fractional part)
+            nf:q2{1}, -- qy
+            nf:dq1{1}, -- dqx / d delta
+            nf:dq2{1}, -- dqy / d delta
+            nf:dq1{2}, -- d2 qx / d delta2
+            nf:dq2{2}, -- d2 qy / d delta2
+            nf:anhx{1, 0}, -- dqx / djx
+            nf:anhy{0, 1}, -- dqy / djy
+            nf:anhx{0, 1}, -- dqx / djy
+            nf:anhy{1, 0}, -- dqy / djx
+            })
+''')
+
+out = mng2.recv()
