8fd350d41b1ed8b222d036b3c1053a9a9d901571
giadarol <giovanni.iadarola@cern.ch>
2024-02-01 15:42:06 +0100

    The vertical orbit is sligtly different?!

--- examples/twiss/028_ddx_ddqx.py
+++ examples/twiss/028_ddx_ddqx.py
@@ -1,3 +1,4 @@
+import numpy as np
 from cpymad.madx import Madx
 import xtrack as xt
 
@@ -7,10 +8,14 @@ mad.call('../../test_data/hllhc15_thick/hllhc_sequence.madx')
 mad.call('../../test_data/hllhc15_thick/opt_round_150_1500.madx')
 mad.input('beam, sequence=lhcb1, particle=proton, pc=7000;')
 mad.use(sequence='lhcb1')
-mad.globals['on_x5'] = 3000
+mad.globals['on_x5'] = 300
 tw_chr = mad.twiss(chrom=True)
 
+twm = xt.Table(tw_chr)
+
 line = xt.Line.from_madx_sequence(mad.sequence.lhcb1, deferred_expressions=True)
+line.config.XTRACK_USE_EXACT_DRIFTS = True
+line.configure_bend_model(core='expanded', edge='linear')
 
 # tt = line.get_table()
 # tt_sol = tt.rows[tt.element_type == 'Solenoid']
@@ -23,7 +28,7 @@ line.particle_ref = xt.Particles(mass0=mad.sequence.lhcb1.beam.mass*1e9,
 
 tw = line.twiss(method='4d')
 nlchr = line.get_non_linear_chromaticity(delta0_range=(-1e-4, 1e-4),
-                                        num_delta=2, fit_order=1, method='4d')
+                                        num_delta=21, fit_order=1, method='4d')
 tw_fw = line.twiss(start='ip4', end='ip6', init_at='ip4',
               x=tw['x', 'ip4'], px=tw['px', 'ip4'],
               y=tw['y', 'ip4'], py=tw['py', 'ip4'],
@@ -47,6 +52,32 @@ tw_bw = line.twiss(start='ip4', end='ip6', init_at='ip6',
               compute_chromatic_properties=True)
 
 
+tw_fw_mad = line.twiss(start='ip4', end='ip6', init_at='ip4',
+              x=twm['x', 'ip4:1'], px=twm['px', 'ip4:1'],
+              y=twm['y', 'ip4:1'], py=twm['py', 'ip4:1'],
+              betx=twm['betx', 'ip4:1'], bety=twm['bety', 'ip4:1'],
+              alfx=twm['alfx', 'ip4:1'], alfy=twm['alfy', 'ip4:1'],
+              dx=twm['dx', 'ip4:1'], dpx=twm['dpx', 'ip4:1'],
+              dy=twm['dy', 'ip4:1'], dpy=twm['dpy', 'ip4:1'],
+              ddx=twm['ddx', 'ip4:1']*2, ddy=twm['ddy', 'ip4:1']*2,
+              ddpx=twm['ddpx', 'ip4:1']*2, ddpy=twm['ddpy', 'ip4:1']*2,
+              compute_chromatic_properties=True)
+
+ddy_mad = np.interp(tw_fw.s, twm.s, twm.ddy)
+ddx_mad = np.interp(tw_fw.s, twm.s, twm.ddx)
+
+nlchr = line.get_non_linear_chromaticity(delta0_range=(-1e-3, 1e-3),
+                                        num_delta=21, fit_order=1, method='4d')
+
+tw_mad = []
+for dd in nlchr.delta0:
+    mad.input(f'twiss, deltap={dd};')
+    tw_mad.append(mad.table.twiss.dframe())
+
+x_mad = np.array([tw.x[0] for tw in tw_mad])
+y_mad = np.array([tw.y[0] for tw in tw_mad])
+x_xs = np.array([tw.x[0] for tw in nlchr.twiss])
+y_xs = np.array([tw.y[0] for tw in nlchr.twiss])
 
 import matplotlib.pyplot as plt
 plt.figure(1)
@@ -74,5 +105,18 @@ plt.plot(tw_chr.s, tw_chr.ddpy * 2)
 plt.plot(tw_fw.s, tw_fw.ddpy, '.')
 plt.plot(tw_bw.s, tw_bw.ddpy, '-')
 plt.ylabel('ddpy')
+
+plt.figure(2)
+plt.subplot(2, 1, 1)
+plt.plot(nlchr.delta0*1e3, x_xs, label='xsuite')
+plt.plot(nlchr.delta0*1e3, x_mad, label='madx')
+plt.ylabel('x')
+plt.legend()
+plt.subplot(2, 1, 2)
+plt.plot(nlchr.delta0*1e3, y_xs, label='xsuite')
+plt.plot(nlchr.delta0*1e3, y_mad, label='madx')
+plt.ylabel('y')
+plt.legend()
+plt.xlabel(r'$\delta$ [1e-3]')
 
 plt.show()
