37b35d0135958b446275d0bb2b60e8be962537c6
Andrea L <andrea.latina@cern.ch>
2024-02-13 18:16:22 +0100

    Update 400_rf_fieldmap_1d.py

--- examples/rf_track/400_rf_fieldmap_1d.py
+++ examples/rf_track/400_rf_fieldmap_1d.py
@@ -27,9 +27,9 @@ freq = 11.994e9 # Hz, RF frequency
 
 # Setup the element
 RF = RFT.RF_FieldMap_1d(Ez, hz/1e3, -1, freq, +1);
-RF.set_P_actual(10e6)
-RF.set_P_map(4)
-RF.set_odeint_algorithm('leapfrog')
+RF.set_P_actual(10e6) # W, actual input power
+RF.set_P_map(4) # W, field map input power
+RF.set_odeint_algorithm('rk2')
 RF.set_smooth(10)
 
 #############################################
