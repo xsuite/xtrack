a9dcf9615ad5c8d3d5241a1d1a51488d6a50c032
giadarol <giovanni.iadarola@cern.ch>
2024-02-17 06:52:22 +0100

    Add tests

--- tests/test_elements_thick.py
+++ tests/test_elements_thick.py
@@ -1095,3 +1095,70 @@ def test_solenoid_thick_analytic(test_context, length, expected):
     assert np.allclose(delta_ell, expected[4], atol=1e-9)
     assert np.allclose(p_sol.delta, expected[5], atol=1e-9)
     assert np.allclose(p_sol.s, length, atol=1e-9)
+
+@for_all_test_contexts
+def test_skew_quadrupole(test_context):
+    k1 = 1.0
+    k1s = 2.0
+
+    length = 0.5
+
+    quad = xt.Quadrupole(k1=k1, k1s=k1s, length=length, _context=test_context)
+
+    n_slices = 1000
+    ele_thin = []
+    for ii in range(n_slices):
+        ele_thin.append(xt.Drift(length=length/n_slices/2))
+        ele_thin.append(xt.Multipole(knl=[0, k1 * length/n_slices],
+                                    ksl=[0, k1s * length/n_slices]))
+        ele_thin.append(xt.Drift(length=length/n_slices/2))
+    lref = xt.Line(ele_thin)
+    lref.build_tracker(_context=test_context)
+
+    p_test = xt.Particles(gamma0=1.2, x=0.1, y=0.2, delta=0.5,
+                           test_context=test_context)
+    p_ref = p_test.copy()
+
+    quad.track(p_test)
+    lref.track(p_ref)
+
+    p_test.move(_context=xo.context_default)
+    p_ref.move(_context=xo.context_default)
+
+    assert np.isclose(p_test.x, p_ref.x, atol=1e-8, rtol=0)
+    assert np.isclose(p_test.px, p_ref.px, atol=5e-8, rtol=0)
+    assert np.isclose(p_test.y, p_ref.y, atol=1e-8, rtol=0)
+    assert np.isclose(p_test.py, p_ref.py, atol=5e-8, rtol=0)
+    assert np.isclose(p_test.zeta, p_ref.zeta, atol=1e-8, rtol=0)
+    assert np.isclose(p_test.delta, p_ref.delta, atol=5e-8, rtol=0)
+
+@for_all_test_contexts
+def test_octupole(test_context):
+
+    k3 = 1.0
+    k3s = 2.0
+
+    length = 0.5
+
+    oct = xt.Octupole(k3=k3, k3s=k3s, length=length, _context=test_context)
+
+    ele_thin = []
+    ele_thin.append(xt.Drift(length=length/2))
+    ele_thin.append(xt.Multipole(knl=[0, 0, 0, k3 * length],
+                                ksl=[0, 0, 0, k3s * length]))
+    ele_thin.append(xt.Drift(length=length/2))
+    lref = xt.Line(ele_thin)
+    lref.build_tracker(_context=test_context)
+
+    p_test = xt.Particles(gamma0=1.2, x=0.1, y=0.2, delta=0.5, _context=test_context)
+    p_ref = p_test.copy()
+
+    oct.track(p_test)
+    lref.track(p_ref)
+
+    assert np.isclose(p_test.x, p_ref.x, atol=1e-12, rtol=0)
+    assert np.isclose(p_test.px, p_ref.px, atol=1e-12, rtol=0)
+    assert np.isclose(p_test.y, p_ref.y, atol=1e-12, rtol=0)
+    assert np.isclose(p_test.py, p_ref.py, atol=1e-12, rtol=0)
+    assert np.isclose(p_test.zeta, p_ref.zeta, atol=1e-12, rtol=0)
+    assert np.isclose(p_test.delta, p_ref.delta, atol=1e-12, rtol=0)
