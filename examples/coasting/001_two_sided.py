458acdbac7f6c61476abf8ae55a748dc28290fcb
giadarol <giovanni.iadarola@cern.ch>
2024-02-21 22:10:05 +0100

    Zeta range stays confined but drifts

--- examples/coasting/001_two_sided.py
+++ examples/coasting/001_two_sided.py
@@ -23,60 +23,75 @@ line.configure_bend_model(core='adaptive', edge='full', num_multipole_kicks=10)
 line.twiss_default['method'] = '4d'
 
 tw = line.twiss()
-
+beta1 = tw.beta0 * 1.1
 class CoastWrap:
 
-    def __init__(self, length, id):
-        self.length = length
+    def __init__(self, circumference, id, beta1):
         assert id > 10000
         self.id = id
-        self.beta1 = tw.beta0*1.1
+        self.beta1 = beta1
+        self.circumference = circumference
 
     def track(self, particles):
 
-        # if (particles.at_turn > 9).any():
-        #     import pdb; pdb.set_trace()
+        particles.sort(interleave_lost_particles=True)
+        particles.get_table().cols['zeta state delta s at_turn'].show()
+        import pdb; pdb.set_trace()
+        particles.reorganize()
 
         # Resume particles previously stopped
         particles.state[particles.state==-self.id] = 1
         particles.reorganize()
 
         zeta_prime = self.zeta_to_zeta_prime(particles.zeta,
-                                             particles.beta0, particles.s)
+                                             particles.beta0, particles.s,
+                                             particles.at_turn)
 
         # Identify particles that need to be stopped
         mask_alive = particles.state > 0
-        mask_stop = mask_alive & (zeta_prime < -self.length / 2)
+        mask_stop = mask_alive & (zeta_prime < -self.circumference / 2)
 
         # Update zeta for particles that are stopped
-        zeta_prime[mask_stop] += self.length
+        zeta_prime[mask_stop] += self.circumference
+        particles.at_turn[mask_stop] -= 1
         zeta_stopped = self.zeta_prime_to_zeta(zeta_prime[mask_stop],
                                                particles.beta0[mask_stop],
-                                               particles.s[mask_stop])
+                                               particles.s[mask_stop],
+                                               particles.at_turn[mask_stop])
         particles.zeta[mask_stop] = zeta_stopped
 
         # Stop particles
         particles.state[mask_stop] = -self.id
-        particles.at_turn[mask_stop] += 1
 
-    def zeta_to_zeta_prime(self, zeta, beta0, s):
+
+        # assert np.all(particles.zeta.max() - particles.zeta.min()
+        #               < self.circumference * tw.beta0 / self.beta1)
+
+        particles.sort(interleave_lost_particles=True)
+        particles.get_table().cols['zeta state delta s at_turn'].show()
+        import pdb; pdb.set_trace()
+        particles.reorganize()
+
+    def zeta_to_zeta_prime(self, zeta, beta0, s, at_turn):
+        S_capital = s + at_turn * self.circumference
         beta1_beta0 = self.beta1 / beta0
-        zeta_prime =  zeta * beta1_beta0 + (1 - beta1_beta0) * s
+        zeta_prime =  zeta * beta1_beta0 + (1 - beta1_beta0) * S_capital
         return zeta_prime
 
-    def zeta_prime_to_zeta(self, zeta_prime, beta0, s):
+    def zeta_prime_to_zeta(self, zeta_prime, beta0, s, at_turn):
+        S_capital = s + at_turn * self.circumference
         beta0_beta1 = beta0 / self.beta1
-        zeta = zeta_prime * beta0_beta1 + (1 - beta0_beta1) * s
+        zeta = zeta_prime * beta0_beta1 + (1 - beta0_beta1) * S_capital
         return zeta
 
 circumference = line.get_length()
-wrap_end = CoastWrap(length=circumference, id=10001)
-wrap_start = CoastWrap(length=circumference, id=10002)
+wrap_end = CoastWrap(circumference=circumference, beta1=beta1, id=10001)
+wrap_start = CoastWrap(circumference=circumference, beta1=beta1, id=10002)
 
 zeta_prime_min = -circumference/2
 zeta_prime_max = circumference/2
-zeta_min = wrap_start.zeta_prime_to_zeta(zeta_prime_min, tw.beta0, 0)
-zeta_max = wrap_start.zeta_prime_to_zeta(zeta_prime_max, tw.beta0, 0)
+zeta_min = wrap_start.zeta_prime_to_zeta(zeta_prime_min, tw.beta0, 0, 0)
+zeta_max = wrap_start.zeta_prime_to_zeta(zeta_prime_max, tw.beta0, 0, 0)
 
 num_particles = 1000
 # p = line.build_particles(
@@ -84,7 +99,7 @@ num_particles = 1000
 #     delta=np.random.uniform(-1e-2, 0, num_particles)
 # )
 zeta_grid= np.linspace(zeta_max-circumference, zeta_max, 5)
-delta_grid = np.linspace(-1e-2, 0, 7)
+delta_grid = np.linspace(-1e-2, 0, 3)
 ZZ, DD = np.meshgrid(zeta_grid, delta_grid)
 p = line.build_particles(
     zeta=ZZ.flatten(),
@@ -101,10 +116,11 @@ def intensity(line, particles):
     mask_alive = particles.state > 0
     zeta_alive = particles.zeta[mask_alive]
 
-    # zeta_min = wrap_start.zeta_prime_to_zeta(zeta_prime_min, tw.beta0, particles.s[0])
-    # zeta_max = wrap_start.zeta_prime_to_zeta(zeta_prime_max, tw.beta0, particles.s[0])
-    # assert np.all(zeta_alive >= zeta_min - 0.1)
-    # assert np.all(zeta_alive <= zeta_max + 0.1)
+    # particles.sort(interleave_lost_particles=True)
+    # particles.get_table().cols['zeta state delta s'].show()
+    # import pdb; pdb.set_trace()
+    # particles.reorganize()
+
     return np.sum(particles.state > 0)/((zeta_max - zeta_min)/tw.beta0/clight)
 
 line.enable_time_dependent_vars = True
