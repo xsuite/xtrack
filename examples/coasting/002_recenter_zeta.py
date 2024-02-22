14477477029eca5256da601666648b1a48bd387c
giadarol <giovanni.iadarola@cern.ch>
2024-02-22 13:10:45 +0100

    Status

--- examples/coasting/002_recenter_zeta.py
+++ examples/coasting/002_recenter_zeta.py
@@ -34,33 +34,32 @@ class CoastWrap:
 
     def track(self, particles):
 
-        # ---- For debugging
-        particles.sort(interleave_lost_particles=True)
-        particles.get_table().cols['zeta state delta s at_turn'].show()
-        import pdb; pdb.set_trace()
-        particles.reorganize()
+        # # ---- For debugging
+        # particles.sort(interleave_lost_particles=True)
+        # particles.get_table().cols['zeta state delta s at_turn'].show()
+        # import pdb; pdb.set_trace()
+        # particles.reorganize()
 
         # Resume particles previously stopped
         particles.state[particles.state==-self.id] = 1
         particles.reorganize()
 
-        beta0_beta1 = particles.beta0 / self.beta1
+        zeta_prime = self.zeta_to_zeta_prime(particles.zeta,
+                                             particles.beta0, particles.s,
+                                             particles.at_turn)
 
         # Identify particles that need to be stopped
         mask_alive = particles.state > 0
-        mask_stop = mask_alive & (
-            particles.zeta < -self.circumference / 2 * beta0_beta1)
-
-        # mask_error = mask_alive & (
-        #     particles.zeta > self.circumference / 2 * beta0_beta1*1.1)
-        # if mask_error.any():
-        #     raise ValueError('Inconsistency!')
+        mask_stop = mask_alive & (zeta_prime < -self.circumference / 2)
 
         # Update zeta for particles that are stopped
-        particles.zeta [mask_stop] += self.circumference * beta0_beta1[mask_stop]
+        zeta_prime[mask_stop] += self.circumference
         particles.at_turn[mask_stop] += 1
         particles.pdg_id[mask_stop] += 1 # HACK!!!!!
-        zeta_stopped = particles.zeta[mask_stop] - self.circumference * beta0_beta1[mask_stop]
+        zeta_stopped = self.zeta_prime_to_zeta(zeta_prime[mask_stop],
+                                               particles.beta0[mask_stop],
+                                               particles.s[mask_stop],
+                                               particles.at_turn[mask_stop])
         particles.zeta[mask_stop] = zeta_stopped
 
         # Stop particles
@@ -70,32 +69,45 @@ class CoastWrap:
         # assert np.all(particles.zeta.max() - particles.zeta.min()
         #               < self.circumference * tw.beta0 / self.beta1)
 
-        # ---- For debugging
-        particles.sort(interleave_lost_particles=True)
-        particles.get_table().cols['zeta state delta s at_turn'].show()
-        import pdb; pdb.set_trace()
-        particles.reorganize()
+        # # ---- For debugging
+        # particles.sort(interleave_lost_particles=True)
+        # particles.get_table().cols['zeta state delta s at_turn'].show()
+        # import pdb; pdb.set_trace()
+        # particles.reorganize()
 
+    def zeta_to_zeta_prime(self, zeta, beta0, s, at_turn):
+        S_capital = s + at_turn * self.circumference
+        beta1_beta0 = self.beta1 / beta0
+        zeta_prime =  zeta * beta1_beta0 + (1 - beta1_beta0) * S_capital
+        return zeta_prime
+
+    def zeta_prime_to_zeta(self, zeta_prime, beta0, s, at_turn):
+        S_capital = s + at_turn * self.circumference
+        beta0_beta1 = beta0 / self.beta1
+        zeta = zeta_prime * beta0_beta1 + (1 - beta0_beta1) * S_capital
+        return zeta
 
 circumference = line.get_length()
 wrap_end = CoastWrap(circumference=circumference, beta1=beta1, id=10001)
 wrap_start = CoastWrap(circumference=circumference, beta1=beta1, id=10002)
 
-zeta_min = -circumference / 2 * tw.beta0 / beta1
-zeta_max = +circumference / 2 * tw.beta0 / beta1
+zeta_prime_min = -circumference/2
+zeta_prime_max = circumference/2
+zeta_min = wrap_start.zeta_prime_to_zeta(zeta_prime_min, tw.beta0, 0, 0)
+zeta_max = wrap_start.zeta_prime_to_zeta(zeta_prime_max, tw.beta0, 0, 0)
 
 num_particles = 1000
 p = line.build_particles(
-    zeta=np.random.uniform(zeta_max, zeta_max - circumference, num_particles),
+    zeta=np.random.uniform(zeta_min, zeta_min + circumference, num_particles),
     delta=np.random.uniform(0e-2, 5e-2, num_particles)
 )
-zeta_grid= np.linspace(zeta_max-circumference, zeta_max, 5)
-delta_grid = np.linspace(-1e-2, 0, 3)
-ZZ, DD = np.meshgrid(zeta_grid, delta_grid)
-p = line.build_particles(
-    zeta=ZZ.flatten(),
-    delta=DD.flatten()
-)
+# zeta_grid= np.linspace(zeta_max-circumference, zeta_max, 5)
+# delta_grid = np.linspace(-1e-2, 0, 3)
+# ZZ, DD = np.meshgrid(zeta_grid, delta_grid)
+# p = line.build_particles(
+#     zeta=ZZ.flatten(),
+#     delta=DD.flatten()
+# )
 p.i_frame = 0
 wrap_start.track(p)
 
