7978fa891a3ffe2676088b56a131273091a347ae
Frederik Van der Veken <frederik@cern.ch>
2024-03-01 21:42:56 +0100

    Fix: When looking for a suitable kernel, an empty config would not find anything. Now it defaults to the default config of a Line.

--- xtrack/prebuild_kernels.py
+++ xtrack/prebuild_kernels.py
@@ -206,6 +206,10 @@ def get_suitable_kernel(
     requested_class_names = [cls for cls in requested_class_names
                              if cls != 'Particles' and cls != 'ParticlesBase']
 
+    # Get a default config
+    if config == {}:
+        config  = xt.Line().config
+
     for module_name, kernel_metadata in enumerate_kernels():
         if verbose:
             _print(f"==> Considering the precompiled kernel `{module_name}`...")
