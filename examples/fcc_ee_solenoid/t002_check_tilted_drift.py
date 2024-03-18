2a0e120a2f19822b2a2b5c6d22d2e95379ff9b1a
giadarol <giovanni.iadarola@cern.ch>
2024-03-18 19:56:43 +0100

    Particle on solenoid axis works correctly

--- examples/fcc_ee_solenoid/t002_check_tilted_drift.py
+++ examples/fcc_ee_solenoid/t002_check_tilted_drift.py
@@ -19,12 +19,14 @@ line = xt.Line(
     element_names=['start', 'tilt_entry', 'shift_entry', 'solenoid', 'shift_exit', 'tilt_exit', 'end']
 )
 line.config.XTRACK_USE_EXACT_DRIFTS = True
+line.config.XTRACK_GLOBAL_XY_LIMIT = 1000.
 
 line2 = xt.Line(
     elements=[xt.Marker(), xt.Drift(length=l_beam), xt.Marker()],
     element_names=['start', 'solenoid', 'end']
 )
-line.config.XTRACK_USE_EXACT_DRIFTS = True
+line2.config.XTRACK_USE_EXACT_DRIFTS = True
+line2.config.XTRACK_GLOBAL_XY_LIMIT = 1000.
 line2.build_tracker()
 
 
@@ -32,8 +34,11 @@ line2.build_tracker()
 line.build_tracker()
 line.tracker.skip_end_turn_actions = True
 
-p0 = xt.Particles(p0c=6500e9, x=np.linspace(-1e-3, 1e-3, 11),
-                  px=0.1)
+# p0 = xt.Particles(p0c=6500e9, x=np.linspace(-1e-3, 1e-3, 11),
+#                   px=0.1)
+# particle on the solenoid axis
+p0 = xt.Particles(p0c=6500e9, px=-np.sin(theta_tilt_rad),
+                  x=l_beam/2 * np.tan(theta_tilt_rad))
 p = p0.copy()
 line.track(p)
 
@@ -44,8 +49,8 @@ line2.track(p2)
 pref = xt.Particles(p0c=6500e9)
 line.particle_ref = pref
 line2.particle_ref = pref
-tw = line.twiss(betx=1, bety=1, start='start', end='end')
-tw2 = line2.twiss(betx=1, bety=1, start='start', end='end')
+tw = line.twiss(betx=1, bety=1, x=p0.x[0], px=p0.px[0], start='start', end='end')
+tw2 = line2.twiss(betx=1, bety=1, x=p0.x[0], px=p0.px[0], start='start', end='end')
 
 pco = pref.copy()
 R = line.compute_one_turn_matrix_finite_differences(particle_on_co=pco)['R_matrix']
