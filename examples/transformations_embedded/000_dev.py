919b1b687b374ff47ef89e5670b1a8bb532e2a71
giadarol <giovanni.iadarola@cern.ch>
2024-03-22 12:15:37 +0100

    A first agreement

--- examples/transformations_embedded/000_dev.py
+++ examples/transformations_embedded/000_dev.py
@@ -1,16 +1,16 @@
 import xtrack as xt
 from cpymad.madx import Madx
 
+import numpy as np
+
 mad = Madx()
 
 mad.input("""
 k1=0.2;
-tilt=0.1;
 
-elm: sbend,
+elm: quadrupole,
     k1:=k1,
     l=1,
-    angle=0.1,
     tilt=0.2;
 
 seq: sequence, l=1;
@@ -23,3 +23,20 @@ use, sequence=seq;
 
 
 lmad = xt.Line.from_madx_sequence(mad.sequence.seq)
+lmad.build_tracker()
+
+bend = lmad['elm'].copy()
+
+bend._sin_tilt = np.sin(0.2)
+bend._cos_tilt = np.cos(0.2)
+
+p0 = xt.Particles(x=1e-3, px=2e-3, y=3e-3, py=4e-3, p0c=1e9)
+
+pmad = p0.copy()
+ptest = p0.copy()
+
+bend.track(ptest)
+lmad.track(pmad)
+
+pmad.get_table()
+ptest.get_table()
