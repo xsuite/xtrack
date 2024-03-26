86e0f9137a28a3038d6c7f3f2c4c3d4395731dc3
giadarol <giovanni.iadarola@cern.ch>
2024-03-26 15:44:00 +0100

    Working without transformations

--- examples/transformations_embedded/002_relative_slices.py
+++ examples/transformations_embedded/002_relative_slices.py
@@ -1,10 +1,43 @@
 import xtrack as xt
 import xobjects as xo
+from pathlib import Path
+import numpy as np
 
-class QuadThickSlice(xt.BeamElement):
+from xtrack.general import _pkg_root
+
+xo.context_default.kernels.clear()
+
+class QuadrupoleThickSlice(xt.BeamElement):
+    allow_rot_and_shift = False
     _xofields = {
+        'length': xo.Float64,
         'parent': xo.Ref(xt.Quadrupole),
     }
 
+    _extra_c_sources = [
+        _pkg_root.joinpath('beam_elements/elements_src/drift.h'),
+        _pkg_root.joinpath('beam_elements/elements_src/track_thick_cfd.h'),
+        _pkg_root.joinpath('beam_elements/elements_src/track_srotation.h'),
+        _pkg_root.joinpath('beam_elements/elements_src/track_quadrupole.h'),
+        Path('./quad_thick_slice.h'),
+    ]
+
 quad = xt.Quadrupole(k1=0.1, length=1)
-quad_slice = QuadThickSlice(parent=quad)
+quad_slice = QuadrupoleThickSlice(length=quad.length/2, parent=quad, _buffer=quad._buffer)
+
+p0 = xt.Particles(p0c=10e9, x=0.1, px=0.2, y=0.3, py=0.4, delta=0.03)
+p_ref = p0.copy()
+p_slice = p0.copy()
+
+quad_slice.track(p_slice)
+quad_slice.track(p_slice)
+
+quad.track(p_ref)
+
+assert_allclose = np.testing.assert_allclose
+assert_allclose(p_slice.x, p_ref.x, rtol=0, atol=1e-14)
+assert_allclose(p_slice.px, p_ref.px, rtol=0, atol=1e-14)
+assert_allclose(p_slice.y, p_ref.y, rtol=0, atol=1e-14)
+assert_allclose(p_slice.py, p_ref.py, rtol=0, atol=1e-14)
+assert_allclose(p_slice.zeta, p_ref.zeta, rtol=0, atol=1e-14)
+assert_allclose(p_slice.delta, p_ref.delta, rtol=0, atol=1e-14)
