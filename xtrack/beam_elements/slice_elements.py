0a9702e90bbdb4a74938c890b017b14e3feaccd9
giadarol <giovanni.iadarola@cern.ch>
2024-03-28 10:08:00 +0100

    Clean up

--- xtrack/beam_elements/slice_elements.py
+++ xtrack/beam_elements/slice_elements.py
@@ -9,29 +9,31 @@ from ..base_element import BeamElement
 
 xo.context_default.kernels.clear()
 
+_common_xofields = {
+    'radiation_flag': xo.Int64,
+    'delta_taper': xo.Float64,
+    'weight': xo.Float64,
+}
+_common_c_sources = [
+    _pkg_root.joinpath('headers/constants.h'),
+    _pkg_root.joinpath('headers/synrad_spectrum.h'),
+    _pkg_root.joinpath('beam_elements/elements_src/track_multipole.h')
+]
+
+_thin_slice_quad_xofields = {
+    '_parent': xo.Ref(Quadrupole)}
+_thin_slice_quad_xofields.update(_common_xofields)
 class ThinSliceQuadrupole(BeamElement):
     allow_rot_and_shift = False
-    _xofields = {
-        '_parent': xo.Ref(Quadrupole),
-        'radiation_flag': xo.Int64,
-        'delta_taper': xo.Float64,
-        'weight': xo.Float64,
-    }
-
     _skip_in_to_dict = ['_parent']
-
     _depends_on = [RandomUniform, RandomExponential]
-
-    _extra_c_sources = [
-        _pkg_root.joinpath('headers/constants.h'),
-        _pkg_root.joinpath('headers/synrad_spectrum.h'),
-        _pkg_root.joinpath('beam_elements/elements_src/track_multipole.h'),
-        _pkg_root.joinpath('beam_elements/elements_src/thin_slice_quadrupole.h'),
-    ]
-
     _internal_record_class = SynchrotronRadiationRecord
-
     has_backtrack = True
+
+    _xofields = _thin_slice_quad_xofields
+
+    _extra_c_sources = _common_c_sources + [
+        _pkg_root.joinpath('beam_elements/elements_src/thin_slice_quadrupole.h')]
 
     def to_dict(self, **kwargs):
         dct = BeamElement.to_dict(self, **kwargs)
