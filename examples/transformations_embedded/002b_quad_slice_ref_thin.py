736f369f66686fe68bf71d647b5c5302cd26c053
giadarol <giovanni.iadarola@cern.ch>
2024-03-28 08:17:52 +0100

    Clean up example and add some checks

--- examples/transformations_embedded/002b_quad_slice_ref_thin.py
+++ examples/transformations_embedded/002b_quad_slice_ref_thin.py
@@ -5,20 +5,6 @@ import numpy as np
 
 
 quad = xt.Quadrupole(k1=0.1, length=1)
-# quad.rot_s = 20.
-# quad.shift_x = 0.1
-# quad.shift_y = 0.2
-
-
-quad_slice = xt.ThinSliceQuadrupole(weight=0.5, _parent=quad, _buffer=quad._buffer)
-quad_mult = xt.Multipole(knl=[0, 0.1/2], length=1./2)
-
-p0 = xt.Particles(p0c=10e9, x=0.1, px=0.2, y=0.3, py=0.4, delta=0.03)
-p_ref = p0.copy()
-p_slice = p0.copy()
-
-quad_mult.track(p_ref)
-quad_slice.track(p_slice)
 
 line = xt.Line(elements=[quad])
 line.build_tracker() # Put everything in the same buffer
@@ -33,6 +19,15 @@ assert line['e0..995']._parent is line['e0']
 p0 = xt.Particles(p0c=10e9, x=0.1, px=0.2, y=0.3, py=0.4, delta=0.03)
 p_ref = p0.copy()
 p_slice = p0.copy()
+
+assert_allclose = np.testing.assert_allclose
+assert_allclose = np.testing.assert_allclose
+assert_allclose(p_slice.x, p_ref.x, rtol=0, atol=1e-14)
+assert_allclose(p_slice.px, p_ref.px, rtol=0, atol=1e-14)
+assert_allclose(p_slice.y, p_ref.y, rtol=0, atol=1e-14)
+assert_allclose(p_slice.py, p_ref.py, rtol=0, atol=1e-14)
+assert_allclose(p_slice.zeta, p_ref.zeta, rtol=0, atol=1e-14)
+assert_allclose(p_slice.delta, p_ref.delta, rtol=0, atol=1e-14)
 
 line.track(p_slice)
 quad.track(p_ref)
