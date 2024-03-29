a4aab6325f5b9cb51b71a097c52e9ffd24116638
giadarol <giovanni.iadarola@cern.ch>
2024-03-29 14:41:30 +0100

    More checks

--- examples/transformations_embedded/002f_bend_with_shift.py
+++ examples/transformations_embedded/002f_bend_with_shift.py
@@ -5,11 +5,16 @@ import numpy as np
 #                knl=[0, 0, 0.03, 0.4, 0.5],
 #                ksl=[0, 0, -0.03, -0.2, -0.1]
 # )
+# bend.num_multipole_kicks = 10000
+# atol=1e-7
+# n_teapot = 10000
 
 bend = xt.Bend(k0=0., h=0., k1=0, length=1,
                knl=[0, 0, 0.03, 0.4, 0.5],
                ksl=[0, 0, -0.03, -0.2, -0.1]
 )
+atol=1e-10
+n_teapot = 1
 
 bend.rot_s = 20.
 bend.shift_x = 0.1
@@ -20,7 +25,7 @@ line = xt.Line(elements=[bend])
 line.configure_bend_model(edge='full', core='expanded')
 
 line.slice_thick_elements(
-    slicing_strategies=[xt.Strategy(xt.Teapot(1))])
+    slicing_strategies=[xt.Strategy(xt.Teapot(n_teapot))])
 line.build_tracker()
 line._line_before_slicing.build_tracker()
 assert line['e0..0']._parent_name == 'e0'
@@ -41,12 +46,12 @@ line._line_before_slicing.track(p_ref)
 
 assert_allclose = np.testing.assert_allclose
 assert_allclose = np.testing.assert_allclose
-assert_allclose(p_slice.x, p_ref.x, rtol=0, atol=1e-10)
-assert_allclose(p_slice.px, p_ref.px, rtol=0, atol=1e-10)
-assert_allclose(p_slice.y, p_ref.y, rtol=0, atol=1e-10)
-assert_allclose(p_slice.py, p_ref.py, rtol=0, atol=1e-10)
-assert_allclose(p_slice.zeta, p_ref.zeta, rtol=0, atol=1e-10)
-assert_allclose(p_slice.delta, p_ref.delta, rtol=0, atol=1e-10)
+assert_allclose(p_slice.x, p_ref.x, rtol=0, atol=atol)
+assert_allclose(p_slice.px, p_ref.px, rtol=0, atol=atol)
+assert_allclose(p_slice.y, p_ref.y, rtol=0, atol=atol)
+assert_allclose(p_slice.py, p_ref.py, rtol=0, atol=atol)
+assert_allclose(p_slice.zeta, p_ref.zeta, rtol=0, atol=atol)
+assert_allclose(p_slice.delta, p_ref.delta, rtol=0, atol=atol)
 
 
 # Test properties
