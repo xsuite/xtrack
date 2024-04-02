dc060ea8a7f3ccd3f1f5df3bef9a6393f0f52d21
giadarol <giovanni.iadarola@cern.ch>
2024-04-02 14:27:00 +0200

    Clean up example

--- examples/transformations_embedded/002a_quad_slice_ref_thick.py
+++ examples/transformations_embedded/002a_quad_slice_ref_thick.py
@@ -7,31 +7,14 @@ from xtrack.general import _pkg_root
 
 xo.context_default.kernels.clear()
 
-class QuadrupoleThickSlice(xt.BeamElement):
-    allow_rot_and_shift = False
-    _xofields = {
-        'length': xo.Float64,
-        'parent': xo.Ref(xt.Quadrupole),
-    }
-
-    _extra_c_sources = [
-        _pkg_root.joinpath('beam_elements/elements_src/drift.h'),
-        _pkg_root.joinpath('beam_elements/elements_src/track_thick_cfd.h'),
-        _pkg_root.joinpath('beam_elements/elements_src/track_srotation.h'),
-        _pkg_root.joinpath('beam_elements/elements_src/track_quadrupole.h'),
-        Path('./quad_thick_slice.h'),
-    ]
-
 quad = xt.Quadrupole(k1=0.1, length=1)
-quad.rot_s_rad = 0 #np.deg2rad(20.)
+quad.rot_s_rad = np.deg2rad(20.)
 quad.shift_x = 0.1
-quad.shift_y = 0; 0.2
+quad.shift_y = 0.2
 
+quad_slice = xt.ThickSliceQuadrupole(weight=0.5, _parent=quad, _buffer=quad._buffer)
 
-quad_slice = QuadrupoleThickSlice(length=quad.length/2, parent=quad, _buffer=quad._buffer)
-
-p0 = xt.Particles(p0c=10e9, x=0.1)
-# p0 = xt.Particles(p0c=10e9, x=0.1, px=0.2, y=0.3, py=0.4, delta=0.03)
+p0 = xt.Particles(p0c=10e9, x=0.1, px=0.2, y=0.3, py=0.4, delta=0.03)
 p_ref = p0.copy()
 p_slice = p0.copy()
 
