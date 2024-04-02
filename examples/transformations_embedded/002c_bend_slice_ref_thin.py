a866f1c6a59a577ff47ca3625a721a6434f2f351
giadarol <giovanni.iadarola@cern.ch>
2024-04-02 20:49:52 +0200

    More checks for bend

--- examples/transformations_embedded/002c_bend_slice_ref_thin.py
+++ examples/transformations_embedded/002c_bend_slice_ref_thin.py
@@ -72,3 +72,23 @@ if bend.shift_x !=0 or bend.shift_y != 0 or bend.rot_s_rad != 0 and bend.k1 != 0
     assert isinstance(line['e0..995'], xt.Multipole)
 else:
     assert isinstance(line['e0..995'], xt.SimpleThinBend)
+
+p_slice = p0.copy()
+line.track(p_slice)
+
+assert_allclose(p_slice.x, p_ref.x, rtol=0, atol=1e-10)
+assert_allclose(p_slice.px, p_ref.px, rtol=0, atol=1e-10)
+assert_allclose(p_slice.y, p_ref.y, rtol=0, atol=1e-10)
+assert_allclose(p_slice.py, p_ref.py, rtol=0, atol=1e-10)
+assert_allclose(p_slice.zeta, p_ref.zeta, rtol=0, atol=1e-10)
+assert_allclose(p_slice.delta, p_ref.delta, rtol=0, atol=1e-10)
+
+line.track(p_slice, backtrack=True)
+
+assert_allclose(p_slice.x, p0.x, rtol=0, atol=1e-10)
+assert_allclose(p_slice.px, p0.px, rtol=0, atol=1e-10)
+assert_allclose(p_slice.y, p0.y, rtol=0, atol=1e-10)
+assert_allclose(p_slice.py, p0.py, rtol=0, atol=1e-10)
+assert_allclose(p_slice.zeta, p0.zeta, rtol=0, atol=1e-10)
+assert_allclose(p_slice.delta, p0.delta, rtol=0, atol=1e-10)
+
