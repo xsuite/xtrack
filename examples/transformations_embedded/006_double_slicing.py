6d54cc3ad9796c3ebce4796423f9121b6c6fed41
giadarol <giovanni.iadarola@cern.ch>
2024-04-06 09:07:05 +0200

    Had not considered case with multiple cuts in the same compound

--- examples/transformations_embedded/006_double_slicing.py
+++ examples/transformations_embedded/006_double_slicing.py
@@ -13,7 +13,7 @@ line.configure_bend_model(edge='linear', core='expanded')
 line.slice_thick_elements(
     slicing_strategies=[xt.Strategy(xt.Uniform(3))])
 
-line.cut_at_s([0.3])
+line.cut_at_s([0.25, 0.3, 0.6, 0.8])
 
 line._insert_thin_elements_at_s([
     (0.4, [('mymark1', xt.Marker()), ('mymark2', xt.Marker())])])
