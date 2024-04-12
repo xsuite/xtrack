52f2d248656013518aac18707f4821e2926cd836
giadarol <giovanni.iadarola@cern.ch>
2024-04-12 17:29:28 +0200

    Check multipole

--- examples/transformations_embedded/005a_check_attr.py
+++ examples/transformations_embedded/005a_check_attr.py
@@ -10,8 +10,11 @@ sext = xt.Sextupole(k2=0.1, k2s=0.2,
                     length=0.3, shift_x=3e-3, shift_y=3e-3, rot_s_rad=0.3)
 octu = xt.Octupole(k3=0.1, k3s=0.2,
                      length=0.4, shift_x=4e-3, shift_y=4e-3, rot_s_rad=0.4)
+mult = xt.Multipole(knl=[0.7, 0.8, 0.9, 1.0], ksl=[0.1, 0.2, 0.3, 0.4],
+                    length=0.4, shift_x=5e-3, shift_y=6e-3, rot_s_rad=0.7,
+                    hxl=0.1)
 
-line = xt.Line(elements=[bend, quad, sext, octu])
+line = xt.Line(elements=[bend, quad, sext, octu, mult])
 line.build_tracker()
 tt = line.get_table(attr=True)
 
@@ -92,3 +95,23 @@ assert_allclose(tt['k0sl', 'e3'], 0.0, rtol=0, atol=1e-14)
 assert_allclose(tt['k1sl', 'e3'], 0.0, rtol=0, atol=1e-14)
 assert_allclose(tt['k2sl', 'e3'], 0, rtol=0, atol=1e-14)
 assert_allclose(tt['k3sl', 'e3'], 0.2 * 0.4, rtol=0, atol=1e-14)
+
+assert tt['element_type','e4'] == 'Multipole'
+assert tt['isreplica', 'e4'] == False
+assert tt['parent_name', 'e4'] is None
+assert tt['isthick', 'e4'] == False
+assert tt['iscollective', 'e4'] == False
+assert_allclose(tt['length', 'e4'], 0.4, rtol=0, atol=1e-14)
+assert_allclose(tt['angle_rad', 'e4'], 0.1, rtol=0, atol=1e-14)
+assert_allclose(tt['rot_s_rad', 'e4'], 0.7, rtol=0, atol=1e-14)
+assert_allclose(tt['shift_x', 'e4'], 5e-3, rtol=0, atol=1e-14)
+assert_allclose(tt['shift_y', 'e4'], 6e-3, rtol=0, atol=1e-14)
+assert_allclose(tt['k0l', 'e4'], 0.7, rtol=0, atol=1e-14)
+assert_allclose(tt['k1l', 'e4'], 0.8, rtol=0, atol=1e-14)
+assert_allclose(tt['k2l', 'e4'], 0.9, rtol=0, atol=1e-14)
+assert_allclose(tt['k3l', 'e4'], 1.0, rtol=0, atol=1e-14)
+assert_allclose(tt['k0sl', 'e4'], 0.1, rtol=0, atol=1e-14)
+assert_allclose(tt['k1sl', 'e4'], 0.2, rtol=0, atol=1e-14)
+assert_allclose(tt['k2sl', 'e4'], 0.3, rtol=0, atol=1e-14)
+assert_allclose(tt['k3sl', 'e4'], 0.4, rtol=0, atol=1e-14)
+
