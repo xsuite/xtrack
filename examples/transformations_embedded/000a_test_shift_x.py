6816a6e064beaa90394a70249238fc34a618e038
giadarol <giovanni.iadarola@cern.ch>
2024-04-12 15:56:05 +0200

    Check horizontal shift of quad

--- examples/transformations_embedded/000a_test_shift_x.py
+++ examples/transformations_embedded/000a_test_shift_x.py
@@ -16,10 +16,33 @@ p = xt.Particles(x=0, p0c=1e12)
 quad.track(p)
 
 assert_allclose = np.testing.assert_allclose
-assert_allclose(p.px, -k1 * length * 1e-3, rtol=0, atol=1e-3)
+assert_allclose(p.px, -k1 * length * -1e-3, rtol=5e-3, atol=0)
 
 # Change the shift
 quad.shift_x = 2e-3
 p = xt.Particles(x=0, p0c=1e12)
 quad.track(p)
-assert_allclose(p.px, -k1 * length*2e-3, rtol=0, atol=1e-3)
+assert_allclose(p.px, -k1 * length * -2e-3, rtol=5e-3, atol=0)
+
+# Make a line
+line = xt.Line(elements=[quad])
+
+# Slice the line:
+line.slice_thick_elements(
+    slicing_strategies=[xt.Strategy(xt.Uniform(3))])
+line.build_tracker()
+
+tt = line.get_table()
+assert len(tt.rows['e0\.\..*']) == 3
+
+p = xt.Particles(x=0, p0c=1e12)
+line.track(p)
+
+assert_allclose(p.px, -k1 * length * -2e-3, rtol=5e-3, atol=0)
+
+# Change the shift
+quad.shift_x = 3e-3
+p = xt.Particles(x=0, p0c=1e12)
+line.track(p)
+
+assert_allclose(p.px, -k1 * length * -3e-3, rtol=5e-3, atol=0)
