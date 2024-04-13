bf928b633f7b4e1d312c04c4b58a00dbbb0af29e
giadarol <giovanni.iadarola@cern.ch>
2024-04-13 14:10:30 +0200

    Trying to check the slicing

--- examples/transformations_embedded/012_check_traansformations_against_sandwitch.py
+++ examples/transformations_embedded/012_check_traansformations_against_sandwitch.py
@@ -1,8 +1,10 @@
 import xtrack as xt
 import numpy as np
 
+slice_mode = None
+
 ele_test = [
-    xt.Bend(k0=0.4, h=0.3, length=1,
+    xt.Bend(k0=0.04, h=0.03, length=1,
             k1=0.1,
             knl=[0.7, 0.8, 0.9, 1.0], ksl=[0.1, 0.2, 0.3, 0.4],
             edge_entry_angle=0.05, edge_exit_angle=0.06,
@@ -10,6 +12,10 @@ ele_test = [
             edge_entry_fint=0.08, edge_exit_fint=0.09,
             ),
     xt.Quadrupole(k1=2., k1s=-3., length=3.),
+    xt.Sextupole(k2=0.1, k2s=0.2, length=0.3),
+    xt.Octupole(k3=0.1, k3s=0.2, length=0.4),
+    xt.Multipole(knl=[0.7, 0.8, 0.9, 1.0], ksl=[0.1, 0.2, 0.3, 0.4],
+                        length=0.4, hxl=0.1)
 ]
 
 for elem in ele_test:
@@ -28,6 +34,10 @@ for elem in ele_test:
         xt.SRotation(angle=np.rad2deg(-rot_s_rad)),
         xt.XYShift(dx=-shift_x, dy=-shift_y),
     ])
+
+    if slice_mode is not None:
+        line_test.slice_thick_elements(
+            slicing_strategies=[xt.Strategy(xt.Teapot(3, mode=slice_mode))])
 
     line_test['e0'].rot_s_rad = rot_s_rad
     line_test['e0'].shift_x = shift_x
@@ -49,4 +59,3 @@ for elem in ele_test:
     assert_allclose(p_test.py, p_ref.py, rtol=0, atol=1e-13)
     assert_allclose(p_test.zeta, p_ref.zeta, rtol=0, atol=1e-13)
     assert_allclose(p_test.delta, p_ref.delta, rtol=0, atol=1e-13)
-
