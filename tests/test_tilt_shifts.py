8d2a1eea896296c691eedf8f5b2604894b28ff27
giadarol <giovanni.iadarola@cern.ch>
2024-04-14 22:45:58 +0200

    Add test

--- tests/test_tilt_shifts.py
+++ tests/test_tilt_shifts.py
@@ -226,7 +226,7 @@ def test_shift_x(test_context):
     line.build_tracker(_context=test_context)
 
     tt = line.get_table()
-    assert len(tt.rows['e0\.\..*']) == 3
+    assert len(tt.rows[r'e0\.\..*']) == 3
 
     p = xt.Particles(x=0, p0c=1e12, _context=test_context)
     line.track(p)
@@ -241,3 +241,54 @@ def test_shift_x(test_context):
     p.move(_context=xo.context_default)
 
     assert_allclose(p.px, -k1 * length * -3e-3, rtol=5e-3, atol=0)
+
+@for_all_test_contexts
+def test_shift_y(test_context):
+
+    k1 = 2.
+    length = 0.1
+
+    quad = xt.Quadrupole(k1=k1, length=length, shift_y=1e-3, _context=test_context)
+
+    assert quad.shift_x == 0
+    assert quad.shift_y == 1e-3
+    assert quad._sin_rot_s == 0.0
+    assert quad._cos_rot_s == 1.0
+
+    p = xt.Particles(y=0, p0c=1e12, _context=test_context)
+    quad.track(p)
+    p.move(_context=xo.context_default)
+
+    assert_allclose(p.py, k1 * length * -1e-3, rtol=5e-3, atol=0)
+
+    # Change the shift
+    quad.shift_y = 2e-3
+    p = xt.Particles(y=0, p0c=1e12, _context=test_context)
+    quad.track(p)
+    p.move(_context=xo.context_default)
+    assert_allclose(p.py, k1 * length * -2e-3, rtol=5e-3, atol=0)
+
+    # Make a line
+    line = xt.Line(elements=[quad])
+
+    # Slice the line:
+    line.slice_thick_elements(
+        slicing_strategies=[xt.Strategy(xt.Uniform(3))])
+    line.build_tracker(_context=test_context)
+
+    tt = line.get_table()
+    assert len(tt.rows[r'e0\.\..*']) == 3
+
+    p = xt.Particles(y=0, p0c=1e12, _context=test_context)
+    line.track(p)
+    p.move(_context=xo.context_default)
+
+    assert_allclose(p.py, k1 * length * -2e-3, rtol=5e-3, atol=0)
+
+    # Change the shift
+    quad.shift_y = 3e-3
+    p = xt.Particles(x=0, p0c=1e12, _context=test_context)
+    line.track(p)
+    p.move(_context=xo.context_default)
+
+    assert_allclose(p.py, k1 * length * -3e-3, rtol=5e-3, atol=0)
