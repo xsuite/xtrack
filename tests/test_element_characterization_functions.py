bd3e3bd5d9f745365fc5d2961749302b8f4f618c
giadarol <giovanni.iadarola@cern.ch>
2024-04-17 14:59:08 +0200

    Add test

--- tests/test_element_characterization_functions.py
+++ tests/test_element_characterization_functions.py
@@ -57,9 +57,6 @@ def test_has_backtrack(test_context):
     assert _allow_backtrack(line['a1'], line)
     assert _allow_backtrack(line['a2'], line)
 
-
-
-
 def test_is_drift_behaves_like_drift():
 
     bend = xt.Bend(k0=0.4, h=0.3, length=1)
@@ -166,3 +163,38 @@ def test_is_drift_behaves_like_drift():
             assert not _is_drift(ee, line)
             assert not _behaves_like_drift(ee, line)
 
+def test_replica_loops():
+
+    bend = xt.Bend(k0=0.4, h=0.3, length=1)
+
+    elements = {
+        # correct case
+        'a0': bend,
+        'a1': xt.Replica(parent_name='a0'),
+        'a2': xt.Replica(parent_name='a1'),
+        # create a simple loop
+        'b0': xt.Replica(parent_name='b0'),
+        # loop with two replicas
+        'c0': xt.Replica(parent_name='c1'),
+        'c1': xt.Replica(parent_name='c0'),
+        # bigger loop
+        'd0': xt.Replica(parent_name='d1'),
+        'd1': xt.Replica(parent_name='d2'),
+        'd2': xt.Replica(parent_name='d0'),
+        'd3': xt.Replica(parent_name='d1'),
+    }
+
+    line = xt.Line(elements=elements, element_names=list(elements.keys()))
+
+    ok = ['a1', 'a2']
+    for kk in ok:
+        assert line[kk].resolve(line) is line['a0']
+
+    error = ['b0', 'c0', 'c1', 'd0', 'd1', 'd2', 'd3']
+    for kk in error:
+        try:
+            line[kk].resolve(line)
+        except RecursionError:
+            pass
+        else:
+            raise Exception(f'Element {kk} should not be resolvable')
