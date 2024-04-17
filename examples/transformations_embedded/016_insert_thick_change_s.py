0530b71f2ceb6618ba6f829ae553a1466d7005b7
giadarol <giovanni.iadarola@cern.ch>
2024-04-17 09:52:55 +0200

    Check for insertion thick reuising the name

--- examples/transformations_embedded/016_insert_thick_change_s.py
+++ examples/transformations_embedded/016_insert_thick_change_s.py
@@ -1,4 +1,8 @@
 import xtrack as xt
+import xobjects as xo
+import numpy as np
+
+assert_allclose = xo.assert_allclose
 
 elements = {
     'd1': xt.Drift(length=1),
@@ -9,4 +13,11 @@ elements = {
 line=xt.Line(elements=elements,
              element_names=list(elements.keys()))
 
+# Note that the name is reused
 line.insert_element(element=xt.Bend(length=1.), name='m1', at_s=0.5)
+
+tt = line.get_table()
+
+assert np.all(tt.name == ['d1..0', 'm1', 'd2..1', '_end_point'])
+assert np.all(tt.parent_name == ['d1', None, 'd2', None])
+assert_allclose(tt.s, [0. , 0.5, 1.5, 2. ], rtol=0, atol=1e-14)
