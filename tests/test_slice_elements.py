9c75fcd3a3aad589b0aa1c789f10115eeac887fe
giadarol <giovanni.iadarola@cern.ch>
2024-04-17 13:52:41 +0200

    A test with multipolar components alone

--- tests/test_slice_elements.py
+++ tests/test_slice_elements.py
@@ -847,3 +847,107 @@ def test_thin_slice_bend_with_multipoles(test_context):
     assert_allclose(p_slice.py, p0.py, rtol=0, atol=1e-10)
     assert_allclose(p_slice.zeta, p0.zeta, rtol=0, atol=1e-10)
     assert_allclose(p_slice.delta, p0.delta, rtol=0, atol=1e-10)
+
+@for_all_test_contexts
+def test_thin_slice_bend_with_multipoles_bend_off(test_context):
+
+    bend = xt.Bend(k0=0, h=0, length=1,
+                   knl=[0, 0.001, 0.01], ksl=[0, 0.002, 0.03],
+                   num_multipole_kicks=10,
+                   edge_entry_angle=0.05, edge_entry_hgap=0.06, edge_entry_fint=0.08,
+                   edge_exit_angle=0.05, edge_exit_hgap=0.06, edge_exit_fint=0.08)
+
+    line = xt.Line(elements=[bend])
+
+    line.configure_bend_model(edge='linear', core='expanded')
+
+    line.slice_thick_elements(
+        slicing_strategies=[xt.Strategy(xt.Uniform(10))])
+    line.build_tracker(_context=test_context)
+    line._line_before_slicing.build_tracker(_context=test_context)
+    assert line['e0..5'].parent_name == 'e0'
+    assert line['e0..5']._parent is line['e0']
+    assert line['drift_e0..5'].parent_name == 'e0'
+    assert line['drift_e0..5']._parent is line['e0']
+    assert line['e0..entry_map'].parent_name == 'e0'
+    assert line['e0..entry_map']._parent is line['e0']
+    assert line['e0..exit_map'].parent_name == 'e0'
+    assert line['e0..exit_map']._parent is line['e0']
+
+    p0 = xt.Particles(p0c=10e9, x=0.1, px=0.2, y=0.3, py=0.4, delta=0.03
+                      ,_context=test_context)
+    p_ref = p0.copy()
+    p_slice = p0.copy()
+
+    line.track(p_slice)
+    line._line_before_slicing.track(p_ref)
+
+    assert_allclose(p_slice.x, p_ref.x, rtol=0, atol=1e-14)
+    assert_allclose(p_slice.px, p_ref.px, rtol=0, atol=1e-14)
+    assert_allclose(p_slice.y, p_ref.y, rtol=0, atol=1e-14)
+    assert_allclose(p_slice.py, p_ref.py, rtol=0, atol=1e-14)
+    assert_allclose(p_slice.zeta, p_ref.zeta, rtol=0, atol=1e-14)
+    assert_allclose(p_slice.delta, p_ref.delta, rtol=0, atol=1e-14)
+
+    line.to_json('ttt.json')
+    line2 = xt.Line.from_json('ttt.json')
+    assert isinstance(line2['e0..5'], xt.ThinSliceBend)
+    assert line2['e0..5'].parent_name == 'e0'
+    assert line2['e0..5']._parent is None
+    assert line2['drift_e0..5'].parent_name == 'e0'
+    assert line2['drift_e0..5']._parent is None
+    assert line2['e0..entry_map'].parent_name == 'e0'
+    assert line2['e0..entry_map']._parent is None
+    assert line2['e0..exit_map'].parent_name == 'e0'
+    assert line2['e0..exit_map']._parent is None
+
+    line2.build_tracker(_context=test_context)
+    assert isinstance(line2['e0..5'], xt.ThinSliceBend)
+    assert line2['e0..5'].parent_name == 'e0'
+    assert line2['e0..5']._parent is line2['e0']
+    assert isinstance(line2['drift_e0..5'], xt.DriftSliceBend)
+    assert line2['drift_e0..5'].parent_name == 'e0'
+    assert line2['drift_e0..5']._parent is line2['e0']
+    assert isinstance(line2['e0..entry_map'], xt.ThinSliceBendEntry)
+    assert line2['e0..entry_map'].parent_name == 'e0'
+    assert line2['e0..entry_map']._parent is line2['e0']
+    assert isinstance(line2['e0..exit_map'], xt.ThinSliceBendExit)
+    assert line2['e0..exit_map'].parent_name == 'e0'
+    assert line2['e0..exit_map']._parent is line2['e0']
+
+    line.track(p_slice, backtrack=True)
+
+    assert (p_slice.state == 1).all()
+    assert_allclose(p_slice.x, p0.x, rtol=0, atol=1e-14)
+    assert_allclose(p_slice.px, p0.px, rtol=0, atol=1e-14)
+    assert_allclose(p_slice.y, p0.y, rtol=0, atol=1e-14)
+    assert_allclose(p_slice.py, p0.py, rtol=0, atol=1e-14)
+    assert_allclose(p_slice.zeta, p0.zeta, rtol=0, atol=1e-14)
+    assert_allclose(p_slice.delta, p0.delta, rtol=0, atol=1e-14)
+
+    line.optimize_for_tracking()
+
+    assert isinstance(line['e0..5'], xt.Multipole)
+    assert isinstance(line['drift_e0..5'], xt.Drift)
+
+    assert isinstance(line['e0..entry_map'], xt.DipoleEdge)
+    assert isinstance(line['e0..exit_map'], xt.DipoleEdge)
+
+    p_slice = p0.copy()
+    line.track(p_slice)
+
+    assert_allclose(p_slice.x, p_ref.x, rtol=0, atol=1e-14)
+    assert_allclose(p_slice.px, p_ref.px, rtol=0, atol=1e-14)
+    assert_allclose(p_slice.y, p_ref.y, rtol=0, atol=1e-14)
+    assert_allclose(p_slice.py, p_ref.py, rtol=0, atol=1e-14)
+    assert_allclose(p_slice.zeta, p_ref.zeta, rtol=0, atol=1e-14)
+    assert_allclose(p_slice.delta, p_ref.delta, rtol=0, atol=1e-14)
+
+    line.track(p_slice, backtrack=True)
+
+    assert_allclose(p_slice.x, p0.x, rtol=0, atol=1e-14)
+    assert_allclose(p_slice.px, p0.px, rtol=0, atol=1e-14)
+    assert_allclose(p_slice.y, p0.y, rtol=0, atol=1e-14)
+    assert_allclose(p_slice.py, p0.py, rtol=0, atol=1e-14)
+    assert_allclose(p_slice.zeta, p0.zeta, rtol=0, atol=1e-14)
+    assert_allclose(p_slice.delta, p0.delta, rtol=0, atol=1e-14)
