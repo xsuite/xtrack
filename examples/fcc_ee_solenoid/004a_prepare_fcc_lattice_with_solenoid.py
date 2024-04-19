c67739b556c1be356daab65178d7057f99210b32
giadarol <giovanni.iadarola@cern.ch>
2024-04-19 06:44:14 +0200

    Clean up examples

--- examples/fcc_ee_solenoid/004a_prepare_fcc_lattice_with_solenoid.py
+++ examples/fcc_ee_solenoid/004a_prepare_fcc_lattice_with_solenoid.py
@@ -7,7 +7,7 @@ from scipy.constants import e as qe
 from cpymad.madx import Madx
 
 fname = 'fccee_z'; pc_gev = 45.6
-# fname = 'fccee_t'; pc_gev = 182.5
+fname = 'fccee_t'; pc_gev = 182.5
 
 mad = Madx()
 mad.call('../../test_data/fcc_ee/' + fname + '.seq')
