7f2c7db56977ec8fff50f39fdb58c26273e4ec6f
giadarol <giovanni.iadarola@cern.ch>
2024-04-23 14:16:51 +0200

    Also vertical

--- examples/fcc_ee_solenoid/t000b_simplified_illustration.py
+++ examples/fcc_ee_solenoid/t000b_simplified_illustration.py
@@ -58,13 +58,13 @@ dz = z_axis[1] - z_axis[0]
 
 plt.figure(100, figsize=(6.4, 4.8*1.6))
 
-sp1 = plt.subplot(3, 1, 1)
+sp1 = plt.subplot(4, 1, 1)
 plt.bar(z_axis - dz/2 - z_sol_center, Bz_axis, width=z_axis[1]-z_axis[0],
         alpha=0.5, align='edge', linewidth=1, edgecolor='C0')
 plt.plot(z_fine- z_sol_center, Bz_fine)
 plt.ylabel(r'$B_{z}$ [T]')
 
-sp2 = plt.subplot(3, 1, 2, sharex=sp1)
+sp2 = plt.subplot(4, 1, 2, sharex=sp1)
 plt.plot(mon.s[i_part_plot, :] - z_sol_center, 1e3 * mon.x[i_part_plot, :])
 plt.plot(mon.s[i_part_plot, :] - z_sol_center, 1e3 * mon.y[i_part_plot, :])
 plt.axhline(0, color='grey', alpha=0.6, linestyle=':')
@@ -77,19 +77,19 @@ kin_px = mon.kin_px[i_part_plot, :]
 py = mon.py[i_part_plot, :]
 kin_py = mon.kin_py[i_part_plot, :]
 
-sp3 = plt.subplot(3, 1, 3, sharex=sp1)
+sp3 = plt.subplot(4, 1, 3, sharex=sp1)
 plt.plot(mon.s[i_part_plot, :] - z_sol_center, 1e6 * (px - px[0]), label=r'Canonical')
 plt.plot(mon.s[i_part_plot, :] - z_sol_center, 1e6 * (kin_px - kin_px[0]), label=r"Kinetic")
-plt.plot(mon.s[i_part_plot, :] - z_sol_center, 1e6 * mon.ax[i_part_plot, :], label=r'$a_y$')
+plt.plot(mon.s[i_part_plot, :] - z_sol_center, 1e6 * mon.ax[i_part_plot, :], label=r'$a_x$')
 plt.legend(fontsize='medium')
 plt.ylabel(r"$\Delta p_x$ [$10^{-6}$]")
 
-# sp4 = plt.subplot(5, 1, 4, sharex=sp1)
-# plt.plot(mon.s[i_part_plot, :] - z_sol_center, 1e6 * (py - py[0]), label=r'Canonical')
-# plt.plot(mon.s[i_part_plot, :] - z_sol_center, 1e6 * (kin_py - kin_py[0]), label=r"Kinetic")
-# plt.plot(mon.s[i_part_plot, :] - z_sol_center, 1e6 * mon.ay[i_part_plot, :], label='ay')
-# plt.legend(fontsize='medium')
-# plt.ylabel(r"$\Delta p_y$ [$10^{-6}$]")
+sp4 = plt.subplot(4, 1, 4, sharex=sp1)
+plt.plot(mon.s[i_part_plot, :] - z_sol_center, 1e6 * (py - py[0]), label=r'Canonical')
+plt.plot(mon.s[i_part_plot, :] - z_sol_center, 1e6 * (kin_py - kin_py[0]), label=r"Kinetic")
+plt.plot(mon.s[i_part_plot, :] - z_sol_center, 1e6 * mon.ay[i_part_plot, :], label=r'$a_y$')
+plt.legend(fontsize='medium')
+plt.ylabel(r"$\Delta p_y$ [$10^{-6}$]")
 
 # sp5 = plt.subplot(5, 1, 5, sharex=sp1)
 # plt.plot(mon.s[i_part_plot, :] - z_sol_center, mon.ay[i_part_plot, :])
