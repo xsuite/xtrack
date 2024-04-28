9f5b4466aa5f19265195d34d01b76a57bfac88f7
giadarol <giovanni.iadarola@cern.ch>
2024-04-28 22:23:17 +0200

    Thick quad slice with multipoles

--- xtrack/beam_elements/elements_src/thick_slice_quadrupole.h
+++ xtrack/beam_elements/elements_src/thick_slice_quadrupole.h
@@ -15,14 +15,29 @@ void ThickSliceQuadrupole_track_local_particle(
     double weight = ThickSliceQuadrupoleData_get_weight(el);
     const double k1 = ThickSliceQuadrupoleData_get__parent_k1(el);
     const double k1s = ThickSliceQuadrupoleData_get__parent_k1s(el);
+    const double order = ThickSliceQuadrupoleData_get__parent_order(el);
+    const double inv_factorial_order = ThickSliceQuadrupoleData_get__parent_inv_factorial_order(el);
+    const double* knl = ThickSliceQuadrupoleData_getp1__parent_knl(el, 0);
+    const double* ksl = ThickSliceQuadrupoleData_getp1__parent_ksl(el, 0);
+    const int64_t num_multipole_kicks_parent = ThickSliceQuadrupoleData_get__parent_num_multipole_kicks(el);
 
     #ifndef XSUITE_BACKTRACK
         double const length = weight * ThickSliceQuadrupoleData_get__parent_length(el); // m
+        double const factor_knl_ksl = weight;
     #else
         double const length = -weight * ThickSliceQuadrupoleData_get__parent_length(el); // m
+        double const factor_knl_ksl = -weight;
     #endif
 
-    Quadrupole_from_params_track_local_particle(length, k1, k1s, part0);
+    int64_t const num_multipole_kicks = (int64_t) ceil(num_multipole_kicks_parent * weight);
+
+    Quadrupole_from_params_track_local_particle(
+        length, k1, k1s,
+        num_multipole_kicks,
+        knl, ksl,
+        order, inv_factorial_order,
+        factor_knl_ksl,
+        part0);
 
 }
 
