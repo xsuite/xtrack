dd8b539c8853e413a26e28945a3d2f4b11cbe5a0
giadarol <giovanni.iadarola@cern.ch>
2024-04-28 09:10:05 +0200

    refactored quad

--- xtrack/beam_elements/elements_src/track_quadrupole.h
+++ xtrack/beam_elements/elements_src/track_quadrupole.h
@@ -7,22 +7,12 @@
 #define XTRACK_TRACKQUADRUPOLE_H
 
 /*gpufun*/
-void Quadrupole_from_params_track_local_particle(
-        double length, double k1, double k1s,
-        LocalParticle* part0
+void normal_quad_with_rotation_track(
+        LocalParticle* part0,
+        double const length, double const k_rotated,
+        const int needs_rotation,
+        double const sin_rot, double const cos_rot
 ) {
-
-    double sin_rot=0;
-    double cos_rot=0;
-    double k_rotated = k1;
-
-    const int needs_rotation = k1s != 0.0;
-    if (needs_rotation) {
-        double angle_rot = -atan2(k1s, k1) / 2.;
-        sin_rot = sin(angle_rot);
-        cos_rot = cos(angle_rot);
-        k_rotated = sqrt(k1*k1 + k1s*k1s);
-    }
 
     if (needs_rotation) {
         //start_per_particle_block (part0->part)
@@ -39,6 +29,38 @@ void Quadrupole_from_params_track_local_particle(
             SRotation_single_particle(part, -sin_rot, cos_rot);
         //end_per_particle_block
     }
+}
+
+/*gpufun*/
+void Quadrupole_from_params_track_local_particle(
+        double length, double k1, double k1s,
+        // int64_t num_multipole_kicks,
+        // double const* knl, double const* ksl,
+        // int64_t order, double inv_factorial_order,
+        // double factor_knl_ksl,
+        LocalParticle* part0
+) {
+
+    double sin_rot=0;
+    double cos_rot=0;
+    double k_rotated = k1;
+
+    // if (num_multipole_kicks == 0) { // auto mode
+    //     num_multipole_kicks = 1;
+    // }
+
+    const int needs_rotation = k1s != 0.0;
+    if (needs_rotation) {
+        double angle_rot = -atan2(k1s, k1) / 2.;
+        sin_rot = sin(angle_rot);
+        cos_rot = cos(angle_rot);
+        k_rotated = sqrt(k1*k1 + k1s*k1s);
+    }
+
+    normal_quad_with_rotation_track(part0, length, k_rotated, needs_rotation,
+                                    sin_rot, cos_rot);
+
+
 }
 
 #endif // XTRACK_QUADRUPOLE_H
