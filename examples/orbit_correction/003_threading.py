dad4948ffe61cc3d8cd81cd323d737eeb9d04255
giadarol <giovanni.iadarola@cern.ch>
2024-05-04 15:37:15 +0200

    Getting ready for vertical

--- examples/orbit_correction/003_threading.py
+++ examples/orbit_correction/003_threading.py
@@ -25,18 +25,22 @@ h_monitor_names = tt_monitors.name
 tt_h_correctors = tt.rows['mcb.*'].rows['.*h\..*'].rows['.*\.b1']
 h_corrector_names = tt_h_correctors.name
 
-orbit_correction = oc.OrbitCorrection(line=line, plane='x', monitor_names=h_monitor_names,
+orbit_correction_h = oc.OrbitCorrection(line=line, plane='x', monitor_names=h_monitor_names,
                                         corrector_names=h_corrector_names,
                                         start=line_range[0], end=line_range[1])
-orbit_correction._add_correction_knobs()
 
 # Introduce some orbit perturbation
 
 h_kicks = {'mcbh.14r2.b1': 1e-5, 'mcbh.26l3.b1':-3e-5}
+# v_kicks = {'mcbv.15r2.b1': 2e-5, 'mcbv.25l3.b1':-2e-5}
 
 for nn_kick, kick in h_kicks.items():
     line.element_refs[nn_kick].knl[0] -= kick
     i_h_kick = np.where(h_corrector_names == nn_kick)[0][0]
+
+# for nn_kick, kick in v_kicks.items():
+#     line.element_refs[nn_kick].knl[1] += kick
+#     i_v_kick = np.where(h_corrector_names == nn_kick)[0][0]
 
 # tt = line.get_table()
 # tt_quad = tt.rows[tt.element_type == 'Quadrupole']
@@ -55,7 +59,7 @@ n_micado = None
 
 for iter in range(3):
     # Measure the orbit
-    orbit_correction.correct()
+    orbit_correction_h.correct()
 
     tw_after = line.twiss4d(only_orbit=True, start=line_range[0], end=line_range[1],
                             betx=betx_start_guess,
@@ -67,7 +71,7 @@ x_meas_after = tw_after.rows[h_monitor_names].x
 s_correctors = tw_after.rows[h_corrector_names].s
 
 # Extract kicks from the knobs
-applied_kicks = orbit_correction.get_kick_values()
+applied_kicks = orbit_correction_h.get_kick_values()
 
 import matplotlib.pyplot as plt
 plt.close('all')
