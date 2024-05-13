b7519bc73f508411cccbe502837ddfa7a0dd7661
giadarol <giovanni.iadarola@cern.ch>
2024-05-13 06:29:49 +0200

    Prepare test

--- examples/orbit_and_tracjectory_correction/t001_closed_orbit_correction_micado.py
+++ examples/orbit_and_tracjectory_correction/t001_closed_orbit_correction_micado.py
@@ -1,4 +1,5 @@
 import xtrack as xt
+import xobjects as xo
 import numpy as np
 
 line = xt.Line.from_json(
@@ -50,38 +51,10 @@ s_y_correctors = orbit_correction.y_correction.s_correctors
 kicks_x = orbit_correction.x_correction.get_kick_values()
 kicks_y = orbit_correction.y_correction.get_kick_values()
 
-#!end-doc-part
+assert tw_before.x.std() > 1e-3
+assert tw_before.y.std() > 1e-3
+assert tw_after.y.std() < 2e-4
+assert tw_after.x.std() < 2e-4
 
-# Plots
-import matplotlib.pyplot as plt
-plt.close('all')
-
-plt.figure(1, figsize=(6.4, 4.8*1.7))
-sp1 = plt.subplot(411)
-sp1.plot(tw_before.s, tw_before.x * 1e3,
-        label=f'before corr. (rms: {tw_before.x.std() * 1e3:.2e} mm)')
-sp1.plot(tw_after.s, tw_after.x * 1e3,
-        label=f'after corr. (rms: {tw_after.x.std() * 1e3:.2e} mm)')
-plt.legend(loc='lower right')
-plt.ylabel('x [mm]')
-
-sp2 = plt.subplot(412, sharex=sp1)
-sp2.stem(s_x_correctors, kicks_x * 1e6)
-plt.ylabel(r'x kick [$\mu$rad]')
-
-sp3 = plt.subplot(413, sharex=sp1)
-sp3.plot(tw_before.s, tw_before.y * 1e3,
-        label=f'before corr. (rms: {tw_before.y.std() * 1e3:.2e} mm)')
-sp3.plot(tw_after.s, tw_after.y * 1e3,
-        label=f'after corr. (rms: {tw_after.y.std() * 1e3:.2e} mm)')
-plt.ylabel('y [mm]')
-plt.legend(loc='lower right')
-
-sp4 = plt.subplot(414, sharex=sp1)
-sp4.stem(s_y_correctors, kicks_y * 1e6)
-plt.ylabel(r'y kick [$\mu$rad]')
-sp4.set_xlabel('s [m]')
-
-plt.subplots_adjust(hspace=0.3, top=0.95, bottom=0.08)
-
-plt.show()
+assert np.sum(np.abs(kicks_x)>0) == 5
+assert np.sum(np.abs(kicks_y)>0) == 5
