f0c7bfee7f6bdc2947c4a4ac8f30523fa7fc606b
giadarol <giovanni.iadarola@cern.ch>
2024-08-22 01:00:36 +0200

    plot betabeat from machine

--- examples/optics_correction/001_md.py
+++ examples/optics_correction/001_md.py
@@ -12,15 +12,22 @@ tt = line.get_table()
 
 observable_list = ['betx', 'bety', 'mux', 'muy', 'dx']
 
-obs_points = tt.rows['bpm.*'].name
+# obs_points = tt.rows['bpm.*'].name
+import tfs
+meas_betx = tfs.read_tfs('mddata/beta_phase_x.tfs')
+obs_points_import = list(map(lambda ss: ss.lower(), list(meas_betx['NAME'].values)))
+# Resort
+obs_points = tw0.rows[obs_points_import].name
 
-# corr_elements = []
-# corr_vars = [nn for nn in line.vars.get_table().rows['kq.*.b1$'].name
-#               if 'from' not in nn]
+corr_elements = []
+corr_vars = [nn for nn in line.vars.get_table().rows['kq.*.b1$'].name
+              if 'from' not in nn]
 
-corr_vars = []
-corr_elements = [nn for nn in list(tt.rows[tt.element_type == 'Quadrupole'].name)
-                 if 'mqm' in nn]
+corr_vars = corr_vars[:10] # DEBUUUUUUUG
+
+# corr_vars = []
+# corr_elements = [nn for nn in list(tt.rows[tt.element_type == 'Quadrupole'].name)
+#                  if 'mqm' in nn]
 
 correctors = []
 for nn in corr_vars:
@@ -53,17 +60,16 @@ for ii, cc in enumerate(correctors):
             twp.rows[obs_points][observable] - tw0.rows[obs_points][observable]) / dk
 
 
-line.vars['kq7.r5b1'] *= 1.01
-tw = line.twiss()
+# line.vars['kq7.r5b1'] *= 1.01
+# tw = line.twiss()
+tw_meas = xt.Table({'name': np.array(obs_points_import), 'betx': meas_betx['BETX']._values})
 
-tw['betx0'] = tw0['betx']
-tw['bety0'] = tw0['bety']
-tw['mux0'] = tw0['mux']
-tw['muy0'] = tw0['muy']
+tw = xt.Table({'name': np.array(obs_points),
+               'betx': np.array([tw_meas['betx', nn] for nn in obs_points])})
 
 from xtrack.trajectory_correction import _compute_correction
 
-correct_on_observables = ['mux', 'muy']
+correct_on_observables = ['betx']
 
 err = None
 response_matrix = None
@@ -89,6 +95,9 @@ plt.close('all')
 plt.figure(1)
 plt.plot(correction_svd, '.', label='SVD')
 plt.plot(correction_micado, '.', label='MICADO')
+
+plt.figure(2)
+plt.plot(tw0.rows[obs_points].s, tw.betx / tw0.rows[obs_points].betx -1)
 
 plt.legend()
 plt.show()
