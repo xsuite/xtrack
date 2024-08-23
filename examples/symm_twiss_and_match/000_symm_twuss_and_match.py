80607bf0a96ac6a55c1eddf168b42b5349afff5b
giadarol <giovanni.iadarola@cern.ch>
2024-08-23 23:10:30 +0200

    Get R from half

--- examples/symm_twiss_and_match/000_symm_twuss_and_match.py
+++ examples/symm_twiss_and_match/000_symm_twuss_and_match.py
@@ -1,4 +1,5 @@
 import xtrack as xt
+import numpy as np
 
 elements={
     'drift0': xt.Drift(length=0.1),
@@ -16,6 +17,20 @@ elements={
 line = xt.Line(elements=elements, element_names=list(elements.keys()))
 line.particle_ref = xt.Particles(p0c=2e9)
 tw = line.twiss4d(strengths=True)
+
+line_half = xt.Line(elements=elements,
+                    element_names=['drift0', 'qf1', 'drift1', 'qd1', 'drift2'])
+line_half.particle_ref = line.particle_ref.copy()
+line_half.build_tracker()
+
+particle_on_co = line_half.build_particles(x=0, px=0, y=0, py=0, delta=0, zeta=0)
+RR = line_half.compute_one_turn_matrix_finite_differences(
+    particle_on_co=particle_on_co)['R_matrix']
+
+
+inv_momenta = np.diag([1, -1, 1, -1, 1, -1])
+
+RR_symm = inv_momenta @ np.linalg.inv(RR) @ inv_momenta @ RR
 
 import matplotlib.pyplot as plt
 plt.close('all')
