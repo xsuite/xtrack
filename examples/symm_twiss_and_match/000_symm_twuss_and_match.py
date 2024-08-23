65a268aea218a0671c8ae061e30f0c587e2cfb09
giadarol <giovanni.iadarola@cern.ch>
2024-08-24 00:13:28 +0200

    Dispersion seems broken

--- examples/symm_twiss_and_match/000_symm_twuss_and_match.py
+++ examples/symm_twiss_and_match/000_symm_twuss_and_match.py
@@ -3,14 +3,18 @@ import numpy as np
 
 elements={
     'drift0': xt.Drift(length=0.1),
-    'qf1': xt.Quadrupole(k1=0.01, length=0.1),
-    'drift1': xt.Drift(length=0.6),
-    'qd1': xt.Quadrupole(k1=-0.01, length=0.1),
+    'qf1': xt.Quadrupole(k1=1., length=0.05),
+    'drift1_1': xt.Drift(length=0.1),
+    'bend1': xt.Bend(k0=0.1, h=0.1, length=2.5),
+    'drift1_2': xt.Drift(length=0.1),
+    'qd1': xt.Quadrupole(k1=-1., length=0.05),
     'drift2': xt.Drift(length=0.1),
     'drift3': xt.Drift(length=0.1),
-    'qd2': xt.Quadrupole(k1=-0.01, length=0.1),
-    'drift4': xt.Drift(length=0.6),
-    'qf2': xt.Quadrupole(k1=0.01, length=0.1),
+    'qd2': xt.Quadrupole(k1=-1.0, length=0.05),
+    'drift4_1': xt.Drift(length=0.1),
+    'bend2': xt.Bend(k0=0.1, h=0.1, length=2.5),
+    'drift4_2': xt.Drift(length=0.1),
+    'qf2': xt.Quadrupole(k1=1.0, length=0.05),
     'drift5': xt.Drift(length=0.1),
 }
 
@@ -19,7 +23,8 @@ line.particle_ref = xt.Particles(p0c=2e9)
 tw = line.twiss4d(strengths=True)
 
 line_half = xt.Line(elements=elements,
-                    element_names=['drift0', 'qf1', 'drift1', 'qd1', 'drift2'])
+                    element_names=['drift0', 'qf1', 'drift1_1', 'bend1',
+                                   'drift1_2', 'qd1', 'drift2'])
 line_half.particle_ref = line.particle_ref.copy()
 tw_half = line_half.twiss4d(strengths=True, init='periodic_symmetric')
 
