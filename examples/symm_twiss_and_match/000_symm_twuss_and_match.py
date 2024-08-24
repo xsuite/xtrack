da76ed438081b150dfb7bdb2dfe6651834801d56
giadarol <giovanni.iadarola@cern.ch>
2024-08-24 22:24:27 +0200

    Separate definition of the two lines

--- examples/symm_twiss_and_match/000_symm_twuss_and_match.py
+++ examples/symm_twiss_and_match/000_symm_twuss_and_match.py
@@ -1,32 +1,40 @@
 import xtrack as xt
 import numpy as np
 
-elements={
-    'drift0': xt.Drift(length=1.),
-    'qf1': xt.Quadrupole(k1=0.027/2, length=1.),
-    'drift1_1': xt.Drift(length=1),
-    'bend1': xt.Bend(k0=3e-4, h=3e-4, length=45.),
-    'drift1_2': xt.Drift(length=1.),
-    'qd1': xt.Quadrupole(k1=-0.0271/2, length=1.),
-    'drift2': xt.Drift(length=1),
-    'drift3': xt.Drift(length=1),
-    'qd2': xt.Quadrupole(k1=-0.0271/2, length=1.),
-    'drift4_1': xt.Drift(length=1),
-    'bend2': xt.Bend(k0=3e-4, h=3e-4, length=45.),
-    'drift4_2': xt.Drift(length=1),
-    'qf2': xt.Quadrupole(k1=0.027/2, length=1.),
-    'drift5': xt.Drift(length=0.1),
-}
+cell = xt.Line(
+    elements={
+        'drift0': xt.Drift(length=1.),
+        'qf1': xt.Quadrupole(k1=0.027/2, length=1.),
+        'drift1_1': xt.Drift(length=1),
+        'bend1': xt.Bend(k0=3e-4, h=3e-4, length=45.),
+        'drift1_2': xt.Drift(length=1.),
+        'qd1': xt.Quadrupole(k1=-0.0271/2, length=1.),
+        'drift2': xt.Drift(length=1),
+        'drift3': xt.Drift(length=1),
+        'qd2': xt.Quadrupole(k1=-0.0271/2, length=1.),
+        'drift4_1': xt.Drift(length=1),
+        'bend2': xt.Bend(k0=3e-4, h=3e-4, length=45.),
+        'drift4_2': xt.Drift(length=1),
+        'qf2': xt.Quadrupole(k1=0.027/2, length=1.),
+        'drift5': xt.Drift(length=0.1),
+    }
+)
+cell.particle_ref = xt.Particles(p0c=2e9)
+tw = cell.twiss4d(strengths=True)
 
-line = xt.Line(elements=elements, element_names=list(elements.keys()))
-line.particle_ref = xt.Particles(p0c=2e9)
-tw = line.twiss4d(strengths=True)
-
-line_half = xt.Line(elements=elements,
-                    element_names=['drift0', 'qf1', 'drift1_1', 'bend1',
-                                   'drift1_2', 'qd1', 'drift2'])
-line_half.particle_ref = line.particle_ref.copy()
-tw_half = line_half.twiss4d(strengths=True, init='periodic_symmetric')
+half_cell = xt.Line(
+    elements={
+        'drift0': xt.Drift(length=1.),
+        'qf1': xt.Quadrupole(k1=0.027/2, length=1.),
+        'drift1_1': xt.Drift(length=1),
+        'bend1': xt.Bend(k0=3e-4, h=3e-4, length=45.),
+        'drift1_2': xt.Drift(length=1.),
+        'qd1': xt.Quadrupole(k1=-0.0271/2, length=1.),
+        'drift2': xt.Drift(length=1),
+    }
+)
+half_cell.particle_ref = cell.particle_ref.copy()
+tw_half = half_cell.twiss4d(strengths=True, init='periodic_symmetric')
 
 import matplotlib.pyplot as plt
 plt.close('all')
