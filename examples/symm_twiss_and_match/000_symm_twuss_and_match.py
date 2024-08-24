5903e11faf2f7d6f2fb081649a53568cf20bb8db
giadarol <giovanni.iadarola@cern.ch>
2024-08-24 22:27:40 +0200

    Use replicas

--- examples/symm_twiss_and_match/000_symm_twuss_and_match.py
+++ examples/symm_twiss_and_match/000_symm_twuss_and_match.py
@@ -4,19 +4,19 @@ import numpy as np
 cell = xt.Line(
     elements={
         'drift0': xt.Drift(length=1.),
-        'qf1': xt.Quadrupole(k1=0.027/2, length=1.),
-        'drift1_1': xt.Drift(length=1),
-        'bend1': xt.Bend(k0=3e-4, h=3e-4, length=45.),
-        'drift1_2': xt.Drift(length=1.),
-        'qd1': xt.Quadrupole(k1=-0.0271/2, length=1.),
-        'drift2': xt.Drift(length=1),
+        'qf1':    xt.Quadrupole(k1=0.027/2, length=1.),
+        'drift1': xt.Drift(length=1),
+        'bend1':  xt.Bend(k0=3e-4, h=3e-4, length=45.),
+        'drift2': xt.Drift(length=1.),
+        'qd1':    xt.Quadrupole(k1=-0.0271/2, length=1.),
         'drift3': xt.Drift(length=1),
-        'qd2': xt.Quadrupole(k1=-0.0271/2, length=1.),
-        'drift4_1': xt.Drift(length=1),
-        'bend2': xt.Bend(k0=3e-4, h=3e-4, length=45.),
-        'drift4_2': xt.Drift(length=1),
-        'qf2': xt.Quadrupole(k1=0.027/2, length=1.),
-        'drift5': xt.Drift(length=0.1),
+        'drift4': xt.Replica('drift3'),
+        'qd2':    xt.Replica('qd1'),
+        'drift5': xt.Replica('drift2'),
+        'bend2':  xt.Replica('bend1'),
+        'drift6': xt.Replica('drift1'),
+        'qf2':    xt.Replica('qf1'),
+        'drift7': xt.Replica('drift0'),
     }
 )
 cell.particle_ref = xt.Particles(p0c=2e9)
