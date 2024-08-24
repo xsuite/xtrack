0e445d97073953ec578593a2471f373b242e79fe
giadarol <giovanni.iadarola@cern.ch>
2024-08-24 21:40:32 +0200

    A nice cell

--- examples/symm_twiss_and_match/000_symm_twuss_and_match.py
+++ examples/symm_twiss_and_match/000_symm_twuss_and_match.py
@@ -2,19 +2,19 @@ import xtrack as xt
 import numpy as np
 
 elements={
-    'drift0': xt.Drift(length=0.1),
-    'qf1': xt.Quadrupole(k1=1.1, length=0.05),
-    'drift1_1': xt.Drift(length=0.1),
-    'bend1': xt.Bend(k0=0.001, h=0.001, length=5.5),
-    'drift1_2': xt.Drift(length=0.1),
-    'qd1': xt.Quadrupole(k1=-1., length=0.05),
-    'drift2': xt.Drift(length=0.1),
-    'drift3': xt.Drift(length=0.1),
-    'qd2': xt.Quadrupole(k1=-1.0, length=0.05),
-    'drift4_1': xt.Drift(length=0.1),
-    'bend2': xt.Bend(k0=0.001, h=0.001, length=5.5),
-    'drift4_2': xt.Drift(length=0.1),
-    'qf2': xt.Quadrupole(k1=1.1, length=0.05),
+    'drift0': xt.Drift(length=1.),
+    'qf1': xt.Quadrupole(k1=0.027 / 2, length=1.),
+    'drift1_1': xt.Drift(length=1),
+    'bend1': xt.Bend(k0=3e-4, h=3e-4, length=45./2),
+    'drift1_2': xt.Drift(length=1.),
+    'qd1': xt.Quadrupole(k1=-0.0271 / 2, length=1.),
+    'drift2': xt.Drift(length=1),
+    'drift3': xt.Drift(length=1),
+    'qd2': xt.Quadrupole(k1=-0.0271 / 2, length=1.),
+    'drift4_1': xt.Drift(length=1),
+    'bend2': xt.Bend(k0=3e-4, h=3e-4, length=45./2),
+    'drift4_2': xt.Drift(length=1),
+    'qf2': xt.Quadrupole(k1=0.027 / 2, length=1.),
     'drift5': xt.Drift(length=0.1),
 }
 
@@ -32,6 +32,7 @@ import matplotlib.pyplot as plt
 plt.close('all')
 
 twplt1 = tw.plot()
+twplt1.ylim(left_lo=0, right_lo=0.5, right_hi=4)
 # twplt2 = tw_half.plot()
 
 plt.show()
