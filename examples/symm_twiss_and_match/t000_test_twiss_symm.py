00a584fe7a0622b28d55159d35cf1b9b16645b31
giadarol <giovanni.iadarola@cern.ch>
2024-08-25 22:29:05 +0200

    More checks

--- examples/symm_twiss_and_match/t000_test_twiss_symm.py
+++ examples/symm_twiss_and_match/t000_test_twiss_symm.py
@@ -102,3 +102,50 @@ xo.assert_allclose(tw_half_cell.mux[-1], 0.2501 / 2, atol=1e-3, rtol=0)
 xo.assert_allclose(tw_half_cell.muy[-1], 0.2502 / 2, atol=1e-3, rtol=0)
 xo.assert_allclose(tw_cell.mux[-1], 0.231, atol=1e-3, rtol=0) # unaffected
 xo.assert_allclose(tw_cell.muy[-1], 0.233, atol=1e-3, rtol=0) # unaffected
+
+opt_cell = cell.match(
+    method='4d',
+    start='start_cell', end='end_cell',
+    init='periodic',
+    targets=xt.TargetSet(mux=0.2501, muy=0.2502, at='end_cell'),
+    vary=xt.VaryList(['kqf', 'kqd'], step=1e-5),
+)
+
+xo.assert_allclose(half_cell.vv['kqf'], cell.vv['kqf'], rtol=1e-9, atol=0)
+xo.assert_allclose(half_cell.vv['kqd'], cell.vv['kqd'], rtol=1e-9, atol=0)
+
+tw_cell = cell.twiss4d(strengths=True)
+tw_half_cell = half_cell.twiss4d(init='periodic_symmetric', strengths=True)
+
+xo.assert_allclose(tw_half_cell.mux[-1], 0.2501 / 2, atol=1e-3, rtol=0)
+xo.assert_allclose(tw_half_cell.muy[-1], 0.2502 / 2, atol=1e-3, rtol=0)
+xo.assert_allclose(tw_cell.mux[-1], 0.2501, atol=1e-3, rtol=0)
+xo.assert_allclose(tw_cell.muy[-1], 0.2502, atol=1e-3, rtol=0)
+
+xo.assert_allclose(tw_half_cell.betx[:-1], # remove '_end_point'
+                   tw_cell.rows[:'mid_cell'].betx, atol=0, rtol=1e-8)
+xo.assert_allclose(tw_half_cell.bety[:-1], # remove '_end_point'
+                   tw_cell.rows[:'mid_cell'].bety, atol=0, rtol=1e-8)
+xo.assert_allclose(tw_half_cell.alfx[:-1], # remove '_end_point'
+                   tw_cell.rows[:'mid_cell'].alfx, atol=1e-8, rtol=0)
+xo.assert_allclose(tw_half_cell.alfy[:-1], # remove '_end_point'
+                   tw_cell.rows[:'mid_cell'].alfy, atol=1e-8, rtol=0)
+xo.assert_allclose(tw_half_cell.dx[:-1], # remove '_end_point'
+                   tw_cell.rows[:'mid_cell'].dx, atol=1e-8, rtol=0)
+xo.assert_allclose(tw_half_cell.dpx[:-1], # remove '_end_point'
+                     tw_cell.rows[:'mid_cell'].dpx, atol=1e-8, rtol=0)
+
+xo.assert_allclose(tw_half_cell.ax_chrom[:-1], # remove '_end_point'
+                    tw_cell.rows[:'mid_cell'].ax_chrom, atol=1e-5, rtol=0)
+xo.assert_allclose(tw_half_cell.ay_chrom[:-1], # remove '_end_point'
+                    tw_cell.rows[:'mid_cell'].ay_chrom, atol=1e-5, rtol=0)
+xo.assert_allclose(tw_half_cell.bx_chrom[:-1], # remove '_end_point'
+                    tw_cell.rows[:'mid_cell'].bx_chrom, atol=1e-5, rtol=0)
+xo.assert_allclose(tw_half_cell.by_chrom[:-1], # remove '_end_point'
+                    tw_cell.rows[:'mid_cell'].by_chrom, atol=1e-5, rtol=0)
+
+xo.assert_allclose(tw_half_cell.qx, tw_cell.qx / 2, atol=1e-9, rtol=0)
+xo.assert_allclose(tw_half_cell.qy, tw_cell.qy / 2, atol=1e-9, rtol=0)
+xo.assert_allclose(tw_half_cell.dqx, tw_cell.dqx / 2, atol=1e-6, rtol=0)
+xo.assert_allclose(tw_half_cell.dqy, tw_cell.dqy / 2, atol=1e-6, rtol=0)
+
