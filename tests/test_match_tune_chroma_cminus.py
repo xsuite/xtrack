b5b4a37f9ef04b2c5264ab7c4c5eec8881502af0
Bernardo Abreu Figueiredo <bernardo.abreufig@outlook.com>
2024-09-11 11:23:06 +0200

    added homotopy test (without testing subtargets)

--- tests/test_match_tune_chroma_cminus.py
+++ tests/test_match_tune_chroma_cminus.py
@@ -6,6 +6,7 @@ import xobjects as xo
 import xpart as xp
 import xtrack as xt
 from xobjects.test_helpers import for_all_test_contexts
+import numpy as np
 
 test_data_folder = pathlib.Path(
             __file__).parent.joinpath('../test_data').absolute()
@@ -129,6 +130,67 @@ def test_match_tune_chromaticity(test_context):
     xo.assert_allclose(tw_final['qy'], 60.31, atol=1e-4)
     xo.assert_allclose(tw_final['dqx'],  6.0, atol=0.05)
     xo.assert_allclose(tw_final['dqy'],  4.0, atol=0.05)
+
+@for_all_test_contexts
+def test_match_tune_chromaticity_homotopy(test_context):
+
+    with open(path_line) as f:
+        dct = json.load(f)
+
+    line = xt.Line.from_dict(dct['line'])
+    line.particle_ref = xp.Particles.from_dict(dct['particle'])
+
+    line.build_tracker(_context=test_context)
+
+    print('\nInitial twiss parameters')
+    tw_before = line.twiss()
+    print(f"Qx = {tw_before['qx']:.5f} Qy = {tw_before['qy']:.5f} "
+        f"Q'x = {tw_before['dqx']:.5f} Q'y = {tw_before['dqy']:.5f}")
+
+    print(f"kqtf.b1 = {line.vars['kqtf.b1']._value}")
+    print(f"kqtd.b1 = {line.vars['kqtd.b1']._value}")
+    print(f"ksf.b1 = {line.vars['ksf.b1']._value}")
+    print(f"ksd.b1 = {line.vars['ksd.b1']._value}")
+
+    t1 = time.time()
+    opt = line.match(
+        solve=False,
+        vary=[
+            xt.Vary('kqtf.b1', step=1e-8),
+            xt.Vary('kqtd.b1', step=1e-8),
+            xt.Vary('ksf.b1', step=1e-8),
+            xt.Vary('ksd.b1', step=1e-8),
+        ],
+        targets = [
+            xt.Target('qx', 62.315, tol=1e-4),
+            xt.Target(lambda tw: tw['qx'] - tw['qy'], 1.99, tol=1e-4),
+            xt.Target('dqx', 10.0, tol=0.05),
+            xt.Target('dqy', 12.0, tol=0.05)])
+    opt.solve_homotopy()
+    t2 = time.time()
+    print('\nTime fsolve: ', t2-t1)
+
+    tw_final = line.twiss()
+    print('\nFinal twiss parameters')
+    print(f"Qx = {tw_final['qx']:.5f} Qy = {tw_final['qy']:.5f} "
+        f"Q'x = {tw_final['dqx']:.5f} Q'y = {tw_final['dqy']:.5f}")
+    print(f"kqtf.b1 = {line.vars['kqtf.b1']._value}")
+    print(f"kqtd.b1 = {line.vars['kqtd.b1']._value}")
+    print(f"ksf.b1 = {line.vars['ksf.b1']._value}")
+    print(f"ksd.b1 = {line.vars['ksd.b1']._value}")
+
+    xo.assert_allclose(tw_final['qx'], 62.315, atol=1e-7)
+    xo.assert_allclose(tw_final['qy'], 60.325, atol=1e-7)
+    xo.assert_allclose(tw_final['dqx'], 10.0, atol=0.05)
+    xo.assert_allclose(tw_final['dqy'], 12.0, atol=0.05)
+
+    # Assert that Homotopy arrays were created accordingly
+    qx = np.linspace(tw_before['qx'], 62.315, 11)[1:]
+    qy = np.linspace(tw_before['qy'], 60.325, 11)[1:]
+    dqx = np.linspace(tw_before['dqx'], 10.0, 11)[1:]
+    dqy = np.linspace(tw_before['dqy'], 12.0, 11)[1:]
+    
+    # Targets are not logged (only reached values when solving), so no test if they match
 
 
 @for_all_test_contexts
