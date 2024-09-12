dfb533e877e980c3d6d3d5180819d8cb181a5a48
giadarol <giovanni.iadarola@cern.ch>
2024-09-12 16:46:12 +0200

    Status

--- examples/lattice_design_shortcuts/t000_test_env.py
+++ examples/lattice_design_shortcuts/t000_test_env.py
@@ -19,6 +19,18 @@ env.vars({'k.1': 'a'})
 assert env.vv['k.1'] == 3.
 assert env.vv['b'] == 2 * 3 + 3.
 
-# line = env.new_line([
-#     env.new
-# ]
+env.vars['k.1'] = 2 * env.vars['a'] + 5
+assert env.vv['k.1'] == 2 * 3 + 5
+assert env.vv['b'] == 2 * 3 + 2 * 3 + 5
+
+
+env.vars({
+    'a': 4.,
+    'b': '2 * a + 5',
+})
+
+env.new('bb', xt.Bend, k0='2 * b', length=3+env.vars['a'] + env.vars['b'],
+        h=5.)
+assert env['bb'].k0 == 2 * (2 * 4 + 5)
+assert env['bb'].length == 3 + 4 + 2 * 4 + 5
+assert env['bb'].h == 5.
