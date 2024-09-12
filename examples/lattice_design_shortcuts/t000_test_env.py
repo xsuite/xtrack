b48cfd85acaf29ae7f34b4e86d460cc742586b16
giadarol <giovanni.iadarola@cern.ch>
2024-09-12 17:12:55 +0200

    Status

--- examples/lattice_design_shortcuts/t000_test_env.py
+++ examples/lattice_design_shortcuts/t000_test_env.py
@@ -41,10 +41,17 @@ assert env['bb'].length == 3 + 2 + 2 * 2 + 5
 assert env['bb'].h == 5.
 
 line = env.new_line([
-    env.new('bb1', 'bb', length=3*env.vars['a'], at='2*a')
+    env.new('bb1', 'bb', length=3*env.vars['a'], at='2*a'),
+    env.place('bb', at=10 * env.vars['a'])
 ])
 
 assert line['bb1'].length == 6
 assert line['bb1'].k0 == 2 * (2 * 2 + 5)
 assert line['bb1'].h == 5.
+
+assert env['bb'].k0 == 2 * (2 * 2 + 5)
+assert env['bb'].length == 3 + 2 + 2 * 2 + 5
+assert env['bb'].h == 5.
+
+
 
