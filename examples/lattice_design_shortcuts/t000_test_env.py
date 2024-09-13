2193ad6da7c73c7f53d1feae351a154f077c803f
giadarol <giovanni.iadarola@cern.ch>
2024-09-13 19:00:59 +0200

    More checks

--- examples/lattice_design_shortcuts/t000_test_env.py
+++ examples/lattice_design_shortcuts/t000_test_env.py
@@ -1,6 +1,9 @@
 import xtrack as xt
 import numpy as np
 
+to_test = 'line'
+to_test = 'env'
+
 env = xt.Environment()
 
 env.vars({
@@ -11,8 +14,7 @@ env.vars({
 
 line = env.new_line([])
 
-ee = env # Test Environment
-ee = line # Test Line
+ee = {'env': env, 'line': line}[to_test]
 
 assert ee.vv['b'] == 2 * 2 + 1
 
@@ -64,13 +66,13 @@ ee.ref['k.1'] = 2 * ee.ref['a'] + 5
 assert ee.vv['k.1'] == 2 * 2 + 5
 assert ee.vv['b'] == 2 * 2 + 2 * 2 + 5
 
+#--------------------------------------------------
+
 ee.vars({
     'a': 4.,
     'b': '2 * a + 5',
     'k.1': '2 * a + 5',
 })
-
-#--------------------------------------------------
 
 env.new('bb', xt.Bend, k0='2 * b', length=3+env.vars['a'] + env.vars['b'],
         h=5.)
@@ -87,6 +89,16 @@ line = env.new_line([
     env.new('bb1', 'bb', length=3*env.vars['a'], at='2*a'),
     env.place('bb', at=10 * env.vars['a'], from_='bb1'),
 ])
+
+assert hasattr(env.ref['bb1'].length, '_value') # is a Ref
+assert not hasattr(env['bb1'].length, '_value') # a number
+assert env.ref['bb1'].length._value == 3 * 2
+assert env['bb1'].length == 3 * 2
+
+assert hasattr(env.ref['bb1'].length, '_value') # is a Ref
+assert not hasattr(env['bb1'].length, '_value') # a number
+assert env.ref['bb1'].length._value == 3 * 2
+assert env['bb1'].length == 3 * 2
 
 assert line['bb1'] is not env['bb']
 assert line['bb'] is env['bb']
