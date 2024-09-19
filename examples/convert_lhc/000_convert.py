b0c1027f80e4517d5440e4deace81f4d6d17194b
Riccardo De Maria <riccardodemaria@gmail.com>
2024-09-19 13:56:26 +0200

    lhc.py example

---
+++ examples/convert_lhc/000_convert.py
@@ -0,0 +1,178 @@
+import xtrack as xt
+import xdeps
+import numpy as np
+import re
+
+from xdeps.madxutils import to_madx
+
+stmt = re.compile(r"([a-z0-9._]+)(,|:=|:|=)(.*);")
+
+
+def parse_command(stmt):
+    groups = []
+    rest = stmt
+    while len(rest) > 0:
+        if res := re.match(r"([a-z0-9._]+)[,;]", rest):
+            groups.append(res.groups())
+        elif res := re.match(r"([a-z0-9._]+)(=|:=)(\{[^\}]+\})[,;]", rest):
+            groups.append(res.groups())
+        elif res := re.match(r"([a-z0-9._]+)(=|:=)([^,]+)[,;]", rest):
+            groups.append(res.groups())
+        else:
+            print(rest)
+            raise ValueError("Input Error {stmt}")
+        rest = rest[len(res.group()) :]
+    return groups
+
+
+# examples=["fd,b={4,6},a=5;"]
+# for ex in examples:
+#    print(parse_command(ex))
+
+
+def parse_madx_expr(av):
+    av_expr = env._xdeps_eval.eval(av)
+    if not isinstance(av_expr, float):
+        return av
+    else:
+        return av_expr
+
+
+def invert_at(groups):
+    out = []
+    for an, eq, av in groups:
+        if an == "at":
+            av = f"lhclength-({av})"
+        out.append((an, eq, av))
+    return out
+
+
+def groups_to_attrs(groups):
+    attrs = {}
+    extra = {}
+    for an, eq, av in groups:
+        if av.startswith("{"):
+            av = [parse_madx_expr(aa) for aa in av[1:-1].split(",")]
+        else:
+            av = parse_madx_expr(av)
+        if an in [
+            "slot_id",
+            "mech_sep",
+            "assembly_id",
+            "kmin",
+            "kmax",
+            "calib",
+            "polarity",
+        ]:
+            extra[an] = (av, eq)
+        else:
+            if an == "l" or an == "lrad":
+                an = "length"
+            if an == "tilt":
+                an = "rot_s_rad"
+            attrs[an] = (av, eq)
+    attrs = ",".join([f"{an}={av!r}" for an, (av, eq) in attrs.items()])
+    extra = ",".join([f"{an}={av!r}" for an, (av, eq) in extra.items()])
+    out = []
+    if attrs:
+        out.append(f"{attrs}")
+    if extra:
+        out.append(f"extra=dict({extra})")
+    return "," + ",".join(out)
+
+
+def default_for_zeros(expr, vv, notinitialized):
+    if hasattr(expr, "_get_dependencies"):
+        for ll in expr._get_dependencies():
+            if hasattr(ll, "_key"):
+                if ll._key not in vv:
+                    vv[ll._key] = 0
+                    notinitialized.add(ll._key)
+
+
+env = xt.Environment()
+env.vars["twopi"] = np.pi * 2
+
+lines = [
+    "import xtrack as xt",
+    "import numpy as np",
+    "env=xt.Environment()",
+    "env.vars['twopi']=np.pi*2",
+    "env.new('vkicker','Multipole')",
+    "env.new('hkicker','Multipole')",
+    "env.new('tkicker','Multipole')",
+    "env.new('collimator','Drift')",
+    "env.new('instrument','Drift')",
+    "env.new('monitor','Drift')",
+    "env.new('placeholder','Drift')",
+    "env.new('sbend','Bend')",
+    "env.new('rbend','Bend')",
+]
+
+notparsed = []
+notinitialized = set()
+inside_sequence = False
+lhcb2 = False
+for line in open("lhc.seq"):
+    line = line.strip()
+    ls = line.replace(" ", "").lower()
+    if res := stmt.match(ls):
+        lhs, eq, rhs = res.groups()
+        if eq == ":=":  # variable
+            expr = env._xdeps_eval.eval(rhs)
+            default_for_zeros(expr, env.vars, notinitialized)
+            env.vars[lhs] = expr
+            if not isinstance(expr, float):
+                expr = rhs
+            lines.append(f"env[{lhs!r}]={expr!r}")
+        elif eq == ":":  # element definition
+            groups = parse_command(rhs + ";")
+            parent = groups.pop(0)[0]
+            if parent == "sequence":
+                lines.append(f"env.new_line({lhs!r}, components=[")
+                inside_sequence = True
+                if lhs == "lhcb2":
+                    lhcb2 = True
+            else:
+                groups = [
+                    (an.replace("from", "from_"), eq, av) for an, eq, av in groups
+                ]
+                if lhcb2:
+                    groups = invert_at(groups)
+                attrs = groups_to_attrs(groups)
+                if inside_sequence:
+                    lines.append(f"  env.new({lhs!r:20},{parent!r}{attrs}),")
+                else:
+                    lines.append(f"env.new({lhs!r:20},{parent!r}{attrs})")
+        elif eq == ",":  # element definition
+            groups = parse_command(rhs + ";")
+            attrs = groups_to_attrs(groups)
+            lines.append(f"env.set({lhs!r:20}{attrs})")
+        else:
+            lines.append("#" + line)
+    else:
+        if line.startswith("ENDSEQUENCE"):
+            lines.append("])")
+            inside_sequence = False
+        elif len(line) > 0:
+            if not line.startswith("if") or not line.startswith("return"):
+                lines.append("#" + line)
+        else:
+            lines.append("")
+
+import re
+
+for ii in range(len(lines)):
+    line = lines[ii]
+    if ",kick" in line:
+        if "h." in line:
+            lines[ii]=re.sub(r"kick=('[-a-z0-9.]+')",r"knl=[-(\1)]",line)
+        else:
+            lines[ii]=re.sub(r"kick=('[-a-z0-9.]+')",r"ksl=[\1]",line)
+    elif ",hkick" in line:
+        lines[ii]=re.sub(r"hkick=('[-a-z0-9.]+')",r"knl=[-(\1)]",line)
+    elif ",vkick" in line:
+        lines[ii]=re.sub(r"vkick=('[-a-z0-9./]+')",r"ksl=[\1]",line)
+
+for ll in lines:
+    print(ll)
