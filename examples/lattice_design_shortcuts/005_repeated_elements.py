c66646bc988596a3fb83d31c46cfea05b7207824
giadarol <giovanni.iadarola@cern.ch>
2024-09-24 09:01:50 +0200

    Another check

--- examples/lattice_design_shortcuts/005_repeated_elements.py
+++ examples/lattice_design_shortcuts/005_repeated_elements.py
@@ -25,3 +25,25 @@ assert np.all(tt.name == np.array(['mb', 'mb', 'drift_1', 'ip1', 'mb',
                                    'mb', '_end_point']))
 assert np.all(tt.s == np.array([
     0. ,  0.5,  1. , 10. , 10. , 10.5, 11. , 19.5, 20. , 20. , 20.5, 21. ]))
+
+l1 = env.new_line(name='l1', components=[
+    'mb',
+    'mb',
+    env.new('mid', 'Marker'),
+    'mb',
+    'mb',
+])
+
+env['s.l1'] = 10
+l_twol1 = env.new_line(components=[
+    env.new('ip', 'Marker', at=20),
+    env.place('l1', at='s.l1', from_='ip'),
+    env.place(l1, at=-env.ref['s.l1'], from_='ip'),
+])
+tt_twol1 = l_twol1.get_table()
+assert np.all(tt_twol1.name == np.array(
+    ['drift_3', 'mb', 'mb', 'mid', 'mb', 'mb', 'drift_4', 'ip',
+       'drift_5', 'mb', 'mb', 'mid', 'mb', 'mb', '_end_point']))
+assert np.all(tt_twol1.s == np.array(
+    [ 0. ,  9. ,  9.5, 10. , 10. , 10.5, 11. , 20. , 20. , 29. , 29.5,
+       30. , 30. , 30.5, 31. ]))
