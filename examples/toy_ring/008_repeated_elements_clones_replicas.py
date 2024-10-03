330eb70a549e0ef547c6ac174e543a9d89555077
giadarol <giovanni.iadarola@cern.ch>
2024-10-03 19:23:12 +0200

    Working on example

--- examples/toy_ring/008_repeated_elements_clones_replicas.py
+++ examples/toy_ring/008_repeated_elements_clones_replicas.py
@@ -13,7 +13,7 @@ env.new('dd', xt.Drift, length=1)
 # We create a line with repeated elements
 line = env.new_line(components=['mq', 'dd', 'mq', 'dd'])
 
-line.get_table(attr=True).cols['name s rot_s_rad'].show()
+line.get_table(attr=True).cols['name s rot_s_rad']
 # is:
 # name                   s     rot_s_rad
 # mq::0                  0             0
@@ -26,7 +26,7 @@ line.get_table(attr=True).cols['name s rot_s_rad'].show()
 # 'mq' is seen directly on 'mq::0' and 'mq::1'. For example, we  tilt mq by 1 mrad:
 line['mq'].rot_s_rad = 1e-3
 
-line.get_table(attr=True).cols['name s rot_s_rad'].show()
+line.get_table(attr=True).cols['name s rot_s_rad']
 # is:
 # name                   s     rot_s_rad
 # mq::0                  0         0.001
@@ -41,18 +41,22 @@ line.get_table(attr=True).cols['name s rot_s_rad'].show()
 
 # Replicas behave in the same way as repeted elements, but allow assignining
 # a different name to each replica. For example:
-env.new('mq_1', 'mq', mode='replica')
-env.new('mq_2', 'mq', mode='replica')
+env.new('my_mq_1', 'mq', mode='replica')
+env.new('my_mq_2', 'mq', mode='replica')
 
-line = env.new_line(components=['mq_1', 'dd', 'mq_2', 'dd'])
-line.get_table(attr=True).cols['name s length'].show()
+line = env.new_line(components=['my_mq_1', 'dd', 'my_mq_2', 'dd'])
+
+# Here 'mq_1' and 'mq::1' are actually the same element. Any modification on
+# 'mq' is seen directly on 'mq::0' and 'mq::1'. For example, we  set the tilt of 
+# mq by 3 mrad:
+line['mq'].rot_s_rad = 3e-3
+
+line.get_table(attr=True).cols['name s rot_s_rad']
 # is:
-# name                   s        length
-# mq_1                   0           0.5
-# dd::0                0.5             1
-# mq_2                 1.5           0.5
-# dd::1                  2             1
-# _end_point             3             0
-
-
-
+# Table: 5 rows, 3 cols
+# name                   s     rot_s_rad
+# my_mq_1                0         0.001
+# dd::0                0.3             0
+# my_mq_2              1.3         0.001
+# dd::1                1.6             0
+# _end_point           2.6             0
