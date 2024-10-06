3f6ebdaf50dbcdc537955f61fdfc905a1cd60cc3
giadarol <giovanni.iadarola@cern.ch>
2024-10-06 22:39:52 +0200

    Some more checks

--- examples/rbendrbarc/000_dev.py
+++ examples/rbendrbarc/000_dev.py
@@ -31,3 +31,5 @@ env.set('rb_rbarc', length='lb', angle='ang', rbend=True, rbarc=True)
 
 xo.assert_allclose(env['rb_rbarc'].length, ds_madx, atol=0, rtol=1e-12)
 xo.assert_allclose(env['rb_rbarc'].h * env['rb_rbarc'].length, 0.6, atol=0, rtol=1e-12)
+xo.assert_allclose(env['rb_rbarc'].edge_entry_angle, 0.3, atol=0, rtol=1e-12)
+xo.assert_allclose(env['rb_rbarc'].edge_exit_angle, 0.3, atol=0, rtol=1e-12)
