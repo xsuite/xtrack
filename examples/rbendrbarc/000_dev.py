4f0409c06444d650ddc7e0da571449625ff3e052
giadarol <giovanni.iadarola@cern.ch>
2024-10-06 22:14:56 +0200

    Found issue

---
+++ examples/rbendrbarc/000_dev.py
@@ -0,0 +1,26 @@
+import numpy as np
+import xtrack as xt
+import xobjects as xo
+from cpymad.madx import Madx
+
+mad = Madx()
+mad.input('''
+    rb: rbend, l=1.0, angle=0.5;
+
+    beam, particle=proton, energy=1.0;
+    seq: sequence, l=2;
+    rb, at=1;
+    endsequence;
+    use, sequence=seq;
+''')
+tw = xt.Table(mad.twiss(betx=1.0, bety=1.0))
+
+ds_madx = tw['s', 'rb:1'] - tw['s', 'rb:1<<1']
+
+line = xt.Line.from_madx_sequence(mad.sequence.seq)
+
+xo.assert_allclose(ds_madx, line['rb'].length, atol=0, rtol=1e-12)
+
+env = xt.Environment()
+env.new('rbe', xt.Bend)
+env.set('rbe', length=1.0, angle=0.5, rbend=True, rbarc=True)
