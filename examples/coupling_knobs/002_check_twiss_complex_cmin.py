44bbe2e79d4c885ab119f9761b16dc86b8a9921e
giadarol <giovanni.iadarola@cern.ch>
2024-10-08 10:35:01 +0200

    Another check

--- examples/coupling_knobs/002_check_twiss_complex_cmin.py
+++ examples/coupling_knobs/002_check_twiss_complex_cmin.py
@@ -1,9 +1,10 @@
 import numpy as np
 import xtrack as xt
+import xobjects as xo
 
 # Load a line and build tracker
 line = xt.Line.from_json(
-    '../../test_data/hllhc14_no_errors_with_coupling_knobs/line_b1.json')
+    '../../test_data/hllhc15_thick/lhc_thick_with_knobs.json')
 line.particle_ref = xt.Particles(mass0=xt.PROTON_MASS_EV, q0=1, energy0=7e12)
 line.cycle('ip1', inplace=True)
 line.twiss_default['method'] = '4d'
@@ -16,13 +17,30 @@ line['cmrskew'] = 0
 line['cmiskew'] = 0
 tw0 = line.twiss()
 
-line['cmrskew'] = 1e-4
-line['cmiskew'] = 1e-4
+line.vars.update({
+    'kqs.r3b1':   -3.41388e-06,
+    'kqs.l4b1':   -3.41388e-06,
+    'kqs.a67b1':  -1.81655e-06,
+    'kqs.r7b1':    4.42367e-06,
+    'kqs.l8b1':    4.42367e-06,
+    'kqs.a23b1':  -4.39673e-06,
+})
 
 tw = line.twiss(strengths = True)
-c_min_cpx = tw.c_minus_re + 1j*tw.c_minus_im
+
 
 c_min_from_k1s = (0+0j) * tw.s
 for ii in xt.progress_indicator.progress(range(len(tw.s))):
     c_min_from_k1s[ii] = 1 / (2*np.pi) * np.sum(tw.k1sl * np.sqrt(tw0.betx * tw0.bety)
             * np.exp(1j * 2 * np.pi * ((tw0.mux - tw0.mux[ii]) - (tw0.muy - tw0.muy[ii]))))
+
+xo.assert_allclose(tw.c_minus_re + 1j*tw.c_minus_im, c_min_from_k1s, rtol=4e-2, atol=0)
+xo.assert_allclose(tw.c_minus_re + 1j*tw.c_minus_im,
+    tw.c_minus * np.exp(1j * tw.c_phi1), rtol=1e-10, atol=0)
+
+import matplotlib.pyplot as plt
+plt.close('all')
+plt.figure(1)
+plt.plot(tw.s, np.abs(tw.c_minus_re + 1j*tw.c_minus_im)/np.abs(c_min_from_k1s))
+
+plt.show()
