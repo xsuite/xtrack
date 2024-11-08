26fd4aeb9930bb9891cc01a376cb0fded753613e
giadarol <giovanni.iadarola@cern.ch>
2024-11-08 14:55:46 +0100

    Add test

--- tests/test_environment.py
+++ tests/test_environment.py
@@ -3,6 +3,7 @@ import xobjects as xo
 import xdeps as xd
 import numpy as np
 import pytest
+import json
 import pathlib
 
 test_data_folder = pathlib.Path(__file__).parent.joinpath('../test_data').absolute()
@@ -1853,3 +1854,50 @@ def test_inpection_methods(container_type):
     tt = ee['bb'].get_table()
     assert tt['value', 'k0'] == 2 * (2 * 2 + 1)
     assert tt['expr', 'k0'] == "(2.0 * vars['b'])"
+
+def test_vars_features(tmpdir):
+    env = xt.Environment()
+
+    try:
+        env['b'] = '3*a'
+    except KeyError:
+        pass
+    else:
+        raise ValueError('Variable a should not be present')
+
+    env.vars.default_to_zero = True
+    env['b'] = '3*a'
+
+    assert env['a'] == 0
+    assert env['b'] == 0
+
+    env['a'] = 3
+    assert env['b'] == 9
+
+    # Test compact and to_dict
+    tt = env.vars.get_table()
+    assert tt['expr', 'b'] == '(3.0 * a)'
+    dd = tt.to_dict()
+    assert dd['b'] == '(3.0 * a)'
+    assert dd['a'] == 3.0
+
+    ee = xt.Environment()
+    ee.vars.update(dd)
+    assert ee['a'] == 3.0
+    assert ee['b'] == 9.0
+    assert ee.vars.get_table()['expr', 'b'] == '(3.0 * a)'
+
+    with open(tmpdir / 'env.json', 'w') as fid:
+        json.dump(dd, fid)
+
+    ee2 = xt.Environment()
+    ee2.vars.load_json(tmpdir / 'env.json')
+    assert ee2['a'] == 3.0
+    assert ee2['b'] == 9.0
+    assert ee2.vars.get_table()['expr', 'b'] == '(3.0 * a)'
+
+    tt1 = env.vars.get_table(compact=False)
+    assert tt1['expr', 'b'] ==  "(3.0 * vars['a'])"
+    dd1 = tt1.to_dict()
+    assert dd1['b'] == "(3.0 * vars['a'])"
+    assert dd1['a'] == 3.0
