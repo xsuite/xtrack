02659e03056b94317b0200ed37b8843b97935bb5
giadarol <giovanni.iadarola@cern.ch>
2024-11-12 13:43:01 +0100

    Clean up

--- examples/mad_writer/001c_dev_madng_interface.py
+++ examples/mad_writer/001c_dev_madng_interface.py
@@ -9,7 +9,14 @@ class MadngVars:
 
     def __setitem__(self, key, value):
         setattr(self.mad.MADX, key.replace('.', '_'), value)
-        #Expressions still to be handled
+        #Expressions still to be handled, could use the following:
+        # mng.send(
+        #     MADX:open_env()
+        #     a = 3
+        #     b =\ 3 * a
+        #     c =\ 4 * a
+        #     MADX:close_env()
+        #     ''')
 
 def _build_madng_model(line, sequence_name='seq'):
     if line.tracker is None:
@@ -22,7 +29,9 @@ def _build_madng_model(line, sequence_name='seq'):
 
 rdts = ["f4000", "f3100", "f2020", "f1120", 'f1001']
 
-def _tw_ng(line, rdts=[], scalars=True):
+def _tw_ng(line, rdts=[], normal_form=True,
+           mapdef_twiss=2, mapdef_normal_form=4
+           ):
 
     tw_kwargs = locals()
     del tw_kwargs['line']
@@ -70,7 +79,9 @@ def _tw_ng(line, rdts=[], scalars=True):
         '''
 
         -- twiss with RDTs
-        local mtbl = twiss {sequence=seq, method=4, mapdef=2, implicit=true, nslice=3}
+        local mtbl = twiss {sequence=seq, method=4,'''
+        f'mapdef=2'
+        ''', implicit=true, nslice=3}
 
         -- send columns to Python
         '''
@@ -88,8 +99,8 @@ def _tw_ng(line, rdts=[], scalars=True):
     for nn in rdts:
         tw[nn] = np.atleast_1d(np.squeeze(out_dct[nn]))[:-1]
 
-    if scalars:
-        mng_script_scalar = (
+    if normal_form:
+        mng_script_nf = (
             f'local seq = MADX.{mng._sequence_name}'
             '''
             local track in MAD  -- like "from MAD import track"
@@ -120,29 +131,29 @@ def _tw_ng(line, rdts=[], scalars=True):
                     nf:anhy{1, 0}, -- dqy / d(2 jx)
                     })
         ''')
-        mng.send(mng_script_scalar)
-        out_scalar = mng.recv()
+        mng.send(mng_script_nf)
+        out_nf = mng.recv()
 
-        dct_scalar = dict(
-            q1 =   out_scalar[0],
-            q2 =   out_scalar[1],
-            dq1 =  out_scalar[2],
-            dq2 =  out_scalar[3],
-            d2q1 = out_scalar[4],
-            d2q2 = out_scalar[5],
-            d3q1 = out_scalar[6],
-            d3q2 = out_scalar[7],
-            d4q1 = out_scalar[8],
-            d4q2 = out_scalar[9],
-            d5q1 = out_scalar[10],
-            d5q2 = out_scalar[11],
-            dqxdjx = out_scalar[6]*2.,
-            dqydjy = out_scalar[7]*2.,
-            dqxdjy = out_scalar[8]*2.,
-            dqydjx = out_scalar[9]*2.,
+        dct_nf = dict(
+            q1 =   out_nf[0],
+            q2 =   out_nf[1],
+            dq1 =  out_nf[2],
+            dq2 =  out_nf[3],
+            d2q1 = out_nf[4],
+            d2q2 = out_nf[5],
+            d3q1 = out_nf[6],
+            d3q2 = out_nf[7],
+            d4q1 = out_nf[8],
+            d4q2 = out_nf[9],
+            d5q1 = out_nf[10],
+            d5q2 = out_nf[11],
+            dqxdjx = out_nf[6]*2.,
+            dqydjy = out_nf[7]*2.,
+            dqxdjy = out_nf[8]*2.,
+            dqydjx = out_nf[9]*2.,
         )
-        for nn in dct_scalar:
-            tw[nn+'_nf_ng'] = dct_scalar[nn]
+        for nn in dct_nf:
+            tw[nn+'_nf_ng'] = dct_nf[nn]
 
     return tw
 
@@ -174,7 +185,7 @@ line['mb.b32l8.b1'].knl[1] = 'test_dk1'
 # line['a'] = 3.
 # assert mng.MADX.a == 3.
 
-tw = line._tw_ng(scalars=False)
+tw = line._tw_ng(normal_form=False)
 
 opt = line.match(
     solve=False,
@@ -187,7 +198,7 @@ opt = line.match(
     ),
 )
 opt.step(3)
-tw_after = line._tw_ng(scalars=False)
+tw_after = line._tw_ng(normal_form=False)
 xo.assert_allclose(tw_after['px_ng', 'ip1'], 50e-6, rtol=5e-3, atol=0)
 
 # tw = line._tw_ng()
