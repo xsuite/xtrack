4d95c6d202fcac8c5f04d20fedcac0b8cecdfde3
giadarol <giovanni.iadarola@cern.ch>
2024-11-13 17:53:34 +0100

    Tilt seems to already work

--- examples/mad_writer/001d_madng_tilt_shift.py
+++ examples/mad_writer/001d_madng_tilt_shift.py
@@ -13,34 +13,32 @@ tt = line.get_table()
 tt_quad = tt.rows['mq\..*']
 rgen = np.random.RandomState(1) # fix seed for random number generator
                                 # (to have reproducible results)
-shift_x = rgen.randn(len(tt_quad)) * 0.01e-3 # 0.01 mm rms shift on all quads
-shift_y = rgen.randn(len(tt_quad)) * 0.01e-3 # 0.01 mm rms shift on all quads
-for nn_quad, sx, sy in zip(tt_quad.name, shift_x, shift_y):
-    line[nn_quad].shift_x = sx
-    line[nn_quad].shift_y = sy
+# shift_x = rgen.randn(len(tt_quad)) * 0.01e-3 # 0.01 mm rms shift on all quads
+# shift_y = rgen.randn(len(tt_quad)) * 0.01e-3 # 0.01 mm rms shift on all quads
+# for nn_quad, sx, sy in zip(tt_quad.name, shift_x, shift_y):
+#     line[nn_quad].shift_x = sx
+#     line[nn_quad].shift_y = sy
 
 tw = line.madng_twiss()
 
-mng = line.tracker._madng
-import time
-t0 = time.perf_counter()
-for nn in tt.name:
-    if nn == '_end_point':
-        continue
-    if not hasattr(line[nn], 'shift_x'):
-        continue
-    nn_ng = nn.replace('.', '_')
-    mng.send(
-             f'MADX.{nn_ng}.dx = 0\n'
-             f'MADX.{nn_ng}.dy = 0\n'
-             f'MADX.{nn_ng}.misalign'
-             ' = {'
-             f'dx={line[nn].shift_x}, dy={line[nn].shift_y}'
-                '}')
-t1 = time.perf_counter()
-print(f'Elapsed time: {t1-t0}')
+# mng = line.tracker._madng
+# import time
+# t0 = time.perf_counter()
+# for nn in line.element_names:
+#     if not hasattr(line[nn], 'shift_x'):
+#         continue
+#     nn_ng = nn.replace('.', '_')
+#     mng.send(
+#              f'MADX.{nn_ng}.dx = 0\n'
+#              f'MADX.{nn_ng}.dy = 0\n'
+#              f'MADX.{nn_ng}.misalign'
+#              ' = {'
+#              f'dx={line[nn].shift_x}, dy={line[nn].shift_y}'
+#                 '}')
+# t1 = time.perf_counter()
+# print(f'Elapsed time: {t1-t0}')
 
-tw1 = line.madng_twiss()
+# tw1 = line.madng_twiss()
 
-xo.assert_allclose(tw1.x, tw1.x_ng, atol=2e-4*tw1.x.std(), rtol=0)
-xo.assert_allclose(tw1.y, tw1.y_ng, atol=2e-4*tw1.y.std(), rtol=0)
+# xo.assert_allclose(tw1.x, tw1.x_ng, atol=2e-4*tw1.x.std(), rtol=0)
+# xo.assert_allclose(tw1.y, tw1.y_ng, atol=2e-4*tw1.y.std(), rtol=0)
