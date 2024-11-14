71824ab0e162659f2e089f05756ba2418c8153a9
giadarol <giovanni.iadarola@cern.ch>
2024-11-14 17:26:08 +0100

    More checks

--- examples/mad_writer/001d_madng_tilt_shift.py
+++ examples/mad_writer/001d_madng_tilt_shift.py
@@ -16,13 +16,13 @@ rgen = np.random.RandomState(1) # fix seed for random number generator
 shift_x = rgen.randn(len(tt_quad)) * 0.01e-3 # 0.01 mm rms shift on all quads
 shift_y = rgen.randn(len(tt_quad)) * 0.01e-3 # 0.01 mm rms shift on all quads
 rot_s = rgen.randn(len(tt_quad)) * 1e-3 # 1 mrad rms rotation on all quads
+k2l = rgen.rand(len(tt_quad)) * 1e-3
 
-for nn_quad, sx, sy, rr in zip(tt_quad.name, shift_x, shift_y, rot_s):
+for nn_quad, sx, sy, rr, kkk in zip(tt_quad.name, shift_x, shift_y, rot_s, k2l):
     line[nn_quad].shift_x = sx
     line[nn_quad].shift_y = sy
     line[nn_quad].rot_s_rad = rr
-
-line['mq.15l7.b1'].knl[2] = 0.5
+    line[nn_quad].knl[2] = kkk
 
 tw = line.madng_twiss()
 
@@ -30,4 +30,9 @@ xo.assert_allclose(tw.x, tw.x_ng, atol=5e-4*tw.x.std(), rtol=0)
 xo.assert_allclose(tw.y, tw.y_ng, atol=5e-4*tw.y.std(), rtol=0)
 xo.assert_allclose(tw.betx2, tw.beta12_ng, atol=0, rtol=2e-3)
 xo.assert_allclose(tw.bety1, tw.beta21_ng, atol=0, rtol=2e-3)
-xo.assert_allclose(tw.wx_chrom, tw.wx_ng, atol=0, rtol=1e-3)
+xo.assert_allclose(tw.wx_chrom, tw.wx_ng, atol=5e-3*tw.wx_chrom.max(), rtol=0)
+xo.assert_allclose(tw.wy_chrom, tw.wy_ng, atol=5e-3*tw.wy_chrom.max(), rtol=0)
+xo.assert_allclose(tw.ax_chrom, tw.ax_ng, atol=5e-3*tw.wx_chrom.max(), rtol=0)
+xo.assert_allclose(tw.ay_chrom, tw.ay_ng, atol=5e-3*tw.wy_chrom.max(), rtol=0)
+xo.assert_allclose(tw.bx_chrom, tw.bx_ng, atol=5e-3*tw.wx_chrom.max(), rtol=0)
+xo.assert_allclose(tw.by_chrom, tw.by_ng, atol=5e-3*tw.wy_chrom.max(), rtol=0)
