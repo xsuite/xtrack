2b90e7f6bbad779158eb80ec4ab6bed1e6998355
John Salvesen <jpts2@cam.ac.uk>
2024-11-24 17:50:31 +0100

    Rotation test of solenoid Tests vertical and horizontal reference frame shifts

--- examples/solenoid/mult_rot_test.py
+++ examples/solenoid/mult_rot_test.py
@@ -3,7 +3,7 @@ Test implementation of the multipole shifts in solenoid slices
 =============================================
 Author(s): John P T Salvesen
 Email:  john.salvesen@cern.ch
-Date:   14-11-2024
+Date:   18-11-2024
 """
 ################################################################################
 # Required Modules
@@ -15,113 +15,138 @@ import matplotlib.pyplot as plt
 ################################################################################
 # User Parameters
 ################################################################################
-N_SLICES    = int(1E3)
+N_SLICES    = int(1E2)
+K0          = 1E-3
+L_SOL       = 1
+XING_RAD    = 1E-3
 
 BETX        = 100E-3
 BETY        = 1E-3
-PX0         = 0
-
-KS          = 0.00
-K0          = 1E-3
-K1          = 1E-3
-K2          = 1E-3
-
-X_ROT       = 1E-3
-Y_ROT       = 1E-3
 
 ################################################################################
-# Build Test Elements
+# Build Test Lines
 ################################################################################
-drift0  = xt.Drift(length = 1)
-drift1  = xt.Drift(length = 1)
 
-bend    = xt.Bend(length = 1, k0 = K0)
-quad    = xt.Quadrupole(length = 1, k1 = K1)
-sext    = xt.Sextupole(length = 1, k2 = K2)
+########################################
+# Build Environment
+########################################
+env     = xt.Environment(particle_ref = xt.Particles(p0c = 1E9))
 
-bend_sol    = xt.Solenoid(length = 1 / N_SLICES, ks = KS,
-    knl = [K0 * (1/N_SLICES), 0, 0], num_multipole_kicks = 1)
-quad_sol    = xt.Solenoid(length = 1 / N_SLICES, ks = KS,
-    knl = [0, K1 * (1/N_SLICES), 0], num_multipole_kicks = 1)
-sext_sol    = xt.Solenoid(length = 1 / N_SLICES, ks = KS,
-    knl = [0, 0, K2 * (1/N_SLICES)], num_multipole_kicks = 1)
+########################################
+# Line (beamline frame)
+########################################
+bl_components_in    = [env.new('bl_drift0', xt.Drift, length  = 1)]
+bl_components_out   = [env.new('bl_drift1', xt.Drift, length  = 1)]
+
+bl_components_sol = [
+    env.new(f'bl_sol.{i}',  xt.Solenoid,
+            length              = (L_SOL / N_SLICES),
+            ks                  = 0,
+            knl                 = [K0 * (L_SOL / N_SLICES), 0, 0],
+            num_multipole_kicks = 1)
+    for i in range(N_SLICES)]
+
+bl_line    = env.new_line(
+    components  = bl_components_in + bl_components_sol + bl_components_out)
+
+########################################
+# Line (horizontal rotated frame)
+########################################
+hrot_components_in = [
+    env.new('hrot_drift0',       xt.Drift,       length  = 1),
+    env.new('hshift_in',     xt.XYShift,     dx      = np.sin(XING_RAD) * L_SOL / 2),
+    env.new('hrot_in',       xt.YRotation,   angle   = -np.rad2deg(XING_RAD))]
+
+hrot_components_out = [
+    env.new('hrot_out',      xt.YRotation,   angle   = np.rad2deg(XING_RAD)),
+    env.new('hshift_out',    xt.XYShift,     dx      = np.sin(XING_RAD) * L_SOL / 2),
+    env.new('hrot_drift1',       xt.Drift,       length  = 1)]
+
+hrot_components_sol = [
+    env.new(f'hrot_sol.{i}',  xt.Solenoid,
+            length              = (L_SOL / N_SLICES) * np.cos(XING_RAD),
+            ks                  = 0,
+            knl                 = [K0 * (L_SOL / N_SLICES), 0, 0],
+            num_multipole_kicks = 1,
+            mult_rot_y_rad      = XING_RAD,
+            mult_shift_x        = np.sin(XING_RAD) * L_SOL * (i/N_SLICES - 1/2))
+    for i in range(N_SLICES)]
+
+hrot_line    = env.new_line(
+    components  = hrot_components_in + hrot_components_sol + hrot_components_out)
+
+########################################
+# Line (vertical rotated frame)
+########################################
+vrot_components_in = [
+    env.new('vrot_drift0',   xt.Drift,       length  = 1),
+    env.new('vshift_in',     xt.XYShift,     dy      = np.sin(XING_RAD) * L_SOL / 2),
+    env.new('vrot_in',       xt.XRotation,   angle   = np.rad2deg(XING_RAD))]
+# TODO: Minus sign difference here as still inconsistent definition with XRotation and YRotation
+vrot_components_out = [
+    env.new('vrot_out',      xt.XRotation,   angle   = -np.rad2deg(XING_RAD)),
+    env.new('vshift_out',    xt.XYShift,     dy      = np.sin(XING_RAD) * L_SOL / 2),
+    env.new('vrot_drift1',   xt.Drift,       length  = 1)]
+
+vrot_components_sol = [
+    env.new(f'vrot_sol.{i}',  xt.Solenoid,
+            length              = (L_SOL / N_SLICES) * np.cos(XING_RAD),
+            ks                  = 0,
+            knl                 = [K0 * (L_SOL / N_SLICES), 0, 0],
+            num_multipole_kicks = 1,
+            mult_rot_x_rad      = XING_RAD,
+            mult_shift_y        = np.sin(XING_RAD) * L_SOL * (i/N_SLICES - 1/2))
+    for i in range(N_SLICES)]
+
+vrot_line    = env.new_line(
+    components  = vrot_components_in + vrot_components_sol + vrot_components_out)
 
 ################################################################################
 # Comparisons
 ################################################################################
+bl_twiss   = bl_line.twiss(
+    method  = '4d',
+    start   = xt.START,
+    end     = xt.END,
+    betx    = BETX,
+    bety    = BETY)
 
-for test_element, test_sol, title in zip(
-    [bend, quad, sext], [bend_sol, quad_sol, sext_sol], ['Bend', 'Quadrupole', 'Sextupole']):
-    ########################################
-    # Build Lines
-    ########################################
-    line    = xt.Line(
-        elements        = [drift0] + [test_element] + [drift0],
-        particle_ref    = xt.Particles(p0c = 1E9, mass0 = xt.ELECTRON_MASS_EV))
-    line.configure_bend_model(edge = 'suppressed')
+hrot_twiss   = hrot_line.twiss(
+    method  = '4d',
+    start   = xt.START,
+    end     = xt.END,
+    betx    = BETX,
+    bety    = BETY)
 
-    sol_line   = xt.Line(
-        elements        = [drift1] + [test_sol] * N_SLICES + [drift1],
-        particle_ref    = xt.Particles(p0c = 1E9, mass0 = xt.ELECTRON_MASS_EV))
+vrot_twiss   = vrot_line.twiss(
+    method  = '4d',
+    start   = xt.START,
+    end     = xt.END,
+    betx    = BETX,
+    bety    = BETY)
 
-    # Slice test line
-    line.slice_thick_elements(
-        slicing_strategies=[
-            xt.Strategy(slicing = xt.Uniform(N_SLICES, mode='thin'), element_type = xt.Bend),
-            xt.Strategy(slicing = xt.Uniform(N_SLICES, mode='thin'), element_type = xt.Quadrupole),
-            xt.Strategy(slicing = xt.Uniform(N_SLICES, mode='thin'), element_type = xt.Sextupole)])
+bl_twiss.plot()
+plt.title('Beamline Frame')
+bl_twiss.plot('x y')
+plt.title('Beamline Frame')
 
-    ########################################
-    # Initialise Plot
-    ########################################
-    fig, axs    = plt.subplots(2, 2, figsize = (10, 10))
-    axs         = axs.flatten()
-    fig.suptitle(title)
+hrot_twiss.plot()
+plt.title('Horizontal Rotated Frame')
+hrot_twiss.plot('x y')
+plt.title('Horizontal Rotated Frame')
 
-    ########################################
-    # Test and plot with shifts
-    ########################################
-    for i, (rot_x, rot_y) in enumerate(
-        zip([0, X_ROT, 0, X_ROT],[0, 0, Y_ROT, Y_ROT])):
-        # TODO: No way to compare this currently?!?
-        test_sol.mult_rot_x = rot_x
-        test_sol.mult_rot_y = rot_y
+vrot_twiss.plot()
+plt.title('Vertical Rotated Frame')
+vrot_twiss.plot('x y')
+plt.title('Vertical Rotated Frame')
 
-        tw     = line.twiss(
-            _continue_if_lost = True,
-            start   = xt.START,
-            end     = xt.END,
-            betx    = BETX,
-            bety    = BETY,
-            px      = PX0)
-        tw_sol  = sol_line.twiss(
-            _continue_if_lost = True,
-            start   = xt.START,
-            end     = xt.END,
-            betx    = BETX,
-            bety    = BETY,
-            px      = PX0)
-
-        axs[i].plot(tw.s,       tw.x,       color = 'k', label = 'Element x')
-        axs[i].plot(tw.s,       tw.y,       color = 'r', label = 'Element y')
-        axs[i].plot(tw_sol.s,   tw_sol.x,   color = 'b', linestyle = ':', label = 'Sol x')
-        axs[i].plot(tw_sol.s,   tw_sol.y,   color = 'g', linestyle = ':', label = 'Sol y')
-
-        axs[i].set_title(f'Rot x = {rot_x} [TBD], Rot y = {rot_y} [TBD]')
-
-        ########################################
-        # Assertions
-        ########################################
-        assert np.isclose(tw.x[-1], tw_sol.x[-1], rtol = 1E-6)
-        assert np.isclose(tw.y[-1], tw_sol.y[-1], rtol = 1E-6)
-
-    ########################################
-    # Figure adjustments
-    ########################################
-    fig.legend(
-        labels = ['Element x', 'Element y', 'Sol x', 'Sol y'],
-        loc = 'upper center',
-        ncol = 4)
+################################################################################
+# Test Assertions
+################################################################################
+assert np.isclose(bl_twiss['x'][-1], hrot_twiss['x'][-1], rtol = 1E-6)
+assert np.isclose(bl_twiss['y'][-1], hrot_twiss['y'][-1], rtol = 1E-6)
+assert np.isclose(bl_twiss['x'][-1], vrot_twiss['x'][-1], rtol = 1E-6)
+assert np.isclose(bl_twiss['y'][-1], vrot_twiss['y'][-1], rtol = 1E-6)
 
 ########################################
 # Show Plots
