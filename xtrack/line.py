61a20893b790744da73fcab3e69e025fbd371f88
Philipp Niedermayer <eltos@outlook.de>
2024-12-02 21:28:45 +0000

    Fix https://github.com/xsuite/xplt/issues/31

--- xtrack/line.py
+++ xtrack/line.py
@@ -4057,6 +4057,8 @@ class Line:
             return self.vv[key]
         elif hasattr(self, 'lines') and key in self.lines: # Want to reuse the method for the env
             return self.lines[key]
+        elif "::" in key and (env_name := key.split("::")[0]) in self.element_dict:
+            return self[env_name]
         else:
             raise KeyError(f'Name {key} not found')
 
