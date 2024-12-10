3e34b63163cac4b8b7ef4d9ee363943823ad0e50
giadarol <giovanni.iadarola@cern.ch>
2024-12-10 16:06:41 +0100

    Also elements with edges are handled correctly

--- examples/slicing/000_dev.py
+++ examples/slicing/000_dev.py
@@ -2,15 +2,15 @@ import xtrack as xt
 
 env = xt.Environment()
 line = env.new_line(components=[
-    env.new('qf', xt.Quadrupole, length=2, k1=0.5),
+    env.new('el', xt.Bend, length=2, k1=0.5),
 ])
 
 line.slice_thick_elements(
     slicing_strategies=[
-        xt.Strategy(slicing=xt.Teapot(4, mode='thick'), element_type=xt.Quadrupole),
+        xt.Strategy(slicing=xt.Teapot(4, mode='thick'), element_type=xt.Bend),
     ])
 
 line.slice_thick_elements(
     slicing_strategies=[
-        xt.Strategy(slicing=xt.Teapot(4, mode='thin'), element_type=xt.ThickSliceQuadrupole),
+        xt.Strategy(slicing=xt.Teapot(4, mode='thin'), element_type=xt.ThickSliceBend),
     ])
