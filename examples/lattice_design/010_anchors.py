e3dabc45eff22b5f97dfaab1e38f669606335687
giadarol <giovanni.iadarola@cern.ch>
2024-12-26 10:44:52 -0300

    Need to work on sorting markers

--- examples/lattice_design/010_anchors.py
+++ examples/lattice_design/010_anchors.py
@@ -8,6 +8,10 @@ line = env.new_line(components=[
    env.new('q1', 'Quadrupole', length=2.0, anchor='start', at=1.),
    env.new('q2', 'q1', anchor='start', at=10., from_='q1', from_anchor='end'),
    env.new('s2', 'Sextupole', length=0.1, anchor='end', at=-1., from_='q2', from_anchor='start'),
+
+   env.new('m2', 'Marker', at=0., from_='q2', from_anchor='start'),
+   env.new('m2.0', 'Marker', at=0., from_='m2', from_anchor='start'),
+   env.new('m2.1', 'Marker', at=0., from_='m2', from_anchor='end'),
 ])
 
 line.get_table().show(cols=['name', 's_start', 's_end', 's_center'])
