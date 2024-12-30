9c6f50cf76c8e935da881d9010a1857d7f8d16f9
giadarol <giovanni.iadarola@cern.ch>
2024-12-30 13:02:31 -0300

    Made full collider

--- examples/legacy_multiline_to_environment/000_convert.py
+++ examples/legacy_multiline_to_environment/000_convert.py
@@ -2,30 +2,35 @@ import xtrack as xt
 
 dct = xt.json.load('hllhc15_collider_thick.json')
 
-single_line = xt.Line.from_json('../../test_data/hllhc15_thick/lhc_thick_with_knobs.json')
+lines = {}
 
-line_name = 'lhcb1'
-dct_line = dct['lines'][line_name].copy()
+for line_name in dct['lines']:
 
-new_man_data = []
-for ee in dct['_var_manager']:
-    new_ee = []
-    skip = False
-    for cc in ee:
-        if 'eref' in cc and f"eref['{line_name}']" not in cc:
-            skip = True
-            break
-        new_cc = cc.replace(f"eref['{line_name}']", 'element_refs')
-        new_ee.append(new_cc)
+    dct_line = dct['lines'][line_name].copy()
 
-    if skip:
-        continue
+    new_man_data = []
+    for ee in dct['_var_manager']:
+        new_ee = []
+        skip = False
+        for cc in ee:
+            if 'eref' in cc and f"eref['{line_name}']" not in cc:
+                skip = True
+                break
+            new_cc = cc.replace(f"eref['{line_name}']", 'element_refs')
+            new_ee.append(new_cc)
 
-    new_man_data.append(tuple(new_ee))
+        if skip:
+            continue
 
-dct_line['_var_management_data'] = {}
-dct_line['_var_management_data']['var_values'] = dct['_var_management_data']['var_values'].copy()
-dct_line['_var_manager'] = new_man_data
+        new_man_data.append(tuple(new_ee))
 
-# line without expressions
-line = xt.Line.from_dict(dct_line)
+    dct_line['_var_management_data'] = {}
+    dct_line['_var_management_data']['var_values'] = dct['_var_management_data']['var_values'].copy()
+    dct_line['_var_manager'] = new_man_data
+
+    line = xt.Line.from_dict(dct_line)
+
+    lines[line_name] = line
+
+env = xt.Environment(lines=lines)
+env.metadata.update(dct['metadata'])
