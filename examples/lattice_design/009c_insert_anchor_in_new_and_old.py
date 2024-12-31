049dc9f45878d8eb1debfc75f39ec64763bf064c
giadarol <giovanni.iadarola@cern.ch>
2024-12-30 21:29:48 -0300

    More checks

--- examples/lattice_design/009c_insert_anchor_in_new_and_old.py
+++ examples/lattice_design/009c_insert_anchor_in_new_and_old.py
@@ -22,8 +22,6 @@ line = env.new_line(
         env.new('q0', 'Quadrupole', length=2.0, at=20.0),
         env.new('ql', 'Quadrupole', length=2.0, at=10.0),
         env.new('qr', 'Quadrupole', length=2.0, at=30),
-        env.new('mk1', 'Marker', at=40),
-        env.new('mk2', 'Marker', at=42),
         env.new('end', 'Marker', at=50.),
     ])
 
@@ -37,7 +35,6 @@ line.insert([
     env.new('m5.3', 'Marker'),
 ])
 
-
 line.insert([
     env.new('q6', 'q0', at=0, from_='qr'),
     env.new('mr.0', 'Marker', at='start@qr'),
@@ -48,3 +45,12 @@ line.insert([
 
 tt = line.get_table()
 tt.show(cols=['name', 's_start', 's_end', 's_center'])
+
+assert np.all(tt.name == np.array(
+    ['drift_1', 'm5.0', 'm5.1', 'q5', 'm5.2', 'm5.3', 'drift_2',
+       'q0_entry', 'q0..0', 'q4', 'drift_3..1', 'mr.0', 'mr.1', 'q6',
+       'mr.2', 'mr.3', 'drift_4', 'end', '_end_point']))
+xo.assert_allclose(tt.s_center, np.array(
+    np.array([ 4.5,  9. ,  9. , 10. , 11. , 11. , 15. , 19. , 19.5, 21. , 25.5,
+       29. , 29. , 30. , 31. , 31. , 40.5, 50. , 50.])),
+    rtol=0., atol=1e-14)
