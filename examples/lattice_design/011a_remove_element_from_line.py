d9171c1227e5e2dede53427f6d3fbc9619d07682
giadarol <giovanni.iadarola@cern.ch>
2025-01-02 08:18:57 -0300

    This seems to work

--- examples/lattice_design/011a_remove_element_from_line.py
+++ examples/lattice_design/011a_remove_element_from_line.py
@@ -4,12 +4,18 @@ import numpy as np
 
 env = xt.Environment()
 
-line = env.new_line(
+line0 = env.new_line(
     components=[
         env.new('q0', 'Quadrupole', length=2.0, at=20.0),
         env.new('ql', 'Quadrupole', length=2.0, at=-10.0, from_='q0'),
         env.new('qr', 'Quadrupole', length=2.0, at=10.0, from_='q0'),
         env.new('mk1', 'Marker', at=40),
         env.new('mk2', 'Marker', at=42),
+        env.new('q0', 'Quadrupole'),
         env.new('end', 'Marker', at=50.),
     ])
+
+line1 = line0.copy()
+line1.remove('q0::1')
+tt1 = line1.get_table()
+tt1.show(cols=['name', 's_start', 's_end', 's_center'])
--- xtrack/line.py
+++ xtrack/line.py
@@ -2263,25 +2263,39 @@ class Line:
         tt = self.get_table()
         tt['idx'] = np.arange(len(tt))
 
-        idx_remove_name = tt.raws.indices[name]
-        idx_remove_env_name = tt.raws.indices[tt.env_name == name]
-        idx_remove = set(idx_remove_name) | set(idx_remove_env_name)
+        idx_remove_name = tt.rows.indices[name]
+        idx_remove_env_name = tt.rows.indices[tt.env_name == name]
+        idx_remove = list(set(idx_remove_name) | set(idx_remove_env_name))
+
+        if len(idx_remove) == 0:
+            raise ValueError(f'Element {name} not found in the line.')
+
         tt_remove = tt.rows[idx_remove]
 
-        mask_thick = tt.is_thick & (tt_remove.s_end - tt_remove.s_start) > s_tol
-        tt_remove_thick = tt_remove[mask_thick]
-        tt_remove_thin = tt_remove[~mask_thick]
+        mask_thick = tt_remove.isthick & (tt_remove.s_end - tt_remove.s_start > s_tol)
+        if mask_thick.any():
+            tt_remove_thick = tt_remove.rows[mask_thick]
+        else:
+            tt_remove_thick = None
+        if mask_thick.all():
+            tt_remove_thin = None
+        else:
+            tt_remove_thin = tt_remove.rows[~mask_thick]
 
-        for ii in range(len(tt_remove_thick)):
-            ll = tt_remove_thick['s_end', ii] - tt_remove_thick['s_start', ii]
-            idx = tt_remove_thick['idx', ii]
-            new_name = self.env._get_a_drift_name()
-            self.env.new(new_name, 'Drift', length=ll)
-            self.element_names[idx] = new_name
+        # Replace thick with drifts
+        if tt_remove_thick:
+            for ii in range(len(tt_remove_thick)):
+                ll = tt_remove_thick['s_end', ii] - tt_remove_thick['s_start', ii]
+                idx = tt_remove_thick['idx', ii]
+                new_name = self.env._get_a_drift_name()
+                self.env.new(new_name, 'Drift', length=ll)
+                self.element_names[idx] = new_name
 
-        idx_remove = tt_remove['idx']
-        self.element_names = [nn for ii, nn in enumerate(self.element_names)
-                              if ii not in idx_remove]
+        # Remove thin elements
+        if tt_remove_thin:
+            idx_remove = tt_remove_thin['idx']
+            self.element_names = [nn for ii, nn in enumerate(self.element_names)
+                                if ii not in idx_remove]
 
     def insert_element(self, name, element=None, at=None, index=None, at_s=None,
                        s_tol=1e-6):
