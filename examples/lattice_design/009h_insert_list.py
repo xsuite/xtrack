a0ac119c13deb73b2fab11deb3597d4f840a0b45
giadarol <giovanni.iadarola@cern.ch>
2025-01-01 22:07:05 -0300

    This does not work yet

--- examples/lattice_design/009h_insert_list.py
+++ examples/lattice_design/009h_insert_list.py
@@ -14,14 +14,11 @@ line = env.new_line(
         env.new('end', 'Marker', at=50.),
     ])
 
-ln_insert = env.new_line(
-    components=[
-        env.new('s1', 'Sextupole', length=0.1),
-        env.new('s2', 's1', anchor='start', at=0.3, from_='end@s1'),
-        env.new('s3', 's1', anchor='start', at=0.3, from_='end@s2')
-    ])
+env.new('s1', 'Sextupole', length=0.1)
+env.new('s2', 's1')
+env.new('s3', 's1')
 
-line.insert(ln_insert, anchor='start', at=1, from_='end@q0')
+line.insert(['s1', 's2', 's3'], anchor='start', at=1, from_='end@q0')
 
 tt = line.get_table()
 tt.show(cols=['name', 's_start', 's_end', 's_center'])
