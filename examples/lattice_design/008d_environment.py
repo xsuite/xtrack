c6ee3069194c8010c0560bbc603d081bce8b522b
giadarol <giovanni.iadarola@cern.ch>
2025-01-08 11:54:42 -0300

    Update example

--- examples/lattice_design/008d_environment.py
+++ examples/lattice_design/008d_environment.py
@@ -81,6 +81,25 @@ env['mq'].get_info('length')
 #
 #  element_refs['mq'].length does not influence any target
 
+# It is also possible to add to the environment elements that are instantiated by
+# the user:
+
+mysext = xt.Sextupole(length=1.0, k2=0.1)
+env.elements['ms'] = mysext
+
+env['ms'] # accesses the sextupole
+env.info('ms')
+
+# prints:
+# Element of type:  Sextupole
+# k2                  0.1                                     None
+# k2s                 0.0                                     None
+# length              1.0                                     None
+# order               5                                       None
+# inv_factorial_order 0.008333333333333333                    None
+# knl                 [0. 0. 0. 0. 0. 0.]                     None
+# ...
+
 #########
 # Lines #
 #########
