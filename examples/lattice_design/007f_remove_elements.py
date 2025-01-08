9beebd61d5a9c5b30903d7342e8bbe6ba3ec1291
giadarol <giovanni.iadarola@cern.ch>
2025-01-08 06:41:23 -0300

    Add example

--- examples/lattice_design/007f_remove_elements.py
+++ examples/lattice_design/007f_remove_elements.py
@@ -22,16 +22,34 @@ tt0.show(cols=['s_start', 's_center', 's_end'])
 # m0                    40            40            40
 # _end_point            40            40            40
 
-# Remove two elements
-line.remove('q1')
-line.remove('m0')
+# Create a set of new elements to be placed
+env.new('s1', xt.Sextupole, length=0.1, k2=0.2)
+env.new('s2', xt.Sextupole, length=0.1, k2=-0.2)
+env.new('m1', xt.Marker)
+env.new('m2', xt.Marker)
+env.new('m3', xt.Marker)
+
+# Insert the new elements in the line
+line.insert([
+    env.place('s1', at=5.),
+    env.place('s2', anchor='end', at=-5., from_='start@q1'),
+    env.place(['m1', 'm2'], at='start@m0'),
+    env.place('m3', at='end@m0'),
+    ])
 
 tt = line.get_table()
 tt.show(cols=['s_start', 's_center', 's_end'])
+# is:
 # name             s_start      s_center         s_end
-# drift_1                0           4.5             9
+# drift_1..0             0         2.475          4.95
+# s1                  4.95             5          5.05
+# drift_1..2          5.05         7.025             9
 # q0                     9            10            11
-# drift_2               11            15            19
-# drift_4               19            20            21
+# drift_2..0            11         12.45          13.9
+# s2                  13.9         13.95            14
+# drift_2..2            14          16.5            19
+# q1                    19            20            21
 # drift_3               21          30.5            40
-# _end_point            40            40            40
+# m1                    40            40            40
+# m0                    40            40            40
+# m3                    40            40            40
