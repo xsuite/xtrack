ee8ccae268bfeacd88705d991b4db98e71814cb8
giadarol <giovanni.iadarola@cern.ch>
2025-01-10 09:20:55 -0300

    Adapt example

--- examples/lattice_design/007d_insert_line.py
+++ examples/lattice_design/007d_insert_line.py
@@ -30,12 +30,12 @@ env.new('m3', xt.Marker)
 subline = env.new_line(components=[
     env.place('s1', at=1.0),
     env.place('s2', at=5.0),
-    env.place('m1', at='start@s1'),
-    env.place('m2', at='end@s2')])
+    env.place('m1', at='s1@start'),
+    env.place('m2', at='s2@start')])
 
 # Insert the new elements in the line
 line.insert([
-    env.place(subline, anchor='start', at=1., from_='end@q1'),
+    env.place(subline, anchor='start', at=1., from_='q1@end'),
     env.place('m3', at=30.)])
 
 tt = line.get_table()
@@ -45,16 +45,15 @@ tt.show(cols=['s_start', 's_center', 's_end'])
 # drift_1                0           4.5             9
 # q0                     9            10            11
 # drift_2               11            15            19
-# q1_entry              19            19            19
-# q1..0                 19          19.5            20
-# drift_4               20        20.475         20.95
-# m1                 20.95         20.95         20.95
-# s1                 20.95            21         21.05
-# drift_5            21.05          21.5         21.95
-# s2                 21.95            22         22.05
-# m2                 22.05         22.05         22.05
-# drift_3..3         22.05        26.025            30
+# q1                    19            20            21
+# drift_3..0            21          21.5            22
+# m1                    22            22            22
+# s1                    22            23            24
+# drift_4               24            25            26
+# m2                    26            26            26
+# s2                    26            27            28
+# drift_3..4            28            29            30
 # m3                    30            30            30
-# drift_3..4            30            40            50
+# drift_3..5            30            40            50
 # m0                    50            50            50
 # _end_point            50            50            50
