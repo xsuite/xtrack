0fa6a7db01400f4d7e5cf550170cf02504e4cb5c
giadarol <giovanni.iadarola@cern.ch>
2025-01-23 17:38:16 +0100

    Python FODO

--- examples/twiss/032_twiss_with_collective.py
+++ examples/twiss/032_twiss_with_collective.py
@@ -16,14 +16,31 @@ kick1 = MyKicker(kickx=1e-4)
 kick2 = MyKicker(kickx=-2e-4)
 kick3 = MyKicker(kickx=1e-4)
 
-
 line = env.new_line(components=[
     env.place('k1', kick1, at=0),
     env.place('k2', kick2, at=2),
     env.place('k3', kick3, at=4),
     ])
 
-line.twiss_default['include_collective'] = True
+tw = line.twiss(betx=1, bety=1, include_collective=True)
 
-tw = line.twiss(betx=1, bety=1)
 
+class MyQuad:
+    def __init__(self, k1l=0):
+        self.k1l = k1l
+
+    def track(self, p):
+        p.px += -self.k1l * p.x
+        p.py += self.k1l * p.y
+
+kq = 0.1
+qf = MyQuad(k1l=0.1)
+qd = MyQuad(k1l=-0.1)
+
+lfodo = env.new_line(components=[
+    env.place('qf', qf, at=0),
+    env.place('qd', qd, at=10),
+    env.new('end', 'Marker', at=20),
+])
+
+twfodo = lfodo.twiss4d(include_collective=True)
