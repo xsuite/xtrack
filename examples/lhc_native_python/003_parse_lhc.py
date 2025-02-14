0ce1d7d2d30c3478063b100919112fcd3524ce0d
giadarol <giovanni.iadarola@cern.ch>
2025-02-15 00:56:36 +0100

    Also builder is working

--- examples/lhc_native_python/003_parse_lhc.py
+++ examples/lhc_native_python/003_parse_lhc.py
@@ -28,8 +28,37 @@ for nn in env.lines.keys():
 
 new_env = xt.Environment(lines=all_lines)
 
+# Adapt builders
+for nn in env.lines.keys():
+    bb = env.lines[nn].builder.__class__(new_env)
+    bb.__dict__.update(env.lines[nn].builder.__dict__)
+    bb.env = new_env
+    this_rename = new_env.lines[nn]._renamed_elements
+    for cc in bb.components:
+        cc.name = this_rename.get(cc.name, cc.name)
+        cc.from_ = this_rename.get(cc.from_, cc.from_)
+
+    if nn in reverse_lines:
+        length = env.lines[nn].get_length()
+        bb.components = bb.components[::-1]
+        for cc in bb.components:
+            if cc.at is not None:
+                assert isinstance(cc.at, str) # Float still to be handled
+                if cc.from_ is not None:
+                    cc.at = f'-({cc.at})'
+                else:
+                    cc.at = f'({length} - {cc.at})'
+    new_env.lines[nn].builder = bb
+
+
 new_env.lhcb1.particle_ref = xt.Particles(p0c=7e12)
 new_env.lhcb2.particle_ref = xt.Particles(p0c=7e12)
 
 new_env.lhcb1.twiss4d().plot()
 new_env.lhcb2.twiss4d(reverse=True).plot()
+
+# Check builder
+new_env.lhcb2.builder.name = None # Not to overwrite the line
+lb2 = new_env.lhcb2.builder.build()
+lb2.particle_ref = xt.Particles(p0c=7e12)
+lb2.twiss4d(reverse=True).plot()
