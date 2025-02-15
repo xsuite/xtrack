de112d7ed35ccec657cf09f345baa5ae80738b56
giadarol <giovanni.iadarola@cern.ch>
2025-02-15 23:13:42 +0100

    Some renames

--- examples/lhc_native_python/001_inspect_lattice.py
+++
@@ -1,8 +0,0 @@
-import xtrack as xt
-
-env = xt.Environment()
-env.call('lhc_out.py')
-
-env.vars.load_madx('../../test_data/lhc_2024/injection_optics.madx')
-
-env.lhcb1.particle_ref = xt.Particles(mass0=xt.PROTON_MASS_EV, p0c=7e12)
