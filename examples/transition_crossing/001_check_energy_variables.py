1ca65c355b9e47ba74727d40b0fbb3099024a467
giadarol <giovanni.iadarola@cern.ch>
2025-02-27 12:57:21 +0100

    Clean up

--- examples/transition_crossing/001_check_energy_variables.py
+++ examples/transition_crossing/001_check_energy_variables.py
@@ -34,9 +34,12 @@ xo.assert_allclose(ptau, (energy - energy0) / energy0 / beta0, rtol=0, atol=1e-1
 xo.assert_allclose(pzeta,(energy - energy0) / energy0 / beta0**2, rtol=0, atol=1e-14)
 xo.assert_allclose(pzeta,(energy - energy0) / p0c / beta0, rtol=0, atol=1e-14)
 
+# Conversions
+xo.assert_allclose(delta, np.sqrt(ptau**2 + 2*ptau/beta0 + 1) - 1, rtol=0, atol=1e-14)
+xo.assert_allclose(delta, np.sqrt(beta0**2 * pzeta**2 + 2*pzeta + 1) - 1, rtol=0, atol=1e-14)
 xo.assert_allclose(delta, beta * ptau + (beta - beta0)/beta0, rtol=0, atol=1e-14)
 xo.assert_allclose(delta, beta * beta0 * pzeta + (beta - beta0)/beta0, rtol=0, atol=1e-14)
-xo.assert_allclose(delta, np.sqrt(ptau**2 + 2*ptau/beta0 + 1) - 1, rtol=0, atol=1e-14)
 
+# More relations from the physics manual
 xo.assert_allclose(gamma, gamma0 * (1 + beta0 * ptau), rtol=0, atol=1e-14)
 xo.assert_allclose(beta, np.sqrt(1 - (1 - beta0**2)/(1 + beta0 * ptau)**2), rtol=0, atol=1e-14)
