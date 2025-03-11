49c41fa04a5a42a4c1bdb71fc23d2416125085ba
giadarol <giovanni.iadarola@cern.ch>
2025-03-11 21:52:32 +0100

    More checks

--- examples/magnet/001_magnet.py
+++ examples/magnet/001_magnet.py
@@ -203,17 +203,22 @@ eref = xt.Bend(length=1.0, h=0.1, k1=0.3, k0=0.2)
 eref.num_multipole_kicks = 10
 eref.edge_entry_active = False
 eref.edge_exit_active = False
-eref.model = 'bend-kick-bend'
 
-p_test = p0.copy()
-p_ref = p0.copy()
 
-mm.track(p_test)
-eref.track(p_ref)
+for model in ['bend-kick-bend', 'rot-kick-rot', 'expanded']:
+    mm.model = model
+    eref.model = model
 
-xo.assert_allclose(p_test.x, p_ref.x, atol=1e-15, rtol=0)
-xo.assert_allclose(p_test.y, p_ref.y, atol=1e-15, rtol=0)
-xo.assert_allclose(p_test.zeta, p_ref.zeta, atol=1e-15, rtol=0)
-xo.assert_allclose(p_test.px, p_ref.px, atol=1e-15, rtol=0)
-xo.assert_allclose(p_test.py, p_ref.py, atol=1e-15, rtol=0)
-xo.assert_allclose(p_test.delta, p_ref.delta, atol=1e-15, rtol=0)
+    p_test = p0.copy()
+    p_ref = p0.copy()
+
+    mm.track(p_test)
+    eref.track(p_ref)
+
+    xo.assert_allclose(p_test.x, p_ref.x, atol=1e-15, rtol=0)
+    xo.assert_allclose(p_test.y, p_ref.y, atol=1e-15, rtol=0)
+    xo.assert_allclose(p_test.zeta, p_ref.zeta, atol=1e-15, rtol=0)
+    xo.assert_allclose(p_test.px, p_ref.px, atol=1e-15, rtol=0)
+    xo.assert_allclose(p_test.py, p_ref.py, atol=1e-15, rtol=0)
+    xo.assert_allclose(p_test.delta, p_ref.delta, atol=1e-15, rtol=0)
+
