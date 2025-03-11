afd7cdb4b7a6d480ad0ce99ed5fe0c959dd8e9fa
giadarol <giovanni.iadarola@cern.ch>
2025-03-11 11:33:05 +0100

    Some more untested code

--- xtrack/beam_elements/elements_src/track_magnet.h
+++ xtrack/beam_elements/elements_src/track_magnet.h
@@ -9,26 +9,20 @@
 H_TOLERANCE = 1e-8
 
 /*gpufun*/
-void track_magnet_drift_single_particle(
-    LocalParticle* part,
-    double length,
-    int64_t order,
-    double inv_factorial_order,
-    /*gpuglmem*/ const double* knl,
-    /*gpuglmem*/ const double* ksl,
-    double const factor_knl_ksl,
-    double kick_weight,
+void configure_tracking_model(
+    int64_t model,
     double k0,
     double k1,
-    double k2,
-    double k3,
-    double k0s,
-    double k1s,
-    double k2s,
-    double k3s,
     double h,
-    int64_t model
-) {
+    double* out_k0_drift,
+    double* out_k1_drift,
+    double* out_h_drift,
+    double* out_k0_kick,
+    double* out_k1_kick,
+    double* out_h_kick,
+    uint8_t* out_kick_rot_frame,
+    uint8_t* out_drift_model
+){
 
 
     // model = 0 or 1 : adaptive
@@ -121,11 +115,81 @@ void track_magnet_drift_single_particle(
         kick_rot_frame = 0;
     }
 
+    *out_k0_drift = k0_drift;
+    *out_k1_drift = k1_drift;
+    *out_h_drift = h_drift;
+    *out_k0_kick = k0_kick;
+    *out_k1_kick = k1_kick;
+    *out_h_kick = h_kick;
+    *out_kick_rot_frame = kick_rot_frame;
+    *out_drift_model = drift_model;
+
+}
 
 
+/*gpufun*/
+void track_magnet_body_single_particle(
+    LocalParticle* part,
+    double length,
+    int64_t order,
+    double inv_factorial_order,
+    /*gpuglmem*/ const double* knl,
+    /*gpuglmem*/ const double* ksl,
+    double const factor_knl_ksl,
+    double kick_weight,
+    int64_t num_multipole_kicks,
+    uint8_t kick_rot_frame,
+    uint8_t drift_model,
+    double k0_drift,
+    double k1_drift,
+    double h_drift,
+    double k0_kick,
+    double k1_kick,
+    double h_kick,
+    double k2,
+    double k3,
+    double k0s,
+    double k1s,
+    double k2s,
+    double k3s,
+) {
 
+    #define MAGNET_KICK(part, weight) \
+        track_magnet_kick_single_particle(\
+            part, length * (weight), order, inv_factorial_order, \
+            knl, ksl, factor_knl_ksl, (weight), \
+            k0_kick, k1_kick, k2, k3, k0s, k1s, k2s, k3s, h_kick, kick_rot_frame\
+        )
 
+    #define MAGNET_DRIFT(part, weight) \
+        track_magnet_drift_single_particle(\
+            part, length * (weight), k0_drift, k1_drift, h_drift, drift_model\
+        )
 
+    if (num_multipole_kicks == 0) { // auto mode
+        num_multipole_kicks = 1;
+    }
+    const double kick_weight = 1. / num_multipole_kicks;
+    double edge_drift_weight = 0.5;
+    double inside_drift_weight = 0;
+    if (num_multipole_kicks > 1) {
+        edge_drift_weight = 1. / (2 * (1 + num_multipole_kicks));
+        inside_drift_weight = (
+            ((float) num_multipole_kicks)
+                / ((float)(num_multipole_kicks*num_multipole_kicks) - 1));
+    }
+
+    // TEAPOT body
+    MAGNET_DRIFT(part, edge_drift_weight);
+    for (int i_kick=0; i_kick<num_multipole_kicks - 1; i_kick++) {
+        MAGNET_KICK(part, kick_weight);
+        MAGNET_DRIFT(part, inside_drift_weight);
+    }
+    MAGNET_KICK(part, kick_weight);
+    MAGNET_DRIFT(part, edge_drift_weight);
+
+    #undef MAGNET_KICK
+    #undef MAGNET_DRIFT
 
 }
 
