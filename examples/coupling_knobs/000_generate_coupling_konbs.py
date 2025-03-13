ae1cc66c772161219c3cd847e15e10a83cd01b23
giadarol <giovanni.iadarola@cern.ch>
2025-03-13 20:55:06 +0100

    Status

--- examples/coupling_knobs/000_generate_coupling_konbs.py
+++ examples/coupling_knobs/000_generate_coupling_konbs.py
@@ -1,21 +1,20 @@
 import numpy as np
 import xtrack as xt
 
-# Load a line and build tracker
+# Load a machine model
 line = xt.Line.from_json(
     '../../test_data/hllhc15_thick/lhc_thick_with_knobs.json')
 line.particle_ref = xt.Particles(mass0=xt.PROTON_MASS_EV, q0=1, energy0=7e12)
 line.cycle('ip1', inplace=True)
 line.twiss_default['method'] = '4d'
 
-
-# Circuits in non-ATS arcs
+# Select circuits with appropriate weights
 vary=[xt.VaryList(['kqs.a23b1', 'kqs.a67b1'], step=5e-5),
-      xt.VaryList(['kqs.l4b1', 'kqs.l8b1','kqs.r3b1', 'kqs.r7b1'], weight=2,
-                  step=5e-5)]
+      xt.VaryList(['kqs.l4b1', 'kqs.l8b1','kqs.r3b1', 'kqs.r7b1'],
+                  weight=2, step=5e-5)]
 
+# Match c_minus_re.b1
 c_min_match = 1e-4
-
 opt_re = line.match_knob(knob_name='c_minus_re.b1',
     knob_value_start=0, knob_value_end=c_min_match,
     run=False,
@@ -27,6 +26,7 @@ opt_re = line.match_knob(knob_name='c_minus_re.b1',
 opt_re.solve()
 opt_re.generate_knob()
 
+# Match c_minus_im.b1
 opt_im = line.match_knob(knob_name='c_minus_im.b1',
     knob_value_start=0, knob_value_end=c_min_match,
     run=False,
@@ -38,34 +38,14 @@ opt_im = line.match_knob(knob_name='c_minus_im.b1',
 opt_im.solve()
 opt_im.generate_knob()
 
-line.vars['c_minus_re.b1'] = 5e-3
-line.vars['c_minus_im.b1'] = 0
-tt_re = line.get_table(attr=True)
+# Test the knob
+line['c_minus_re.b1'] = 1e-3
+tw = line.twiss4d()
+tw.c_minus_re_0 # is 0.00099998
+tw.c_minus_im_0 # is 4.05e-09
 
-line.vars['c_minus_re.b1'] = 0
-line.vars['c_minus_im.b1'] = 5e-3
-tt_im = line.get_table(attr=True)
-
-# Check orthogonality
-line.vars['c_minus_re.b1'] = 1e-3
-line.vars['c_minus_im.b1'] = 1e-3
-assert np.isclose(line.twiss().c_minus/np.sqrt(2), 1e-3, rtol=0, atol=1.5e-5)
-
-
-import matplotlib.pyplot as plt
-plt.close('all')
-plt.figure(1)
-
-plt.figure(2)
-ax21 = plt.subplot(1, 1, 1)
-plt.plot(tt_re.s, tt_re['k1sl'], '.', label='c_minus_re.b1=1e-3')
-plt.plot(tt_im.s, tt_im['k1sl'], '.', label='c_minus_im.b1=1e-3')
-plt.ylabel('k1s * L')
-plt.xlabel('s [m]')
-plt.suptitle('Xsuite')
-plt.legend()
-
-plt.show()
-
-
-
+line['c_minus_re.b1'] = 0
+line['c_minus_im.b1'] = 1e-3
+tw = line.twiss4d()
+tw.c_minus_re_0 # is 2.16e-8
+tw.c_minus_im_0 # is 0.001000003
