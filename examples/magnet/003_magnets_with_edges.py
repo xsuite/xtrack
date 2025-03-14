4ae4d73a6da4e2915e8b65b0dc8906ccfe7f3d0c
giadarol <giovanni.iadarola@cern.ch>
2025-03-14 12:09:32 +0100

    It works

--- examples/magnet/003_magnets_with_edges.py
+++ examples/magnet/003_magnets_with_edges.py
@@ -106,7 +106,43 @@ p_test = p0.copy()
 p_ref = p0.copy()
 
 mm.track(p_test)
-print('----------------')
+bb.track(p_ref)
+
+xo.assert_allclose(p_test.x, p_ref.x, atol=1e-15, rtol=0)
+xo.assert_allclose(p_test.y, p_ref.y, atol=1e-15, rtol=0)
+xo.assert_allclose(p_test.zeta, p_ref.zeta, atol=1e-15, rtol=0)
+xo.assert_allclose(p_test.px, p_ref.px, atol=1e-15, rtol=0)
+xo.assert_allclose(p_test.py, p_ref.py, atol=1e-15, rtol=0)
+xo.assert_allclose(p_test.delta, p_ref.delta, atol=1e-15, rtol=0)
+
+# ===============================================================================
+# Test exit non-linear edge alone
+bb = xt.Bend(h=0.1, k0=0.11, length=0,
+             edge_entry_angle=0.02, edge_exit_angle=0.03,
+             edge_entry_hgap=0.04, edge_exit_hgap=0.05,
+             edge_entry_fint=0.1, edge_exit_fint=0.2)
+
+bb.edge_entry_active = 0
+bb.edge_exit_active = 1
+bb.model = 'rot-kick-rot'
+bb.num_multipole_kicks = 10
+bb.edge_exit_model = 'full'
+
+mm = Magnet(h=0.1, k0=0.11, length=0,
+                edge_entry_angle=0.02, edge_exit_angle=0.03,
+                edge_entry_hgap=0.04, edge_exit_hgap=0.05,
+                edge_entry_fint=0.1, edge_exit_fint=0.2)
+
+mm.edge_entry_active = 0
+mm.edge_exit_active = 1
+mm.model = 'rot-kick-rot'
+mm.num_multipole_kicks = 10
+mm.edge_exit_model = 'full'
+
+p_test = p0.copy()
+p_ref = p0.copy()
+
+mm.track(p_test)
 bb.track(p_ref)
 
 xo.assert_allclose(p_test.x, p_ref.x, atol=1e-15, rtol=0)
