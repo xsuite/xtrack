258971fefa16ed6c3a408f51a536dd96a99639a8
giadarol <giovanni.iadarola@cern.ch>
2025-03-18 11:31:39 +0100

    Understood issue, before the bend with no curvature set on "rot_kick_rot" was using the expanded drift while new implementation falls back on the exact one

--- xtrack/beam_elements/__init__.py
+++ xtrack/beam_elements/__init__.py
@@ -6,6 +6,7 @@
 from .elements import *
 from .exciter import Exciter
 from .apertures import *
+from .magnets import Magnet
 from .beam_interaction import BeamInteraction, ParticlesInjectionSample
 from .slice_elements import (ThinSliceQuadrupole, ThinSliceSextupole,
                              ThinSliceOctupole, ThinSliceBend,
