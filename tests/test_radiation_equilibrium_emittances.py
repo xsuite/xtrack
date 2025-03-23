5e0420d933fe98074b3fa4e5dc342e0cd5bd8776
giadarol <giovanni.iadarola@cern.ch>
2025-03-23 21:54:28 +0100

    Adapt test

--- tests/test_radiation_equilibrium_emittances.py
+++ tests/test_radiation_equilibrium_emittances.py
@@ -122,9 +122,9 @@ def test_eq_emitt(conf):
     # for regression testing
     checked = False
     if not tilt_machine_by_90_degrees and not vertical_orbit_distortion and not wiggler_on:
-        xo.assert_allclose(ex, 7.0592e-10, atol=0,     rtol=1e-4)
+        xo.assert_allclose(ex, 7.1357e-10, atol=0,     rtol=1e-4)
         xo.assert_allclose(ey, 0,          atol=1e-14, rtol=0)
-        xo.assert_allclose(ez, 3.6000e-6,  atol=0,     rtol=1e-4)
+        xo.assert_allclose(ez, 3.4595e-6,  atol=0,     rtol=1e-4)
         checked = True
     elif tilt_machine_by_90_degrees and not vertical_orbit_distortion and not wiggler_on:
         xo.assert_allclose(ex, 0,          atol=1e-14, rtol=0)
@@ -147,9 +147,9 @@ def test_eq_emitt(conf):
         xo.assert_allclose(ez, 3.5790e-6,  atol=0,     rtol=1e-4)
         checked = True
     elif tilt_machine_by_90_degrees and vertical_orbit_distortion and not wiggler_on:
-        xo.assert_allclose(ex, 2.3008e-12, atol=0,     rtol=5e-3)
-        xo.assert_allclose(ey, 7.0576e-10, atol=0,     rtol=1e-4)
-        xo.assert_allclose(ez, 3.5790e-6,  atol=0,     rtol=1e-4)
+        xo.assert_allclose(ex, 2.2071e-12, atol=0,     rtol=5e-3)
+        xo.assert_allclose(ey, 7.1345e-10, atol=0,     rtol=1e-4)
+        xo.assert_allclose(ez, 3.4433e-6,  atol=0,     rtol=1e-4)
         checked = True
     else:
         raise ValueError('Unknown configuration')
