b6b0402e5d4beb3ee089669f68b16cbe3b03744b
giadarol <giovanni.iadarola@cern.ch>
2025-03-26 22:36:13 +0100

    Preparing the ground

---
+++ 007_bpm_misalignments.py
@@ -0,0 +1,55 @@
+import xtrack as xt
+
+env = xt.Environment()
+env.particle_ref = xt.Particles(p0c=450e9, mass0=xt.PROTON_MASS_EV, q0=1)
+env.vars.default_to_zero = True
+
+env.new('mq', 'Quadrupole', length=0.8)
+env.new('bpm', 'Marker')
+env.new('corrector', 'Multipole', knl=[0])
+
+line = env.new_line(components=[
+
+    env.new('line.start', 'Marker'),
+    env.new('line.end', 'Marker', at=10.),
+
+    env.new('mq1', 'mq', k1='kq1', at=3.),
+    env.new('mq2', 'mq', k1='kq2', at=5.),
+    env.new('mq3', 'mq', k1='kq3', at=7.),
+    env.new('mq4', 'mq', k1='kq4', at=9.),
+
+    env.new('bpm1', 'bpm', at='mq1@start'),
+    env.new('bpm2', 'bpm', at='mq2@start'),
+    env.new('bpm3', 'bpm', at='mq3@start'),
+    env.new('bpm4', 'bpm', at='mq4@start'),
+
+    env.new('corrector1', 'corrector', at='mq1@start'),
+    env.new('corrector2', 'corrector', at='mq2@start'),
+    env.new('corrector3', 'corrector', at='mq3@start'),
+    env.new('corrector4', 'corrector', at='mq4@start'),
+])
+
+env['kq1'] = 0.02
+env['kq2'] = -0.02
+env['kq3'] = 0.02
+env['kq4'] = -0.02
+
+tw0 = line.twiss(betx=100, bety=80)
+
+env.set(['mq1', 'mq2', 'mq3', 'mq4'], shift_x=1e-3, shift_y=2e-3)
+
+tw = line.twiss(betx=100, bety=80, x=1e-3, y=2e-3)
+
+line.steering_monitors_x = ['bpm1', 'bpm2', 'bpm3', 'bpm4']
+line.steering_monitors_y = ['bpm1', 'bpm2', 'bpm3', 'bpm4']
+line.steering_correctors_x = ['corrector1', 'corrector3', ]
+line.steering_correctors_y = ['corrector2', 'corrector4']
+
+
+correction = line.correct_trajectory(twiss_table=tw0,
+                                     start='line.start', end='line.end',
+                                     run=False)
+
+correction.correct(n_iter=1)
+
+correction.x_correction._position_before
