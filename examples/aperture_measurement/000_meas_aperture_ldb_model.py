8ecb935f42491200783376e5aae649ed91dec786
giadarol <giovanni.iadarola@cern.ch>
2025-04-05 12:54:30 +0200

    Usable ldb model

---
+++ examples/aperture_measurement/000_meas_aperture_ldb_model.py
@@ -0,0 +1,65 @@
+import xtrack as xt
+import numpy as np
+import time
+
+from aperture_meas import measure_aperture
+
+aper_blacklist = [
+    'vtaf.51632.b_aper', 'vbrta.51633.a_aper', 'vbrta.51633.b_aper',
+       'bgiha.51634.a_aper', 'bgiva.51674.a_aper']
+
+env = xt.load_madx_lattice('EYETS 2024-2025.seq')
+env.vars.load_madx('lhc_q20.str')
+line = env.sps
+line.particle_ref = xt.Particles(mass0=xt.PROTON_MASS_EV, q0=1, p0c=26e9)
+
+tw0 = line.twiss4d()
+
+from cpymad.madx import Madx
+mad = Madx()
+mad.input('''
+SPS : SEQUENCE, refer = centre,    L = 7000;
+a: marker, at = 20;
+endsequence;
+''')
+mad.call('APERTURE_EYETS 2024-2025.seq')
+mad.beam()
+mad.use('SPS')
+line_aper = xt.Line.from_madx_sequence(mad.sequence.SPS, install_apertures=True)
+
+tt_aper = line_aper.get_table().rows['.*_aper']
+
+insertions = []
+for nn in tt_aper.name:
+    if nn in aper_blacklist:
+        continue
+    env.elements[nn] = line_aper.get(nn).copy()
+    insertions.append(env.place(nn, at=tt_aper['s', nn]))
+
+for ins in insertions:
+    if ins.name.endswith('.a_aper'):
+        ins.at += 1e-3
+    if ins.name.endswith('.b_aper'):
+        ins.at -= 1e-3
+
+line = env.sps
+line.insert(insertions)
+
+
+tw1 = line.twiss4d()
+aper = measure_aperture(line)
+
+
+import matplotlib.pyplot as plt
+plt.close('all')
+plt.figure(figsize=(10, 8))
+tw1.plot(lattice_only=True)
+plt.plot(aper.s, aper.x_aper_low, 'k-')
+plt.plot(aper.s, aper.x_aper_high, 'k-')
+plt.plot(aper.s, aper.x_aper_low_discrete, '.k')
+plt.plot(aper.s, aper.x_aper_high_discrete, '.k')
+plt.plot(aper.s, aper.y_aper_low, 'r-')
+plt.plot(aper.s, aper.y_aper_high, 'r-')
+plt.plot(aper.s, aper.y_aper_low_discrete, '.r')
+plt.plot(aper.s, aper.y_aper_high_discrete, '.r')
+plt.show()
