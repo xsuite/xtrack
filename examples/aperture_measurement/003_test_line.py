8bf72bf3a2755962a72426faaf83d5f5a8f70cfb
giadarol <giovanni.iadarola@cern.ch>
2025-04-08 15:06:52 +0200

    Status

--- examples/aperture_measurement/003_test_line.py
+++ examples/aperture_measurement/003_test_line.py
@@ -13,3 +13,13 @@ line = env.new_line(components=[
 line.particle_ref = xt.Particles(mass0=xt.PROTON_MASS_EV, q0=1, p0c=26e9)
 
 tt = line.get_aperture_table()
+#
+# Table: 7 rows, 10 cols
+# name                    s    x_aper_low   x_aper_high x_aper_low_discrete x_aper_high_discrete ...
+# drift_1                 0       -0.0505        0.0495                 nan                  nan
+# lrect                   3       -0.0505        0.0495             -0.0505               0.0495
+# lellipse::0             3       -0.0205        0.0195             -0.0205               0.0195
+# drift_2                 3       -0.0205        0.0195                 nan                  nan
+# m                       5       -0.0205        0.0195                 nan                  nan
+# lellipse::1             5       -0.0205        0.0195             -0.0205               0.0195
+# _end_point              5       -0.0205        0.0195                 nan                  nan
