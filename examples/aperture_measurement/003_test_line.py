b3ea5ffdb376d7332069aecf9118a7a673aa1199
giadarol <giovanni.iadarola@cern.ch>
2025-04-08 17:02:36 +0200

    Some asserts

--- examples/aperture_measurement/003_test_line.py
+++ examples/aperture_measurement/003_test_line.py
@@ -23,3 +23,29 @@ tt = line.get_aperture_table()
 # m                       5       -0.0205        0.0195                 nan                  nan
 # lellipse::1             5       -0.0205        0.0195             -0.0205               0.0195
 # _end_point              5       -0.0205        0.0195                 nan                  nan
+
+import numpy as np
+import xobjects as xo
+assert np.all(tt.name == ['drift_1', 'lrect', 'lellipse', 'drift_2', 'm', 'lellipse',
+       '_end_point'])
+xo.assert_allclose(tt.s, np.array([0., 3., 3., 3., 5., 5., 5.]), rtol=0, atol=1e-6)
+xo.assert_allclose(tt.x_aper_low, np.array([-0.0505, -0.0505, -0.0205, -0.0205, -0.0205,
+       -0.0205, -0.0205]), rtol=0, atol=1e-6)
+xo.assert_allclose(tt.x_aper_high, np.array([0.0495, 0.0495, 0.0195, 0.0195, 0.0195,
+       0.0195, 0.0195]), rtol=0, atol=1e-6)
+xo.assert_allclose(tt.x_aper_low_discrete, np.array([np.nan, -0.0505, -0.0205, np.nan,
+       np.nan, -0.0205, np.nan]), rtol=0, atol=1e-6)
+xo.assert_allclose(tt.x_aper_high_discrete, np.array([np.nan, 0.0495, 0.0195, np.nan,
+       np.nan, 0.0195, np.nan]), rtol=0, atol=1e-6)
+xo.assert_allclose(tt.y_aper_low, np.array(
+    [-0.0205, -0.0205, -0.0105, -0.0105, -0.0105, -0.0105, -0.0105],
+    dtype=float), rtol=0, atol=1e-6)
+xo.assert_allclose(tt.y_aper_high, np.array(
+    [0.0195, 0.0195, 0.0095, 0.0095, 0.0095, 0.0095, 0.0095],
+    dtype=float), rtol=0, atol=1e-6)
+xo.assert_allclose(tt.y_aper_low_discrete, np.array(
+    [np.nan, -0.0205, -0.0105, np.nan, np.nan, -0.0105, np.nan],
+    dtype=float), rtol=0, atol=1e-6)
+xo.assert_allclose(tt.y_aper_high_discrete, np.array(
+    [np.nan, 0.0195, 0.0095, np.nan, np.nan, 0.0095, np.nan],
+    dtype=float), rtol=0, atol=1e-6)
