2e53a35fefe47e0ce85f719b8b697fde6e1be348
giadarol <giovanni.iadarola@cern.ch>
2025-04-21 22:04:37 +0100

    Some more checks

--- examples/spin/005_check_spin_xrot_yrot.py
+++ examples/spin/005_check_spin_xrot_yrot.py
@@ -1,4 +1,5 @@
 import xtrack as xt
+import xobjects as xo
 import numpy as np
 
 env = xt.Environment()
@@ -6,7 +7,7 @@ env.particle_ref = xt.Particles(
     mass0=xt.ELECTRON_MASS_EV, q0=1, p0c=700e9,
     anomalous_magnetic_moment=0.00115965218128
 )
-breakpoint()
+
 line = env.new_line(length=1., components=[
     env.new('yrot', xt.YRotation, angle=12, at=0.2),
     env.new('myrot', xt.YRotation, angle=-12, at=0.4),
@@ -15,8 +16,24 @@ line = env.new_line(length=1., components=[
 tw = line.twiss(spin=True,
            betx=10,
            bety=10,
-           px=0.001,
-           spin_x=0.001)
+           spin_z=1.)
 
 assert np.all(tw.name == np.array(
     ['drift_1', 'yrot', 'drift_2', 'myrot', 'drift_3', '_end_point']))
+xo.assert_allclose(tw.s, np.array([0. , 0.2, 0.2, 0.4, 0.4, 1. ]), rtol=0, atol=1e-10)
+xo.assert_allclose(tw.px, np.array(
+    [ 0.,  0., -2.07911691e-01, -2.07911691e-01, 0.,  0.]), rtol=0, atol=1e-9)
+xo.assert_allclose(tw.spin_x, np.array(
+    [ 0.,  0., -2.07911691e-01, -2.07911691e-01, 0.,  0.]), rtol=0, atol=1e-9)
+xo.assert_allclose(tw.spin_y, 0, rtol=0, atol=1e-9)
+xo.assert_allclose(tw.spin_x**2 + tw.spin_y**2 + tw.spin_z**2, 1.,
+    rtol=0, atol=1e-9)
+
+tw = line.twiss(spin=True,
+           betx=10,
+           bety=10,
+           spin_y=1.)
+
+xo.assert_allclose(tw.spin_y, 1., rtol=0, atol=1e-9)
+xo.assert_allclose(tw.spin_x, 0., rtol=0, atol=1e-9)
+xo.assert_allclose(tw.spin_z, 0., rtol=0, atol=1e-9)
