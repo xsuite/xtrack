fcebaddfbccccecb5070b18612faefd18b8a8946
giadarol <giovanni.iadarola@cern.ch>
2025-04-22 22:41:09 +0200

    Spin also in SRotation

--- xtrack/beam_elements/elements_src/srotation.h
+++ xtrack/beam_elements/elements_src/srotation.h
@@ -20,6 +20,21 @@ void SRotation_track_local_particle(SRotationData el, LocalParticle* part0){
         SRotation_single_particle(part, sin_z, cos_z);
     //end_per_particle_block
 
+    /* Spin tracking is disabled by the synrad compile flag */
+    #ifndef XTRACK_MULTIPOLE_NO_SYNRAD
+        // Rotate spin
+        //start_per_particle_block (part0->part)
+            double const spin_x_0 = LocalParticle_get_spin_x(part);
+            double const spin_y_0 = LocalParticle_get_spin_y(part);
+            if ((spin_x_0 != 0) || (spin_y_0 != 0)){
+                double const spin_x_1 = cos_z*spin_x_0 + sin_z*spin_y_0;
+                double const spin_y_1 = -sin_z*spin_x_0 + cos_z*spin_y_0;
+                LocalParticle_set_spin_x(part, spin_x_1);
+                LocalParticle_set_spin_y(part, spin_y_1);
+            }
+        //end_per_particle_block
+    #endif
+
 }
 
 #endif /* XTRACK_SROTATION_H */
