66bc7f0524427fd9acfa433ec31a618b9907a2c7
giadarol <giovanni.iadarola@cern.ch>
2025-04-24 20:48:32 +0200

    Line with solenoids and correction knobs

--- examples/spin_lep/001_solenoid.py
+++ examples/spin_lep/001_solenoid.py
@@ -99,9 +99,135 @@ opt = line.match_knob(
 opt.run_jacobian(40)
 opt.generate_knob()
 
+line['on_sol.2'] = 0
+line['on_spin_bump.2'] = 0
+line['on_coupl_sol.2'] = 0
+line['on_coupl_sol_bump.2'] = 0
+
+# Correct coupling ip4 sol alone
+line['on_sol.4'] = 1
+opt = line.match_knob(
+    'on_coupl_sol.4',
+    run=False,
+    method='4d',
+    compute_chromatic_properties=False,
+    vary=xt.VaryList(['kqt5.l4', 'kqt4.4', 'kqt3.4','kqt2.4',
+                      'kqt1.l4', 'kqt1.r4', 'kqt5.r4'], step=1e-5),
+    targets=xt.TargetList(c_minus_re_0=0, c_minus_im_0=0, tol=1e-4),
+)
+opt.run_jacobian(30)
+opt.generate_knob()
+
+# Correct coupling ip4 sol and spin bump
+line['on_sol.4'] = 1
+line['on_spin_bump.4'] = 1
+line['on_coupl_sol.4'] = 1
+opt = line.match_knob(
+    'on_coupl_sol_bump.4',
+    run=False,
+    method='4d',
+    compute_chromatic_properties=False,
+    vary=xt.VaryList(['kqt5.l4', 'kqt4.4', 'kqt3.4','kqt2.4',
+                      'kqt1.l4', 'kqt1.r4', 'kqt5.r4'], step=1e-5),
+    targets=xt.TargetList(c_minus_re_0=0, c_minus_im_0=0, tol=5e-4),
+)
+opt.run_jacobian(30)
+opt.generate_knob()
+
+line['on_sol.4'] = 0
+line['on_spin_bump.4'] = 0
+line['on_coupl_sol.4'] = 0
+line['on_coupl_sol_bump.4'] = 0
+
+# Correct coupling ip6 sol alone
+line['on_sol.6'] = 1
+opt = line.match_knob(
+    'on_coupl_sol.6',
+    run=False,
+    method='4d',
+    compute_chromatic_properties=False,
+    vary=xt.VaryList(['kqt4.6', 'kqt3.6', 'kqt2.6', 'kqt1.l6', 'kqt1.r6'],
+                     step=1e-5),
+    targets=xt.TargetList(c_minus_re_0=0, c_minus_im_0=0, tol=1e-4),
+)
+opt.run_jacobian(30)
+opt.generate_knob()
+
+# Correct coupling ip6 sol and spin bump
+line['on_sol.6'] = 1
+line['on_spin_bump.6'] = 1
+line['on_coupl_sol.6'] = 1
+opt = line.match_knob(
+    'on_coupl_sol_bump.6',
+    run=False,
+    method='4d',
+    compute_chromatic_properties=False,
+    vary=xt.VaryList(['kqt4.6', 'kqt3.6', 'kqt2.6', 'kqt1.l6', 'kqt1.r6'],
+                     step=1e-5),
+    targets=xt.TargetList(c_minus_re_0=0, c_minus_im_0=0, tol=5e-4),
+)
+opt.run_jacobian(30)
+opt.generate_knob()
+
+line['on_sol.6'] = 0
+line['on_spin_bump.6'] = 0
+line['on_coupl_sol.6'] = 0
+line['on_coupl_sol_bump.6'] = 0
+
+# Correct coupling ip8 sol alone
+line['on_sol.8'] = 1
+opt = line.match_knob(
+    'on_coupl_sol.8',
+    run=False,
+    method='4d',
+    compute_chromatic_properties=False,
+    vary=xt.VaryList(['kqt5.l8', 'kqt4.8', 'kqt3.8', 'kqt2.8', 'kqt1.l8',
+                      'kqt1.r8', 'kqt5.r8'],
+                     step=1e-5),
+    targets=xt.TargetList(c_minus_re_0=0, c_minus_im_0=0, tol=1e-4),
+)
+opt.run_jacobian(30)
+opt.generate_knob()
+
+# Correct coupling ip8 sol and spin bump
+line['on_sol.8'] = 1
+line['on_spin_bump.8'] = 1
+line['on_coupl_sol.8'] = 1
+opt = line.match_knob(
+    'on_coupl_sol_bump.8',
+    run=False,
+    method='4d',
+    compute_chromatic_properties=False,
+    vary=xt.VaryList(['kqt5.l8', 'kqt4.8', 'kqt3.8', 'kqt2.8', 'kqt1.l8',
+                      'kqt1.r8', 'kqt5.r8'],
+                     step=1e-5),
+    targets=xt.TargetList(c_minus_re_0=0, c_minus_im_0=0, tol=5e-4),
+)
+opt.run_jacobian(30)
+opt.generate_knob()
+
+line['on_sol.8'] = 0
+line['on_spin_bump.8'] = 0
+line['on_coupl_sol.8'] = 0
+line['on_coupl_sol_bump.8'] = 0
+
+# All on
 line['on_sol.2'] = 1
+line['on_sol.4'] = 1
+line['on_sol.6'] = 1
+line['on_sol.8'] = 1
 line['on_spin_bump.2'] = 1
+line['on_spin_bump.4'] = 1
+line['on_spin_bump.6'] = 1
+line['on_spin_bump.8'] = 1
 line['on_coupl_sol.2'] = 1
+line['on_coupl_sol.4'] = 1
+line['on_coupl_sol.6'] = 1
+line['on_coupl_sol.8'] = 1
 line['on_coupl_sol_bump.2'] = 1
+line['on_coupl_sol_bump.4'] = 1
+line['on_coupl_sol_bump.6'] = 1
+line['on_coupl_sol_bump.8'] = 1
 tw = line.twiss4d(spin=True, radiation_integrals=True)
 
+line.to_json('lep_sol.json')
