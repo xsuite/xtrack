5a61b625de8882c12d7ff75c565cee06e1514e56
giadarol <giovanni.iadarola@cern.ch>
2025-04-24 22:54:43 +0200

    Still pol is too low

--- examples/spin_lep/002_spin_tracking.py
+++ examples/spin_lep/002_spin_tracking.py
@@ -3,7 +3,7 @@ import xpart as xp
 import xobjects as xo
 import numpy as np
 
-num_turns = 10000
+num_turns = 500
 
 line = xt.Line.from_json('lep_sol.json')
 line.particle_ref.anomalous_magnetic_moment=0.00115965218128
@@ -147,13 +147,88 @@ line.configure_radiation(model=None)
 line.discard_tracker()
 line.build_tracker(_context=xo.ContextCpu(omp_num_threads=0))
 
+from scipy.constants import e as qe
+from scipy.constants import c as clight
+from scipy.constants import hbar
+
+for ttww in [tw_bare, tw_sol, tw_sol_bump]:
+
+    kappa_x = ttww.rad_int_kappa_x
+    kappa_y = ttww.rad_int_kappa_y
+    kappa = ttww.rad_int_kappa
+    iv_x = ttww.rad_int_iv_x
+    iv_y = ttww.rad_int_iv_y
+    iv_z = ttww.rad_int_iv_z
+
+    n0_iv = ttww.spin_x * iv_x + ttww.spin_y * iv_y + ttww.spin_z * iv_z
+    r0 = ttww.particle_on_co.get_classical_particle_radius0()
+    m0_J = ttww.particle_on_co.mass0 * qe
+    m0_kg = m0_J / clight**2
+
+    # reference https://lib-extopc.kek.jp/preprints/PDF/1980/8011/8011060.pdf
+
+    alpha_plus_co = 1. / ttww.circumference * np.sum(
+        kappa**3 * (1 - 2./9. * n0_iv**2) * ttww.length)
+
+    tp_inv = 5 * np.sqrt(3) / 8 * r0 * hbar * ttww.gamma0**5 / m0_kg * alpha_plus_co
+    tp_s = 1 / tp_inv
+    tp_turn = tp_s / ttww.T_rev0
+
+    brho_ref = ttww.particle_on_co.p0c[0] / clight / ttww.particle_on_co.q0
+    brho_part = (brho_ref * ttww.particle_on_co.rvv[0] * ttww.particle_on_co.energy[0]
+                / ttww.particle_on_co.energy0[0])
+
+    By = kappa_x * brho_part
+    Bx = -kappa_y * brho_part
+    Bz = ttww.ks * brho_ref
+    B_mod = np.sqrt(Bx**2 + By**2 + Bz**2)
+    B_mod[B_mod == 0] = 999. # avoid division by zero
+
+    ib_x = Bx / B_mod
+    ib_y = By / B_mod
+    ib_z = Bz / B_mod
+
+    n0_ib = ttww.spin_x * ib_x + ttww.spin_y * ib_y + ttww.spin_z * ib_z
+
+    alpha_minus_co = 1. / ttww.circumference * np.sum(kappa**3 * n0_ib *  ttww.length)
+
+    pol_inf = 8 / 5 / np.sqrt(3) * alpha_minus_co / alpha_plus_co
+
+    ttww._data['alpha_plus_co'] = alpha_plus_co
+    ttww._data['alpha_minus_co'] = alpha_minus_co
+    ttww._data['pol_inf'] = pol_inf
+    ttww['n0_ib'] = n0_ib
+    ttww['t_pol_turn'] = tp_turn
+
+# Fit depolarization time (linear fit)
+from scipy.stats import linregress
+def fit_depolarization_time(turns, pol):
+    # Remove NaN values
+    mask = ~np.isnan(pol)
+    turns = turns[mask]
+    pol = pol[mask]
+
+    # Perform linear regression
+    slope, intercept, r_value, p_value, std_err = linregress(turns, np.log(pol))
+
+    # Calculate depolarization time
+    depolarization_time = -1 / slope
+
+    return depolarization_time
+
+tw_bare._data['t_dep_turn'] = fit_depolarization_time(np.arange(num_turns), pol_bare)
+tw_sol._data['t_dep_turn'] = fit_depolarization_time(np.arange(num_turns), pol_sol)
+tw_sol_bump._data['t_dep_turn']= fit_depolarization_time(np.arange(num_turns), pol_sol_bump)
+
+for ttww in [tw_bare, tw_sol, tw_sol_bump]:
+    ttww._data['pol'] = ttww['pol_inf'] * (1 / (1 + ttww['t_pol_turn'] / ttww['t_dep_turn']))
 
 import matplotlib.pyplot as plt
 plt.close('all')
 plt.figure(1)
-plt.plot(pol_bare, label='bare')
-plt.plot(pol_sol, label='sol')
-plt.plot(pol_sol_bump, label='sol & bump')
+plt.plot(pol_bare, label=r'bare - $P_{eq}$ = ' f'{tw_bare["pol"]:.2f}')
+plt.plot(pol_sol, label=r'sol - $P_{eq}$ = ' f'{tw_sol["pol"]:.2f}')
+plt.plot(pol_sol_bump, label=r'sol & bump - $P_{eq}$ = ' f'{tw_sol_bump["pol"]:.2f}')
 
 plt.xlabel('turns')
 plt.ylabel('polarization')
