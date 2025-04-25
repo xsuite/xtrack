da9a9f3a53b271fe118ba292cddcffe70b771525
giadarol <giovanni.iadarola@cern.ch>
2025-04-25 23:19:36 +0200

    We have the matrix

--- examples/spin_lep/005_slim.py
+++ examples/spin_lep/005_slim.py
@@ -52,33 +52,70 @@ p0.spin_z = np.array([l0[2], n0[2], m0[2]])
 line.track(p0, num_turns=1, turn_by_turn_monitor='ONE_TURN_EBE')
 mon0 = line.record_last_track
 
-ll = np.zeros((3, len(tw)))
-mm = np.zeros((3, len(tw)))
-nn = np.zeros((3, len(tw)))
+# ll = np.zeros((3, len(tw)))
+# mm = np.zeros((3, len(tw)))
+# nn = np.zeros((3, len(tw)))
 
-ll[0, :] = mon0.spin_x[0, :]
-ll[1, :] = mon0.spin_y[0, :]
-ll[2, :] = mon0.spin_z[0, :]
-nn[0, :] = mon0.spin_x[1, :]
-nn[1, :] = mon0.spin_y[1, :]
-nn[2, :] = mon0.spin_z[1, :]
-mm[0, :] = mon0.spin_x[2, :]
-mm[1, :] = mon0.spin_y[2, :]
-mm[2, :] = mon0.spin_z[2, :]
+# ll[0, :] = mon0.spin_x[0, :]
+# ll[1, :] = mon0.spin_y[0, :]
+# ll[2, :] = mon0.spin_z[0, :]
+# nn[0, :] = mon0.spin_x[1, :]
+# nn[1, :] = mon0.spin_y[1, :]
+# nn[2, :] = mon0.spin_z[1, :]
+# mm[0, :] = mon0.spin_x[2, :]
+# mm[1, :] = mon0.spin_y[2, :]
+# mm[2, :] = mon0.spin_z[2, :]
 
 out = line.compute_one_turn_matrix_finite_differences(particle_on_co=tw.particle_on_co,
                                                       element_by_element=True)
 mon_r_ebe = out['mon_ebe']
+part = out['part_temp']
 
-mon_alpha = mon_r_ebe.spin_x * 0
-mon_beta = mon_r_ebe.spin_x * 0
+spin = np.zeros((3, len(part.spin_x)))
+spin[0, :] = part.spin_x
+spin[1, :] = part.spin_y
+spin[2, :] = part.spin_z
 
-for ipart in range(mon_alpha.shape[0]):
+alpha = np.zeros(len(part.spin_x))
+beta = np.zeros(len(part.spin_x))
+for ii in range(len(part.spin_x)):
+    alpha[ii] = np.dot(spin[:, ii], l0)
+    beta[ii] = np.dot(spin[:, ii], m0)
 
-    spin_part = np.zeros((3, mon_alpha.shape[1]))
-    spin_part[0, :] = mon_r_ebe.spin_x[ipart, :]
-    spin_part[1, :] = mon_r_ebe.spin_y[ipart, :]
-    spin_part[2, :] = mon_r_ebe.spin_z[ipart, :]
 
-    mon_alpha[ipart, :] = np.sum(spin_part * ll, axis=0)
-    mon_beta[ipart, :] = np.sum(spin_part * mm, axis=0)
+steps_r_matrix = out['steps_r_matrix']
+
+dx = steps_r_matrix["dx"]
+dpx = steps_r_matrix["dpx"]
+dy = steps_r_matrix["dy"]
+dpy = steps_r_matrix["dpy"]
+dzeta = steps_r_matrix["dzeta"]
+ddelta = steps_r_matrix["ddelta"]
+
+dpzeta = float(part.ptau[6] - part.ptau[12])/2/part.beta0[0]
+
+temp_mat = np.zeros((2, len(part.spin_x)))
+temp_mat[0, :] = alpha
+temp_mat[1, :] = beta
+
+DD = np.zeros((2, 6))
+
+for jj, dd in enumerate([dx, dpx, dy, dpy, dzeta, dpzeta]):
+    DD[:, jj] = (temp_mat[:, jj+1] - temp_mat[:, jj+1+6])/(2*dd)
+
+RR = np.eye(8)
+RR[:6, :6] = out['R_matrix']
+RR[6:, :6] = DD
+
+def symplectic_matrix(n):
+
+    assert n%2 == 0, "n must be even"
+    n = int(n)
+    M = np.zeros((n, n))
+    for i in range(n//2):
+        M[2*i, 2*i+1] = 1
+        M[2*i+1, 2*i] = -1
+
+    return M
+
+S = symplectic_matrix(8)
