ef11cfcc07cfc6993917f9ca2795dfa61e6c2041
giadarol <giovanni.iadarola@cern.ch>
2025-05-01 15:53:15 +0200

    Status

--- examples/spin_lep/007d_pol_refactor.py
+++ examples/spin_lep/007d_pol_refactor.py
@@ -286,78 +286,8 @@ i_sorted = np.argsort(norm_orbital_part)
 v0 = eivec[:, i_sorted[3:]]
 w0 = eival[i_sorted[3:]]
 
-a0 = np.real(v0)
-b0 = np.imag(v0)
 
-index_list = [0,5,1,2,3,4] # we mix them up to check the algorithm
-
-##### Sort modes in pairs of conjugate modes #####
-conj_modes = np.zeros([3,2], dtype=np.int64)
-for j in [0,1]:
-    conj_modes[j,0] = index_list[0]
-    del index_list[0]
-
-    min_index = 0
-    min_diff = abs(np.imag(w0[conj_modes[j,0]] + w0[index_list[min_index]]))
-    for i in range(1,len(index_list)):
-        diff = abs(np.imag(w0[conj_modes[j,0]] + w0[index_list[i]]))
-        if min_diff > diff:
-            min_diff = diff
-            min_index = i
-
-    conj_modes[j,1] = index_list[min_index]
-    del index_list[min_index]
-
-conj_modes[2,0] = index_list[0]
-conj_modes[2,1] = index_list[1]
-
-modes = [conj_modes[0,0], conj_modes[1,0], conj_modes[2,0]]
-
-# ##################################################
-# #### Select mode from pairs with positive (real @ S @ imag) #####
-
-# modes = np.empty(3, dtype=np.int64)
-# for ii,ind in enumerate(conj_modes):
-#     if np.matmul(np.matmul(a0[:6,ind[0]], S), b0[:6,ind[0]]) > 0:
-#         modes[ii] = ind[0]
-#     else:
-#         modes[ii] = ind[1]
-
-# ##################################################
-# #### Sort modes such that (1,2,3) is close to (x,y,zeta) ####
-# # Identify the longitudinal mode
-# for i in [0,1]:
-#     if abs(v0[:,modes[2]])[5] < abs(v0[:,modes[i]])[5]:
-#         modes[2], modes[i] = modes[i], modes[2]
-
-# # Identify the vertical mode
-# if abs(v0[:,modes[1]])[2] < abs(v0[:,modes[0]])[2]:
-#     modes[0], modes[1] = modes[1], modes[0]
-
-# a1 = v0[:6, modes[0]].real
-# a2 = v0[:6, modes[1]].real
-# a3 = v0[:6, modes[2]].real
-# b1 = v0[:6, modes[0]].imag
-# b2 = v0[:6, modes[1]].imag
-# b3 = v0[:6, modes[2]].imag
-
-# n1_inv_sq = np.abs(np.matmul(np.matmul(a1, S), b1))
-# n2_inv_sq = np.abs(np.matmul(np.matmul(a2, S), b2))
-# n3_inv_sq = np.abs(np.matmul(np.matmul(a3, S), b3))
-
-# n1 = 1./np.sqrt(n1_inv_sq)
-# n2 = 1./np.sqrt(n2_inv_sq)
-# n3 = 1./np.sqrt(n3_inv_sq)
-
-# n1 = 1
-# n2 = 1
-# n3 = 1
-
-# e1 = v0[:, modes[0]] * n1
-# e2 = v0[:, modes[1]] * n2
-# e3 = v0[:, modes[2]] * n3
-
-
+# Scale and track eigenvectors
 eee = v0.copy()
 n_eigen = eee.shape[1]
 
