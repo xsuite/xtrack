9d97db53c22d85fd10ed7f5ba72c89029997cd98
giadarol <giovanni.iadarola@cern.ch>
2025-06-25 18:47:39 +0200

    Seems to work

--- examples/solenoid_new/000_dev.py
+++ examples/solenoid_new/000_dev.py
@@ -1,3 +1,15 @@
 import xtrack as xt
 
-sol = xt.Slnd()
+length = 3.
+ks = 2.
+
+sol = xt.Slnd(length=length, ks=ks)
+ref_sol = xt.Solenoid(length=length, ks=ks)
+
+p0 = xt.Particles(p0c=1e9, x=1e-3, y=2e-3)
+
+p = p0.copy()
+p_ref = p0.copy()
+
+sol.track(p)
+ref_sol.track(p_ref)
