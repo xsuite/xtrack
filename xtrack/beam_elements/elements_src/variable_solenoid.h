d96636ab97713cc75292d5e2b6ca67bd5d28e6c4
giadarol <giovanni.iadarola@cern.ch>
2025-06-27 22:31:06 +0200

    Drafted variable solenoid, untested

---
+++ xtrack/beam_elements/elements_src/variable_solenoid.h
@@ -0,0 +1,83 @@
+// copyright ############################### //
+// This file is part of the Xtrack Package.  //
+// Copyright (c) CERN, 2023.                 //
+// ######################################### //
+
+#ifndef XTRACK_VARIABLESOLENOID_H
+#define XTRACK_VARIABLESOLENOID_H
+
+#include <headers/track.h>
+#include <beam_elements/elements_src/track_magnet.h>
+
+GPUFUN
+void VariableSolenoid_track_local_particle(
+        VariableSolenoidData el,
+        LocalParticle* part0
+) {
+    int64_t integrator = VariableSolenoidData_get_integrator(el);
+    int64_t num_multipole_kicks = VariableSolenoidData_get_num_multipole_kicks(el);
+
+    if (integrator == 0) {  // adaptive
+        integrator = 3;  // uniform
+    }
+    if (num_multipole_kicks == 0) {
+        num_multipole_kicks = 1;
+    }
+
+    double const ks_entry = VariableSolenoidData_get_ks(el, 0);
+    double const ks_exit = VariableSolenoidData_get_ks(el, 1);
+    double const length = VariableSolenoidData_get_length(el);
+
+    double ks, dks_ds;
+    if (length != 0){
+        ks = 0.5 * (ks_exit + ks_entry);
+        dks_ds = (ks_exit - ks_entry) / length;
+    }
+    else {
+        ks = 0.;
+        dks_ds = 0.;
+    }
+
+
+    track_magnet_particles(
+        /*part0*/                 part0,
+        /*length*/                length,
+        /*order*/                 VariableSolenoidData_get_order(el),
+        /*inv_factorial_order*/   VariableSolenoidData_get_inv_factorial_order(el),
+        /*knl*/                   VariableSolenoidData_getp1_knl(el, 0),
+        /*ksl*/                   VariableSolenoidData_getp1_ksl(el, 0),
+        /*factor_knl_ksl*/        1.,
+        /*num_multipole_kicks*/   num_multipole_kicks,
+        /*model*/                 -2, // sol-kick-sol
+        /*integrator*/            integrator,
+        /*radiation_flag*/        VariableSolenoidData_get_radiation_flag(el),
+        /*radiation_record*/      NULL,
+        /*delta_taper*/           VariableSolenoidData_get_delta_taper(el),
+        /*h*/                     0.,
+        /*hxl*/                   0.,
+        /*k0*/                    0.,
+        /*k1*/                    0.,
+        /*k2*/                    0.,
+        /*k3*/                    0.,
+        /*k0s*/                   0.,
+        /*k1s*/                   0.,
+        /*k2s*/                   0.,
+        /*k3s*/                   0.,
+        /*ks*/                    ks,
+        /*dks_ds*/                dks_ds,
+        /*edge_entry_active*/     0,
+        /*edge_exit_active*/      0,
+        /*edge_entry_model*/      1,
+        /*edge_exit_model*/       1,
+        /*edge_entry_angle*/      0.,
+        /*edge_exit_angle*/       0.,
+        /*edge_entry_angle_fdown*/0.,
+        /*edge_exit_angle_fdown*/ 0.,
+        /*edge_entry_fint*/       0.,
+        /*edge_exit_fint*/        0.,
+        /*edge_entry_hgap*/       0.,
+        /*edge_exit_hgap*/        0.
+    );
+}
+
+#endif // XTRACK_OCTUPOLE_H
