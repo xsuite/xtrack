b2472cf9b0799066d80f4dabe53cdbc789f9eae9
giadarol <giovanni.iadarola@cern.ch>
2025-07-04 18:42:36 +0200

    Adapt example

--- tests/test_line.py
+++ tests/test_line.py
@@ -880,6 +880,7 @@ def test_insert_thin_elements_at_s_lhc(test_context):
     Teapot = xt.Teapot
     slicing_strategies = [
         Strategy(slicing=Teapot(1)),  # Default catch-all as in MAD-X
+        Strategy(slicing=None, element_type=xt.UniformSolenoid),
         Strategy(slicing=Teapot(4), element_type=xt.Bend),
         Strategy(slicing=Teapot(20), element_type=xt.Quadrupole),
         Strategy(slicing=Teapot(2), name=r'^mb\..*'),
