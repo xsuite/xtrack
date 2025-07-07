d20211846eee3738bb9546933a6fd7fcc91a69b2
giadarol <giovanni.iadarola@cern.ch>
2025-07-07 09:47:12 +0200

    Some more checks

--- examples/solenoid_new/000_dev.py
+++ examples/solenoid_new/000_dev.py
@@ -8,6 +8,7 @@ ks = 2.
 sol = xt.UniformSolenoid(length=length, ks=ks)
 ref_sol = xt.Solenoid(length=length, ks=ks) # Old solenoid
 
+
 p0 = xt.Particles(p0c=1e9, x=1e-3, y=2e-3)
 
 p = p0.copy()
@@ -40,3 +41,30 @@ xo.assert_allclose(p.ay, p_ref.ay, rtol=0, atol=1e-10)
 xo.assert_allclose(p.kin_px, p_ref.kin_px, rtol=0, atol=1e-10)
 xo.assert_allclose(p.kin_py, p_ref.kin_py, rtol=0, atol=1e-10)
 
+p0_for_backtrack = p.copy()
+p_for_backtrack = p0_for_backtrack.copy()
+lsol = xt.Line([sol])
+
+lsol.track(p_for_backtrack, backtrack=True)
+xo.assert_allclose(p_for_backtrack.x, p0.x, rtol=0, atol=1e-10)
+xo.assert_allclose(p_for_backtrack.y, p0.y, rtol=0, atol=1e-10)
+xo.assert_allclose(p_for_backtrack.px, p0.px, rtol=0, atol=1e-10)
+xo.assert_allclose(p_for_backtrack.py, p0.py, rtol=0, atol=1e-10)
+xo.assert_allclose(p_for_backtrack.delta, p0.delta, rtol=0, atol=1e-10)
+xo.assert_allclose(p_for_backtrack.ax, 0., rtol=0, atol=1e-10)
+xo.assert_allclose(p_for_backtrack.ay, 0., rtol=0, atol=1e-10)
+xo.assert_allclose(p_for_backtrack.kin_px, p0.px, rtol=0, atol=1e-10)
+xo.assert_allclose(p_for_backtrack.kin_py, p0.py, rtol=0, atol=1e-10)
+
+sol.edge_entry_active = False
+p_for_backtrack = p0_for_backtrack.copy()
+lsol.track(p_for_backtrack, backtrack=True)
+xo.assert_allclose(p_for_backtrack.x, p0.x, rtol=0, atol=1e-10)
+xo.assert_allclose(p_for_backtrack.y, p0.y, rtol=0, atol=1e-10)
+xo.assert_allclose(p_for_backtrack.px, p0.px, rtol=0, atol=1e-10)
+xo.assert_allclose(p_for_backtrack.py, p0.py, rtol=0, atol=1e-10)
+xo.assert_allclose(p_for_backtrack.delta, p0.delta, rtol=0, atol=1e-10)
+xo.assert_allclose(p_for_backtrack.ax, -ks /2 *p0.y, rtol=0, atol=1e-10)
+xo.assert_allclose(p_for_backtrack.ay, ks /2 *p0.x, rtol=0, atol=1e-10)
+xo.assert_allclose(p_for_backtrack.kin_px, p0.px + ks /2 *p0.y, rtol=0, atol=1e-10)
+xo.assert_allclose(p_for_backtrack.kin_py, p0.py - ks /2 *p0.x, rtol=0, atol=1e-10)
