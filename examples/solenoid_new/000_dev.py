afba42e02eb5bb804a3aba32422f42e324c25e83
giadarol <giovanni.iadarola@cern.ch>
2025-07-07 15:54:42 +0200

    Update checks

--- examples/solenoid_new/000_dev.py
+++ examples/solenoid_new/000_dev.py
@@ -144,6 +144,6 @@ xo.assert_allclose(tw_back.ax, tw.ax, rtol=0, atol=1e-10)
 xo.assert_allclose(tw_back.ay, tw.ay, rtol=0, atol=1e-10)
 
 sol.edge_entry_active = False
-tw_back = lsol_sliced.twiss(init=tw.get_twiss_init('e0_entry'))
+tw_back = lsol_sliced.twiss(init=tw.get_twiss_init('e0_exit'))
 tw_back['ax'] = tw_back.px - tw_back.kin_px
 tw_back['ay'] = tw_back.py - tw_back.kin_py
