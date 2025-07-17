c9a0950f1710895c8e17d16f3b53b255e8cbb264
Silke <silkevdschueren@gmail.com>
2025-07-17 16:11:11 +0200

    adding quadrupole wedge - in progress

--- xtrack/beam_elements/elements_src/track_magnet_edge.h
+++ xtrack/beam_elements/elements_src/track_magnet_edge.h
@@ -113,6 +113,9 @@ void track_magnet_edge_particles(
         #define MAGNET_WEDGE(PART) \
             if (should_rotate) Wedge_single_particle((PART), -face_angle, knorm[0])
 
+        #define MAGNET_QUAD_WEDGE(PART) \
+            if (should_rotate) Quad_wedge_single_particle((PART), -face_angle, kn[1])
+
         if (is_exit == 0){ // entry
             START_PER_PARTICLE_BLOCK(part0, part);
                 MAGNET_Y_ROTATE(part);
@@ -121,10 +124,16 @@ void track_magnet_edge_particles(
                     MAGNET_MULTIPOLE_FRINGE(part);
                 }
                 MAGNET_WEDGE(part);
+                if (model == 1){
+                    MAGNET_QUAD_WEDGE(part);
+                }
             END_PER_PARTICLE_BLOCK;
         }
         else { // exit
             START_PER_PARTICLE_BLOCK(part0, part);
+                if (model == 1){
+                    MAGNET_QUAD_WEDGE(part);
+                }
                 MAGNET_WEDGE(part);
                 if (model == 1){
                     MAGNET_MULTIPOLE_FRINGE(part);
@@ -138,6 +147,7 @@ void track_magnet_edge_particles(
         #undef MAGNET_DIPOLE_FRINGE
         #undef MAGNET_MULTIPOLE_FRINGE
         #undef MAGNET_WEDGE
+        #undef MAGNET_QUAD_WEDGE
     }
     else if (model == 3) { // only ax ay cancellation (already done above)
         // do nothing
