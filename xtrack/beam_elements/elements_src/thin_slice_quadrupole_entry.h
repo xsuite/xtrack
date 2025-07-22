381adb0390f9bbf4cbf8434ca48d61406be0c7d7
giadarol <giovanni.iadarola@cern.ch>
2025-07-22 23:32:48 +0200

    Adapt one edge

--- xtrack/beam_elements/elements_src/thin_slice_quadrupole_entry.h
+++ xtrack/beam_elements/elements_src/thin_slice_quadrupole_entry.h
@@ -6,7 +6,7 @@
 #define XTRACK_THIN_SLICE_QUADRUPOLE_ENTRY_H
 
 #include <headers/track.h>
-#include <beam_elements/elements_src/track_magnet_edge.h>
+#include <beam_elements/elements_src/track_magnet.h>
 
 GPUFUN
 void ThinSliceQuadrupoleEntry_track_local_particle(
@@ -14,45 +14,46 @@ void ThinSliceQuadrupoleEntry_track_local_particle(
         LocalParticle* part0
 ) {
 
-    const int64_t edge_entry_active = ThinSliceQuadrupoleEntryData_get__parent_edge_entry_active(el);
-
-    if (edge_entry_active){
-
-        double const k1 = ThinSliceQuadrupoleEntryData_get__parent_k1(el);
-        double const k1s = ThinSliceQuadrupoleEntryData_get__parent_k1s(el);
-
-        double const knorm[2] = {0, k1};
-        double const kskew[2] = {0, k1s};
-
-        // Backtracking
-        #ifdef XSUITE_BACKTRACK
-            const int64_t is_exit = 1;
-            const double factor_backtrack_edge = -1.;
-        #else
-            const int64_t is_exit = 0;
-            const double factor_backtrack_edge = 1.;
-        #endif
-
-        track_magnet_edge_particles(
-            part0,
-            1, // model
-            is_exit,
-            0, // half_gap,
-            knorm, // knorm,
-            kskew, // kskew,
-            1, // k_order,
-            NULL, // knl - not considered in edge for now!
-            NULL, // ksl - not considered in edge for now!
-            0, // factor_knl_ksl,
-            -1, // kl_order,
-            0., //ksol,
-            0., // length, - not needed if no knl ksl
-            0., // face_angle,
-            0., // face_angle_feed_down,
-            0., // fringe_integral,
-            factor_backtrack_edge // factor_for_backtrack
-        );
-    } // end edge entry
+    track_magnet_particles(
+        /*part0*/                 part0,
+        /*length*/                ThinSliceQuadrupoleData_get__parent_length(el),
+        /*order*/                 ThinSliceQuadrupoleData_get__parent_order(el),
+        /*inv_factorial_order*/   ThinSliceQuadrupoleData_get__parent_inv_factorial_order(el),
+        /*knl*/                   ThinSliceQuadrupoleData_get__parentp1_knl(el, 0),
+        /*ksl*/                   ThinSliceQuadrupoleData_get__parentp1_ksl(el, 0),
+        /*factor_knl_ksl*/        1.,
+        /*num_multipole_kicks*/   num_multipole_kicks,
+        /*model*/                 model,
+        /*integrator*/            integrator,
+        /*radiation_flag*/        ThinSliceQuadrupoleData_get__parent_radiation_flag(el),
+        /*radiation_record*/      NULL,
+        /*delta_taper*/           ThinSliceQuadrupoleData_get__parent_delta_taper(el),
+        /*h*/                     0.,
+        /*hxl*/                   0.,
+        /*k0*/                    0.,
+        /*k1*/                    ThinSliceQuadrupoleData_get__parent_k1(el),
+        /*k2*/                    0.,
+        /*k3*/                    0.,
+        /*k0s*/                   0.,
+        /*k1s*/                   ThinSliceQuadrupoleData_get__parent_k1s(el),
+        /*k2s*/                   0.,
+        /*k3s*/                   0.,
+        /*ks*/                    0.,
+        /*dks_ds*/                0.,
+        /*body_active*/           0,
+        /*edge_entry_active*/     ThinSliceQuadrupoleData_get__parent_edge_entry_active(el),
+        /*edge_exit_active*/      ThinSliceQuadrupoleData_get__parent_edge_exit_active(el),
+        /*edge_entry_model*/      1,
+        /*edge_exit_model*/       1,
+        /*edge_entry_angle*/      0.,
+        /*edge_exit_angle*/       0.,
+        /*edge_entry_angle_fdown*/0.,
+        /*edge_exit_angle_fdown*/ 0.,
+        /*edge_entry_fint*/       0.,
+        /*edge_exit_fint*/        0.,
+        /*edge_entry_hgap*/       0.,
+        /*edge_exit_hgap*/        0.
+    );
 
 }
 
