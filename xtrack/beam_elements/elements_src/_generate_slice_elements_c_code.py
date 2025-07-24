e3d18bfd8ac1a051ce32b37a074cee99dae94f4d
giadarol <giovanni.iadarola@cern.ch>
2025-07-24 12:36:19 +0200

    Working on generation

--- xtrack/beam_elements/elements_src/_generate_slice_elements_c_code.py
+++ xtrack/beam_elements/elements_src/_generate_slice_elements_c_code.py
@@ -40,7 +40,35 @@ out_content = out_content.replace(parent_class, generated_class)
 generated_data_class = generated_class + "Data"
 
 assert generated_data_class + '_get_' in out_content
-out_content = out_content.replace(generated_data_class + '_get_', generated_class + '_get_parent__')
+out_content = out_content.replace(generated_data_class + '_get_', generated_data_class + '_get_parent__')
 
 assert generated_data_class + '_getp1_' in out_content
-out_content = out_content.replace(generated_data_class + '_getp1_', generated_class + '_getp1_parent__')
+out_content = out_content.replace(generated_data_class + '_getp1_', generated_data_class + '_getp1_parent__')
+
+# delta_taper must come from the slice and not from the parent
+assert "_get_parent__delta_taper" in out_content
+out_content = out_content.replace("_get_parent__delta_taper", "_get_delta_taper")
+
+# Handle "radiation_flag" and "radiation_flag_parent"
+out_lines = out_content.splitlines()
+
+# Identify the line with "/*radiation_flag*/"
+i_radiation_flag_line = None
+for i, line in enumerate(out_lines):
+    if "/*radiation_flag*/" in line:
+        i_radiation_flag_line = i
+        break
+assert i_radiation_flag_line is not None, "Could not find radiation_flag line"
+i_parent_line = i_radiation_flag_line + 1
+
+assert "get_parent__radiation_flag" in out_lines[i_radiation_flag_line]
+out_lines[i_radiation_flag_line] = out_lines[i_radiation_flag_line].replace(
+    "get_parent__radiation_flag", "get_radiation_flag")
+
+
+ln_parent_radiation_flag = out_lines[i_parent_line]
+assert "0" in ln_parent_radiation_flag, "Expected '0' in radiation_flag line"
+ln_parent_radiation_flag = ln_parent_radiation_flag.split('0')[0]
+out_lines[i_parent_line] = ln_parent_radiation_flag + generated_data_class + "_get_parent__radiation_flag(el),"
+
+out_content = "\n".join(out_lines)
