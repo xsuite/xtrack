3136ec56e7ccb31e35b16467aef111580f7f25b9
giadarol <giovanni.iadarola@cern.ch>
2025-07-25 08:49:45 +0200

    Investigating fringe kick...

--- examples/true_rbend/000_dev.py
+++ examples/true_rbend/000_dev.py
@@ -1,4 +1,7 @@
 import xtrack as xt
+import numpy as np
+
+# TODO, check backtrack
 
 b_ref = xt.RBend(angle=0.1, k0_from_h=True, length_straight=3.)
 lref = xt.Line([b_ref])
@@ -14,3 +17,8 @@ b_test.edge_exit_model = 'full'
 l_test = xt.Line([b_test])
 l_test.particle_ref = xt.Particles(p0c=10e9)
 tw_test = l_test.twiss(betx=1, bety=1)
+
+l_sliced = l_test.copy(shallow=True)
+l_sliced.cut_at_s(np.linspace(0, l_test.get_length(), 10))
+tw_sliced = l_sliced.twiss(betx=1, bety=1)
+
