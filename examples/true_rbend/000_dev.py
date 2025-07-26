6ec6497afb5281def6af27e20e3d6ee21d4f3e48
giadarol <giovanni.iadarola@cern.ch>
2025-07-26 08:29:07 +0200

    More asserts

--- examples/true_rbend/000_dev.py
+++ examples/true_rbend/000_dev.py
@@ -27,17 +27,23 @@ tw_test = l_test.twiss(betx=1, bety=1)
 
 xo.assert_allclose(tw_ref.betx, tw_test.betx, rtol=1e-5, atol=0.0)
 xo.assert_allclose(tw_ref.bety, tw_test.bety, rtol=1e-5, atol=0.0)
-xo.assert_allclose(tw_ref.x, tw_test.x, rtol=0, atol=1e-12)
-xo.assert_allclose(tw_ref.y, tw_test.y, rtol=0, atol=1e-12)
-xo.assert_allclose(tw_ref.s, tw_test.s, rtol=0, atol=1e-12)
+xo.assert_allclose(tw_ref.x,    tw_test.x, rtol=0, atol=1e-12)
+xo.assert_allclose(tw_ref.y,    tw_test.y, rtol=0, atol=1e-12)
+xo.assert_allclose(tw_ref.s,    tw_test.s, rtol=0, atol=1e-12)
 xo.assert_allclose(tw_ref.zeta, tw_test.zeta, rtol=0, atol=1e-11)
-xo.assert_allclose(tw_ref.px, tw_test.px, rtol=0, atol=1e-12)
-xo.assert_allclose(tw_ref.py, tw_test.py, rtol=0, atol=1e-12)
-
+xo.assert_allclose(tw_ref.px,   tw_test.px, rtol=0, atol=1e-12)
+xo.assert_allclose(tw_ref.py,   tw_test.py, rtol=0, atol=1e-12)
 
 l_sliced = l_test.copy(shallow=True)
 l_sliced.cut_at_s(np.linspace(0, l_test.get_length(), 10))
 tw_test_sliced = l_sliced.twiss(betx=1, bety=1)
 
-
+xo.assert_allclose(tw_test_sliced.betx[-1], tw_test.betx[-1], rtol=1e-5, atol=0.0)
+xo.assert_allclose(tw_test_sliced.bety[-1], tw_test.bety[-1], rtol=1e-5, atol=0.0)
+xo.assert_allclose(tw_test_sliced.x[-1],    tw_test.x[-1], rtol=0, atol=1e-12)
+xo.assert_allclose(tw_test_sliced.y[-1],    tw_test.y[-1], rtol=0, atol=1e-12)
+xo.assert_allclose(tw_test_sliced.s[-1],    tw_test.s[-1], rtol=0, atol=1e-12)
+xo.assert_allclose(tw_test_sliced.zeta[-1], tw_test.zeta[-1], rtol=0, atol=1e-11)
+xo.assert_allclose(tw_test_sliced.px[-1],   tw_test.px[-1], rtol=0, atol=1e-12)
+xo.assert_allclose(tw_test_sliced.py[-1],   tw_test.py[-1], rtol=0, atol=1e-12)
 
