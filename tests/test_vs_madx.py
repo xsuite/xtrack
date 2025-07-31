9fec4a1992fffb9fd728046c2c6d4f41a53fa1dc
giadarol <giovanni.iadarola@cern.ch>
2025-07-31 16:39:39 +0200

    Adapt test

--- tests/test_vs_madx.py
+++ tests/test_vs_madx.py
@@ -353,7 +353,7 @@ def test_twiss_and_survey(
                     # For now not checking the sign of the angles, convetion in mad-X to be calrified
                     xo.assert_allclose(np.abs(survxt.angle[ixt-1]),
                             np.abs(survmad['angle'][imad]), atol=1e-10)
-                    xo.assert_allclose(survxt.tilt[ixt-1], survmad['tilt'][imad], atol=1e-10)
+                    xo.assert_allclose(survxt.rot_s_rad[ixt-1], survmad['tilt'][imad], atol=1e-10)
 
         # Check to_pandas (not extensively for now)
         dftw = twtst.to_pandas()
