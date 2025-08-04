3d5850e72a936100ce91480245e55c6d673c10b7
giadarol <giovanni.iadarola@cern.ch>
2025-08-04 18:05:04 +0200

    Clean up example

--- examples/true_rbend/000_dev.py
+++ examples/true_rbend/000_dev.py
@@ -16,10 +16,11 @@ b_ref.edge_entry_model = edge_model
 b_ref.edge_exit_model = edge_model
 b_ref.model = 'rot-kick-rot'
 b_ref.num_multipole_kicks = 100
-lref = xt.Line([b_ref])
-lref.particle_ref = xt.Particles(p0c=10e9)
-tw_ref0 = lref.twiss(betx=1, bety=1)
-tw_ref = lref.twiss(betx=1, bety=1, x=2e-3, px=1e-3, y=2e-3, py=2e-3, delta=1e-3)
+l_ref = xt.Line([b_ref])
+l_ref.append('end', xt.Marker())
+l_ref.particle_ref = xt.Particles(p0c=10e9)
+tw_ref0 = l_ref.twiss(betx=1, bety=1)
+tw_ref = l_ref.twiss(betx=1, bety=1, x=2e-3, px=1e-3, y=2e-3, py=2e-3, delta=1e-3)
 
 b_test = xt.RBend(
     angle=0.1, k0_from_h=True, length_straight=3)
@@ -29,6 +30,7 @@ b_test.num_multipole_kicks = 100
 b_test.edge_entry_model = edge_model
 b_test.edge_exit_model = edge_model
 l_test = xt.Line([b_test])
+l_test.append('end', xt.Marker())
 l_test.particle_ref = xt.Particles(p0c=10e9)
 tw_test0 = l_test.twiss(betx=1, bety=1)
 tw_test = l_test.twiss(betx=1, bety=1, x=2e-3, px=1e-3, y=2e-3, py=2e-3, delta=1e-3)
