906c12914a331320efad023c8e71ef979b502e72
giadarol <giovanni.iadarola@cern.ch>
2025-08-08 21:30:30 +0200

    Clean up examples

--- examples/spin_lep/000a_build_model.py
+++ examples/spin_lep/000a_build_model.py
@@ -8,7 +8,7 @@ mad.use('lep')
 
 line = xt.Line.from_madx_sequence(mad.sequence.lep, deferred_expressions=True,
                                   enable_thick_kickers=True)
-line.vars.load_madx('../../test_data/lep/n6060pol70v5.str')
+line.vars.load('../../test_data/lep/n6060pol70v5.str')
 line.particle_ref = xt.Particles(
     mass0=xt.ELECTRON_MASS_EV, q0=1, energy0=45.6e9,
     anomalous_magnetic_moment=0.00115965218128
