9209a6744d2040ec06deb2d0e5af6cce3186a3fa
giadarol <giovanni.iadarola@cern.ch>
2025-08-11 22:39:38 +0200

    Cleaner solution

--- xtrack/base_element.py
+++ xtrack/base_element.py
@@ -341,21 +341,26 @@ class MetaBeamElement(xo.MetaHybridClass):
 
         data_in = data.copy()
         data = {}
-        is_old_thing = False
-        for bb in bases: # Patch to support old-style "beam element" inheritance used in xcoll
-            if bb.__name__ != 'BeamElement' and'BeamElement' in set(cc.__name__ for cc in bb.__mro__):
-                is_old_thing = True
-                break
-        if not is_old_thing: # Presently we assume that none of the parents is inheriting from BeamElement
-            for bb in bases:
-                if bb.__name__ == 'HybridClass':
+        for bb in bases:
+            if bb.__name__ == 'HybridClass':
+                continue
+            if bb.__name__ == 'BeamElement':
+                continue
+            for kk, vv in bb.__dict__.items():
+                if kk.startswith('__') or kk in data_in.keys():
                     continue
-                if bb.__name__ == 'BeamElement':
-                    continue
-                for kk, vv in bb.__dict__.items():
-                    if kk.startswith('__') or kk in data_in.keys():
-                        continue
-                    data[kk] = vv
+                data[kk] = vv
+
+        # If inheriting _extra_c_sources, remove get_record function
+        if '_extra_c_sources' in data:
+            ii_remove = None
+            for ii, ss in enumerate(data['_extra_c_sources']):
+                if isinstance(ss, str) and '/*---GENERATED GET RECORD FUNCTION---*/' in ss:
+                   ii_remove = ii
+                   break
+            if ii_remove is not None:
+                data['_extra_c_sources'].pop(ii_remove)
+
         data.update(data_in)
 
         # Take xofields from data['_xofields'] or from bases
