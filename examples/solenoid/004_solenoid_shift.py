5004f960c01f47433c828db50a66cb2b91c0521a
giadarol <giovanni.iadarola@cern.ch>
2025-08-13 20:58:29 +0200

    Check also backtrack

--- examples/solenoid/004_solenoid_shift.py
+++ examples/solenoid/004_solenoid_shift.py
@@ -4,12 +4,15 @@ import xobjects as xo
 
 # TODO:
 # - field for radiation and spin
+# - check backtrack
+# - check variable solenoid
 
 env = xt.Environment()
 env.particle_ref = xt.Particles(mass0=xt.ELECTRON_MASS_EV, p0c=20e9)
 
 line_ref = env.new_line(components=[
     env.new('sol_ref',xt.UniformSolenoid, length=3, ks=0.2),
+    env.new('end', xt.Marker)
     ])
 line_ref_thick = line_ref.copy(shallow=True)
 line_ref.cut_at_s(np.linspace(0, 3, 5))
@@ -20,6 +23,7 @@ x0 = 0.05
 y0 = 0.15
 line_test = env.new_line(components=[
     env.new('solt_test', xt.UniformSolenoid, length=3, ks=0.2, x0=x0, y0=y0),
+    env.place('end')
     ])
 line_test_thick = line_test.copy(shallow=True)
 line_test.cut_at_s(np.linspace(0, 3, 5))
@@ -46,3 +50,17 @@ xo.assert_allclose(tw_test_thick.px[-1], tw_test.px[-1], rtol=0, atol=1e-14)
 xo.assert_allclose(tw_test_thick.py[-1], tw_test.py[-1], rtol=0, atol=1e-14)
 xo.assert_allclose(tw_test_thick.kin_px[-1], tw_test.kin_px[-1], rtol=0, atol=1e-14)
 xo.assert_allclose(tw_test_thick.kin_py[-1], tw_test.kin_py[-1], rtol=0, atol=1e-14)
+
+tw_ref_back = line_ref.twiss(init=tw_ref, init_at='end')
+tw_test_back = line_test.twiss(init=tw_test, init_at='end')
+tw_ref_thick_back = line_ref_thick.twiss(init=tw_ref_thick, init_at='end')
+tw_test_thick_back = line_test_thick.twiss(init=tw_test_thick, init_at='end')
+
+for ttest, tref in zip([tw_ref_back, tw_test_back, tw_ref_thick_back, tw_test_thick_back],
+                       [tw_ref, tw_test, tw_ref_thick, tw_test_thick]):
+    xo.assert_allclose(ttest.x, tref.x, rtol=0, atol=1e-14)
+    xo.assert_allclose(ttest.y, tref.y, rtol=0, atol=1e-14)
+    xo.assert_allclose(ttest.px, tref.px, rtol=0, atol=1e-14)
+    xo.assert_allclose(ttest.py, tref.py, rtol=0, atol=1e-14)
+    xo.assert_allclose(ttest.kin_px, tref.kin_px, rtol=0, atol=1e-14)
+    xo.assert_allclose(ttest.kin_py, tref.kin_py, rtol=0, atol=1e-14)
