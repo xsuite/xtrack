2fc1c822b4beeb4be42532a98dadb0e01d2c49ea
giadarol <giovanni.iadarola@cern.ch>
2025-08-20 09:07:09 +0200

    Adapt test

--- tests/test_madloader.py
+++ tests/test_madloader.py
@@ -626,34 +626,19 @@ def test_mad_elements_import():
         assert line['r0'].frequency == 100e6
         assert line['r0'].lag == 180
 
-        assert isinstance(line['cb0'], xt.RFMultipole)
+        assert isinstance(line['cb0'], xt.CrabCavity)
         assert line.get_s_position('cb0') == 0.41
-        assert len(line['cb0'].knl) == 6
-        assert len(line['cb0'].ksl) == 6
-        xo.assert_allclose(line['cb0'].knl[0], 2 * 1e6 / line.particle_ref.p0c[0],
+        xo.assert_allclose(line['cb0'].voltage, 2 * 1e6,
                            rtol=0, atol=1e-12)
-        assert np.all(line['cb0'].ksl == 0)
-        assert np.all(line['cb0'].pn[0] == np.array([270]))
-        assert np.all(line['cb0'].ps == 0.)
-        assert line['cb0'].voltage == 0
-        assert line['cb0'].order == 5
+        assert np.all(line['cb0'].lag == 180)
         assert line['cb0'].frequency == 100e6
-        assert line['cb0'].lag == 0
 
-        assert isinstance(line['cb1'], xt.RFMultipole)
+        assert isinstance(line['cb1'], xt.CrabCavity)
         assert line.get_s_position('cb1') == 0.42
-        assert len(line['cb1'].knl) == 6
-        assert len(line['cb1'].ksl) == 6
-        xo.assert_allclose(line['cb1'].knl[0], 2 * 1e6 / line.particle_ref.p0c[0],
-                           rtol=0, atol=1e-12)
+        assert line['cb1'].voltage == 2 * 1e6
+        assert np.all(line['cb1'].lag == 180)
         xo.assert_allclose(line['cb1'].rot_s_rad, np.pi / 2, rtol=0, atol=1e-12)
-        assert np.all(line['cb1'].ksl == 0)
-        assert np.all(line['cb1'].pn[0] == np.array([270]))
-        assert np.all(line['cb1'].ps == 0.)
-        assert line['cb1'].voltage == 0
-        assert line['cb1'].order == 5
         assert line['cb1'].frequency == 100e6
-        assert line['cb1'].lag == 0
 
         assert isinstance(line['w'], xt.Wire)
         assert line.get_s_position('w') == 1
