913b017ebd660f48300602d264673d6fc6b37c69
giadarol <giovanni.iadarola@cern.ch>
2025-08-20 22:27:20 +0200

    Undo

--- xtrack/environment.py
+++ xtrack/environment.py
@@ -1525,8 +1525,7 @@ def _reverse_element(env, name):
     _reverse_field('rot_s_rad')
 
     if hasattr(ee, 'lag'):
-        # ee_ref.lag = 180 - (ee_ref.lag._expr or ee_ref.lag._value)
-        ee_ref.lag = -(ee_ref.lag._expr or ee_ref.lag._value)
+        ee_ref.lag = 180 - (ee_ref.lag._expr or ee_ref.lag._value)
 
     if hasattr(ee, 'knl'):
         for i in range(1, len(ee.knl), 2):
