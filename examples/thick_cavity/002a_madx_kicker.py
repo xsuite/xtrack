9bcba6cc46192282936375991e40003706b5e356
giadarol <giovanni.iadarola@cern.ch>
2025-08-23 21:27:24 +0200

    First checks

--- examples/thick_cavity/002a_madx_kicker.py
+++ examples/thick_cavity/002a_madx_kicker.py
@@ -1,15 +1,14 @@
 from cpymad.madx import Madx
 import xtrack as xt
+import xobjects as xo
+import numpy as np
 
 loader_mode = 'cpymad' #cpymad/native
 
 
-
-
-
 mad_data = """
 
-hk: hkicker, l=0.1, kick=1e-3;
+hk: hkicker, l=1, kick=1e-3;
 
 ss: sequence, l=3;
   hk1: hk, at=1;
@@ -38,5 +37,20 @@ elif loader_mode == 'cpymad':
 line.particle_ref = xt.Particles(p0c=1e9)
 
 tw = line.twiss(betx=1, bety=1)
+tt = line.get_table(attr=True)
+# is:
+# Table: 6 rows, 122 cols
+# name                   s element_type isthick isreplica ...
+# ss$start               0 Marker         False     False
+# drift_0                0 Drift           True     False
+# hk1                  0.5 Multipole       True     False
+# drift_1              1.5 Drift           True     False
+# ss$end                 3 Marker         False     False
+# _end_point             3                False     False
 
-
+assert np.all(tt.name == np.array(
+    ['ss$start', 'drift_0', 'hk1', 'drift_1', 'ss$end', '_end_point']))
+xo.assert_allclose(tt.s, np.array([0, 0, 0.5, 1.5, 3, 3]))
+assert np.all(tt.element_type == np.array(
+    ['Marker', 'Drift', 'Multipole', 'Drift', 'Marker', '']))
+assert np.all(tt.isthick == np.array([False, True, True, True, False, False]))
