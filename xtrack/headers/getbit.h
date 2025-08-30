6c7ae4af1a97f1ae0f0fb75ec2c614ab8a93a122
giadarol <giovanni.iadarola@cern.ch>
2025-08-30 10:27:15 +0200

    Prepare function to read the thing

--- xtrack/headers/getbit.h
+++ xtrack/headers/getbit.h
@@ -1,7 +1,7 @@
-#ifndef XSTUITE_GET_FLAGS_H
-#define XSTUITE_GET_FLAGS_H
+#ifndef XSTUITE_GETBIT_H
+#define XSTUITE_GETBIT_H
 
 #define GET_BIT(flags, flag_index) \
     (((flags) >> (flag_index)) & 1ULL)
 
-#endif // XSTUITE_GET_FLAGS_H
+#endif // XSTUITE_GETBIT_H
