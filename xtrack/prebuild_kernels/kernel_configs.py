1535f718c269e75f8c8128e16ce982fdbf767577
Frederik F. Van der Veken <frederik@cern.ch>
2025-09-04 01:15:04 +0200

    Removed obsolete kernel configs.

--- xtrack/prebuild_kernels/kernel_configs.py
+++ xtrack/prebuild_kernels/kernel_configs.py
@@ -10,15 +10,3 @@ BASE_CONFIG = {
     'XFIELDS_BB3D_NO_BHABHA': True,
     'XTRACK_GLOBAL_XY_LIMIT': 1.0,
 }
-
-FREEZE_ENERGY = {
-    'FREEZE_VAR_delta': True,
-    'FREEZE_VAR_ptau': True,
-    'FREEZE_VAR_rpp': True,
-    'FREEZE_VAR_rvv': True,
-}
-
-FREEZE_LONGITUDINAL = {
-    **FREEZE_ENERGY,
-    'FREEZE_VAR_zeta': True,
-}
