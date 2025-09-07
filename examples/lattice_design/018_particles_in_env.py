49dfff21666fda5e6217420cd55e212ee6a1fc90
giadarol <giovanni.iadarola@cern.ch>
2025-09-07 10:10:57 +0200

    Reorganize checks

--- examples/lattice_design/018_particles_in_env.py
+++ examples/lattice_design/018_particles_in_env.py
@@ -10,19 +10,24 @@ xo.assert_allclose(env['my_particle'].p0c, 4e12, rtol=0, atol=1e-9)
 env['a'] = 5.
 xo.assert_allclose(env['my_particle'].p0c, 5e12, rtol=0, atol=1e-9)
 
+env.particle_ref = 'my_particle'
+env._particle_ref = 'my_particle'
+xo.assert_allclose(env.particle_ref.p0c, 5e12, rtol=0, atol=1e-9)
+assert env.particle_ref.__class__.__name__ == 'EnvParticleRef'
+
+env.particle_ref.p0c = '2e12 * a'
+xo.assert_allclose(env.particle_ref.p0c, 10e12, rtol=0, atol=1e-9)
+env['my_particle'].p0c = '1e12 * a'
+xo.assert_allclose(env.particle_ref.p0c, 5e12, rtol=0, atol=1e-9)
+
+
 env2 = xt.Environment.from_dict(env.to_dict())
 assert 'my_particle' in env2.particles
 xo.assert_allclose(env2['my_particle'].p0c, 5e12, rtol=0, atol=1e-9)
 env2['a'] = 6.
 xo.assert_allclose(env2['my_particle'].p0c, 6e12, rtol=0, atol=1e-9)
 
-env2.particle_ref = 'my_particle'
-env2._particle_ref = 'my_particle'
-xo.assert_allclose(env2.particle_ref.p0c, 6e12, rtol=0, atol=1e-9)
-assert env2.particle_ref.__class__.__name__ == 'EnvParticleRef'
 
-env2.particle_ref.p0c = '2e12 * a'
-xo.assert_allclose(env2.particle_ref.p0c, 12e12, rtol=0, atol=1e-9)
 
 ll = env.new_line(name='my_line', components=[])
 ll.particle_ref = 'my_particle'
