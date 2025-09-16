2ddf08f7f36d20d3c81f1b4c97cb36d57e27f2e7
giadarol <giovanni.iadarola@cern.ch>
2025-09-16 11:27:11 +0200

    Adapt some names

--- xtrack/prebuilt_kernels_definitions/__init__.py
+++ xtrack/prebuilt_kernels_definitions/__init__.py
@@ -5,4 +5,3 @@
 
 from .elements import ONLY_XTRACK_ELEMENTS, NO_SYNRAD_ELEMENTS, NON_TRACKING_ELEMENTS
 from .element_inits import XTRACK_ELEMENTS_INIT_DEFAULTS
-from .kernel_configs import BASE_CONFIG
