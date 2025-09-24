9540e7f19af1cd3ff60bd3c03df98ce05609aabd
giadarol <giovanni.iadarola@cern.ch>
2025-09-24 09:54:48 +0200

    Add test

--- examples/true_rbend/007_check_loading.py
+++ examples/true_rbend/007_check_loading.py
@@ -1,7 +1,13 @@
 from cpymad.madx import Madx
 import xtrack as xt
+import xobjects as xo
+import numpy as np
 
 madx = Madx()
+
+l_straight = 1.0
+angle = 0.5
+l_arc = l_straight / np.sinc(angle / np.pi / 2) # np.sinc is sin(pi*x)/(pi*x)
 
 madx.input('''
 b: rbend, l=1.0, angle=0.5;
@@ -15,13 +21,20 @@ twiss, betx=1, bety=1;
 survey;
 ''')
 sv_madx = xt.Table(madx.table.survey, _copy_cols=True)
+xo.assert_allclose(madx.elements['b'].l, l_straight, rtol=1e-12)
+xo.assert_allclose(sv_madx['s', 'b:1'], 2.5 + l_arc / 2, rtol=1e-12) # b si at exit
 
+# Check cpymad loader
 line = xt.Line.from_madx_sequence(madx.sequence.ss)
 line.remove('ss$start')
 line.remove('ss$end')
 line.particle_ref = xt.Particles(p0c=1e9)
 sv_xs = line.survey()
+xo.assert_allclose(line['b'].length_straight, l_straight, rtol=1e-12)
+xo.assert_allclose(line['b'].length, l_arc, rtol=1e-12)
+xo.assert_allclose(sv_xs['s', 'b'], 2.5 - l_arc / 2, rtol=1e-12) # b si at entrance
 
+# Check to madx
 mad_str = line.to_madx_sequence(sequence_name='ggg')
 madx2 = Madx()
 madx2.input(mad_str)
@@ -32,34 +45,15 @@ twiss, betx=1, bety=1;
 survey;
 ''')
 sv_madx2 = xt.Table(madx2.table.survey, _copy_cols=True)
+xo.assert_allclose(madx2.elements['b'].l, l_straight, rtol=1e-12)
 
+# Check native madx loader
 env2 = xt.load(string=mad_str, format='madx')
 sv_xs2 = env2.ggg.survey()
+xo.assert_allclose(env2['b'].length_straight, l_straight, rtol=1e-12)
+xo.assert_allclose(env2['b'].length, l_arc, rtol=1e-12)
+xo.assert_allclose(sv_xs2['s', 'b'], 2.5 - l_arc / 2, rtol=1e-12) # b si at entrance
 
-madng = line.to_madng(keep_files=True)
-
-with open('temp_ng.mad', 'w') as fid:
-
-    fid.write('''
-do	 -- Begin chunk
-t_turn_s = 0.0
-end	 -- End chunk
-do	 -- Begin chunk
-seq_chunk_0 = sequence 'seq_chunk_0' {
-rbend 'b' { at = 2.5, l = 1.0, angle = 0.5, k0 = 0.4948079185090459, true_rbend=false, e1 = 0.0, e2 = 0.0, fint = 0.0, fintx = 0.0, hgap = 0.0, kill_ent_fringe =\ false, kill_exi_fringe =\ false, k1 = 0.0, knl := {0.0,0.0,0.0,0.0,0.0,0.0}, ksl := {0.0,0.0,0.0,0.0,0.0,0.0}, misalign =\ {dx=0.0, dy=0.0}},
-}
-end	 -- End chunk
-do	 -- Begin chunk
-seq = sequence 'seq' { refer='centre', l=5, seq_chunk_0 }
-seq_chunk_0 = nil
-end	 -- End chunk
-    ''')
-
-import pymadng as pg
-mng = pg.MAD()
-mng.send(f"""
-            mad_func = loadfile('temp_ng.mad', nil, MADX)
-            assert(mad_func)
-            mad_func()
-            """)
-sv = mng.survey(sequence='MADX.seq')[0].to_df()
+# Check madng
+sv_ng = line.madng_survey()
+xo.assert_allclose(sv_ng['s', 'b'], 2.5 - l_arc / 2, rtol=1e-12) # b si at entrance
