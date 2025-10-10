5ef447b874908f86ff28b667281b49348b033b7a
giadarol <giovanni.iadarola@cern.ch>
2025-10-10 09:14:05 +0200

    Add asserts

--- examples/boris_spatial/002_check_zeta_against_drift.py
+++ examples/boris_spatial/002_check_zeta_against_drift.py
@@ -1,4 +1,5 @@
 import xtrack as xt
+import xobjects as xo
 
 def zero_field(x, y, z):
     return (0*x, 0*y, 0*z)
@@ -17,3 +18,12 @@ integrator.track(p_integ)
 
 p_drift = p0.copy()
 drift.track(p_drift)
+
+xo.assert_allclose(p_integ.x, p_drift.x, rtol=0, atol=1e-10)
+xo.assert_allclose(p_integ.px, p_drift.px, rtol=0, atol=1e-10)
+xo.assert_allclose(p_integ.y, p_drift.y, rtol=0, atol=1e-10)
+xo.assert_allclose(p_integ.py, p_drift.py, rtol=0, atol=1e-10)
+xo.assert_allclose(p_integ.s, p_drift.s, rtol=0, atol=1e-10)
+xo.assert_allclose(p_integ.delta, p_drift.delta, rtol=0, atol=1e-10)
+xo.assert_allclose(p_integ.zeta, p_drift.zeta, rtol=0, atol=1e-10)
+
