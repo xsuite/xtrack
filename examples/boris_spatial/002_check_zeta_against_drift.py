1a578adf35b706e6591810e3eef7770f108a0567
giadarol <giovanni.iadarola@cern.ch>
2025-10-10 09:22:21 +0200

    Found bug

--- examples/boris_spatial/002_check_zeta_against_drift.py
+++ examples/boris_spatial/002_check_zeta_against_drift.py
@@ -11,7 +11,8 @@ integrator = xt.BorisSpatialIntegrator(fieldmap_callable=zero_field,
 
 drift = xt.Drift(length=10, model='exact')
 
-p0 = xt.Particles('proton', x=[1e-2, -1e-2], px=[15e-3, 0], delta=[0, 2], p0c=1e9, zeta=1)
+p0 = xt.Particles('proton',
+        x=[1e-2, -1e-2], px=[15e-3, 0], delta=[0, 2], p0c=1e9, zeta=1)
 
 p_integ = p0.copy()
 integrator.track(p_integ)
@@ -27,3 +28,19 @@ xo.assert_allclose(p_integ.s, p_drift.s, rtol=0, atol=1e-10)
 xo.assert_allclose(p_integ.delta, p_drift.delta, rtol=0, atol=1e-10)
 xo.assert_allclose(p_integ.zeta, p_drift.zeta, rtol=0, atol=1e-10)
 
+# Check behavior with lost particles
+p0 = xt.Particles('proton',
+        x=[1e-2, -1e-2], px=[15e-3, 0], delta=[0, 2], p0c=1e9, zeta=1,
+        state=[1, -1])
+
+p_integ = p0.copy()
+integrator.track(p_integ)
+p_drift = p0.copy()
+drift.track(p_drift)
+
+xo.assert_allclose(p_integ.x, p_drift.x, rtol=0, atol=1e-10)
+xo.assert_allclose(p_integ.px, p_drift.px, rtol=0, atol=1e-10)
+xo.assert_allclose(p_integ.y, p_drift.y, rtol=0, atol=1e-10)
+xo.assert_allclose(p_integ.py, p_drift.py, rtol=0, atol=1e-10)
+xo.assert_allclose(p_integ.s, p_drift.s, rtol=0, atol=1e-10)
+xo.assert_allclose(p_integ.delta, p_drift.delta, rtol=0, atol=1e-10)
