bbef4ab69eb68b7ccc9be7c7f3ced9b3f5bec0ec
giadarol <giovanni.iadarola@cern.ch>
2025-10-12 20:34:42 +0200

    Add deprecation list

---
+++ xtrack/_temp/deprecation.md
@@ -0,0 +1,15 @@
+
+# Line class
+
+| Name | Replaced by |
+|------|--------------|
+| append_element | append |
+| compute_one_turn_matrix_finite_differences | compute_R_matrix (missing) |
+| element_dict | elements (returns views) |
+| element_refs | ref.elements |
+| from_sequence | Environment.new_line |
+| insert_element | insert |
+| items | presently returns underlying object, will return views |
+| skip_end_turn_actions |  |
+| slice_thick_elements | slice_elements (missing) |
+| to_pandas | remove ? |
--- xtrack/line.py
+++ xtrack/line.py
@@ -4136,13 +4136,6 @@ class Line:
                 'This action is not allowed as the line is frozen! '
                 'You can unfreeze the line by calling the `discard_tracker()` method.')
 
-    def transform_compound(self, *args, **kwargs):
-        raise NotImplementedError('`transform_compound` is not anymore supported. '
-            'Tilt and shifts can be applied directly to the elements.\n'
-            'For more details, see: '
-            'https://xsuite.readthedocs.io/en/latest/line.html#apply-transformations-tilt-shift-to-elements'
-        )
-
     def mirror(self, inplace=True):
         assert inplace in [True, False]
         if inplace == False:
