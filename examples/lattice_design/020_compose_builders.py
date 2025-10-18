6b2c6f9e8dc0475805ed9afacec6f6061473d00e
giadarol <giovanni.iadarola@cern.ch>
2025-10-18 23:13:59 +0200

    This seems ok

--- examples/lattice_design/020_compose_builders.py
+++ examples/lattice_design/020_compose_builders.py
@@ -2,16 +2,17 @@ import xtrack as xt
 
 env = xt.Environment()
 
-env['a'] = 3.
+env['a'] = 1.
 
 env.new_builder(name='l1')
-env['l1'].new('q1', 'Quadrupole', length='a/3.', at=2.5)
-env['l1'].new('q2', 'q1', at='a', from_='q1@center')
+env['l1'].new('q1', 'Quadrupole', length='a', at='0.5*a')
+env['l1'].new('q2', 'q1', at='4*a', from_='q1@center')
 
 env.new_builder(name='l_compose',
+                length='20*a',
                 components=[
-                    env.place('l1', at='3*a'),
-                    env.place(-env['l1'], at='10*a'),
+                    env.place('l1', at='7.5*a'),
+                    env.place(-env['l1'], at='17.5*a'),
                 ])
 env['l_compose'].build()
 
