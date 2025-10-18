43c45730e5ab8043dd2f782261fdef4ab3bbb989
giadarol <giovanni.iadarola@cern.ch>
2025-10-18 23:18:57 +0200

    To be investigate

--- examples/lattice_design/020_compose_builders.py
+++ examples/lattice_design/020_compose_builders.py
@@ -8,11 +8,13 @@ env.new_builder(name='l1')
 env['l1'].new('q1', 'Quadrupole', length='a', at='0.5*a')
 env['l1'].new('q2', 'q1', at='4*a', from_='q1@center')
 
-env.new_builder(name='l_compose',
-                length='20*a',
-                components=[
+b_compose = env.new_builder(components=[
                     env.place('l1', at='7.5*a'),
                     env.place(-env['l1'], at='17.5*a'),
                 ])
-env['l_compose'].build()
+l1 = b_compose.build()
+tt1 = l1.twiss4d()
 
+env['a'] = 2.
+l2 = b_compose.build()
+tt2 = l2.twiss4d()
