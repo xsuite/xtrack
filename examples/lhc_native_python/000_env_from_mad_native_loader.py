c5133e00bc3305a1a7af0233667ebeb95f12917e
giadarol <giovanni.iadarola@cern.ch>
2025-10-20 23:08:49 +0200

    Some more checks

--- examples/lhc_native_python/000_env_from_mad_native_loader.py
+++ examples/lhc_native_python/000_env_from_mad_native_loader.py
@@ -1,11 +1,30 @@
 import xtrack as xt
+import xobjects as xo
 
 env = xt.load('../../test_data/lhc_2024/lhc.seq',
                reverse_lines=['lhcb2'],
 )
 env.vars.load('../../test_data/lhc_2024/injection_optics.madx')
 
+# Some checks based on direct inpection of MAD-X file
 
+xo.assert_allclose(env['ip8ofs.b2'],  -154, atol=1e-12)
+assert str(env.ref['aip2']._expr) == "f['atan'](((vars['sep_arc'] / 2.0) / vars['dsep2']))"
+
+assert env['tanb'].prototype == 'collimator'
+assert env['collimator'].prototype is None
+assert isinstance(env['collimator'], xt.Drift)
+
+assert env['mcbch'].prototype == 'hcorrector'
+assert env['hcorrector'].prototype == 'hkicker'
+env['hkicker'].prototype is None
+assert isinstance(env['mcbch'], xt.Multipole)
+assert isinstance(env['hcorrector'], xt.Multipole)
+assert isinstance(env['hkicker'], xt.Multipole)
+
+
+
+prrrr
 # from cpymad.madx import Madx
 # madx = Madx()
 # madx.call('../../test_data/lhc_2024/lhc.seq')
