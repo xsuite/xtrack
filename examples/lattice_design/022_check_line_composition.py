dfca2a2f9a027710844f2adf308e6dd8cbe38f7b
giadarol <giovanni.iadarola@cern.ch>
2025-10-24 21:23:38 +0200

    More checks

--- examples/lattice_design/022_check_line_composition.py
+++ examples/lattice_design/022_check_line_composition.py
@@ -102,3 +102,34 @@ assert np.all(tmm.element_type ==
      'Quadrupole', 'Drift', 'Quadrupole',
      'Quadrupole', 'Drift', 'Quadrupole',
      ''])
+
+l1.end_compose()
+mm2 = 3*l1
+assert mm2.mode == 'normal'
+tmm2 = mm2.get_table()
+# tmm2.cols['s element_type env_name'] is:
+# Table: 10 rows, 4 cols
+# name                    s element_type env_name
+# q1::0                   0 Quadrupole   q1
+# drift_13::0             1 Drift        drift_13
+# q2::0                   4 Quadrupole   q2
+# q1::1                   5 Quadrupole   q1
+# drift_13::1             6 Drift        drift_13
+# q2::1                   9 Quadrupole   q2
+# q1::2                  10 Quadrupole   q1
+# drift_13::2            11 Drift        drift_13
+# q2::2                  14 Quadrupole   q2
+# _end_point             15              _end_point
+assert np.all(tmm2.name == np.array([
+    'q1::0', 'drift_13::0', 'q2::0',
+     'q1::1', 'drift_13::1', 'q2::1',
+     'q1::2', 'drift_13::2', 'q2::2',
+     '_end_point']))
+xo.assert_allclose(tmm2.s,
+        [ 0.,  1.,  4.,  5.,  6.,  9., 10., 11., 14., 15.],
+        rtol=0, atol=1e-12)
+assert np.all(tmm2.element_type ==
+    ['Quadrupole', 'Drift', 'Quadrupole',
+     'Quadrupole', 'Drift', 'Quadrupole',
+     'Quadrupole', 'Drift', 'Quadrupole',
+     ''])
