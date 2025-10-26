1dcb0c1eb7c66013f672b63662a66f96ba853f0b
giadarol <giovanni.iadarola@cern.ch>
2025-10-26 09:21:25 +0100

    Adapt examples

--- examples/psb/000b_setup_chicane.py
+++ examples/psb/000b_setup_chicane.py
@@ -29,7 +29,7 @@ line['k0bi1bsw1l14'] = (line.ref['bsw_k0l'] / line['bi1.bsw1l1.4'].length)
 line.ref['k0bi1bsw1l11']._info()
 
 # Build knob to model eddy currents (k2)
-line.vars['bsw_k2l'] = 0
+line['bsw_k2l'] = 0
 line['bi1.bsw1l1.1'].knl[2] = 'bsw_k2l'
 line['bi1.bsw1l1.2'].knl[2] = '-bsw_k2l'
 line['bi1.bsw1l1.3'].knl[2] = '-bsw_k2l'
