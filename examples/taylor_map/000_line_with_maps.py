655fa80b0f61d19374726cca4b4855529255106d
giadarol <giovanni.iadarola@cern.ch>
2025-10-26 12:04:07 +0100

    Adapt example

--- examples/taylor_map/000_line_with_maps.py
+++ examples/taylor_map/000_line_with_maps.py
@@ -6,14 +6,14 @@ line = xt.load('../../test_data/hllhc15_thick/lhc_thick_with_knobs.json')
 line.build_tracker()
 
 # Switch RF on
-line.vars['vrf400'] = 16
-line.vars['lagrf400.b1'] = 0.5
+line['vrf400'] = 16
+line['lagrf400.b1'] = 0.5
 
 # Enable crossing angle orbit bumps
-line.vars['on_x1'] = 10
-line.vars['on_x2'] = 20
-line.vars['on_x5'] = 10
-line.vars['on_x8'] = 30
+line['on_x1'] = 10
+line['on_x2'] = 20
+line['on_x5'] = 10
+line['on_x8'] = 30
 
 # Generate line made on maps (splitting at defined markers)
 ele_cut = ['ip1', 'ip2', 'ip5', 'ip8'] # markers where to split the line
@@ -50,17 +50,17 @@ tw_map.dqx  # is 1.9137
 tw.rows[['ip1', 'ip2', 'ip5', 'ip8']].cols['x px y py betx bety'].show()
 # prints
 #
-# name           x          px           y          py    betx    bety
-# ip8  9.78388e-09 3.00018e-05 6.25324e-09 5.47624e-09 1.50001 1.49999
-# ip1  1.92278e-10 1.00117e-05 2.64869e-09  1.2723e-08    0.15    0.15
-# ip2  1.47983e-08 2.11146e-09 -2.1955e-08 2.00015e-05      10 9.99997
-# ip5 -3.03593e-09 5.75413e-09 5.13184e-10  1.0012e-05    0.15    0.15
+# name             x            px             y            py          betx          bety
+# ip1    1.95075e-10   1.00117e-05    2.6487e-09   1.27231e-08          0.15          0.15
+# ip2    1.48028e-08   2.11126e-09  -2.19551e-08   2.00015e-05            10       9.99997
+# ip5   -3.03607e-09   5.75852e-09   5.13183e-10    1.0012e-05          0.15          0.15
+# ip8    9.79345e-09   3.00018e-05   6.25326e-09   5.47627e-09       1.50001       1.49999
 
 tw_map.rows[['ip1', 'ip2', 'ip5', 'ip8']].cols['x px y py betx bety']
 # prints
 #
-# name             x          px           y          py    betx    bety
-# ip8    9.78388e-09 3.00018e-05 6.25324e-09 5.47624e-09 1.50001 1.49999
-# ip1    1.92278e-10 1.00117e-05 2.64869e-09  1.2723e-08    0.15    0.15
-# ip2    1.47983e-08 2.11146e-09 -2.1955e-08 2.00015e-05      10 9.99997
-# ip5   -3.03593e-09 5.75413e-09 5.13184e-10  1.0012e-05    0.15    0.15
+# name             x            px             y            py          betx          bety
+# ip1    1.95051e-10   1.00117e-05   2.64871e-09   1.27232e-08          0.15          0.15
+# ip2    1.48027e-08   2.11128e-09  -2.19552e-08   2.00015e-05            10       9.99997
+# ip5   -3.03608e-09   5.75835e-09   5.13188e-10    1.0012e-05          0.15          0.15
+# ip8    9.79336e-09   3.00018e-05   6.25327e-09    5.4763e-09       1.50001       1.49999
