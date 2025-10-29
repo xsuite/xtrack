bc8b22a7aad377966ca05d9c1ace5dc9ac1a9a6e
giadarol <giovanni.iadarola@cern.ch>
2025-10-29 17:52:59 +0100

    Symplectic error is quadratic

--- examples/boris_spatial/004_study_convergence.py
+++ examples/boris_spatial/004_study_convergence.py
@@ -15,13 +15,47 @@ p0 = xt.Particles(mass0=xt.ELECTRON_MASS_EV, q0=1,
 
 p = p0.copy()
 
-l_region = 100.
 
-sf = SolenoidField(L=4, a=0.3, B0=1.5, z0=l_region/2)
+class LinearFringeSolenoid:
 
+    def __init__(self, B0, s1, s2, s3, s4):
+        self.B0 = B0
+        self.s1 = s1
+        self.s2 = s2
+        self.s3 = s3
+        self.s4 = s4
 
-# n_steps_vect = [1000, 5000, 10000, 20000, 50000]#, 100000, 200000]
-n_steps_vect = [5000, 10000, 20000, 50000, 100000] # 200000]
+    def get_field(self, x, y, s):
+
+        mask_entry_taper = (s >= self.s1) & (s < self.s2)
+        mask_constant = (s >= self.s2) & (s < self.s3)
+        mask_exit_taper = (s >= self.s3) & (s < self.s4)
+
+        Bx = np.zeros_like(x)
+        By = np.zeros_like(y)
+        Bz = np.zeros_like(s)
+
+        Bz[mask_entry_taper] = self.B0 * (s[mask_entry_taper] - self.s1) / (self.s2 - self.s1)
+        Bz[mask_constant] = self.B0
+        Bz[mask_exit_taper] = self.B0 * (self.s4 - s[mask_exit_taper]) / (self.s4 - self.s3)
+
+        # Bx = -x dBz/dz /2
+        # By = -y dBz/dz /2
+
+        dBz_ds_entry = self.B0 / (self.s2 - self.s1)
+        dBz_ds_exit = -self.B0 / (self.s4 - self.s3)
+
+        Bx[mask_entry_taper] = -x[mask_entry_taper] * dBz_ds_entry / 2
+        Bx[mask_exit_taper] = -x[mask_exit_taper] * dBz_ds_exit / 2
+        By[mask_entry_taper] = -y[mask_entry_taper] * dBz_ds_entry / 2
+        By[mask_exit_taper] = -y[mask_exit_taper] * dBz_ds_exit / 2
+
+        return Bx, By, Bz
+
+sf = LinearFringeSolenoid(B0=1.0, s1=0.0, s2=3, s3=7, s4=10)
+
+n_steps_vect = [100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000]
+# n_steps_vect = [5000, 10000, 20000, 50000, 100000] # 200000]
 sympl_error = []
 S = xt.linear_normal_form.S
 x = []
@@ -29,7 +63,7 @@ for n_steps in n_steps_vect:
     print('n_steps=', n_steps)
     integrator = xt.BorisSpatialIntegrator(fieldmap_callable=sf.get_field,
                                             s_start=0,
-                                            s_end=l_region,
+                                            s_end=sf.s4,
                                             n_steps=n_steps)
     line = xt.Line(elements=[integrator])
     line.build_tracker()
@@ -39,7 +73,7 @@ for n_steps in n_steps_vect:
 
     RR = R_obj["R_matrix"]
 
-    se = np.linalg.norm(RR.T @ S @ RR - S, ord=2) - 1
+    se = np.linalg.norm(RR.T @ S @ RR - S, ord=2)
     sympl_error.append(se)
 
     p_boris = p.copy()
