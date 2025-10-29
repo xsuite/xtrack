936742a1b8b9ed69f4825a36f38b7fba5bb1638a
giadarol <giovanni.iadarola@cern.ch>
2025-10-29 11:02:06 +0100

    Vector potential, untested

--- examples/boris_spatial/004_study_convergence.py
+++ examples/boris_spatial/004_study_convergence.py
@@ -6,19 +6,22 @@ import xobjects as xo
 import xtrack as xt
 from xtrack._temp.boris_and_solenoid_map.solenoid_field import SolenoidField
 
-
 p0 = xt.Particles(mass0=xt.ELECTRON_MASS_EV, q0=1,
                 energy0=45.6e9/1000,
                 x=[-1e-3,],
-                px=-1e-3,
+                px=0,
                 y=1e-3,
                 delta=0)
 
 p = p0.copy()
 
-sf = SolenoidField(L=4, a=0.3, B0=1.5, z0=20)
+l_region = 100.
 
-n_steps_vect = [1000, 5000, 10000, 20000, 50000, 100000, 200000]
+sf = SolenoidField(L=4, a=0.3, B0=1.5, z0=l_region/2)
+
+
+# n_steps_vect = [1000, 5000, 10000, 20000, 50000]#, 100000, 200000]
+n_steps_vect = [5000, 10000, 20000, 50000, 100000] # 200000]
 sympl_error = []
 S = xt.linear_normal_form.S
 x = []
@@ -26,7 +29,7 @@ for n_steps in n_steps_vect:
     print('n_steps=', n_steps)
     integrator = xt.BorisSpatialIntegrator(fieldmap_callable=sf.get_field,
                                             s_start=0,
-                                            s_end=30,
+                                            s_end=l_region,
                                             n_steps=n_steps)
     line = xt.Line(elements=[integrator])
     line.build_tracker()
