96ebf2168700d155043705ad34a9babff2136fed
giadarol <giovanni.iadarola@cern.ch>
2025-10-31 21:42:56 +0100

    Clean up example

--- examples/lhc_native_loader_and_py_lattice/002_inspect_py_lattice.py
+++ examples/lhc_native_loader_and_py_lattice/002_inspect_py_lattice.py
@@ -23,28 +23,6 @@ env.lhcb1.end_compose()
 tt1 = env.lhcb1.get_table()
 print(f'{tt1.rows["mb.a8r1.b1"].s=}')
 
-
-
 tt2 = env.lhcb2.get_table()
 tw1 = env.lhcb1.twiss4d()
 tw2 = env.lhcb2.twiss4d(reverse=True)
-
-t1 = time.time()
-env2 = xt.load('lhc_composers.json')
-t2 = time.time()
-print(f'Time to load LHC from json: {t2-t1:.3f} s')
-
-env2.lhcb1.particle_ref = xt.Particles(p0c=0.45e12)
-env2.lhcb2.particle_ref = xt.Particles(p0c=0.45e12)
-
-tt1_ref = env2.lhcb1.get_table()
-tt2_ref = env2.lhcb2.get_table()
-tw1_ref = env2.lhcb1.twiss4d()
-tw2_ref = env2.lhcb2.twiss4d(reverse=True)
-
-tt1_nodr = tt1.rows[tt1.element_type != 'Drift']
-tt1_ref_nodr = tt1_ref.rows[tt1_ref.element_type != 'Drift']
-
-for ii, cc in enumerate(env.lhcb1.composer.components):
-    if cc.name == 'mb.a8r1.b1':
-        break
