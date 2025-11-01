2d1af91b024b4f0fe9dbddc415dc840e3c5ccdb8
giadarol <giovanni.iadarola@cern.ch>
2025-11-01 21:54:03 +0100

    Update example

--- examples/element_transformations/000_element_transform.py
+++ examples/element_transformations/000_element_transform.py
@@ -1,23 +1,21 @@
 import xtrack as xt
 from cpymad.madx import Madx
 
-# Load a very simple sequence from MAD-X
-mad = Madx()
-mad.input("""
-    seq: sequence, l=4;
-    b1: sbend, at=0.5, angle=0.2, l=1;
-    b2: sbend, at=2.5, angle=0.3, l=1;
-    endsequence;
+env = xt.Environment()
+line = env.new_line(length=4.0, name='seq',
+    components=[
+        env.new('b1', 'Bend', length=1.0, angle=0.2, at=0.5),
+        env.new('b2', 'Bend', length=1.0, angle=0.3, at=2.5),
+    ])
 
-    beam;
-    use,sequence=seq;
-""")
-
-line = xt.Line.from_madx_sequence(mad.sequence.seq)
-line.build_tracker()
-
-print('The line as imported from MAD-X:')
+print('The line as created:')
 line.get_table().show()
+# name                   s element_type isthick isreplica parent_name ...
+# b1                     0 Bend            True     False None
+# drift_1                1 Drift           True     False None
+# b2                     2 Bend            True     False None
+# drift_2                3 Drift           True     False None
+# _end_point             4                False     False None
 
 # Shift and tilt selected elements
 line['b1'].shift_x = -0.01
@@ -29,11 +27,10 @@ tt = line.get_table(attr=True)
 tt.cols['s', 'element_type', 'isthick', 'shift_x', 'shift_y', 'shift_s', 'rot_s_rad']
 # returns:
 #
-# name       s element_type isthick shift_x shift_y shift_s rot_s_rad
-# seq$start  0 Marker         False       0       0       0         0
-# b1         0 Bend            True   -0.01       0       0       0.8
-# drift_0    1 Drift           True       0       0       0         0
-# b2         2 Bend            True       0       0    0.02      -0.8
-# drift_1    3 Drift           True       0       0       0         0
-# seq$end    4 Marker         False       0       0       0         0
-# _end_point 4                False       0       0       0         0
+# Table: 5 rows, 8 cols
+# name        s element_type isthick   shift_x   shift_y   shift_s   rot_s_rad
+# b1          0 Bend            True     -0.01         0         0         0.8
+# drift_1     1 Drift           True         0         0         0           0
+# b2          2 Bend            True         0         0      0.02        -0.8
+# drift_2     3 Drift           True         0         0         0           0
+# _end_point  4                False         0         0         0           0
