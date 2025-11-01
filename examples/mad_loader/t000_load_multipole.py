91dfe3d022603e14e4a4a0400b632846e2898c26
giadarol <giovanni.iadarola@cern.ch>
2025-11-01 21:33:10 +0100

    Check that array gets extended

--- examples/mad_loader/t000_load_multipole.py
+++ examples/mad_loader/t000_load_multipole.py
@@ -1,4 +1,5 @@
 import xtrack as xt
+import xobjects as xo
 
 mad_src = '''
     m1: multipole, l=1.0, knl={0, 0.01, 0.0, 0,0, 0.1, 0.3, 0.7};
@@ -9,3 +10,5 @@ mad_src = '''
 '''
 
 env = xt.load(string=mad_src, format='madx')
+xo.assert_allclose(env.elements['m1'].knl,
+                   [0, 0.01, 0.0, 0, 0, 0.1, 0.3, 0.7], rtol=0, atol=1e-12)
