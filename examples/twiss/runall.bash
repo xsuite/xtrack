7d4672c624245614be9d6b73c44167f72778c645
giadarol <giovanni.iadarola@cern.ch>
2025-11-02 08:21:16 +0100

    Made script to run examples

--- examples/twiss/runall.bash
+++ examples/twiss/runall.bash
@@ -1,6 +1,7 @@
 # run all files called 0xx_*.py in this directory and stop if error
 for f in $(ls $(dirname "$0")/0??_*.py | sort); do
     echo "Running $f"
+    export MPLBACKEND=Agg
     python $f
     if [ $? -ne 0 ]; then
         echo "Error in $f"
