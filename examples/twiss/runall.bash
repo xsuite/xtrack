5dffa39d7b7d5271ce60ad2fb04fceebe980ee2f
giadarol <giovanni.iadarola@cern.ch>
2025-11-02 08:11:26 +0100

    Remove examples ported to tests

---
+++ examples/twiss/runall.bash
@@ -0,0 +1,10 @@
+# run all files called 0xx_*.py in this directory and stop if error
+for f in $(ls $(dirname "$0")/0??_*.py | sort); do
+    echo "Running $f"
+    python $f
+    if [ $? -ne 0 ]; then
+        echo "Error in $f"
+        exit 1
+    fi
+done
+echo "All examples ran successfully."
