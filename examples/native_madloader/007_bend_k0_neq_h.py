cac346255c3e775905f603b48adcf1e566211171
giadarol <giovanni.iadarola@cern.ch>
2025-11-08 21:02:58 +0100

    The native loader does not set edge_exit_angle_fdown

---
+++ examples/native_madloader/007_bend_k0_neq_h.py
@@ -0,0 +1,22 @@
+import xtrack as xt
+from cpymad.madx import Madx
+
+mad_src = """
+    a = 0.1;
+    b1: sbend, l=1.0, angle:=a, k0:=a/2;
+    seq: sequence, l=1.0;
+    b1: b1, at=0.5;
+    endsequence;
+"""
+
+mad = Madx()
+mad.input(mad_src)
+mad.beam()
+mad.use('seq')
+
+lmad = xt.Line.from_madx_sequence(mad.sequence.seq, deferred_expressions=True)
+env = xt.load(string=mad_src, format='madx')
+lenv = env['seq']
+
+print(lenv.ref['b1'].edge_exit_angle_fdown._expr)
+print(lmad.ref['b1'].edge_exit_angle_fdown._expr)
