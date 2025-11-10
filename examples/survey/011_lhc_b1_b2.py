a7071662c84c7fcaa864edbcfa5840f8f4b8282c
giadarol <giovanni.iadarola@cern.ch>
2025-11-10 20:47:10 +0100

    Adapt example

--- examples/survey/011_lhc_b1_b2.py
+++ examples/survey/011_lhc_b1_b2.py
@@ -1,8 +1,9 @@
 import xtrack as xt
 import numpy as np
 
-env = xt.load('../../test_data/lhc_2024/lhc.seq', reverse_lines=['lhcb2'])
-env.vars.load('../../test_data/lhc_2024/injection_optics.madx')
+env = xt.load(['../../test_data/lhc_2024/lhc.seq',
+               '../../test_data/lhc_2024/injection_optics.madx'],
+                reverse_lines=['lhcb2'])
 
 sv1 = env.lhcb1.survey(element0='ip5')
 sv2_rev = env.lhcb2.survey(element0='ip5').reverse()
