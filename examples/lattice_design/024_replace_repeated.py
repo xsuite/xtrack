344a64ae9be4a9b9e81e46d1b68953a1a39ebdac
giadarol <giovanni.iadarola@cern.ch>
2025-11-12 20:18:56 +0100

    Chech for replace_repeated elements

--- examples/lattice_design/024_replace_repeated.py
+++ examples/lattice_design/024_replace_repeated.py
@@ -1,25 +1,39 @@
 import xtrack as xt
+import numpy as np
 
 env = xt.Environment()
-line = env.new_line(length=10.0,
+l1 = env.new_line(length=9.0,
     components=[
-        env.new('m', xt.Marker, at=5.0)])
+        env.new('m', xt.Marker, at=3.0),
+        env.place('m', at=6.0)])
 
-line.get_table().cols['element_type']
-# Table: 4 rows, 2 cols
-# name         element_type
-# ||drift_1::0 Drift
-# m            Marker
-# ||drift_1::1 Drift
-# _end_point
+tt1 = l1.get_table()
 
-breakpoint()
-line.replace_all_repeated_elements()
+l2 = l1.copy(shallow=True)
+l2.replace_all_repeated_elements()
+tt2 = l2.get_table()
 
-line.get_table().cols['element_type']
-# Table: 4 rows, 2 cols
-# name        element_type
-# ||drift_1.0 Drift
-# m           Marker
-# ||drift_1.1 Drift
-# _end_point
+l3 = l1.copy(shallow=True)
+l3.replace_all_repeated_elements(replace_generated_drifts=True)
+tt3 = l3.get_table()
+
+assert np.all(tt1.name == [
+    '||drift_1::0', 'm::0', '||drift_1::1', 'm::1', '||drift_1::2',
+    '_end_point'])
+assert np.all(tt1.env_name == [
+    '||drift_1', 'm', '||drift_1', 'm', '||drift_1',
+    '_end_point'])
+
+assert np.all(tt2.name == [
+       '||drift_1::0', 'm.0', '||drift_1::1', 'm.1', '||drift_1::2',
+       '_end_point'])
+assert np.all(tt2.env_name == [
+       '||drift_1', 'm.0', '||drift_1', 'm.1', '||drift_1',
+       '_end_point'])
+
+assert np.all(tt3.name == [
+         'drift_1.0', 'm.2', 'drift_1.1', 'm.3', 'drift_1.2',
+         '_end_point'])
+assert np.all(tt3.env_name == [
+         'drift_1.0', 'm.2', 'drift_1.1', 'm.3', 'drift_1.2',
+         '_end_point'])
