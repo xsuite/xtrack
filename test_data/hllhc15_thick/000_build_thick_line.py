467672dbeb98c461791ddc4751e79a2abbcd866a
giadarol <giovanni.iadarola@cern.ch>
2025-11-15 22:45:40 +0100

    hllhc15_collider_thick.json hllhc15_collider_thick.json from native loader

--- test_data/hllhc15_thick/000_build_thick_line.py
+++ test_data/hllhc15_thick/000_build_thick_line.py
@@ -14,18 +14,30 @@ call,file="hllhc_sequence.madx";
 seqedit,sequence=lhcb1;flatten;cycle,start=IP7;flatten;endedit;
 seqedit,sequence=lhcb2;flatten;cycle,start=IP7;flatten;endedit;
 beam, sequence=lhcb1, particle=proton, pc=7000;
+beam, sequence=lhcb2, particle=proton, pc=7000, bv=-1;
 call,file="opt_round_150_1500.madx";
 use, sequence=lhcb1;
+use, sequence=lhcb2;
 twiss;
 save, file="temp_lhc_thick.seq";
 """)
 
-env = xt.load('temp_lhc_thick.seq', s_tol=5e-6, _rbend_correct_k0=True)
-line = env.lhcb1
-line.set_particle_ref('proton', gamma0=mad.sequence.lhcb1.beam.gamma)
+env = xt.load('temp_lhc_thick.seq', s_tol=5e-6, _rbend_correct_k0=True,
+              reverse_lines=['lhcb2'])
 
-tt_cav = line.get_table().rows.match(element_type='Cavity')
+env.lhcb1.set_particle_ref('proton', p0c=7000e9)
+env.lhcb2.set_particle_ref('proton', p0c=7000e9)
+
+tt_cav = env.elements.get_table().rows.match(element_type='Cavity')
 for nn in tt_cav.name:
-    line[nn].frequency = 400.79e6  # Hz
+    env[nn].frequency = 400.79e6  # Hz
+
+line = env.lhcb1
 
 line.to_json('lhc_thick_with_knobs.json', include_var_management=True)
+
+env.lhcb1.twiss_default['method'] = '4d'
+env.lhcb2.twiss_default['method'] = '4d'
+env.lhcb2.twiss_default['reverse'] = True
+
+env.to_json('hllhc15_collider_thick.json')
