3e725c17780370c2bf8f918db2ed45ea969c9993
giadarol <giovanni.iadarola@cern.ch>
2025-11-15 17:30:29 +0100

    This patch works, to be deployed in loader

--- test_data/hllhc15_thick/t000_compare_lines.py
+++ test_data/hllhc15_thick/t000_compare_lines.py
@@ -3,12 +3,20 @@ import xtrack as xt
 line_old = xt.load('temp_line_before.json')
 line_new = xt.load('lhc_thick_with_knobs.json')
 
-# Patch 11 T dipoles
-tt_new = line_new.get_table()
-tt_new_mbh = tt_new.rows['mbh.*'].rows.match('Bend', 'element_type')
-for nn in tt_new_mbh.name:
-    line_new[nn].k0 = line_new[nn].h
+env = line_new.env
 
+sinc = env.functions['sinc']
+
+tt_rbend = env.elements.get_table().rows.match('RBend', 'element_type')
+for nn in tt_rbend.name:
+    ee_ref = env.ref[nn]
+    ee = env.get(nn)
+    if ee.k0_from_h:
+        continue
+    k0 = ee_ref.k0._expr or ee_ref.k0._value
+    angle = ee_ref.angle._expr or ee_ref.angle._value
+
+    line_new[nn].k0 = k0 * sinc(angle / 2)
 tw_old = line_old.twiss4d()
 tw_new = line_new.twiss4d()
 
