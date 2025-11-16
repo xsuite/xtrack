0e40a27bafd6404a4deb438a045bdce843e6464d
giadarol <giovanni.iadarola@cern.ch>
2025-11-16 07:58:23 +0100

    Update generation scripts

--- test_data/hllhc15_thick/001_build_thick_collider.py
+++ test_data/hllhc15_thick/001_build_thick_collider.py
@@ -11,8 +11,6 @@ mad = Madx()
 mad.input(f"""
 call,file="lhc.seq";
 call,file="hllhc_sequence.madx";
-seqedit,sequence=lhcb1;flatten;cycle,start=IP7;flatten;endedit;
-seqedit,sequence=lhcb2;flatten;cycle,start=IP7;flatten;endedit;
 beam, sequence=lhcb1, particle=proton, pc=7000;
 beam, sequence=lhcb2, particle=proton, pc=7000, bv=-1;
 call,file="opt_round_150_1500.madx";
@@ -52,6 +50,9 @@ tt_cav = env.elements.get_table().rows.match(element_type='Cavity')
 for nn in tt_cav.name:
     env[nn].frequency = 400.79e6  # Hz
 
-line = env.lhcb1
 
-line.to_json('lhc_thick_with_knobs.json', include_var_management=True)
+env.lhcb1.twiss_default['method'] = '4d'
+env.lhcb2.twiss_default['method'] = '4d'
+env.lhcb2.twiss_default['reverse'] = True
+
+env.to_json('hllhc15_collider_thick.json')
