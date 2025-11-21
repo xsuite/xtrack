5e705ee1a27a23a07abbebd17da599eaa33cfcf4
giadarol <giovanni.iadarola@cern.ch>
2025-11-21 17:23:18 +0100

    Right length!

--- examples/true_rbend/008_d1_d2_example.py
+++ examples/true_rbend/008_d1_d2_example.py
@@ -3,21 +3,24 @@ import numpy as np
 
 env = xt.Environment()
 env.vars.default_to_zero = True
-line = env.new_line(length=10., components=[
-    env.new('start', 'Marker', at=0.),
-    env.new('d1a', 'RBend', length_straight=1.0, k0='k0d1a', anchor='start', at=1.),
-    env.new('d1b', 'RBend', length_straight=1.0, k0='k0d1b', anchor='start',   at=3.),
-    env.new('d2',  'RBend', length_straight=1.0, k0='k0d2',  anchor='start', at=8.),
-    env.new('end', 'Marker', at=10.),
-])
+line = env.new_line(compose=True)
+line.new('start', 'Marker', at=0.),
+line.new('d1a', 'RBend', length_straight=1.0, k0='k0d1a', anchor='start', at='dz_d1a'),
+line.new('d1b', 'RBend', length_straight=1.0, k0='k0d1b', anchor='start', at='dz_d1b'),
+line.new('d2',  'RBend', length_straight=1.0, k0='k0d2',  anchor='start', at='dz_d2'),
+line.new('end', 'Marker', at='dz_end'),
+
+# Positions in the straight reference frame
+env['dz_d1a'] = 1.
+env['dz_d1b'] = 3.
+env['dz_d2'] = 8.
+env['dz_end'] = 10.
+
+line.end_compose()
 line.set_particle_ref('proton', p0c=1e9)
 line.configure_drift_model('exact')
 
-l_sliced =line.copy(shallow=True)
-l_sliced.slice_thick_elements(
-        slicing_strategies=[
-            xt.Strategy(slicing=xt.Uniform(6, mode='thick'))
-        ])
+
 
 env['k0d1a'] = 'k0d1'
 env['k0d1b'] = 'k0d1'
@@ -30,7 +33,14 @@ opt = line.match(
 )
 opt.solve()
 
-tw0 = line.twiss(betx=1, bety=1)
+tw0 = line.twiss(betx=1, bety=1, strengths=True)
+
+line.regenerate_from_composer()
+
+env['dz_d1a'] = tw0['s', 'd1a'] - tw0['zeta', 'd1a']
+env['dz_d1b'] = tw0['s', 'd1b'] - tw0['zeta', 'd1b']
+env['dz_d2'] = tw0['s', 'd2'] - tw0['zeta', 'd2']
+env['dz_end'] = tw0['s', 'end'] - tw0['zeta', 'end']
 
 # Introduce magnet curvatures
 d1a_angle_in = np.arcsin(tw0['px', 'd1a'])
@@ -64,9 +74,15 @@ line['d1b'].rbend_shift += line['d1b']._x0_in - tw0['x', 'd1b']
 # d2 exits at 1
 line['d2'].rbend_shift += line['d1a']._x0_out - 1.
 
+line.end_compose()
+
+l_sliced =line.copy(shallow=True)
+l_sliced.slice_thick_elements(
+        slicing_strategies=[
+            xt.Strategy(slicing=xt.Uniform(6, mode='thick'))
+        ])
+
 sv = l_sliced.survey()
-
-
 
 import matplotlib.pyplot as plt
 plt.close('all')
