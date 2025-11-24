23fc79cd1f7405950e91a9f6866a8ae150ae1ef6
giadarol <giovanni.iadarola@cern.ch>
2025-11-24 07:40:16 +0100

    More checks

--- examples/true_rbend/008_d1_d2_example.py
+++ examples/true_rbend/008_d1_d2_example.py
@@ -111,6 +111,15 @@ xo.assert_allclose(tw0['px', 'd1b'], -np.sin(line['d1a']._angle_out), atol=1e-14
 xo.assert_allclose(tw0['px', 'd2'], -np.sin(line['d1b']._angle_out), atol=1e-14)
 xo.assert_allclose(tw0['px', 'end'], -np.sin(line['d2']._angle_out), atol=1e-14)
 
+xo.assert_allclose(tw0['x', 'd1a'], line['d1a']._x0_in, atol=1e-14)
+xo.assert_allclose(tw0['x', 'd1b'], line['d1b']._x0_in, atol=1e-14)
+xo.assert_allclose(tw0['x', 'd2'], line['d2']._x0_in, atol=1e-14)
+
+xo.assert_allclose(tw0['x', 'd1a>>1'], line['d1a']._x0_out, atol=1e-14)
+xo.assert_allclose(tw0['x', 'd1b>>1'], line['d1b']._x0_out, atol=1e-14)
+xo.assert_allclose(tw0['x', 'd2>>1'], line['d2']._x0_out, atol=1e-14)
+
+
 xo.assert_allclose(sv.Z, tw0.s, atol=0, rtol=5e-9)
 xo.assert_allclose(sv.X, tw0.x, atol=0, rtol=3e-8)
 xo.assert_allclose(sv.Y, tw0.y, atol=1e-14)
@@ -119,7 +128,8 @@ xo.assert_allclose(tw.x, 0, atol=1e-14)
 xo.assert_allclose(tw.zeta, 0, atol=1e-14)
 xo.assert_allclose(tw0.y, 0, atol=1e-14)
 
-xo.assert_allclose(tw0['x', 'd2'], line['d2']._x0_in, atol=1e-14)
+
+
 
 sv_sliced.cols['s angle theta X'].show()
 # name                       s         angle         theta             X
