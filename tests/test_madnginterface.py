0538bf1b944b3e08b0f246ce3183ba7de03ddfbe
Bernardo Abreu Figueiredo <bernardo.abreufig@outlook.com>
2025-11-25 08:58:27 +0100

    Added orbit bump test

--- tests/test_madnginterface.py
+++ tests/test_madnginterface.py
@@ -471,3 +471,51 @@ def test_madng_match_optics():
 
     xo.assert_allclose(tw['mux', 'ip1.l1'] - tw['mux', 's.ds.l8.b1'], tw0['mux', 'ip1.l1'] - tw0['mux', 's.ds.l8.b1'], atol=1e-6, rtol=0)
     xo.assert_allclose(tw['muy', 'ip1.l1'] - tw['muy', 's.ds.l8.b1'], tw0['muy', 'ip1.l1'] - tw0['muy', 's.ds.l8.b1'], atol=1e-6, rtol=0)
+
+def test_madng_orbit_bump():
+    env = xt.Environment()
+    env.vars.default_to_zero = True
+    line = env.new_line(length=10, components=[
+        env.new('corr1', xt.Multipole, isthick=True,
+                knl=['kick_h_1'], ksl=['kick_v_1'], length=0.1, at=1),
+        env.new('corr2', xt.Multipole, isthick=True,
+                knl=['kick_h_2'], ksl=['kick_v_2'], length=0.1, at=2),
+        env.new('corr3', xt.Multipole, isthick=True,
+                knl=['kick_h_3'], ksl=['kick_v_3'], length=0.1, at=8),
+        env.new('corr4', xt.Multipole, isthick=True,
+                knl=['kick_h_4'], ksl=['kick_v_4'], length=0.1, at=9),
+        env.new('mid', xt.Marker, at=5),
+        env.new('end', xt.Marker, at=10)
+        ])
+    line.set_particle_ref('proton', p0c=26e9)
+
+    opt = line.match(
+        solve=False,
+        betx=1, bety=1,
+        vary=xt.VaryList(['kick_h_1', 'kick_v_1',
+                        'kick_h_2', 'kick_v_2',
+                        'kick_h_3', 'kick_v_3',
+                        'kick_h_4', 'kick_v_4']),
+        targets=[
+            xt.TargetSet(x=1e-3, y=-2e-3, px=0, py=0, at='mid'),
+            xt.TargetSet(x=0, y=0, px=0, py=0, at='end'),
+        ],
+        use_tpsa=True
+    )
+
+    jac_ng = opt._err.get_jacobian(opt._err._get_x())
+
+    jac_opt = np.array([[-40, 0, -30, 0, 0, 0, 0, 0],
+                        [-100, 0, -100, 0, 0, 0, 0, 0],
+                        [0, 40, 0, 30, 0, 0, 0, 0],
+                        [0, 100, 0, 100, 0, 0, 0, 0],
+                        [-90, 0, -80, 0, -20, 0, -10, 0],
+                        [-100, 0, -100, 0, -100, 0, -100, 0],
+                        [0, 90, 0, 80, 0, 20, 0, 10],
+                        [0, 100, 0, 100, 0, 100, 0, 100]])
+
+    xo.assert_allclose(jac_ng, jac_opt, rtol=1e-12, atol=1e-12)
+
+    opt.solve()
+
+    assert opt._err.call_counter < 7
