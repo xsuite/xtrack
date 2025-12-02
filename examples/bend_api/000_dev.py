88302c9efdc8643a7c6ca6cef6a4c778032b7687
giadarol <giovanni.iadarola@cern.ch>
2025-12-02 09:30:29 +0100

    Fix

--- examples/bend_api/000_dev.py
+++ examples/bend_api/000_dev.py
@@ -10,7 +10,11 @@ bend2.angle = 0.1
 
 bend3 = xt.Bend(angle=0.1, length=1.0)
 
-for bb in [bend1, bend2, bend3]:
+bend1d = xt.Bend.from_dict(bend1.to_dict())
+bend2d = xt.Bend.from_dict(bend2.to_dict())
+bend3d = xt.Bend.from_dict(bend3.to_dict())
+
+for bb in [bend1, bend2, bend3, bend1d, bend2d, bend3d]:
     assert bb.h == 0.1
     assert bb.k0 == 'from_h'
     assert bb.k0_from_h == True
@@ -23,8 +27,8 @@ assert bend1.h == 0.1
 assert bend1.k0_from_h == False
 
 bend2.k0_from_h = False
-assert bend2.k0 == 0.1
+assert bend2.k0 == 0.
 assert bend2.h == 0.1
 assert bend2.k0_from_h == False
 
-dd1 = bend1.to_dict()
+
