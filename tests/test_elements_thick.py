db22c3e72fcd1939e985b4472e991d594cd23efc
giadarol <giovanni.iadarola@cern.ch>
2025-12-03 16:23:20 +0100

    Add tests

--- tests/test_elements_thick.py
+++ tests/test_elements_thick.py
@@ -2991,3 +2991,69 @@ def test_variable_solenoid_x0y0(test_context):
     xo.assert_allclose(tw_test_rad.kin_px, tw_ref_rad.kin_px, rtol=0, atol=1e-14)
     xo.assert_allclose(tw_test_rad.kin_py, tw_ref_rad.kin_py, rtol=0, atol=1e-14)
     xo.assert_allclose(tw_test_rad.delta, tw_ref_rad.delta, rtol=0, atol=1e-14)
+
+def test_api_rbend():
+    bend1 = xt.RBend()
+    bend1.angle = 0.1
+    bend1.length_straight= 1.0
+
+    bend2 = xt.RBend()
+    bend2.length_straight= 1.0
+    bend2.angle = 0.1
+
+    bend3 = xt.RBend(angle=0.1, length_straight=1.0)
+
+    bend1d = xt.RBend.from_dict(bend1.to_dict())
+    bend2d = xt.RBend.from_dict(bend2.to_dict())
+    bend3d = xt.RBend.from_dict(bend3.to_dict())
+
+    for bb in [bend1, bend2, bend3, bend1d, bend2d, bend3d]:
+        assert bb.h == 0.1 / bb.length
+        assert bb.k0 == 'from_h'
+        assert bb.k0_from_h == True
+        assert bb.length_straight == 1.0
+        assert bb.length > 1.0
+        assert bb.angle == 0.1
+
+    bend1.k0 = 0.2
+    assert bend1.k0 == 0.2
+    assert bend1.h == 0.1 / bend1.length
+    assert bend1.k0_from_h == False
+
+    bend2.k0_from_h = False
+    assert bend2.k0 == 0.
+    assert bend2.h == 0.1 / bend2.length
+    assert bend2.k0_from_h == False
+
+def test_api_bend():
+
+    bend1 = xt.Bend()
+    bend1.angle = 0.1
+    bend1.length = 1.0
+
+    bend2 = xt.Bend()
+    bend2.length = 1.0
+    bend2.angle = 0.1
+
+    bend3 = xt.Bend(angle=0.1, length=1.0)
+
+    bend1d = xt.Bend.from_dict(bend1.to_dict())
+    bend2d = xt.Bend.from_dict(bend2.to_dict())
+    bend3d = xt.Bend.from_dict(bend3.to_dict())
+
+    for bb in [bend1, bend2, bend3, bend1d, bend2d, bend3d]:
+        assert bb.h == 0.1
+        assert bb.k0 == 'from_h'
+        assert bb.k0_from_h == True
+        assert bb.length == 1.0
+        assert bb.angle == 0.1
+
+    bend1.k0 = 0.2
+    assert bend1.k0 == 0.2
+    assert bend1.h == 0.1
+    assert bend1.k0_from_h == False
+
+    bend2.k0_from_h = False
+    assert bend2.k0 == 0.
+    assert bend2.h == 0.1
+    assert bend2.k0_from_h == False
