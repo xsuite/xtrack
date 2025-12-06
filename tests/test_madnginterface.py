0d466e466b22a58d1878e2b3f95cdf8b1400f8e3
giadarol <giovanni.iadarola@cern.ch>
2025-12-06 21:51:43 +0100

    Add test

--- tests/test_madnginterface.py
+++ tests/test_madnginterface.py
@@ -324,3 +324,19 @@ def test_madng_slices():
     xo.assert_allclose(twng_ip.wy_ng, twng_ip_sliced.wy_ng, rtol=1e-3)
     xo.assert_allclose(twng_ip.dx_ng, twng_ip_sliced.dx_ng, atol=1e-6)
     xo.assert_allclose(twng_ip.dy_ng, twng_ip_sliced.dy_ng, atol=1e-6)
+
+def test_madng_interface_amplitude_detuning_and_second_order_chrom():
+    line = xt.load(test_data_folder /
+                            'hllhc15_thick/lhc_thick_with_knobs.json')
+
+    twng = line.madng_twiss()
+    det = line.get_amplitude_detuning_coefficients(num_turns=512)
+
+    xo.assert_allclose(twng.dqxdjx_nf_ng, det['det_xx'], rtol=7e-2)
+    xo.assert_allclose(twng.dqydjy_nf_ng, det['det_yy'], rtol=7e-2)
+    xo.assert_allclose(twng.dqxdjy_nf_ng, det['det_xy'], rtol=7e-2)
+    xo.assert_allclose(twng.dqydjx_nf_ng, det['det_yx'], rtol=7e-2)
+
+    tw = line.twiss4d()
+    xo.assert_allclose(tw.ddqx, twng.d2q1_nf_ng, rtol=1e-2)
+    xo.assert_allclose(tw.ddqy, twng.d2q2_nf_ng, rtol=1e-2)
