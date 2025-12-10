787013f487697ae74bcb20608b30b61b1dd1d87b
giadarol <giovanni.iadarola@cern.ch>
2025-12-10 17:08:44 +0100

    Clean example highlighting the problem

--- examples/lattice_design/t007_investigate_insertion.py
+++ examples/lattice_design/t007_investigate_insertion.py
@@ -7,11 +7,23 @@ env.new('m0', xt.Marker)
 env.new('m1', xt.Marker)
 env.new('m2', xt.Marker)
 env.new('m3', xt.Marker)
+env.new('m4', xt.Marker)
+env.new('m5', xt.Marker)
+env.new('m6', xt.Marker)
+env.new('m7', xt.Marker)
+
+
 
 env.new_line(name='myline', compose=True)
 composer = env['myline'].composer
 
-composer.append(
-    env.place('m0', at=0.0),
-    env.place(['m1', 'm2'])
-)
+composer.components.extend([
+    env.place('m0'),
+    env.place('m3', at=10.),
+    env.place(['m1', 'm2']),
+    env.place(['m4', 'm5'], at='m3@start'),
+    env.place(['m6', 'm7'], at='m3@end'),
+])
+
+tt_unsorted = composer.resolve_s_positions(sort=False)
+tt_unsorted.cols['s from_ from_anchor'].show()
