f59eb39027af8f9e54f9a2ff86f85df86d1af4e5
giadarol <giovanni.iadarola@cern.ch>
2025-12-12 21:38:38 +0100

    Using x instead of ddx

--- examples/twiss/035_ddx_ddqx_twiss_6d.py
+++ examples/twiss/035_ddx_ddqx_twiss_6d.py
@@ -3,6 +3,7 @@ import numpy as np
 
 env = xt.load(['../../test_data/sps_thick/sps.seq',
                '../../test_data/sps_thick/lhc_q20.str'])
+
 line = env.sps
 line.set_particle_ref('proton', p0c=26e9)
 
@@ -47,28 +48,52 @@ p_co_minus.zeta -= dzeta
 tw_plus = line.twiss6d(particle_on_co=p_co_plus, compute_chromatic_properties=False)
 tw_minus = line.twiss6d(particle_on_co=p_co_minus, compute_chromatic_properties=False)
 
-delta_plus_ave = np.trapezoid(tw_plus.delta, tw_plus.s) / tw_plus.s[-1]
-delta_minus_ave = np.trapezoid(tw_minus.delta, tw_minus.s) / tw_minus.s[-1]
 
-ddx = (tw_plus.dx - tw_minus.dx) / (tw_plus.delta - tw_minus.delta)
+
 
 tw_center = tw6d
+
 
 delta_plus_mean = np.trapezoid(tw_plus.delta, tw_plus.s) / tw_plus.s[-1]
 delta_minus_mean = np.trapezoid(tw_minus.delta, tw_minus.s) / tw_minus.s[-1]
 delta_center_mean = np.trapezoid(tw_center.delta, tw_center.s) / tw_center.s[-1]
 
-
-
-
 dqx_plus = (tw_plus.mux[-1] - tw_center.mux[-1]) / (delta_plus_mean - delta_center_mean)
 dqx_minus = (tw_center.mux[-1] - tw_minus.mux[-1]) / (delta_center_mean - delta_minus_mean)
 dqy_plus = (tw_plus.muy[-1] - tw_center.muy[-1]) / (delta_plus_mean - delta_center_mean)
 dqy_minus = (tw_center.muy[-1] - tw_minus.muy[-1]) / (delta_center_mean - delta_minus_mean)
-ddqx = (dqx_plus - dqx_minus) / (0.5*(delta_plus_mean - delta_minus_mean))
-ddqy = (dqy_plus - dqy_minus) / (0.5*(delta_plus_mean - delta_minus_mean))
 
+delta_dqxy_plus = 0.5 * (delta_plus_mean + delta_center_mean)
+delta_dqxy_minus = 0.5 * (delta_center_mean + delta_minus_mean)
+ddqx = (dqx_plus - dqx_minus) / (delta_dqxy_plus - delta_dqxy_minus)
+ddqy = (dqy_plus - dqy_minus) / (delta_dqxy_plus - delta_dqxy_minus)
 
+delta_dxdy_plus = 0.5 * (tw_plus.delta + tw_center.delta)
+delta_dxdy_minus = 0.5 * (tw_center.delta + tw_minus.delta)
+
+dx_plus = (tw_plus.x - tw_center.x) / (tw_plus.delta - tw_center.delta)
+dpx_plus = (tw_plus.px - tw_center.px) / (tw_plus.delta - tw_center.delta)
+dy_plus = (tw_plus.y - tw_center.y) / (tw_plus.delta - tw_center.delta)
+dpy_plus = (tw_plus.py - tw_center.py) / (tw_plus.delta - tw_center.delta)
+
+dx_minus = (tw_center.x - tw_minus.x) / (tw_center.delta - tw_minus.delta)
+dpx_minus = (tw_center.px - tw_minus.px) / (tw_center.delta - tw_minus.delta)
+dy_minus = (tw_center.y - tw_minus.y) / (tw_center.delta - tw_minus.delta)
+dpy_minus = (tw_center.py - tw_minus.py) / (tw_center.delta - tw_minus.delta)
+
+# Note unusual definition inherited from MAD-X (ddx = 1/2 d^2 x / d delta^2)
+ddx = (dx_plus - dx_minus) / (delta_dxdy_plus - delta_dxdy_minus)
+ddpx = (dpx_plus - dpx_minus) / (delta_dxdy_plus - delta_dxdy_minus)
+ddy = (dy_plus - dy_minus) / (delta_dxdy_plus - delta_dxdy_minus)
+ddpy = (dpy_plus - dpy_minus) / (delta_dxdy_plus - delta_dxdy_minus)
+
+from cpymad.madx import Madx
+mad = Madx()
+mad.call('../../test_data/sps_thick/sps.seq')
+mad.call('../../test_data/sps_thick/lhc_q20.str')
+mad.beam(particle='proton', pc=26e9)
+mad.use('sps')
+tw = mad.twiss(chrom=True)
 
 
 # ddqx = (tw_plus.dqx - tw_minus.dqx) / (delta_plus_ave - delta_minus_ave)
