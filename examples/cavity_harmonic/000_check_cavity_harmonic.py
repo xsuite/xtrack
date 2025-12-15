503f45f48ab497747ed2b2b56cd3a0f8132430f6
giadarol <giovanni.iadarola@cern.ch>
2025-12-15 17:34:13 +0100

    Preparing checks

--- examples/cavity_harmonic/000_check_cavity_harmonic.py
+++ examples/cavity_harmonic/000_check_cavity_harmonic.py
@@ -90,28 +90,85 @@ xo.assert_allclose(tw_harm.qs, tw3.qs, rtol=0, atol=1e-7)
 line_slice_thick = line.copy()
 line_slice_thick.slice_thick_elements([
     xt.Strategy(slicing=None),
-    xt.Strategy(slicing=xt.Uniform(2, mode='thick'), name='acsca.*'),
+    xt.Strategy(slicing=xt.Uniform(3, mode='thick'), name='acsca.*'),
 ])
 
 tt_sliced = line_slice_thick.get_table(attr=True)
 tt_sliced_cav = tt_sliced.rows.match(element_type='ThickSliceCav.*')
-tt_sliced_cav
-# is (WRONG!!!!)
-Table: 16 rows, 4 cols
-name                   voltage     frequency      harmonic
-acsca.d5l4.b1..0         1e+06             0         35640
-acsca.d5l4.b1..1         1e+06             0         35640
-acsca.c5l4.b1..0         1e+06    4.0079e+08             0
-acsca.c5l4.b1..1         1e+06    4.0079e+08             0
-acsca.b5l4.b1..0         1e+06             0         35640
-acsca.b5l4.b1..1         1e+06             0         35640
-acsca.a5l4.b1..0         1e+06    4.0079e+08             0
-acsca.a5l4.b1..1         1e+06    4.0079e+08             0
-acsca.a5r4.b1..0         1e+06             0         35640
-acsca.a5r4.b1..1         1e+06             0         35640
-acsca.b5r4.b1..0         1e+06    4.0079e+08             0
-acsca.b5r4.b1..1         1e+06    4.0079e+08             0
-acsca.c5r4.b1..0         1e+06             0         35640
-acsca.c5r4.b1..1         1e+06             0         35640
-acsca.d5r4.b1..0         1e+06    4.0079e+08             0
-acsca.d5r4.b1..1         1e+06    4.0079e+08             0
+
+assert np.all(tt_sliced_cav.name == np.array([
+       'acsca.d5l4.b1..0', 'acsca.d5l4.b1..1', 'acsca.d5l4.b1..2',
+       'acsca.c5l4.b1..0', 'acsca.c5l4.b1..1', 'acsca.c5l4.b1..2',
+       'acsca.b5l4.b1..0', 'acsca.b5l4.b1..1', 'acsca.b5l4.b1..2',
+       'acsca.a5l4.b1..0', 'acsca.a5l4.b1..1', 'acsca.a5l4.b1..2',
+       'acsca.a5r4.b1..0', 'acsca.a5r4.b1..1', 'acsca.a5r4.b1..2',
+       'acsca.b5r4.b1..0', 'acsca.b5r4.b1..1', 'acsca.b5r4.b1..2',
+       'acsca.c5r4.b1..0', 'acsca.c5r4.b1..1', 'acsca.c5r4.b1..2',
+       'acsca.d5r4.b1..0', 'acsca.d5r4.b1..1', 'acsca.d5r4.b1..2']))
+
+xo.assert_allclose(tt_sliced_cav.frequency,
+                   [0, 0, 0, 400.79e6, 400.79e6, 400.79e6,
+                    0, 0, 0, 400.79e6, 400.79e6, 400.79e6,
+                    0, 0, 0, 400.79e6, 400.79e6, 400.79e6,
+                    0, 0, 0, 400.79e6, 400.79e6, 400.79e6], rtol=1e-15)
+xo.assert_allclose(tt_sliced_cav.harmonic,
+                     [35640, 35640, 35640, 0, 0, 0,
+                      35640, 35640, 35640, 0, 0, 0,
+                      35640, 35640, 35640, 0, 0, 0,
+                      35640, 35640, 35640, 0, 0, 0], rtol=1e-15)
+xo.assert_allclose(tt_sliced_cav.voltage, 16 / 8 / 3 * 1e6, rtol=1e-3)
+
+tw_slice_thick = line_slice_thick.twiss6d()
+xo.assert_allclose(tw_harm.qs, tw_slice_thick.qs, rtol=0, atol=1e-7)
+
+line_slice_thin = line.copy()
+line_slice_thin.slice_thick_elements([
+    xt.Strategy(slicing=None),
+    xt.Strategy(slicing=xt.Uniform(2, mode='thin'), name='acsca.*'),
+])
+
+tt_sliced_thin = line_slice_thin.get_table(attr=True)
+tt_sliced_thin_cav = tt_sliced_thin.rows.match(element_type='.*SliceCav.*')
+
+assert np.all(tt_sliced_thin_cav.name == np.array(['drift_acsca.d5l4.b1..0', 'acsca.d5l4.b1..0',
+       'drift_acsca.d5l4.b1..1', 'acsca.d5l4.b1..1',
+       'drift_acsca.d5l4.b1..2', 'drift_acsca.c5l4.b1..0',
+       'acsca.c5l4.b1..0', 'drift_acsca.c5l4.b1..1', 'acsca.c5l4.b1..1',
+       'drift_acsca.c5l4.b1..2', 'drift_acsca.b5l4.b1..0',
+       'acsca.b5l4.b1..0', 'drift_acsca.b5l4.b1..1', 'acsca.b5l4.b1..1',
+       'drift_acsca.b5l4.b1..2', 'drift_acsca.a5l4.b1..0',
+       'acsca.a5l4.b1..0', 'drift_acsca.a5l4.b1..1', 'acsca.a5l4.b1..1',
+       'drift_acsca.a5l4.b1..2', 'drift_acsca.a5r4.b1..0',
+       'acsca.a5r4.b1..0', 'drift_acsca.a5r4.b1..1', 'acsca.a5r4.b1..1',
+       'drift_acsca.a5r4.b1..2', 'drift_acsca.b5r4.b1..0',
+       'acsca.b5r4.b1..0', 'drift_acsca.b5r4.b1..1', 'acsca.b5r4.b1..1',
+       'drift_acsca.b5r4.b1..2', 'drift_acsca.c5r4.b1..0',
+       'acsca.c5r4.b1..0', 'drift_acsca.c5r4.b1..1', 'acsca.c5r4.b1..1',
+       'drift_acsca.c5r4.b1..2', 'drift_acsca.d5r4.b1..0',
+       'acsca.d5r4.b1..0', 'drift_acsca.d5r4.b1..1', 'acsca.d5r4.b1..1',
+       'drift_acsca.d5r4.b1..2']))
+xo.assert_allclose(tt_sliced_thin_cav.frequency,
+                   [0.0000e+00, 0.0000e+00, 0.0000e+00, 0.0000e+00, 0.0000e+00,
+                    0.0000e+00, 4.0079e+08, 0.0000e+00, 4.0079e+08, 0.0000e+00,
+                    0.0000e+00, 0.0000e+00, 0.0000e+00, 0.0000e+00, 0.0000e+00,
+                    0.0000e+00, 4.0079e+08, 0.0000e+00, 4.0079e+08, 0.0000e+00,
+                    0.0000e+00, 0.0000e+00, 0.0000e+00, 0.0000e+00, 0.0000e+00,
+                    0.0000e+00, 4.0079e+08, 0.0000e+00, 4.0079e+08, 0.0000e+00,
+                    0.0000e+00, 0.0000e+00, 0.0000e+00, 0.0000e+00, 0.0000e+00,
+                    0.0000e+00, 4.0079e+08, 0.0000e+00, 4.0079e+08, 0.0000e+00], rtol=1e-15)
+
+xo.assert_allclose(tt_sliced_thin_cav.harmonic,
+                   [    0., 35640.,     0., 35640.,     0.,     0.,     0.,     0.,
+                        0.,     0.,     0., 35640.,     0., 35640.,     0.,     0.,
+                        0.,     0.,     0.,     0.,     0., 35640.,     0., 35640.,
+                        0.,     0.,     0.,     0.,     0.,     0.,     0., 35640.,
+                        0., 35640.,     0.,     0.,     0.,     0.,     0.,     0.], rtol=1e-15)
+
+xo.assert_allclose(tt_sliced_thin_cav.voltage,
+                   [      0., 1000000.,       0., 1000000.,       0.,       0.,
+                    1000000.,       0., 1000000.,       0.,       0., 1000000.,
+                          0., 1000000.,       0.,       0., 1000000.,       0.,
+                    1000000.,       0.,       0., 1000000.,       0., 1000000.,
+                          0.,       0., 1000000.,       0., 1000000.,       0.,
+                          0., 1000000.,       0., 1000000.,       0.,       0.,
+                    1000000.,       0., 1000000.,       0.], rtol=1e-15)
