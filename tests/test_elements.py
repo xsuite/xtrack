9a3aa36ee2dd1ccb3bff8933984fbadbff2ece23
giadarol <giovanni.iadarola@cern.ch>
2026-01-08 21:34:18 +0100

    Add test

--- tests/test_elements.py
+++ tests/test_elements.py
@@ -1349,3 +1349,33 @@ def test_beam_element_shifts_backwards_compatible():
     assert sext.shift_x == legacy_dict['_shift_x']
     assert sext.shift_y == legacy_dict['_shift_y']
     assert sext.shift_s == legacy_dict['_shift_s']
+
+def test_cavity_lag_taper():
+
+    env = xt.Environment()
+
+    line = env.new_line(components=[
+        env.new('cav', xt.Cavity, frequency=400e6, voltage=16e6),
+    ])
+    p0 = xt.Particles(p0c=450e9)
+
+    p1 = p0.copy()
+    line.track(p1)
+    xo.assert_allclose(p1.delta, 0, atol=1e-10)
+
+    env['cav'].lag = 60
+    p2 = p0.copy()
+    line.track(p2)
+    assert p2.delta != 0
+
+    env['cav'].lag_taper = -60
+    p3 = p0.copy()
+    line.track(p3)
+    # lag_taper should be ignored because radiation is off
+    xo.assert_allclose(p3.delta, p2.delta, atol=1e-10)
+
+    line.configure_radiation(model='mean')
+    p4 = p0.copy()
+    line.track(p4)
+    # lag_taper should be applied now
+    xo.assert_allclose(p4.delta, 0, atol=1e-10)
