b33563e986a01efc097edc18ea116535127a090b
giadarol <giovanni.iadarola@cern.ch>
2026-01-14 15:29:30 +0100

    Fix

--- xtrack/environment.py
+++ xtrack/environment.py
@@ -91,7 +91,7 @@ class Environment:
         self._particles = {}
         self.particle_ref = particle_ref
 
-        self._var_management = self._init_var_management(dct=_var_management_dct)
+        self._init_var_management(dct=_var_management_dct)
         self._line_vars = EnvVars(self)
 
         self.lines = EnvLines(self)
