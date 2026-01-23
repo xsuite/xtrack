7ddd61e0798e4f5bf03fda7a2e6daad1ac4aa211
giadarol <giovanni.iadarola@cern.ch>
2026-01-23 12:19:17 +0100

    This works

--- examples/rdt/000_dev.py
+++ examples/rdt/000_dev.py
@@ -48,13 +48,25 @@ h_pqrt_l = -(
 
 delta_mux = 2 * math.pi * (mux - mux[0])
 delta_muy = 2 * math.pi * (muy - muy[0])
-
 integrand = h_pqrt_l * np.exp(1j * ((p - q) * delta_mux + (r - t) * delta_muy))
 
 denominator = 1 - np.exp(1j * 2 * math.pi * ((p - q) * qx + (r - t) * qy))
 
 # Integral evaluated at s=0 over one turn
 f_pqrt_s0 = np.sum(integrand) / denominator
+
+delta_mux_second_turn = delta_mux + 2 * math.pi * qx
+delta_muy_second_turn = delta_muy + 2 * math.pi * qy
+integrand_second_turn = h_pqrt_l * np.exp(1j * ((p - q) * delta_mux_second_turn + (r - t) * delta_muy_second_turn))
+
+integrand_two_turns = np.concatenate((integrand, integrand_second_turn))
+
+integrand_two_turns_cumsum = np.cumsum(integrand_two_turns)
+
+# nn = len(s)
+# f_pqrt = np.zeros_like(s, dtype=complex)
+# for i in range(nn):
+#     f_pqrt[i] = (integrand_two_turns_cumsum[i + nn] - integrand_two_turns_cumsum[i]) / denominator
 
 # RTD at all s
 f_pqrt = 0 * integrand
@@ -64,6 +76,22 @@ for i in range(len(s)):
     delta_mux_i[delta_mux_i < 0] += 2 * math.pi * qx
     delta_muy_i[delta_muy_i < 0] += 2 * math.pi * qy
     integrand_i = h_pqrt_l * np.exp(1j * ((p - q) * delta_mux_i + (r - t) * delta_muy_i))
+
+    delta_mux_source = -2 * math.pi * mux
+    delta_muy_source = -2 * math.pi * muy
+    delta_mux_obs = 2 * math.pi * mux[i]
+    delta_muy_obs = 2 * math.pi * muy[i]
+
+    delta_mux_turn = -2 * math.pi * qx * np.ones_like(mux)
+    delta_muy_turn = -2 * math.pi * qy * np.ones_like(muy)
+
+    delta_mux_turn[delta_mux_i > 0] = 0
+    delta_muy_turn[delta_muy_i > 0] = 0
+
+    # integrand_i_2 = h_pqrt_l * np.exp(1j * ((p - q) * (delta_mux_source + delta_mux_obs + delta_mux_turn)
+    #                                + (r - t) * (delta_muy_source + delta_muy_obs + delta_muy_turn)))
+
+
     f_pqrt[i] = np.sum(integrand_i) / denominator
 
 # tw_ng = env.lhcb1.madng_twiss(rdts=[rdt])
