782d8a8f7ebaae0e25889b4230c6a6512a303398
giadarol <giovanni.iadarola@cern.ch>
2026-01-23 12:36:42 +0100

    This also works

--- examples/rdt/000_dev.py
+++ examples/rdt/000_dev.py
@@ -46,42 +46,30 @@ h_pqrt_l = -(
     * bety ** ((r + t) / 2)
 )
 
-delta_mux = 2 * math.pi * (mux - mux[0])
-delta_muy = 2 * math.pi * (muy - muy[0])
-integrand = h_pqrt_l * np.exp(1j * ((p - q) * delta_mux + (r - t) * delta_muy))
 
 denominator = 1 - np.exp(1j * 2 * math.pi * ((p - q) * qx + (r - t) * qy))
 
-# Integral evaluated at s=0 over one turn
-f_pqrt_s0 = np.sum(integrand) / denominator
-
-delta_mux_second_turn = delta_mux + 2 * math.pi * qx
-delta_muy_second_turn = delta_muy + 2 * math.pi * qy
-integrand_second_turn = h_pqrt_l * np.exp(1j * ((p - q) * delta_mux_second_turn + (r - t) * delta_muy_second_turn))
-
-integrand_two_turns = np.concatenate((integrand, integrand_second_turn))
-
-integrand_two_turns_cumsum = np.cumsum(integrand_two_turns)
-
-# nn = len(s)
-# f_pqrt = np.zeros_like(s, dtype=complex)
-# for i in range(nn):
-#     f_pqrt[i] = (integrand_two_turns_cumsum[i + nn] - integrand_two_turns_cumsum[i]) / denominator
+integrand = h_pqrt_l * np.exp(1j * 2 * math.pi * ((p - q) * (-mux) + (r - t) * (-muy)))
+integrand_turn_m1 = h_pqrt_l * np.exp(1j * 2 * math.pi * ((p - q) * (-mux + qx)
+                                          + (r - t) * (-muy + qy)))
+integrand_two_turns = np.concatenate((integrand_turn_m1, integrand))
 
 # RTD at all s
 f_pqrt = 0 * integrand
 for i in range(len(s)):
-    delta_mux_i = 2 * math.pi * (-mux)
-    delta_muy_i = 2 * math.pi * (-muy)
-    delta_mux_add = 2 * math.pi * qx * np.ones_like(mux)
-    delta_muy_add = 2 * math.pi * qy * np.ones_like(muy)
-    delta_mux_add[:i] = 0
-    delta_muy_add[:i] = 0
+    # delta_mux_i = 2 * math.pi * (-mux)
+    # delta_muy_i = 2 * math.pi * (-muy)
+    # delta_mux_add = 2 * math.pi * qx * np.ones_like(mux)
+    # delta_muy_add = 2 * math.pi * qy * np.ones_like(muy)
+    # delta_mux_add[:i] = 0
+    # delta_muy_add[:i] = 0
 
-    delta_mux_i += delta_mux_add
-    delta_muy_i += delta_muy_add
-    integrand_i = h_pqrt_l * np.exp(1j * ((p - q) * delta_mux_i + (r - t) * delta_muy_i))
+    # delta_mux_i += delta_mux_add
+    # delta_muy_i += delta_muy_add
+    # integrand_i = h_pqrt_l * np.exp(1j * ((p - q) * delta_mux_i + (r - t) * delta_muy_i))
 
-    f_pqrt[i] = np.sum(integrand_i) / denominator * np.exp(1j * ((p - q) * (delta_mux[i]) + (r - t) * (delta_muy[i])))
+    integrand_i = integrand_two_turns[i:i+len(s)]
+
+    f_pqrt[i] = np.sum(integrand_i) / denominator * np.exp(1j * 2 * math.pi * ((p - q) * (mux[i]) + (r - t) * (muy[i])))
 
 # tw_ng = env.lhcb1.madng_twiss(rdts=[rdt])
