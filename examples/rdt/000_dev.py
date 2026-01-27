8b54d8540c489f6685630b141e9bc5e97ed25af4
giadarol <giovanni.iadarola@cern.ch>
2026-01-27 17:45:44 +0100

    Octupolar resonance

--- examples/rdt/000_dev.py
+++ examples/rdt/000_dev.py
@@ -70,10 +70,12 @@ def compute_rdt_first_order_perturbation(rdt, twiss, strengths):
 
     return f_pqrt
 
-# line['kqs.a45b1'] = 1e-4  # Example error
+line['kqs.a45b1'] = 1e-4  # Example error
+line['kof.a56b1'] = 1000  # Example error
 tw = line.twiss4d(coupling_edw_teng=True)
 strengths = line.get_table(attr=True)
 f1020 = compute_rdt_first_order_perturbation('f1020', tw, strengths)
 f1001 = compute_rdt_first_order_perturbation('f1001', tw, strengths)
+f2020 = compute_rdt_first_order_perturbation('f2020', tw, strengths)
 
-tw_ng = line.madng_twiss(rdts=['f1020'])
+tw_ng = line.madng_twiss(rdts=['f1020', 'f2020'])
