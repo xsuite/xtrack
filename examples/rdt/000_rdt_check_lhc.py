e3f807beff36bc646d646afe3053b46beeefe337
giadarol <giovanni.iadarola@cern.ch>
2026-01-31 10:04:24 +0100

    RDT checks with skew quads

--- examples/rdt/000_rdt_check_lhc.py
+++ examples/rdt/000_rdt_check_lhc.py
@@ -12,14 +12,19 @@ tw0 = line.twiss(method='4d')
 # Coupling RDTs
 line['kqs.a45b1'] = 1e-4 # Introduce a skew quadrupole
 rdts = xt.rdt_first_order_perturbation(
-    rdt=['f1001', 'f1010'],
+    rdt=['f1001', 'f1010', 'f0110'],
     twiss=tw0,
     strengths=line.get_table(attr=True)
 )
 tw_edw_teng = line.twiss4d(coupling_edw_teng=True)
+line['kqs.a45b1'] = 0.0 # Remove skew quadrupole
 
+# outliers came from the fact that at the sources the RDT jumps at entry
+# instead of exit of the element or viceversa
 xo.assert_allclose(rdts.f1001, tw_edw_teng.f1001, rtol=0.05)
-xo.assert_allclose(rdts.f1010, tw_edw_teng.f1010, rtol=0.05)
+xo.assert_allclose(rdts.f1010, tw_edw_teng.f1010, rtol=0.05, max_outliers=5)
+xo.assert_allclose(rdts.f0110, tw_edw_teng.f0110, rtol=0.05)
+xo.assert_allclose(rdts.f1001, np.conj(rdts.f0110), rtol=1e-12)
 
 
 # #   # Example error
