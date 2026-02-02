576e83ef8cdc58814c04fd9ed59a79ce23144473
giadarol <giovanni.iadarola@cern.ch>
2026-02-02 09:31:38 +0100

    Allocated monitor

--- examples/monitor_multi_element/000_dev.py
+++ examples/monitor_multi_element/000_dev.py
@@ -24,3 +24,21 @@ at_element_mapping[:] = -1
 at_element_mapping[indeces_bpm] = np.arange(len(indeces_bpm), dtype=np.int64)
 
 
+start_at_turn = 0
+stop_at_turn = 10
+part_id_start = 5
+part_id_end = 12
+
+n_turns = stop_at_turn - start_at_turn
+n_particles = part_id_end - part_id_start
+n_elements = len(indeces_bpm)
+n_coordinates = 6
+
+multi_elem_monit = MultiElementMonitor(
+    start_at_turn=start_at_turn,
+    stop_at_turn=stop_at_turn,
+    part_id_start=part_id_start,
+    part_id_end=part_id_end,
+    at_element_mapping=at_element_mapping,
+    data=(n_turns, n_particles, n_coordinates, n_elements)
+)
