605ab308da3dc4198f9707e09d2bee65c07122da
giadarol <giovanni.iadarola@cern.ch>
2026-03-03 21:39:16 +0100

    Rename

--- examples/multipole_errors_relative/001a_check_quadrupole.py
+++ examples/multipole_errors_relative/001a_check_quadrupole.py
@@ -11,7 +11,7 @@ ksl_rel = np.array([2, 40, 60, 800, 100000, 1200000])
 k1 = 0.001
 k1s = 0.002
 
-q_test = xt.Quadrupole(k1=k1, k1s=k1s, length=0.2,
+el_test = xt.Quadrupole(k1=k1, k1s=k1s, length=0.2,
                        knl=knl,
                        ksl=ksl,
                        knl_rel=knl_rel,
@@ -19,41 +19,41 @@ q_test = xt.Quadrupole(k1=k1, k1s=k1s, length=0.2,
                        main_is_skew=main_is_skew)
 
 if main_is_skew:
-    xo.assert_allclose(q_test.main_strength, 0.002 * 0.2, rtol=0, atol=1e-12)
+    xo.assert_allclose(el_test.main_strength, 0.002 * 0.2, rtol=0, atol=1e-12)
 else:
-    xo.assert_allclose(q_test.main_strength, 0.001 * 0.2, rtol=0, atol=1e-12)
+    xo.assert_allclose(el_test.main_strength, 0.001 * 0.2, rtol=0, atol=1e-12)
 
-expected_knl = knl + knl_rel * q_test.main_strength
-expected_ksl = ksl + ksl_rel * q_test.main_strength
+expected_knl = knl + knl_rel * el_test.main_strength
+expected_ksl = ksl + ksl_rel * el_test.main_strength
 
-expected_knl[1] += k1 * q_test.length
-expected_ksl[1] += k1s * q_test.length
+expected_knl[1] += k1 * el_test.length
+expected_ksl[1] += k1s * el_test.length
 
-knl_tot, ksl_tot = q_test.get_total_knl_ksl()
+knl_tot, ksl_tot = el_test.get_total_knl_ksl()
 
 xo.assert_allclose(knl_tot, expected_knl, rtol=0, atol=1e-12)
 xo.assert_allclose(ksl_tot, expected_ksl, rtol=0, atol=1e-12)
 
-q_ref = xt.Quadrupole(k1=k1, k1s=k1s, length=0.2,
+el_ref = xt.Quadrupole(k1=k1, k1s=k1s, length=0.2,
                       knl=expected_knl, ksl=expected_ksl)
-q_ref.knl[1] -= k1 * q_ref.length  # Not to double count the main quadrupole contribution
-q_ref.ksl[1] -= k1s * q_ref.length
+el_ref.knl[1] -= k1 * el_ref.length  # Not to double count the main quadrupole contribution
+el_ref.ksl[1] -= k1s * el_ref.length
 
 p0 = xt.Particles(p0c=1e9, x=1e-2, y=2e-2)
 
 p_test = p0.copy()
-q_test.track(p_test)
+el_test.track(p_test)
 
 p_ref = p0.copy()
-q_ref.track(p_ref)
+el_ref.track(p_ref)
 
 xo.assert_allclose(p_test.x, p_ref.x, rtol=0, atol=1e-13)
 xo.assert_allclose(p_test.y, p_ref.y, rtol=0, atol=1e-13)
 xo.assert_allclose(p_test.px, p_ref.px, rtol=0, atol=1e-13)
 xo.assert_allclose(p_test.py, p_ref.py, rtol=0, atol=1e-13)
 
-line_test = xt.Line(elements=[q_test])
-line_ref = xt.Line(elements=[q_ref])
+line_test = xt.Line(elements=[el_test])
+line_ref = xt.Line(elements=[el_ref])
 
 # Check thick slicing
 line_test_slice_thick = line_test.copy(shallow=True)
