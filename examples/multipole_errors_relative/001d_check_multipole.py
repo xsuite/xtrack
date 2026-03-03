8143ecc875038d6574a7b7a16be359bf13eddbd5
giadarol <giovanni.iadarola@cern.ch>
2026-03-03 21:50:17 +0100

    More checks

--- examples/multipole_errors_relative/001d_check_multipole.py
+++ examples/multipole_errors_relative/001d_check_multipole.py
@@ -8,36 +8,29 @@ knl = np.array([0.001, 1e-3, 2e-2, 3e-2, 4, 50])
 ksl = np.array([0.002, 2e-3, 3e-2, 4e-2, 5, 60])
 knl_rel = np.array([1, 20, 30, 400, 50000, 6000000])
 ksl_rel = np.array([2, 40, 60, 800, 100000, 1200000])
-k3 = 0.001
-k3s = 0.002
 
-el_test = xt.Octupole(k3=k3, k3s=k3s, length=0.2,
+el_test = xt.Multipole(length=0.2,
                        knl=knl,
                        ksl=ksl,
                        knl_rel=knl_rel,
                        ksl_rel=ksl_rel,
+                       main_order=4,
                        main_is_skew=main_is_skew)
 
 if main_is_skew:
-    xo.assert_allclose(el_test.main_strength, 0.002 * 0.2, rtol=0, atol=1e-12)
+    xo.assert_allclose(el_test.main_strength, 5., rtol=0, atol=1e-12)
 else:
-    xo.assert_allclose(el_test.main_strength, 0.001 * 0.2, rtol=0, atol=1e-12)
+    xo.assert_allclose(el_test.main_strength, 4., rtol=0, atol=1e-12)
 
 expected_knl = knl + knl_rel * el_test.main_strength
 expected_ksl = ksl + ksl_rel * el_test.main_strength
-
-expected_knl[3] += k3 * el_test.length
-expected_ksl[3] += k3s * el_test.length
 
 knl_tot, ksl_tot = el_test.get_total_knl_ksl()
 
 xo.assert_allclose(knl_tot, expected_knl, rtol=0, atol=1e-12)
 xo.assert_allclose(ksl_tot, expected_ksl, rtol=0, atol=1e-12)
 
-el_ref = xt.Octupole(k3=k3, k3s=k3s, length=0.2,
-                      knl=expected_knl, ksl=expected_ksl)
-el_ref.knl[3] -= k3 * el_ref.length  # Not to double count the main octupole contribution
-el_ref.ksl[3] -= k3s * el_ref.length
+el_ref = xt.Multipole(length=0.2, knl=expected_knl, ksl=expected_ksl)
 
 p0 = xt.Particles(p0c=1e9, x=1e-2, y=2e-2)
 
