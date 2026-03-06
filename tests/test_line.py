322e7b07e28f4e6456e124578584bd31b08f8c9a
giadarol <giovanni.iadarola@cern.ch>
2026-03-06 18:29:07 +0100

    Adapt test

--- tests/test_line.py
+++ tests/test_line.py
@@ -355,7 +355,7 @@ def test_to_pandas():
 
     assert tuple(df.columns) == (
         's', 'element_type', 'name', 'isthick', 'isreplica', 'parent_name',
-       'iscollective', 'element', 's_start', 's_center', 's_end')
+       'parent_type', 'iscollective', 'element', 's_start', 's_center', 's_end')
     assert len(df) == 4
 
 def test_check_aperture():
