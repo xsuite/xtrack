dfa0fc15fbecafc01391e3798d19ba2cbad4e5ea
giadarol <giovanni.iadarola@cern.ch>
2026-03-10 11:45:51 +0100

    In mad writer raise if knl_rel is present

--- examples/multipole_errors_relative/000_dev.py
+++ examples/multipole_errors_relative/000_dev.py
@@ -6,25 +6,12 @@ import xtrack as xt
 # - [DONE] need a property on rel_ref_is_skew
 # - [DONE] need method to see the total multipole strength
 # - [DONE] need method to extend knl_rel and ksl_rel when needed
-# - MAD-X and MAD-NG interface
+# - [DONE] Raise in MAD-X and MAD-NG interface if relative multipole errors are used, as they are not supported in MAD-X and MAD-NG
 # - [DONE] Add new parameters to docstring
 # - [DONE] Add new parameters to to __repr__
-# - Update env.set_multipole_errors_relative
-# - Update docs
+# - [DONE] Warning env.set_multipole_errors_relative
 # - [DONE] Remove rel_ref_is_skew from bends
 # - [Checked] RDT calculation
 # - [Checked] Radiation integrals
 # - [Checked] Polarization calculation
 # - [DONE] Backtracking
-
-m = xt.Multipole(knl=[1e-3], ksl=[2e-3])
-
-p0 = xt.Particles(p0c=1e9)
-
-p = p0.copy()
-m.track(p)
-
-m.knl_rel = [0.5]
-m.rel_ref_is_skew = True
-p = p0.copy()
-m.track(p)
