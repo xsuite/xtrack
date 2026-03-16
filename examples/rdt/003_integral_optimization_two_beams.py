6cc37e1ebc605f8f776b50d72a416bbd3989dde4
giadarol <giovanni.iadarola@cern.ch>
2026-03-16 20:18:58 +0100

    Going in the right direction

--- examples/rdt/003_integral_optimization_two_beams.py
+++ examples/rdt/003_integral_optimization_two_beams.py
@@ -18,112 +18,55 @@ env['mbrd.4l1.b2'].ksl[1] = -6e-5
 env['mbrd.4r1.b1'].ksl[1] = -3e-5
 env['mbrd.4r1.b2'].ksl[1] = 4e-5
 
-env['mbrd.4l5.b1'].ksl[1] = -5e-5
-env['mbrd.4l5.b2'].ksl[1] = 6e-5
-env['mbrd.4r5.b1'].ksl[1] = -3e-5
-env['mbrd.4r5.b2'].ksl[1] = 4e-5
+# env['mbrd.4l5.b1'].ksl[1] = -5e-5
+# env['mbrd.4l5.b2'].ksl[1] = 6e-5
+# env['mbrd.4r5.b1'].ksl[1] = -3e-5
+# env['mbrd.4r5.b2'].ksl[1] = 4e-5
 
 tw_uncorr_b1 = line_b1.twiss4d()
 tw_uncorr_b2 = line_b2.twiss4d()
 
-
-# Local correction IP1 right
+# Local correction b1 ir1
 integral_optim_ir1_b1  = xt.IntegralOptimization(
     twiss=tw_ref_b1,
     line=line_b1,
-    start='ip1', end='s.ds.r1.b1',
-    vary=xt.VaryList(['kqsx3.r1'], step=1e-6),
+    start='e.ds.l1.b1', end='s.ds.r1.b1',
+    vary=xt.VaryList(['kqsx3.r1', 'kqsx3.l1'], step=1e-6),
     target_quantities={'f1001_b1': 'f1001'},
-    generated_knob_name='on_corr_k1s_r1'
+    generated_knob_name='on_corr_k1s_ir1_local'
 )
 
+# Local correction b2 ir1 (only targets here)
 integral_optim_ir1_b2  = xt.IntegralOptimization(
     twiss=tw_ref_b2,
     line=line_b2,
-    start='s.ds.r1.b2', end='ip1',
+    start='s.ds.r1.b2', end='e.ds.l1.b2',
     vary=[], # only targets here
     target_quantities={'f1001_b2': 'f1001'},
     generated_knob_name=None
 )
 
 # Build combined optimizer
-opt_r1_b1 = integral_optim_ir1_b1.get_optimizer()
-opt_r1_b2 = integral_optim_ir1_b2.get_optimizer()
-opt_r1 = opt_r1_b1.opt.clone(add_targets=opt_r1_b2.opt.targets)
+opt_ir1_b1 = integral_optim_ir1_b1.get_optimizer()
+opt_ir1_b2 = integral_optim_ir1_b2.get_optimizer()
+opt_ir1 = opt_ir1_b1.opt.clone(add_targets=opt_ir1_b2.opt.targets)
 
+# impose the two correctors are powered equally
+# (they are not independed because the phase advance is pi and leaving them free
+# leads to an ill-conditioned optimization problem)
+env['kqsx3.l1_from_on_corr_k1s_ir1_local'] = 'kqsx3.r1_from_on_corr_k1s_ir1_local'
+opt_ir1.disable(vary_name='kqsx3.l1_from_on_corr_k1s_ir1_local')
+opt_ir1.step(n_steps=5)
+opt_ir1_b1.generate_knob()
 
-# Local correction IP1
-integral_optim_ir1_left_b1  = xt.IntegralOptimization(
-    twiss=tw_ref_b1,
-    line=line_b1,
-    start='e.ds.l1.b1', end='ip1',
-    vary=xt.VaryList(['kqsx3.l1'], step=1e-6),
-    target_quantities={'f1001_b1': 'f1001'},
-    generated_knob_name='on_corr_k1s_l1'
-)
-integral_optim_ir1_left_b2  = xt.IntegralOptimization(
-    twiss=tw_ref_b2,
-    line=line_b2,
-    start='ip1', end='e.ds.l1.b2',
-    vary=[], # only targets here
-    target_quantities={'f1001_b2': 'f1001'},
-    generated_knob_name=None
-)
-opt_l1_b1 = integral_optim_ir1_left_b1.get_optimizer()
-opt_l1_b2 = integral_optim_ir1_left_b2.get_optimizer()
-opt_l1 = opt_l1_b1.opt.clone(add_targets=opt_l1_b2.opt.targets)
+env['on_corr_k1s_ir1_local'] = 1.0
 
-opt_r1.step(n_steps=5)
-opt_l1.step(n_steps=5)
-opt_r1_b1.generate_knob()
-opt_l1_b1.generate_knob()
+prrrrr
 
-# Local correction IP5 right
-integral_optim_ir5_b1  = xt.IntegralOptimization(
-    twiss=tw_ref_b1,
-    line=line_b1,
-    start='ip5', end='s.ds.r5.b1',
-    vary=xt.VaryList(['kqsx3.r5'], step=1e-6),
-    target_quantities={'f1001_b1': 'f1001'},
-    generated_knob_name='on_corr_k1s_r5'
-)
-integral_optim_ir5_b2  = xt.IntegralOptimization(
-    twiss=tw_ref_b2,
-    line=line_b2,
-    start='s.ds.r5.b2', end='ip5',
-    vary=[], # only targets here
-    target_quantities={'f1001_b2': 'f1001'},
-    generated_knob_name=None
-)
-opt_r5_b1 = integral_optim_ir5_b1.get_optimizer()
-opt_r5_b2 = integral_optim_ir5_b2.get_optimizer()
-opt_r5 = opt_r5_b1.opt.clone(add_targets=opt_r5_b2.opt.targets)
+# # Optimize and generate knob
+# opt_ir1.step(n_steps=5)
+# opt_ir1_b1.generate_knob()
 
-integral_optim_ir5_left_b1  = xt.IntegralOptimization(
-    twiss=tw_ref_b1,
-    line=line_b1,
-    start='e.ds.l5.b1', end='ip5',
-    vary=xt.VaryList(['kqsx3.l5'], step=1e-6),
-    target_quantities={'f1001_b1': 'f1001'},
-    generated_knob_name='on_corr_k1s_l5'
-)
-integral_optim_ir5_left_b2  = xt.IntegralOptimization(
-    twiss=tw_ref_b2,
-    line=line_b2,
-    start='ip5', end='e.ds.l5.b2',
-    vary=[], # only targets here
-    target_quantities={'f1001_b2': 'f1001'},
-    generated_knob_name=None
-)
-opt_l5_b1 = integral_optim_ir5_left_b1.get_optimizer()
-opt_l5_b2 = integral_optim_ir5_left_b2.get_optimizer()
-opt_l5 = opt_l5_b1.opt.clone(add_targets=opt_l5_b2.opt.targets)
-
-# Corrections ON
-env['on_corr_k1s_r1'] = 1.0
-env['on_corr_k1s_l1'] = 1.0
-env['on_corr_k1s_r5'] = 1.0
-env['on_corr_k1s_l5'] = 1.0
 
 tw_local_corr_b1 = line_b1.twiss4d()
 tw_local_corr_b2 = line_b2.twiss4d()
