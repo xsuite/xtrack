6e721eb154bc159f38283bb98497fd9633db41e1
giadarol <giovanni.iadarola@cern.ch>
2026-04-22 19:06:03 +0200

    Issue with RBend

---
+++ examples/true_rbend/t001_check_s.py
@@ -0,0 +1,27 @@
+import xtrack as xt
+import numpy as np
+
+env = xt.Environment()
+line = env.new_line(components=[
+    env.new('rb', xt.RBend, length_straight=2, angle=np.pi/3, rbend_model='straight-body'),
+])
+
+line_thin = line.copy(shallow=True)
+line_thin.slice_thick_elements(
+    slicing_strategies=[
+        xt.Strategy(slicing=xt.Teapot(3, mode='thin'))
+    ]
+)
+
+p0 = xt.Particles(mass0=xt.ELECTRON_MASS_EV, q0=1, p0c=45.6e9)
+p_thick = p0.copy()
+p_thin = p0.copy()
+
+line.reset_s_at_end_turn = False
+line_thin.reset_s_at_end_turn = False
+line.discard_tracker()
+line_thin.discard_tracker()
+line.build_tracker(compile=True, use_prebuilt_kernels=False)
+line_thin.build_tracker(compile=True, use_prebuilt_kernels=False)
+line.track(p_thick, num_turns=1)
+line_thin.track(p_thin, num_turns=1)
