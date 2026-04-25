bfd341e7b12cccfdf2941df0d193e53853dc4fc3
giadarol <giovanni.iadarola@cern.ch>
2026-04-25 22:28:47 +0200

    Compare trajectories on linear fringe solenoid

--- examples/boris_spatial/004_study_convergence.py
+++ examples/boris_spatial/004_study_convergence.py
@@ -1,10 +1,7 @@
 import numpy as np
-from scipy.constants import c as clight
-from scipy.constants import e as qe
-
-import xobjects as xo
 import xtrack as xt
-from xtrack._temp.boris_and_solenoid_map.solenoid_field import SolenoidField
+
+from linear_fringe_solenoid import LinearFringeSolenoid
 
 p0 = xt.Particles(mass0=xt.ELECTRON_MASS_EV, q0=1,
                 energy0=45.6e9/1000,
@@ -14,41 +11,6 @@ p0 = xt.Particles(mass0=xt.ELECTRON_MASS_EV, q0=1,
                 delta=0)
 
 
-class LinearFringeSolenoid:
-
-    def __init__(self, B0, s1, s2, s3, s4):
-        self.B0 = B0
-        self.s1 = s1
-        self.s2 = s2
-        self.s3 = s3
-        self.s4 = s4
-
-    def get_field(self, x, y, s):
-
-        mask_entry_taper = (s >= self.s1) & (s < self.s2)
-        mask_constant = (s >= self.s2) & (s < self.s3)
-        mask_exit_taper = (s >= self.s3) & (s < self.s4)
-
-        Bx = np.zeros_like(x)
-        By = np.zeros_like(y)
-        Bz = np.zeros_like(s)
-
-        Bz[mask_entry_taper] = self.B0 * (s[mask_entry_taper] - self.s1) / (self.s2 - self.s1)
-        Bz[mask_constant] = self.B0
-        Bz[mask_exit_taper] = self.B0 * (self.s4 - s[mask_exit_taper]) / (self.s4 - self.s3)
-
-        # Bx = -x dBz/dz /2
-        # By = -y dBz/dz /2
-
-        dBz_ds_entry = self.B0 / (self.s2 - self.s1)
-        dBz_ds_exit = -self.B0 / (self.s4 - self.s3)
-
-        Bx[mask_entry_taper] = -x[mask_entry_taper] * dBz_ds_entry / 2
-        Bx[mask_exit_taper] = -x[mask_exit_taper] * dBz_ds_exit / 2
-        By[mask_entry_taper] = -y[mask_entry_taper] * dBz_ds_entry / 2
-        By[mask_exit_taper] = -y[mask_exit_taper] * dBz_ds_exit / 2
-
-        return Bx, By, Bz
 
 sf = LinearFringeSolenoid(B0=1.0, s1=0.0, s2=3, s3=7, s4=10)
 
