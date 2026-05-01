a5dac88f1d91286e6f43f258b17a241b8dfe37eb
giadarol <giovanni.iadarola@cern.ch>
2026-05-01 23:38:44 +0200

    Adapt tests

--- tests/test_vs_madx.py
+++ tests/test_vs_madx.py
@@ -3,6 +3,7 @@
 # Copyright (c) CERN, 2021.                 #
 # ######################################### #
 
+import math
 import pathlib
 
 import numpy as np
@@ -537,11 +538,12 @@ def test_line_import_from_madx(test_context, mad_with_errors):
     line.vars['vrf400'] = 8
     xo.assert_allclose(line['acsca.b5l4.b1'].voltage,
                       1e6, rtol=0, atol=1e-14)
-
-    xo.assert_allclose(line['acsca.b5l4.b1'].lag, 180,
+    xo.assert_allclose(line['acsca.b5l4.b1'].lag, 0,
+                    rtol=0, atol=1e-14)
+    xo.assert_allclose(line['acsca.b5l4.b1'].phase, math.pi,
                     rtol=0, atol=1e-14)
     line.vars['lagrf400.b1'] = 0.75
-    xo.assert_allclose(line['acsca.b5l4.b1'].lag, 270,
+    xo.assert_allclose(line['acsca.b5l4.b1'].phase, 0.75*2*math.pi,
                     rtol=0, atol=1e-14)
 
     assert np.abs(line['acfcav.bl5.b1'].crab_voltage) > 0
@@ -549,17 +551,17 @@ def test_line_import_from_madx(test_context, mad_with_errors):
     line.vars['on_crab5'] = 0
     assert np.abs(line['acfcav.bl5.b1'].crab_voltage) == 0
 
-    xo.assert_allclose(line['acfcav.bl5.b1'].lag, 0, rtol=0, atol=1e-14)
+    xo.assert_allclose(line['acfcav.bl5.b1'].phase, 0, rtol=0, atol=1e-14)
     line.vars['phi_crab_l5b1'] = 0.5
-    xo.assert_allclose(line['acfcav.bl5.b1'].lag, 180, rtol=0, atol=1e-14)
+    xo.assert_allclose(line['acfcav.bl5.b1'].phase, math.pi, rtol=0, atol=1e-14)
 
     assert np.abs(line['acfcah.bl1.b1'].crab_voltage) > 0
     line.vars['on_crab1'] = 0
     assert np.abs(line['acfcah.bl1.b1'].crab_voltage) == 0
 
-    xo.assert_allclose(line['acfcah.bl1.b1'].lag, 0, rtol=0, atol=1e-14)
+    xo.assert_allclose(line['acfcah.bl1.b1'].phase, 0, rtol=0, atol=1e-14)
     line.vars['phi_crab_l1b1'] = 0.5
-    xo.assert_allclose(line['acfcah.bl1.b1'].lag, 180, rtol=0, atol=1e-14)
+    xo.assert_allclose(line['acfcah.bl1.b1'].phase, math.pi, rtol=0, atol=1e-14)
 
     assert np.abs(line['acfcah.bl1.b1'].frequency) > 0
     assert np.abs(line['acfcav.bl5.b1'].frequency) > 0
