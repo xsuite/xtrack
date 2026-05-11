d9ce41cbb7b85576052ce9fcccbe304c2aa4b7a8
giadarol <giovanni.iadarola@cern.ch>
2026-05-11 21:46:08 +0200

    Update solenoid example

--- examples/solenoid/005a_on_beam_ref_benchmark.py
+++ examples/solenoid/005a_on_beam_ref_benchmark.py
@@ -9,8 +9,13 @@ class TiltedSolenoid:
 
         self._sf = SolenoidField(L=L, a=a, B0=B0, z0=0)
         self.theta = theta
+        self._break = False
 
     def get_field(self, x, y, z):
+
+        if self._break:
+            breakpoint()
+
         theta = self.theta
         stheta = np.sin(theta)
         ctheta = np.cos(theta)
@@ -46,6 +51,7 @@ k0 = by / p.rigidity0[0]
 env['on_sol'] = 1
 
 ele_names = []
+boris_ele_names = []
 for ii in range(len(s)-1):
     ks_entry = ks[ii]
     ks_exit = ks[ii+1]
@@ -65,15 +71,29 @@ for ii in range(len(s)-1):
         ks_profile=[ks_entry * env.ref['on_sol'], ks_exit * env.ref['on_sol']],
         knl=[0.5 * (k0_exit + k0_entry) * length * env.ref['on_sol']],
         ksl=[0.5 * (k0s_exit + k0s_entry) * length * env.ref['on_sol']],
-        x0=x0_mid,
-        y0=0,
+        # # x0=x0_mid,
+        # y0=0,
     )
     ele_names.append(f'sol_slice_{ii}')
+
+    new_boris = xt.BorisSpatialIntegrator(
+        fieldmap_callable=sf.get_field,
+        s_start=s_entry,
+        s_end=s_exit,
+        n_steps=100,
+        verbose=False,
+    )
+    env.elements[f'boris_{ii}'] = new_boris
+    boris_ele_names.append(f'boris_{ii}')
 
 line_solenoid = env.new_line(components=ele_names)
 line_solenoid.set_particle_ref('ref_part')
 
+line_boris = env.new_line(components=boris_ele_names)
+line_boris.set_particle_ref('ref_part')
+
 tw = line_solenoid.twiss4d(betx=1, bety=1)
+tw_boris = line_boris.twiss4d(betx=1, bety=1, include_collective=True)
 
 fcc = env.new_line(length=100, components=[
     env.new('ip', xt.Marker, at=50)
@@ -85,6 +105,7 @@ fcc.select('ip', "sol_slice_183").get_table()
 
 import matplotlib.pyplot as plt
 plt.close('all')
+plt.figure(1)
 plt.plot(s, bx, '.-',label='bx')
 plt.plot(s, by, '.-', label='by')
 plt.plot(s, bz, '.-', label='bz')
@@ -93,5 +114,38 @@ plt.xlabel('s [m]')
 plt.ylabel('B [T]')
 plt.title('Field on the reference trajectory')
 plt.grid()
+
+plt.figure(2)
+ax1 = plt.subplot(211)
+ax1.plot(tw.s, tw.x, label='Xsuite')
+ax1.plot(tw_boris.s, tw_boris.x, '--', label='Boris + Fieldmap')
+ax1.legend()
+ax1.set_xlabel('s [m]')
+ax1.set_ylabel('x [m]')
+ax1.grid()
+ax2 = plt.subplot(212, sharex=ax1)
+ax2.plot(tw.s, tw.y, label='Xsuite')
+ax2.plot(tw_boris.s, tw_boris.y, '--', label='Boris + Fieldmap')
+ax2.legend()
+ax2.set_xlabel('s [m]')
+ax2.set_ylabel('y [m]')
+ax2.grid()
+
+plt.figure(3)
+ax31 = plt.subplot(211)
+ax31.plot(tw.s, tw.kin_px, label='Xsuite')
+ax31.plot(tw_boris.s, tw_boris.kin_px, '--', label='Boris + Fieldmap')
+ax31.legend()
+ax31.set_xlabel('s [m]')
+ax31.set_ylabel(r'$p_{x,kin}$')
+ax31.grid()
+ax32 = plt.subplot(212, sharex=ax31)
+ax32.plot(tw.s, tw.kin_py, label='Xsuite')
+ax32.plot(tw_boris.s, tw_boris.kin_py, '--', label='Boris + Fieldmap')
+ax32.legend()
+ax32.set_xlabel('s [m]')
+ax32.set_ylabel(r'$p_{y,kin}$')
+ax32.grid()
+plt.subplots_adjust(left=.15)
 
 plt.show()
