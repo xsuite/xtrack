99a59d69c7aaa47d3e674708c40f2d7445b0098c
giadarol <giovanni.iadarola@cern.ch>
2026-05-15 15:08:08 +0200

    Enable warning on Cavity and CrabCavity

--- xtrack/beam_elements/elements.py
+++ xtrack/beam_elements/elements.py
@@ -817,14 +817,13 @@ class Cavity(_HasModelRF, _HasIntegrator, BeamElement):
 
     @lag.setter
     def lag(self, value):
-        # TODO: Warning to be activated when acc-models repos are updated
-        # if value != 0:
-        #     warn("`lag` (in degrees) is deprecated and will be removed in a future version. "
-        #          "Please use `phase` (in radians) instead. "
-        #          "Note that if both `lag` and `phase` are set, the effect is the sum of the two," \
-        #          " with `lag` converted to radians. "
-        #          + DEPRECATION_INFO_PREP_1_0,
-        #          FutureWarning, stacklevel=2)
+        if value != 0:
+            warn("`lag` (in degrees) is deprecated and will be removed in a future version. "
+                 "Please use `phase` (in radians) instead. "
+                 "Note that if both `lag` and `phase` are set, the effect is the sum of the two," \
+                 " with `lag` converted to radians. "
+                 + DEPRECATION_INFO_PREP_1_0,
+                 FutureWarning, stacklevel=2)
         self._lag = value
 
     @property
@@ -937,14 +936,13 @@ class CrabCavity(_HasModelRF, _HasIntegrator, BeamElement):
 
     @lag.setter
     def lag(self, value):
-        # TODO: Warning to be activated when acc-models repos are updated
-        # if value != 0:
-        #     warn("`lag` (in degrees) is deprecated and will be removed in a future version. "
-        #          "Please use `phase` (in radians) instead. "
-        #          "Note that if both `lag` and `phase` are set, the effect is the sum of the two," \
-        #          " with `lag` converted to radians. "
-        #          + DEPRECATION_INFO_PREP_1_0,
-        #          FutureWarning, stacklevel=2)
+        if value != 0:
+            warn("`lag` (in degrees) is deprecated and will be removed in a future version. "
+                 "Please use `phase` (in radians) instead. "
+                 "Note that if both `lag` and `phase` are set, the effect is the sum of the two," \
+                 " with `lag` converted to radians. "
+                 + DEPRECATION_INFO_PREP_1_0,
+                 FutureWarning, stacklevel=2)
         self._lag = value
 
     @property
