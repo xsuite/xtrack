4a2b7cff802d89c86b4d952f46136f48f1fb4b2c
giadarol <giovanni.iadarola@cern.ch>
2026-05-22 14:17:04 +0200

    Start parametrizing the script

--- examples/fcc_ee_solenoid/000_dev.py
+++ examples/fcc_ee_solenoid/000_dev.py
@@ -14,8 +14,8 @@ line.insert('dy_match_r_'+ip_name, xt.Marker(), at=11.95, from_=ip_name)
 line.insert('dy_match_l_'+ip_name, xt.Marker(), at=-11.95, from_=ip_name)
 
 line.insert([
-    env.new('corr_sol_right'+ip_name, xt.Multipole, at=0, from_=f'dy_match_r_{ip_name}@start'),
-    env.new('corr_sol_left'+ip_name, xt.Multipole, at=0, from_=f'dy_match_l_{ip_name}@end'),
+    env.new(f'corr_sol_right_{ip_name}', xt.Multipole, at=0, from_=f'dy_match_r_{ip_name}@start'),
+    env.new(f'corr_sol_left_{ip_name}', xt.Multipole, at=0, from_=f'dy_match_l_{ip_name}@end'),
 ])
 
 sf = SolenoidField(L=1.23*2, a=0.13, B0=2., z0=0)
@@ -46,7 +46,7 @@ k0s = bx / rigidity0
 k0 = by / rigidity0
 
 # Build solenoid line
-env['on_sol'] = 1
+env[f'on_sol_{ip_name}'] = 1
 ele_names = []
 for ii in range(len(s)-1):
     ks_entry = ks[ii]
@@ -61,13 +61,13 @@ for ii in range(len(s)-1):
     length = s_exit - s_entry
     s_mid = 0.5 * (s_entry + s_exit)
 
-    env.new(f'sol_slice_{ii}', xt.VariableSolenoid,
+    env.new(f'sol_slice_{ii}_{ip_name}', xt.VariableSolenoid,
         length=length,
-        ks_profile=[ks_entry * env.ref['on_sol'], ks_exit * env.ref['on_sol']],
-        knl=[0.5 * (k0_exit + k0_entry) * length * env.ref['on_sol']],
-        ksl=[0.5 * (k0s_exit + k0s_entry) * length * env.ref['on_sol']],
+        ks_profile=[ks_entry * env.ref[f'on_sol_{ip_name}'], ks_exit * env.ref[f'on_sol_{ip_name}']],
+        knl=[0.5 * (k0_exit + k0_entry) * length * env.ref[f'on_sol_{ip_name}']],
+        ksl=[0.5 * (k0s_exit + k0s_entry) * length * env.ref[f'on_sol_{ip_name}']],
     )
-    ele_names.append(f'sol_slice_{ii}')
+    ele_names.append(f'sol_slice_{ii}_{ip_name}')
 
 line_solenoid = env.new_line(components=ele_names)
 ksol_l_main_solenoid = 0
@@ -86,8 +86,8 @@ sfc = SolenoidField(L=1.5, a=0.03, B0=1., z0=0)
 s_comp = np.linspace(-1, 1., 51)
 _, _, bzc = sfc.get_field(0*s_comp, 0*s_comp, s_comp)
 ks_comp = bzc / rigidity0
-env['on_comp_sol'] = 1
-env['field_comp_sol'] = 1.
+env[f'on_comp_sol_{ip_name}'] = 1
+env[f'field_comp_sol_{ip_name}'] = 1.
 ele_names_comp = []
 for ii in range(len(s_comp)-1):
 
@@ -96,12 +96,12 @@ for ii in range(len(s_comp)-1):
 
     length = s_exit - s_entry
 
-    env.new(f'comp_sol_slice_{ii}', xt.VariableSolenoid,
+    env.new(f'comp_sol_slice_{ii}_{ip_name}', xt.VariableSolenoid,
         length=length,
-        ks_profile=[ks_comp[ii] * env.ref['on_comp_sol'] * env.ref['field_comp_sol'],
-                    ks_comp[ii+1] * env.ref['on_comp_sol'] * env.ref['field_comp_sol']],
+        ks_profile=[ks_comp[ii] * env.ref[f'on_comp_sol_{ip_name}'] * env.ref[f'field_comp_sol_{ip_name}'],
+                    ks_comp[ii+1] * env.ref[f'on_comp_sol_{ip_name}'] * env.ref[f'field_comp_sol_{ip_name}']],
     )
-    ele_names_comp.append(f'comp_sol_slice_{ii}')
+    ele_names_comp.append(f'comp_sol_slice_{ii}_{ip_name}')
 
 line_comp_solenoid = env.new_line(components=ele_names_comp)
 
@@ -112,12 +112,12 @@ for nn in line_comp_solenoid.element_names:
         ksol_l_comp_solenoid += ee.ks_profile.mean() * ee.length
 
 # Scale to have zero integrated field
-env['field_comp_sol'] = -ksol_l_main_solenoid / ksol_l_comp_solenoid / 2
+env[f'field_comp_sol_{ip_name}'] = -ksol_l_main_solenoid / ksol_l_comp_solenoid / 2
 
-line_comp_solenoid_left = line_comp_solenoid.clone(suffix='_left')
-line_comp_solenoid_right = line_comp_solenoid.clone(suffix='_right')
+line_comp_solenoid_left = line_comp_solenoid.clone(suffix=f'left_{ip_name}')
+line_comp_solenoid_right = line_comp_solenoid.clone(suffix=f'right_{ip_name}')
 
-# Put the solenoids in the fcc lattice
+# Put the solenoids in the fcc lattice``
 s_ip = tw0['s', ip_name]
 line.insert(line_solenoid, anchor='center', at=s_ip)
 line.insert(ip_name, at=s_ip, s_tol=1e-9) # Put back the ip
@@ -130,18 +130,18 @@ doublet_quad_left = [
 doublet_quad_right = [
        'qd0ar.2', 'qd0br.2', 'qd0cr.2', 'qf1ar.2', 'qf1br.2', 'qf1cr.2', 'qf1dr.2']
 
-env['phi_rot_doublet'] = (ksol_l_main_solenoid / 2) / 2 # in parentheses is the full solenoid rotation, we want half of it for each doublet
-env['on_rot_doublet_left'] = 1
-env['on_rot_doublet_right'] = 1
+env[f'phi_rot_doublet_{ip_name}'] = (ksol_l_main_solenoid / 2) / 2 # in parentheses is the full solenoid rotation, we want half of it for each doublet
+env[f'on_rot_doublet_left_{ip_name}'] = 1
+env[f'on_rot_doublet_right_{ip_name}'] = 1
 for nn in doublet_quad_left:
-    env[nn].rot_s_rad = +env.ref['phi_rot_doublet'] * env.ref['on_rot_doublet_left']
+    env[nn].rot_s_rad = +env.ref[f'phi_rot_doublet_{ip_name}'] * env.ref[f'on_rot_doublet_left_{ip_name}']
 for nn in doublet_quad_right:
-    env[nn].rot_s_rad = -env.ref['phi_rot_doublet'] * env.ref['on_rot_doublet_right']
+    env[nn].rot_s_rad = -env.ref[f'phi_rot_doublet_{ip_name}'] * env.ref[f'on_rot_doublet_right_{ip_name}']
 
 
 # Overlay dipole corrector in between 1.23m and 2.29
 tt = line.get_table()
-tt_region = tt.rows['end_ds_start_straight_ipg':'end_straight_start_ds_ipg']
+tt_region = tt.rows[f'end_ds_start_straight_{ip_name}':f'end_straight_start_ds_{ip_name}']
 s_ip = tt_region['s', ip_name]
 
 # Inner kicker right
@@ -152,12 +152,12 @@ tt_kicker_right= tt_region.rows[s_ip + ds_start: s_ip + ds_end:'s']
 assert np.all(tt_kicker_right.element_type == 'VariableSolenoid')
 l_tot = tt_kicker_right['s_end'][-1] - tt_kicker_right['s_start'][0]
 
-env['acbh1_sol_right'] = 0
-env['acbv1_sol_right'] = 0
+env[f'acbh1_sol_right_{ip_name}'] = 0
+env[f'acbv1_sol_right_{ip_name}'] = 0
 for nn in tt_kicker_right.name:
     ee = env.get(nn)
-    env.ref[nn].knl[0] += env.ref['acbh1_sol_right']/l_tot * ee.length
-    env.ref[nn].ksl[0] += env.ref['acbv1_sol_right']/l_tot * ee.length
+    env.ref[nn].knl[0] += env.ref[f'acbh1_sol_right_{ip_name}']/l_tot * ee.length
+    env.ref[nn].ksl[0] += env.ref[f'acbv1_sol_right_{ip_name}']/l_tot * ee.length
 
 # Inner kicker left
 ds_start = -2.29
@@ -166,81 +166,81 @@ tt_kicker_left = tt_region.rows[s_ip + ds_start: s_ip + ds_end:'s']
 assert np.all(tt_kicker_left.element_type == 'VariableSolenoid')
 l_tot = tt_kicker_left['s_end'][-1] - tt_kicker_left['s_start'][0]
 
-env['acbh1_sol_left'] = 0
-env['acbv1_sol_left'] = 0
+env[f'acbh1_sol_left_{ip_name}'] = 0
+env[f'acbv1_sol_left_{ip_name}'] = 0
 for nn in tt_kicker_left.name:
     ee = env.get(nn)
-    env.ref[nn].knl[0] += env.ref['acbh1_sol_left']/l_tot * ee.length
-    env.ref[nn].ksl[0] += env.ref['acbv1_sol_left']/l_tot * ee.length
+    env.ref[nn].knl[0] += env.ref[f'acbh1_sol_left_{ip_name}']/l_tot * ee.length
+    env.ref[nn].ksl[0] += env.ref[f'acbv1_sol_left_{ip_name}']/l_tot * ee.length
 
 # Outer kickers
 
-env['acbh2_sol_right'] = 0
-env['acbh3_sol_right'] = 0
-env['acbh4_sol_right'] = 0
-env['acbh5_sol_right'] = 0
-env['acbh6_sol_right'] = 0
-env['acbv2_sol_right'] = 0
-env['acbv3_sol_right'] = 0
-env['acbv4_sol_right'] = 0
-env['acbv5_sol_right'] = 0
-env['acbv6_sol_right'] = 0
-env['acbh2_sol_left'] = 0
-env['acbh3_sol_left'] = 0
-env['acbh4_sol_left'] = 0
-env['acbh5_sol_left'] = 0
-env['acbh6_sol_left'] = 0
-env['acbv2_sol_left'] = 0
-env['acbv3_sol_left'] = 0
-env['acbv4_sol_left'] = 0
-env['acbv5_sol_left'] = 0
-env['acbv6_sol_left'] = 0
+env[f'acbh2_sol_right_{ip_name}'] = 0
+env[f'acbh3_sol_right_{ip_name}'] = 0
+env[f'acbh4_sol_right_{ip_name}'] = 0
+env[f'acbh5_sol_right_{ip_name}'] = 0
+env[f'acbh6_sol_right_{ip_name}'] = 0
+env[f'acbv2_sol_right_{ip_name}'] = 0
+env[f'acbv3_sol_right_{ip_name}'] = 0
+env[f'acbv4_sol_right_{ip_name}'] = 0
+env[f'acbv5_sol_right_{ip_name}'] = 0
+env[f'acbv6_sol_right_{ip_name}'] = 0
+env[f'acbh2_sol_left_{ip_name}'] = 0
+env[f'acbh3_sol_left_{ip_name}'] = 0
+env[f'acbh4_sol_left_{ip_name}'] = 0
+env[f'acbh5_sol_left_{ip_name}'] = 0
+env[f'acbh6_sol_left_{ip_name}'] = 0
+env[f'acbv2_sol_left_{ip_name}'] = 0
+env[f'acbv3_sol_left_{ip_name}'] = 0
+env[f'acbv4_sol_left_{ip_name}'] = 0
+env[f'acbv5_sol_left_{ip_name}'] = 0
+env[f'acbv6_sol_left_{ip_name}'] = 0
 
-env['qd0ar.2'].knl[0] += env.ref['acbh2_sol_right']
-env['qd0br.2'].knl[0] += env.ref['acbh3_sol_right']
-env['qf1ar.2'].knl[0] += env.ref['acbh4_sol_right']
-env['qf1br.2'].knl[0] += env.ref['acbh5_sol_right']
-env['corr_sol_right'+ip_name].knl[0] += env.ref['acbh6_sol_right']
+env['qd0ar.2'].knl[0] += env.ref[f'acbh2_sol_right_{ip_name}']
+env['qd0br.2'].knl[0] += env.ref[f'acbh3_sol_right_{ip_name}']
+env['qf1ar.2'].knl[0] += env.ref[f'acbh4_sol_right_{ip_name}']
+env['qf1br.2'].knl[0] += env.ref[f'acbh5_sol_right_{ip_name}']
+env['corr_sol_right_'+ip_name].knl[0] += env.ref[f'acbh6_sol_right_{ip_name}']
 
-env['qd0al.1'].knl[0] += env.ref['acbh2_sol_left']
-env['qd0bl.1'].knl[0] += env.ref['acbh3_sol_left']
-env['qf1al.1'].knl[0] += env.ref['acbh4_sol_left']
-env['qf1bl.1'].knl[0] += env.ref['acbh5_sol_left']
-env['corr_sol_left'+ip_name].knl[0] += env.ref['acbh6_sol_left']
+env['qd0al.1'].knl[0] += env.ref[f'acbh2_sol_left_{ip_name}']
+env['qd0bl.1'].knl[0] += env.ref[f'acbh3_sol_left_{ip_name}']
+env['qf1al.1'].knl[0] += env.ref[f'acbh4_sol_left_{ip_name}']
+env['qf1bl.1'].knl[0] += env.ref[f'acbh5_sol_left_{ip_name}']
+env['corr_sol_left_'+ip_name].knl[0] += env.ref[f'acbh6_sol_left_{ip_name}']
 
-env['qd0ar.2'].ksl[0] += env.ref['acbv2_sol_right']
-env['qd0br.2'].ksl[0] += env.ref['acbv3_sol_right']
-env['qf1ar.2'].ksl[0] += env.ref['acbv4_sol_right']
-env['qf1br.2'].ksl[0] += env.ref['acbv5_sol_right']
-env['corr_sol_right'+ip_name].ksl[0] += env.ref['acbv6_sol_right']
+env['qd0ar.2'].ksl[0] += env.ref[f'acbv2_sol_right_{ip_name}']
+env['qd0br.2'].ksl[0] += env.ref[f'acbv3_sol_right_{ip_name}']
+env['qf1ar.2'].ksl[0] += env.ref[f'acbv4_sol_right_{ip_name}']
+env['qf1br.2'].ksl[0] += env.ref[f'acbv5_sol_right_{ip_name}']
+env['corr_sol_right_'+ip_name].ksl[0] += env.ref[f'acbv6_sol_right_{ip_name}']
 
-env['qd0al.1'].ksl[0] += env.ref['acbv2_sol_left']
-env['qd0bl.1'].ksl[0] += env.ref['acbv3_sol_left']
-env['qf1al.1'].ksl[0] += env.ref['acbv4_sol_left']
-env['qf1bl.1'].ksl[0] += env.ref['acbv5_sol_left']
-env['corr_sol_left'+ip_name].ksl[0] += env.ref['acbv6_sol_left']
+env['qd0al.1'].ksl[0] += env.ref[f'acbv2_sol_left_{ip_name}']
+env['qd0bl.1'].ksl[0] += env.ref[f'acbv3_sol_left_{ip_name}']
+env['qf1al.1'].ksl[0] += env.ref[f'acbv4_sol_left_{ip_name}']
+env['qf1bl.1'].ksl[0] += env.ref[f'acbv5_sol_left_{ip_name}']
+env['corr_sol_left_'+ip_name].ksl[0] += env.ref[f'acbv6_sol_left_{ip_name}']
 
 # Match orbit and vertical dispersion right
 opt_orbit = line.match_knob(
-    knob_name='on_sol_orbit_corr',
+    knob_name=f'on_sol_orbit_corr_{ip_name}',
     run=False,
     init=tw0,
     start='dy_match_l_'+ip_name,
     end='dy_match_r_'+ip_name,
     init_at=ip_name,
     vary=xt.VaryList([
-        'acbh1_sol_right', 'acbv1_sol_right',
-        'acbh2_sol_right', 'acbh3_sol_right',
-        'acbh4_sol_right', 'acbh5_sol_right',
-        'acbh6_sol_right', 'acbv2_sol_right',
-        'acbv3_sol_right', 'acbv4_sol_right',
-        'acbv5_sol_right', 'acbv6_sol_right',
-        'acbh1_sol_left', 'acbv1_sol_left',
-        'acbh2_sol_left', 'acbh3_sol_left',
-        'acbh4_sol_left', 'acbh5_sol_left',
-        'acbh6_sol_left', 'acbv2_sol_left',
-        'acbv3_sol_left', 'acbv4_sol_left',
-        'acbv5_sol_left', 'acbv6_sol_left',
+        f'acbh1_sol_right_{ip_name}', f'acbv1_sol_right_{ip_name}',
+        f'acbh2_sol_right_{ip_name}', f'acbh3_sol_right_{ip_name}',
+        f'acbh4_sol_right_{ip_name}', f'acbh5_sol_right_{ip_name}',
+        f'acbh6_sol_right_{ip_name}', f'acbv2_sol_right_{ip_name}',
+        f'acbv3_sol_right_{ip_name}', f'acbv4_sol_right_{ip_name}',
+        f'acbv5_sol_right_{ip_name}', f'acbv6_sol_right_{ip_name}',
+        f'acbh1_sol_left_{ip_name}', f'acbv1_sol_left_{ip_name}',
+        f'acbh2_sol_left_{ip_name}', f'acbh3_sol_left_{ip_name}',
+        f'acbh4_sol_left_{ip_name}', f'acbh5_sol_left_{ip_name}',
+        f'acbh6_sol_left_{ip_name}', f'acbv2_sol_left_{ip_name}',
+        f'acbv3_sol_left_{ip_name}', f'acbv4_sol_left_{ip_name}',
+        f'acbv5_sol_left_{ip_name}', f'acbv6_sol_left_{ip_name}',
         ], step=1e-6),
     targets=[
         xt.TargetSet(x=0, px=0, y=0, py=0, dy=0, dpy=0, at=xt.END),
@@ -267,7 +267,7 @@ for nn in quad_for_optics_correction:
 name_start = tt_region.name[0]
 name_end = tt_region.name[-1]
 opt_optics = line.match_knob(
-    knob_name='on_sol_optics_corr',
+    knob_name=f'on_sol_optics_corr_{ip_name}',
     run=False,
     init=tw0,
     init_at=ip_name,
@@ -294,27 +294,27 @@ opt_optics.solve()
 opt_orbit.generate_knob()
 opt_optics.generate_knob()
 
-line['on_sol'] = 0
-line['on_comp_sol'] = 0
-line['on_rot_doublet_right'] = 0
-line['on_rot_doublet_left'] = 0
-line['on_sol_orbit_corr'] = 0
-line['on_sol_optics_corr'] = 0
+line[f'on_sol_{ip_name}'] = 0
+line[f'on_comp_sol_{ip_name}'] = 0
+line[f'on_rot_doublet_right_{ip_name}'] = 0
+line[f'on_rot_doublet_left_{ip_name}'] = 0
+line[f'on_sol_orbit_corr_{ip_name}'] = 0
+line[f'on_sol_optics_corr_{ip_name}'] = 0
 tw_off = line.twiss4d(strengths=True, zero_at=ip_name)
 nl_chrom_off = line.get_non_linear_chromaticity(delta0_range=(-1e-2, 1e-2))
 
-line['on_sol'] = 1
-line['on_comp_sol'] = 1
-line['on_rot_doublet_right'] = 1
-line['on_rot_doublet_left'] = 1
-line['on_sol_orbit_corr'] = 1
-line['on_sol_optics_corr'] = 1
+line[f'on_sol_{ip_name}'] = 1
+line[f'on_comp_sol_{ip_name}'] = 1
+line[f'on_rot_doublet_right_{ip_name}'] = 1
+line[f'on_rot_doublet_left_{ip_name}'] = 1
+line[f'on_sol_orbit_corr_{ip_name}'] = 1
+line[f'on_sol_optics_corr_{ip_name}'] = 1
 tw_on_corr = line.twiss4d(strengths=True, zero_at=ip_name)
 nl_chrom_on_corr = line.get_non_linear_chromaticity(delta0_range=(-1e-2, 1e-2))
 two_on_corr = line.twiss(
     strengths=True,
-    start='end_ds_start_straight_ipg',
-    end='end_straight_start_ds_ipg',
+    start=f'end_ds_start_straight_{ip_name}',
+    end=f'end_straight_start_ds_{ip_name}',
     init_at=ip_name,
     init=tw_off,
     zero_at=ip_name)
