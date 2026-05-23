75a52eca2491b001d22d7ab84127c8b514334ccd
giadarol <giovanni.iadarola@cern.ch>
2026-05-23 21:41:31 +0200

    Trying to get the spin to work

--- examples/fcc_ee_solenoid/006c_plots.py
+++ examples/fcc_ee_solenoid/006c_plots.py
@@ -1,7 +1,20 @@
 import xtrack as xt
+import numpy as np
 
 env = xt.load('fccee_z_lcc_solenoid.json')
-line = env.fccee_p_ring
+
+# work on a copy of the line for slicing
+line = env.fccee_p_ring.copy(shallow=True)
+
+# Slice the IP regions
+line.cycle('end_ds_start_straight_ipa')
+ip_names = ['ipa', 'ipd', 'ipg', 'ipj']
+tt = line.get_table()
+for ip_name in ip_names:
+    s_cut_right = np.arange(tt['s', ip_name] + 2.4, tt['s', ip_name] + 11, 0.2)
+    line.cut_at_s(s_cut_right)
+    s_cut_left = np.arange(tt['s', ip_name] - 11, tt['s', ip_name] - 2.4, 0.2)
+    line.cut_at_s(s_cut_left)
 
 # Turn off exprimental solenoids
 line['on_sol_ipa'] = 0
@@ -31,9 +44,13 @@ line['on_sol_corr_ipd'] = 1
 line['on_sol_corr_ipg'] = 1
 line['on_sol_corr_ipj'] = 1
 
-tw = line.twiss6d(strengths=True)
+tw = line.twiss6d(strengths=True, polarization_analysis=True)
 
 two = line.twiss(betx=tw_off.betx[0], bety=tw_off.bety[0])
+
+line.configure_radiation(model='mean')
+line.compensate_radiation_energy_loss()
+tw_rad = line.twiss6d(strengths=True)
 
 import matplotlib.pyplot as plt
 plt.close('all')
