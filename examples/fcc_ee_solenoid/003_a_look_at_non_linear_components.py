14bf318224cd1cb2f9c3e594b8279039aa20a9fa
giadarol <giovanni.iadarola@cern.ch>
2026-05-27 20:46:24 +0200

    Clean up

--- examples/fcc_ee_solenoid/003_a_look_at_non_linear_components.py
+++ examples/fcc_ee_solenoid/003_a_look_at_non_linear_components.py
@@ -5,52 +5,6 @@ import xtrack as xt
 from tilted_solenoid import TiltedSolenoid
 
 
-def finite_difference_coefficients(offsets, derivative_order):
-    offsets = np.asarray(offsets, dtype=float)
-    matrix = np.vstack([offsets**ii for ii in range(len(offsets))])
-    rhs = np.zeros(len(offsets))
-    rhs[derivative_order] = factorial(derivative_order)
-    return np.linalg.solve(matrix, rhs)
-
-
-def compute_pure_field_derivatives(
-        field, s, direction, step, component, max_order=4, min_order=1):
-    offsets = np.arange(-4, 5)
-    zero = np.zeros_like(s)
-    component_index = {'x': 0, 'y': 1, 'z': 2}[component]
-    field_at_offsets = []
-
-    for offset in offsets:
-        if direction == 'x':
-            x = zero + offset * step
-            y = zero
-        elif direction == 'y':
-            x = zero
-            y = zero + offset * step
-        else:
-            raise ValueError("direction must be 'x' or 'y'")
-
-        field_at_offsets.append(field.get_field(x, y, s)[component_index])
-
-    field_at_offsets = np.array(field_at_offsets)
-
-    derivatives = {}
-    for order in range(min_order, max_order + 1):
-        coefficients = finite_difference_coefficients(offsets, order)
-        derivatives[order] = (
-            np.tensordot(coefficients, field_at_offsets, axes=(0, 0))
-            / step**order
-        )
-
-    return derivatives
-
-
-def compute_pure_by_derivatives(field, s, direction, step, max_order=4):
-    return compute_pure_field_derivatives(
-        field=field, s=s, direction=direction, step=step,
-        component='y', max_order=max_order, min_order=1)
-
-
 theta = -0.015
 sf = TiltedSolenoid(L=1.23*2, a=0.13, B0=2., theta=theta)
 
