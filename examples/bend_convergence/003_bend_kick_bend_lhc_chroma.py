77a1ae6726eed3022b81177904710e13637bcf88
giadarol <giovanni.iadarola@cern.ch>
2026-06-08 14:53:51 +0300

    Print chromaticity

--- examples/bend_convergence/003_bend_kick_bend_lhc_chroma.py
+++ examples/bend_convergence/003_bend_kick_bend_lhc_chroma.py
@@ -2,12 +2,19 @@ import xtrack as xt
 
 line = xt.load('../../test_data/hllhc15_thick/lhc_thick_with_knobs.json')
 
-tw = line.twiss4d()
+tw_rot_kick_rot = line.twiss4d()
 
 line.configure_bend_model(core='bend-kick-bend')
-tw_bkb = line.twiss4d()
+tw_bend_kick_bend = line.twiss4d()
 
 import matplotlib.pyplot as plt
-tw_bkb.plot('x')
+tw_bend_kick_bend.plot('x')
+
+print("Q'x:")
+print(f"  rot-kick-rot:   {tw_rot_kick_rot.dqx:.10e}")
+print(f"  bend-kick-bend: {tw_bend_kick_bend.dqx:.10e}")
+print("Q'y:")
+print(f"  rot-kick-rot:   {tw_rot_kick_rot.dqy:.10e}")
+print(f"  bend-kick-bend: {tw_bend_kick_bend.dqy:.10e}")
 
 plt.show()
