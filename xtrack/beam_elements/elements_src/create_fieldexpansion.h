3f5659e52681d2c2f1ffce94b54c59711a5a316a
Silke <silkevdschueren@gmail.com>
2026-06-08 18:05:47 +0200

    Progress on evaluating field expansion from python, keeping c values to be solved

--- xtrack/beam_elements/elements_src/create_fieldexpansion.h
+++ xtrack/beam_elements/elements_src/create_fieldexpansion.h
@@ -1,35 +1,36 @@
 #ifndef create_fieldexpansion_H
 #define create_fieldexpansion_H
 
+
+
 void build_expansion(
-    double h,
-    int64_t ny,
-    int64_t na,
-    int64_t nb,
-    int64_t deg,
-    const double *bs,
-    const double *a,
-    const double *b,
+    FieldExpansionData el
 )
 {
-    Expansion *f = (Expansion *)calloc(1, sizeof(*f));
-    f->h = h;
-    f->ny = ny;
-    f->ncoef = ny + 2;      /* store phi_0..phi_{ny+1} so By is also order ny */
-    f->na = na;
-    f->nb = nb;
-    f->deg = deg;
-    f->mmax = (na > nb - 1) ? na : (nb - 1);
-    f->mmin = -2 * ((f->ncoef - 1) / 2);
-    f->moff = -f->mmin;
-    f->nm = f->mmax - f->mmin + 1; 
-    f->qemin = f->mmin - 1;
-    f->nq = (f->mmax + 2) - f->qemin + 1;
-    f->c = (double *)calloc((size_t)f->ncoef * (size_t)f->nm * (size_t)(deg + 1), sizeof(double));
-    f->V = (double *)calloc((size_t)f->ncoef * (size_t)f->nm, sizeof(double));
-    f->D1 = (double *)calloc((size_t)f->ncoef * (size_t)f->nm, sizeof(double));
-    f->D2 = (double *)calloc((size_t)f->ncoef * (size_t)f->nm, sizeof(double));
-    f->Q = (double *)calloc((size_t)f->nq, sizeof(double));
+    const double h = FieldExpansionData_get_h(el);
+    const int ncoef = FieldExpansionData_get__ncoef(el);
+    const int na = FieldExpansionData_get_na(el);
+    const int nb = FieldExpansionData_get_nb(el);
+    const int deg = FieldExpansionData_get_deg(el);
+    double a[na * (deg + 1)];
+    for (int i = 0; i < na*(deg+1); ++i){
+        a[i] = FieldExpansionData_get_a(el,i);
+    }
+    double b[nb * (deg + 1)];
+    for (int i = 0; i < nb*(deg+1); ++i){
+        b[i] = FieldExpansionData_get_b(el,i);
+    }
+    double bs[deg + 1];
+    for (int i = 0; i < deg + 1; ++i){
+        bs[i] = FieldExpansionData_get_bs(el,i);
+    }
+
+    const int mmax = FieldExpansionData_get__mmax(el);
+    const int mmin = FieldExpansionData_get__mmin(el);
+    const int moff = FieldExpansionData_get__moff(el);
+    const int nm = FieldExpansionData_get__nm(el);
+
+    double c[ncoef * nm * (deg + 1)];
 
     int nmax = (na > nb) ? na : nb;
     double invfact[nmax + 1];
@@ -44,7 +45,7 @@ void build_expansion(
     /* phi_0(s) = sum_m c[0,m](s) q^m
     c[0,m] = - sum_(n>=m) (-1)^(n-m) / (h^n m! (n-m)!) a_n(s) */
     for (int m = 0; m <= na; ++m) {
-        double *dst = cptr(f, 0, m + f->moff);
+        double *dst = cptr(f, 0, m + moff);
         /* a_0(s)=int_0^s b_s(u)du contributes -a_0 to phi_0. 
         CAREFUL: this will neglect the highest order in the polynomial, 
         only up to given degree in a0 is kept */
@@ -62,9 +63,9 @@ void build_expansion(
     
     /* phi_1(q,s) = sum_m c[1,m](s) q^m
     c[1,m] = - sum_(n>=m+1) (-1)^(n-1-m) / (h^(n-1) m! (n-1-m)!) b_n(s) */
-    if (f->ncoef > 1) {
+    if (ncoef > 1) {
         for (int m = 0; m <= nb - 1; ++m) {
-            double *dst = cptr(f, 1, m + f->moff);
+            double *dst = cptr(f, 1, m + moff);
             for (int n = m + 1; n <= nb; ++n) {
                 double sgn = ((n - 1 - m) & 1) ? -1.0 : 1.0;
                 double fac = -sgn * invhpow[n - 1] * invfact[m] * invfact[n - 1 - m];
@@ -77,10 +78,11 @@ void build_expansion(
     /* Recursion: c[i+2,m] = -(d_s^2 + h^2 (m+2)^2) c[i,m+2] 
     implemented for polynomial expansion of c[i,m] in powers of s 
     C[i+2,m,k] = -(C[i,m+2,k+2]*(k+2)*(k+1) + C[i,m+2,k]*h^2*(m+2)^2) */
-    for (int i = 0; i + 2 < f->ncoef; ++i) {
-        for (int m = f->mmin; m <= f->mmax - 2; ++m) {
-            const double *src = ccptr(f, i, (m + 2) + f->moff);  
-            double *dst = cptr(f, i + 2, m + f->moff);  
+    for (int i = 0; i + 2 < ncoef; ++i) {
+        for (int m = mmin; m <= mmax - 2; ++m) {
+
+            const double *src = ccptr(f, i, (m + 2) + moff);  /* memory location of C[i,m+2,0], taking into account that the minimal value is not zero by moff */
+            double *dst = cptr(f, i + 2, m + moff);  /* memory location of C[i+2,m,0] */
             double lam = h * h * (double)(m + 2) * (double)(m + 2);
             for (int k = 0; k <= deg; ++k) {
                 double v = lam * src[k];
@@ -90,19 +92,10 @@ void build_expansion(
         }
     }
 
-    return f;
+    for (int i = 0; i < ncoef * nm * (deg+1); ++i){
+        FieldExpansionData_set__c(el, c[i], i);
+    }
 
-}
-
-
-void free_expansion(Expansion *f) {
-    if (!f) return;
-    free(f->c);
-    free(f->V);
-    free(f->D1);
-    free(f->D2);
-    free(f->Q);
-    free(f);
 }
 
 
