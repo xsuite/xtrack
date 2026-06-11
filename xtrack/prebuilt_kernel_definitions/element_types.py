6d9d0ca592bb9d2c00a7e5d91d49790357ccf54f
Szymon Łopaciuk <szymon@lopaciuk.eu>
2026-06-11 15:46:02 +0300

    Prebuild Polygon

--- xtrack/prebuilt_kernel_definitions/element_types.py
+++ xtrack/prebuilt_kernel_definitions/element_types.py
@@ -7,7 +7,7 @@ from ..beam_elements import *
 from ..monitors import *
 from ..multisetter import MultiSetter
 from ..random import *
-from ..aperture.structures import ApertureModel, Profile, SurveyData
+from ..aperture.structures import ApertureModel, Profile, Polygon, SurveyData
 
 
 ONLY_XTRACK_ELEMENTS = [
@@ -124,4 +124,5 @@ NON_TRACKING_ELEMENTS = [
     ApertureModel,
     Profile,
     SurveyData,
+    Polygon,
 ]
