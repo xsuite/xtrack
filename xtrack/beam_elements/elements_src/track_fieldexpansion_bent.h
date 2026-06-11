5441775c82531c4e2c72b561fe470e439dc09b3c
Silke <silkevdschueren@gmail.com>
2026-06-11 17:54:29 +0200

    Splitted in two elements

---
+++ xtrack/beam_elements/elements_src/track_fieldexpansion_bent.h
@@ -0,0 +1,111 @@
+#ifndef XTRACK_TRACK_FIELDEXPANSION_BENT_H
+#define XTRACK_TRACK_FIELDEXPANSION_BENT_H
+
+#include "track_fieldexpansion_helpers.h"
+
+
+int evaluate_expansion_bent(Expansion *f, double x, double y, double s, FieldValue *out) {
+    const double q = 1.0 + f->h * x;
+        if (q == 0.0) return -1; /* singular chart */
+
+    memset(out, 0, sizeof(*out));
+
+    double *V = f->V;
+    double *D1 = f->D1;
+    double *D2 = f->D2;
+    double *Q = f->Q;
+
+    fs_prepare_s(f, s);
+
+    /* q powers from e = mmin-1 .. mmax+2 */
+    Q[0] = pow(q, (double)f->qemin);
+    for (int t = 1; t < f->nq; ++t) Q[t] = Q[t - 1] * q;
+    #define QPOW(E) Q[(E) - f->qemin]
+
+    /* As(x,0,s) 
+    = 1/(1+hx) int_0^x dx' *(1+hx) By(x',0,s) 
+    = 1/qh int_1^q dq' q' phi_1(q',s)
+    = 1/qh sum_m c[1,m] q^(m+2)/(m+2) - 1/q sum_m c[1,m] 1/(m+2) */
+    if (f->ncoef > 1) {
+        for (int m = 0; m <= f->mmax; ++m) {
+            int j = m + f->moff;
+            const double c1m = V[1 * f->nm + j];  /* c[1,m] */
+            const double dc1m = f->D1[1 * f->nm + j];  /* c[1,m]'*/
+            const double g = QPOW(m + 1) - QPOW(-1);
+            const double den = f->h * (double)(m + 2);
+            if (c1m != 0.0) {
+                out->As     += c1m * g / den;
+                out->dAs_dx += c1m * (((double)(m + 1)) * QPOW(m) + QPOW(-2)) / (double)(m + 2);
+            }
+            if (dc1m != 0.0) out->dAs_ds += dc1m * g / den;         
+        }
+    }
+
+    double yi = 1.0; /* y^i / i! */
+    for (int i = 0; i <= f->ny; ++i) {
+        double sphi = 0.0, gx = 0.0, gs = 0.0, gy = 0.0;
+        double dgx_dx = 0.0, dgx_ds = 0.0;
+        double dgs_dx = 0.0, dgs_ds = 0.0;
+
+        for (int m = f->mmin; m <= f->mmax; ++m) {
+            const int j = m + f->moff;
+            const double cim   = V[i * f->nm + j];        /* c[i,m] */
+            const double ci1m  = V[(i + 1) * f->nm + j];  /* c[i+1,m] */
+            const double dcim  = D1[i * f->nm + j];       /* c[i,m]' */
+            const double ddcim = D2[i * f->nm + j];       /* c[i,m]'' */
+
+            const double qm  = QPOW(m);                  /* q^m */
+            const double qm1 = QPOW(m - 1);              /* q^(m-1) */
+            const double qm2 = QPOW(m - 2);              /* q^(m-2) */
+
+            sphi += cim * qm;                            /* c[i,m] q^m */
+            gx   += f->h * (double)m * cim * qm1;        /* h m c[i,m] q^(m-1) */
+            gy   += ci1m * qm;                           /* c[i+1,m] q^m */
+            gs   += dcim * qm1;                          /* c[i,m]' q^(m-1) */
+
+            dgx_dx += f->h * f->h * (double)m * (double)(m-1) * cim * qm2;
+            dgx_ds += f->h * (double)m * dcim * qm1;
+            dgs_dx += f->h * (double)(m-1) * dcim * qm2;
+            dgs_ds += ddcim * qm1;
+        }
+
+        out->phi += sphi * yi;  /* c[i,m] q^m y^i/i!*/
+        out->Bx  -= gx   * yi;  /* -h m c[i,m] q^(m-1) y^i/i! */
+        out->By  -= gy   * yi;  /* -c[i+1,m] q^m y^i/i! */
+        out->Bs  -= gs   * yi;  /* -c[i,m]' q^(m-1) y^i/i! */
+
+        /* A_x, A_s through order ny in y: need i = 0..ny-1 */
+        if (i < f->ny) {
+            double yi1 = yi * y / (double)(i + 1);  /* y^(i+1)/(i+1)! */
+            out->Ax += gs * yi1;                    /* -c[i,m]' q^(m-1) y^(i+1)/(i+1)! */
+            out->As -= gx * yi1;                    /* -h m c[i,m] q^(m-1) y^i/i! */
+            out->dAx_dx += dgs_dx  * yi1;
+            out->dAx_ds += dgs_ds  * yi1;
+            out->dAs_dx += -dgx_dx * yi1;
+            out->dAs_ds += -dgx_ds * yi1;
+        }
+
+        yi *= y / (double)(i + 1);
+    }
+
+    out->dAx_dy = -out->Bs;
+    out->dAs_dy =  out->Bx;
+
+    return 0;
+    #undef QPOW
+}
+
+
+#define TRACK_EXPANSION BentFieldExpansion_track_local_particle
+#define HAMILTONIAN_FLOW hamiltonian_flow_bent
+#define EVALUATE_EXPANSION evaluate_expansion_bent
+#define FIELDEXPANSIONDATA BentFieldExpansionData
+#define DATA BentFieldExpansionData
+#include "track_fieldexpansion.inc"
+#undef TRACK_EXPANSION
+#undef HAMILTONIAN_FLOW
+#undef EVALUATE_EXPANSION
+#undef FIELDEXPANSIONDATA
+#undef DATA
+
+#endif
---
+++ xtrack/beam_elements/elements_src/track_fieldexpansion_helpers.h
@@ -0,0 +1,71 @@
+typedef struct {
+    int ny;      /* requested output order in y */
+    int ncoef;   /* stored phi_i coefficients: 0..ny+1 */
+    int na, nb, deg;
+    int mmin, mmax, moff, nm;
+    int qemin, nq;
+    double h;
+    double straight;
+    double *c;   /* c[i,m,k], polynomial coeff of s^k in q^m term */
+    double *V;   /* scratch: c[i,m](s)   */
+    double *D1;   /* scratch: d_s c[i,m]  */
+    double *D2;   /* scratch: d2_s c[i,m]  */
+    double *Q;   /* scratch: q^e, e=qemin.. */
+} Expansion;
+
+typedef struct {
+    double phi;
+    double Bx, By, Bs;
+    double Ax, Ay, As;
+    double dAx_dx, dAx_dy, dAx_ds;
+    double dAs_dx, dAs_dy, dAs_ds;
+} FieldValue;
+
+typedef struct {
+    double H;
+    double delta;
+    double one_plus_delta;
+    double radicand;
+    double root;
+    double grad[6];  /* dH/d{x,px,y,py,tau,ptau} */
+    double rhs[6];   /* canonical flow dz/ds */
+    double dH_ds;    /* explicit derivative at fixed canonical variables */
+    FieldValue pot;
+} HamiltonianFlow;
+
+static inline const double *ccptr(const Expansion *f, int i, int m) {
+    return f->c + (((size_t)i * (size_t)f->nm + (size_t)m) * (size_t)(f->deg + 1));
+}
+
+static inline void poly_eval_d2(const double *p, int deg, double s, double *v, double *d1, double *d2) {
+    double a = p[deg], b = 0.0, c=0.0;
+    for (int k = deg - 1; k >= 0; --k) {
+        c = c * s + 2.0 * b;
+        b = b * s + a;
+        a = a * s + p[k];
+    }
+    *v = a;
+    *d1 = b;
+    *d2 = c;
+}
+
+static void fs_prepare_s(Expansion *f, double s) {
+    for (int i = 0; i < f->ncoef; ++i) {
+        for (int m = 0; m < f->nm; ++m) {
+            poly_eval_d2(ccptr(f, i, m), f->deg, s,
+                         &f->V[i * f->nm + m],
+                         &f->D1[i * f->nm + m],
+                         &f->D2[i * f->nm + m]);
+        }
+    }
+}
+
+void delta_from_ptau(const double beta0, double ptau,
+                     double *delta, double *delta1, double *ddelta1) {
+    {
+        const double r = 1.0 + 2.0 * ptau / beta0 + ptau * ptau;
+        *delta1 = sqrt(r);
+        *delta = *delta1 - 1.0;
+        *ddelta1 = (1.0 / beta0 + ptau) / (*delta1);
+    }
+}
