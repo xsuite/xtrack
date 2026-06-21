b576df1ab0386be5d78930f5016f2b06c60e9a44
giadarol <giovanni.iadarola@cern.ch>
2026-06-21 22:33:32 +0200

    This also works

--- examples/splineboris/004a_build_undulator.py
+++ examples/splineboris/004a_build_undulator.py
@@ -9,6 +9,10 @@ p0 = xt.Particles(mass0=xt.ELECTRON_MASS_EV, q0=1, p0c=2.7e9)
 
 env = xt.Environment()
 
+#################################################
+# Polynomial fit on the data from the field map #
+#################################################
+
 # Load the raw field map data from shared test_data
 field_map_path = "../../test_data/sls/undulator_field_map.txt"
 df_raw_data = pd.read_csv(
@@ -18,18 +22,54 @@ df_raw_data = pd.read_csv(
     names=["X", "Y", "Z", "Bskew", "Bnorm", "Bs"],
 ).set_index(["X", "Y", "Z"])
 
-# Distance unit in meters (the dataset uses mm, so 1 mm = 0.001 m)
+# Use fitting procedure to extract field and derivatives on the reference trajectory.
+# This class is taylored for this example data, use your own fitting procedure for other datasets.
+from xtrack._temp.splineboris.field_fitter import FieldFitter
 field_fitter = FieldFitter(
     raw_data=df_raw_data,
     xy_point=(0, 0),
-    distance_unit= 0.001,
+    distance_unit=0.001, # dataset uses mm
     min_region_size=10,
-    deg=2
+    deg=2,
 )
-
-# Get the fitted field data for each longitudinal interval. Tracking settings
-# are intentionally applied below when constructing the SplineBoris elements.
 spline_data = field_fitter.get_spline_data()
+
+# `spline_data` contains for each longitudinal interval the 4th-order
+# polynomial coefficients (in the form of value at start/end of interval,
+# longitudinal derivative at start/end of interval, and mean value) for the field
+# components and their transverse derivatives. For example:
+# spline_data[0] is:
+# {'s_start': -1.1,
+#  's_end': -1.095,
+#  'idx_start': 0,
+#  'idx_end': 5,
+#  'bs':
+#      Spline4(val_start=0.0, der_start=0.0, val_end=0.0, der_end=0.0, mean=0.0),
+#  'bx': (
+#         # bx on axis (x=0,y=0)
+#         Spline4(val_start=0.0002597788559479, der_start=0.003908159160349505,
+#                 val_end=0.00027929455770183206, der_end=0.00389142774801777,
+#                 mean=0.00026954160840155075),
+#         # d bx/d x on axis (x=0,y=0)
+#         Spline4(val_start=0.0, der_start=0.0, val_end=0.0, der_end=0.0, mean=0.0),
+#         # d^2 bx/d x^2 on axis (x=0,y=0)
+#         Spline4(val_start=-0.03587500889997759, der_start=-42.00397475010047,
+#                 val_end=-0.04413421209996192, der_end=-6.919120799986231,
+#                 mean=-0.04245506171996625)
+#        ),
+#  'by': (
+#       # by on axis (x=0,y=0)
+#       Spline4(val_start=0.0020494067017488, der_start=0.0627448571810936,
+#               val_end=0.0023965826590473483, der_end=0.07647819250243666,
+#               mean=0.0022172898542134225),
+#       # d by/d x on axis (x=0,y=0)
+#       Spline4(val_start=0.0, der_start=0.0, val_end=0.0, der_end=0.0, mean=0.0),
+#       # d^2 by/d x^2 on axis (x=0,y=0)
+#       Spline4(val_start=-0.5104028836997457, der_start=-29.057401352068492,
+#               val_end=-0.6429682228011369, der_end=-48.71395930245775,
+#               mean=-0.5725836650301926)
+#      )
+# }
 
 # Build and register the SplineBoris elements explicitly.
 undulator_element_names = []
