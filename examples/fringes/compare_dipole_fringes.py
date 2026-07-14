6ba93a9a0c36d93089761035b3354eee676ab7d6
Silke <silkevdschueren@gmail.com>
2026-07-14 14:36:58 +0200

    Added comparison of thin fringe (backwards drift, fringe, drift) to Etienne fringe

---
+++ examples/fringes/compare_dipole_fringes.py
@@ -0,0 +1,47 @@
+import xtrack as xt
+import numpy as np
+
+# Fringe from 0 to 0.1 over length 0.05, with derivatives zero at the endpoints
+bfringe=np.array([[0,0,120,-1600]])
+length = 0.05
+bmax = 0.1
+gap = 0.04
+ss = np.linspace(0,length,100)
+bvals = bfringe[0,0]+bfringe[0,1]*ss+bfringe[0,2]*ss**2+bfringe[0,3]*ss**3
+fint = np.trapezoid((bmax - bvals)*bvals / bmax**2 / gap, ss)
+
+exactFringe = xt.FieldExpansion(length=length, h=0, a=np.array([[0,0,0,0]]), b=bfringe, bs=np.array([0,0,0,0]), ny=10)
+invDrift = xt.FieldExpansion(length=-length/2, a=np.array([[0]]), b=np.array([[0]]), bs=np.array([0]), ny=5)
+invBend = xt.FieldExpansion(length=-length/2, h=0, a=np.array([[0]]), b=np.array([[0]]), bs=np.array([0]), ny=5)
+thinFringe = xt.Line(elements=[invDrift, exactFringe, invBend])
+
+# Xsuite fringe with same parameters
+PTCfringe = xt.Bend(length=0, k0=0.1, edge_entry_model="full", edge_entry_fint=fint, edge_entry_hgap=gap/2, edge_exit_active=0)
+
+# Check effect of vertical offset including "SAD"-term
+yy = np.linspace(-0.1, 0.1, 100)
+p0 = xt.Particles(y=yy, x=0, px=0, py=0, zeta=0, delta=0, beta0=1)
+p1 = p0.copy()
+
+thinFringe.track(p0, _force_no_end_turn_actions=True)
+PTCfringe.track(p1)
+
+plt.plot(yy, p0.py, label='Thin fringe')
+plt.plot(yy, p1.py, label='PTC fringe')
+plt.legend()
+plt.xlabel('y')
+plt.ylabel('py')
+
+# Effect of delta at given y
+dd = np.linspace(-0.1, 0.1, 100)
+p0 = xt.Particles(y=0.002, x=0, px=0, py=0, zeta=0, delta=dd, beta0=1)
+p1 = p0.copy()
+
+thinFringe.track(p0, _force_no_end_turn_actions=True)
+PTCfringe.track(p1)
+
+plt.plot(dd, p0.py, label='Thin fringe')
+plt.plot(dd, p1.py, label='PTC fringe')
+plt.legend()
+plt.xlabel('delta')
+plt.ylabel('py')
