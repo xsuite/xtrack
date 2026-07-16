83b83143876f9d4cd67561c6b0131113caedc86d
Silke <silkevdschueren@gmail.com>
2026-07-16 15:55:13 +0200

    Test for momentum dependence of fringe field map against Boris integrator

--- tests/test_elements_thick.py
+++ tests/test_elements_thick.py
@@ -3079,3 +3079,43 @@ def test_api_bend():
     assert bend2.k0 == 0.
     assert bend2.h == 0.1
     assert bend2.k0_from_h == False
+
+
+def test_delta_dipole_fringe():
+    # Test momentum dependence of dipole fringe, as corrected with respect to the PTC implementation
+    
+    dd = np.linspace(-0.05, 0.05, 11)
+    p0 = xt.Particles(y=0.002, delta=dd)
+    p1 = p0.copy()
+
+    def fieldvalue(x,y,z):  
+        # Polynomial dipole fringe field between 0 and 0.05 with max b1=0.1
+        # b1 = -1600 z^3 + 120 z^2 converted to tesla
+        return [0, (-1600*z**3 + 120*z**2 - 120*y**2*(1 - 40*z))* p0.rigidity0[0], (1600*y**3 + y*(-4800*z**2 + 240*z))* p0.rigidity0[0]]
+
+    length = 0.05
+    b1 = 0.1
+    
+    # Backwards drift, fringe, backwards bend
+    drift = xt.Line(elements=[xt.Drift(length=length/2)])
+    bend = xt.Line(elements=[xt.Bend(k0=b1, length=length/2)])
+    exactfringe = xt.BorisSpatialIntegrator(fieldmap_callable=fieldvalue, s_start=0, s_end=length, n_steps=1000)
+
+    drift.track(p0, backtrack=True)
+    exactfringe.track(p0)
+    bend.track(p0, backtrack=True)
+    
+    # Xsuite fringe with same parameters
+    gap = 0.04
+    ss = np.linspace(0,length,100)
+    bvals = -1600 * ss**3 + 120 * ss**2
+    fint = np.trapezoid((b1 - bvals)*bvals / b1**2 / gap, ss)
+    PTCfringe = xt.Bend(length=0, k0=b1, edge_entry_model="full", edge_entry_fint=fint, edge_entry_hgap=gap/2, edge_exit_active=0)
+    
+    PTCfringe.track(p1)
+     
+    deg0 = np.polyfit(p0.delta, p0.py, 1)
+    deg1 = np.polyfit(p1.delta, p1.py, 1)
+    
+    # Slopes should be similar, original implementation had sign difference
+    assert np.isclose(deg0[0]/deg1[0], 1, rtol=1e-2)
