a189d8e653f5e90ce71cfb70c1f96aa94ce07156
giadarol <giovanni.iadarola@cern.ch>
2026-08-07 22:18:50 +0200

    Refactor twiss constants into module

--- xtrack/twiss/__init__.py
+++ xtrack/twiss/__init__.py
@@ -25,48 +25,29 @@ else:
 import xobjects as xo
 import xdeps as xd
 
-from . import linear_normal_form as lnf
-from .general import _print, DEPRECATION_INFO_PREP_1_0
-from .twissplot import TwissPlot
-from . import json as json_utils
-from .table import Table
+from .. import linear_normal_form as lnf
+from ..general import _print, DEPRECATION_INFO_PREP_1_0
+from ..twissplot import TwissPlot
+from .. import json as json_utils
+from ..table import Table
+from .constants import (
+    AT_TURN_FOR_TWISS,
+    CYCLICAL_QUANTITIES,
+    DEFAULT_CO_SEARCH_TOL,
+    DEFAULT_COL_ORDER,
+    DEFAULT_MATRIX_RESPONSIVENESS_TOL,
+    DEFAULT_MATRIX_STABILITY_TOL,
+    DEFAULT_NUM_TURNS_SEARCH_T_REV,
+    DEFAULT_STEPS_R_MATRIX,
+    NORMAL_STRENGTHS_FROM_ATTR,
+    OTHER_FIELDS_FROM_ATTR,
+    OTHER_FIELDS_FROM_TABLE,
+    SIGN_FLIP_FOR_ATTR_REVERSE,
+    SKEW_STRENGTHS_FROM_ATTR,
+    VARS_FOR_TWISS_INIT_GENERATION,
+)
 
 import xtrack as xt  # To avoid circular imports
-
-DEFAULT_STEPS_R_MATRIX = {
-    'dx':1e-6, 'dpx':1e-7,
-    'dy':1e-6, 'dpy':1e-7,
-    'dzeta':1e-5, 'ddelta':1e-6
-}
-
-DEFAULT_CO_SEARCH_TOL = [1e-11, 1e-11, 1e-11, 1e-11, 1e-5, 1e-9]
-
-DEFAULT_MATRIX_RESPONSIVENESS_TOL = 1e-15
-DEFAULT_MATRIX_STABILITY_TOL = 2e-3
-DEFAULT_NUM_TURNS_SEARCH_T_REV = 10
-
-AT_TURN_FOR_TWISS = -10 # # To avoid writing in monitors installed in the line
-
-VARS_FOR_TWISS_INIT_GENERATION = [
-    'x', 'px', 'y', 'py', 'zeta', 'delta',
-    'betx', 'alfx', 'bety', 'alfy', 'bets',
-    'dx', 'dpx', 'dy', 'dpy', 'dzeta',
-    'mux', 'muy', 'muzeta',
-    'ax_chrom', 'bx_chrom', 'ay_chrom', 'by_chrom',
-    'ddx', 'ddpx', 'ddy', 'ddpy',
-]
-
-CYCLICAL_QUANTITIES = ['mux', 'muy', 'dzeta', 's']
-
-NORMAL_STRENGTHS_FROM_ATTR=['k0l', 'k1l', 'k2l', 'k3l', 'k4l', 'k5l']
-SKEW_STRENGTHS_FROM_ATTR=['k0sl', 'k1sl', 'k2sl', 'k3sl', 'k4sl', 'k5sl']
-OTHER_FIELDS_FROM_ATTR=['angle', 'angle_rad', 'rot_s_rad', 'hkick', 'vkick', 'ks', 'bs', 'length', '_angle_force_body']
-OTHER_FIELDS_FROM_TABLE=['element_type', 'isthick', 'parent_name', 'parent_type', 'prototype']
-SIGN_FLIP_FOR_ATTR_REVERSE=['k0l', 'k2l', 'k4l', 'k1sl', 'k3sl', 'k5sl', 'vkick', 'angle', 'angle_rad']
-
-DEFAULT_COL_ORDER = [
-    'name', 'element_type', 's', 'betx', 'bety', 'alfx', 'alfy', 'dx', 'dy'
-    'dpx', 'dpy', 'x', 'y', 'px', 'py', 'delta', 'zeta']
 
 
 def twiss_line(line, particle_ref=None, method=None,
