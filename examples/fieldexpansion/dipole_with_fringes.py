287759e429ba1b81fa4cd98b930990ac886d561f
Riccardo De Maria <riccardodemaria@gmail.com>
2026-08-31 13:46:58 +0200

    update example

--- examples/fieldexpansion/dipole_with_fringes.py
+++ examples/fieldexpansion/dipole_with_fringes.py
@@ -14,7 +14,7 @@ def get_bshape4(L, bb0, bp0, bbL, bpL, bint):
     :param bint: integral between 0 and L
     :return: set of coefficients, lowest order first
     """
-    
+
     t1 = np.array([1, 0, -18,  32, -15 ])  # p(0)=1
     t2 = np.array([0, 1, -9/2, 6,  -5/2])  # p'(0)=1
     t3 = np.array([0, 0, -12,  28, -15 ])  # p(L)=1
@@ -28,7 +28,7 @@ def calc_value(coeffs, s):
     return np.sum(coeffs[:, None] * s[None, :]**np.arange(order + 1)[:, None], axis=0)
 
 gap = 0.05
-length = 0.5
+length = 0.5 # magnetic length
 bmax = 0.5
 h = bmax  # 1 / bending radius
 
@@ -59,3 +59,43 @@ dipole = xt.Line(elements = [
 
 p0 = xt.Particles()
 dipole.track(p0)
+
+### Fodo
+env=xt.Environment()
+env['k1quad'] = 1.6
+env['lengthquad'] = 0.2
+env.new("quad1", xt.Quadrupole, k1='k1quad', length='lengthquad')
+env.new("quad2", xt.Quadrupole, k1='-k1quad', length='lengthquad')
+nstep=5
+env.elements['e0']=xt.FieldExpansion(length=fringe_length/2, b=np.array([b1_in]), a=0*np.array([b1_in]), bs=0*b1_in, ny=5, nstep=nstep)
+env.elements['e1']=xt.FieldExpansion(length=fringe_length/2, b=np.array([b1_in]), a=0*np.array([b1_in]), bs=0*b1_in, ny=5, nstep=nstep, h=h)
+env.elements['e2']=xt.FieldExpansion(length=body_length, b=np.array([b1_body]), a=0*np.array([b1_body]), bs=0*b1_body, ny=5, nstep=nstep, h=h)
+env.elements['e3']=xt.FieldExpansion(length=fringe_length/2, b=np.array([b1_out]), a=0*np.array([b1_out]), bs=0*b1_out, ny=5, nstep=nstep, h=h)
+env.elements['e4']=xt.FieldExpansion(length=fringe_length/2, b=np.array([b1_out]), a=0*np.array([b1_out]), bs=0*b1_out, ny=5, nstep=nstep)
+env.new_line(name="dipole",components=['e0', 'e1', 'e2', 'e3', 'e4'])
+fodo= env.new_line(name='myline', length=5,
+                 components=[
+                     env.place('quad1', at=0.1),
+                     env.place('dipole',  at=1.0),
+                     env.place('quad2', at=2.2),
+                     env.place('dipole',  at=3.5),
+                     ])
+fodo.set_particle_ref()
+tw = fodo.twiss4d()
+tw.plot()
+x0,px0=tw.x[0],tw.px[0]
+part=fodo.build_particles(x=x0+np.array([0.0,0.01,0.0]),
+                          px=px0+np.array([0,0,0]),
+                          y=np.array([0.0,0.0,0.01]))
+fodo.track(part,num_turns=100000,turn_by_turn_monitor=True)
+out=fodo.record_last_track
+fig,(a1,a2)=plt.subplots(2,1)
+
+for i in range(len(out.x)):
+    a1.plot(fodo.record_last_track.x[i],fodo.record_last_track.px[i],',')
+    a2.plot(fodo.record_last_track.y[i],fodo.record_last_track.py[i],',')
+
+
+
+
+
