bc021fedb130717d751815c4a0068a42a1c4f2d4
giadarol <giovanni.iadarola@cern.ch>
2026-09-02 13:41:31 +0200

    Generate file

--- examples/survey_ccs/000_dev.py
+++ examples/survey_ccs/000_dev.py
@@ -73,6 +73,150 @@ tt_out = xt.Table(dct_out)
 tt_out.to_csv('m2_ccs_align.csv')
 tt_out.to_tfs('m2_ccs_align.tfs', float_precision=15)
 
+header = """
+@ NAME             %06s "SURVEY"
+@ TYPE             %06s "SURVEY"
+@ TITLE            %08s "no-title"
+@ ORIGIN           %17s "5.09.03 Darwin 64"
+@ DATE             %08s "02/09/26"
+@ TIME             %08s "12.10.29"
+* NAME               KEYWORD                                    S                         L                     ANGLE                         X                         Y                         Z                     THETA                       PHI                       PSI                GLOBALTILT                      TILT    SLOT_ID ASSEMBLY_ID                  MECH_SEP                     V_POS
+$ %s                 %s                                       %le                       %le                       %le                       %le                       %le                       %le                       %le                       %le                       %le                       %le                       %le         %d         %d                       %le                       %le
+"""
+header = header.lstrip()
+
+lines = []
+for ii, nn in enumerate(names_align):
+    ff = sv.get_all_frames(nn)
+    fthis = ff['elem_end']
+    for place in ['start', 'end']:
+
+        if place == 'start':
+            fthis = ff['elem_start']
+            this_name = f'drift_{ii}'
+            this_s = sv['s', nn]
+            this_length = np.linalg.norm(ff['elem_end'].XYZ - ff['elem_start'].XYZ, 2)
+            this_slot_id = 0
+        else:
+            fthis = ff['elem_end']
+            this_name = nn
+            this_s = sv['s', nn + '>>1']
+            this_length = 0
+            this_slot_id = env[nn].extra['slot_id']
+
+        this_line = ' '
+
+        # NAME
+        name_str = '"' + this_name.upper() + '"'
+        name_str = name_str.ljust(20)
+        this_line += name_str
+
+        # KEYWORD
+        keyword_str = '"' + 'ELEMENT' + '"'
+        keyword_str = keyword_str.ljust(20)
+        this_line += keyword_str
+
+        # S (need to handle if entry or exit)
+        val = this_s
+        val_str = f"{val:26.9f}"
+        this_line += val_str
+
+        # L
+        val = this_length
+        val_str = f"{val:26.9f}"
+        this_line += val_str
+
+        # ANGLE
+        val = 0
+        val_str = f"{val:26.9f}"
+        this_line += val_str
+
+        # X
+        val = fthis.X
+        val_str = f"{val:26.9f}"
+        this_line += val_str
+
+        # Y
+        val = fthis.Y
+        val_str = f"{val:26.9f}"
+        this_line += val_str
+
+        # Z
+        val = fthis.Z
+        val_str = f"{val:26.9f}"
+        this_line += val_str
+
+        # THETA
+        val = fthis.theta
+        val_str = f"{val:26.9f}"
+        this_line += val_str
+
+        # PHI
+        val = fthis.phi
+        val_str = f"{val:26.9f}"
+        this_line += val_str
+
+        # PSI
+        val = fthis.psi
+        val_str = f"{val:26.9f}"
+        this_line += val_str
+
+        # GLOBALTILT
+        val = fthis.psi
+        val_str = f"{val:26.9f}"
+        this_line += val_str
+
+        # TILT
+        val = 0
+        val_str = f"{val:26.9f}"
+        this_line += val_str
+
+        # SLOT_ID
+        val = this_slot_id
+        val_str = f"{int(val):11d}"
+        this_line += val_str
+
+        # ASSEMBLY_ID
+        val = 0
+        val_str = f"{val:11d}"
+        this_line += val_str
+
+        # MECH_SEP
+        val = 0
+        val_str = f"{val:26.9f}"
+        this_line += val_str
+
+        # V_POS
+        val = 0
+        val_str = f"{val:26.9f}"
+        this_line += val_str
+
+        # # COMMENT
+        # val = ' ""'
+        # val_str = val.ljust(20)
+        # this_line += val_str
+
+        lines.append(this_line)
+
+out = header
+out += '\n'.join(lines)
+with open('survey_output.tfs', 'w') as f:
+    f.write(out)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 import matplotlib.pyplot as plt
 plt.close('all')
 plt.figure(figsize=(10, 6))
